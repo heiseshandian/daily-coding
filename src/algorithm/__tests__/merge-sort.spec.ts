d65878275e97969b71d5df130f27ee08f7d40e92
baymax <476803711@qq.com>
2022-11-22 15:17:27 +0800

    归并排序

--- src/algorithm/__tests__/merge-sort.spec.ts
+++ src/algorithm/__tests__/merge-sort.spec.ts
@@ -1,69 +1,55 @@
-import { mergeSort, mergeSort2 } from '../merge-sort';
+import {
+    getInversionPairCount,
+    getInversionPairCount2,
+    getMinSumOfArray,
+    getMinSumOfArray2,
+    mergeSort,
+    mergeSort2,
+} from '../merge-sort';
 
 describe('mergeSort', () => {
-    test('input empty array output empty array', () => {
-        const input: number[] = [];
-        const expected: number[] = [];
-        mergeSort(input);
+    const testData = [
+        { input: [], expected: [] },
+        { input: [1], expected: [1] },
+        { input: [3, 5, 2, 1, 4, 3], expected: [1, 2, 3, 3, 4, 5] },
+        { input: [3, 5, 2, 1, 4, 3, 10, 2, 11, 24, 0], expected: [0, 1, 2, 2, 3, 3, 4, 5, 10, 11, 24] },
+    ];
 
-        expect(input).toEqual(expected);
-    });
+    it.each(testData)('mergeSort %j', ({ input, expected }) => {
+        const clone1 = input.slice();
+        const clone2 = input.slice();
+        mergeSort(clone1);
+        mergeSort2(clone2);
 
-    test('input [1] output [1]', () => {
-        const input = [1];
-        const expected = [1];
-        mergeSort(input);
-
-        expect(input).toEqual(expected);
-    });
-
-    test('input 3,5,2,1,4,3 output 1,2,3,3,4,5', () => {
-        const input = [3, 5, 2, 1, 4, 3];
-        const expected = [1, 2, 3, 3, 4, 5];
-        mergeSort(input);
-
-        expect(input).toEqual(expected);
-    });
-
-    test('input 3,5,2,1,4,3,10,2,11,24,0 output 0,1,2,2,3,3,4,5,10,11,24', () => {
-        const input = [3, 5, 2, 1, 4, 3, 10, 2, 11, 24, 0];
-        const expected = [0, 1, 2, 2, 3, 3, 4, 5, 10, 11, 24];
-        mergeSort(input);
-
-        expect(input).toEqual(expected);
+        expect(clone1).toEqual(expected);
+        expect(clone2).toEqual(expected);
     });
 });
 
-describe('mergeSort2', () => {
-    test('input empty array output empty array', () => {
-        const input: number[] = [];
-        const expected: number[] = [];
-        mergeSort2(input);
+describe('getMinSumOfArray', () => {
+    const testData = [
+        { input: [1, 3, 4, 2, 5], expected: 16 },
+        { input: [1, 3, 4, 2, 5, 0], expected: 16 },
+        { input: [1, 3, 4, 2, 5, 6], expected: 31 },
+        { input: [1, 3, 4, 2, 5, 6, 7], expected: 52 },
+    ];
 
-        expect(input).toEqual(expected);
+    it.each(testData)('getMinSumOfArray %j', ({ input, expected }) => {
+        expect(getMinSumOfArray(input)).toBe(expected);
+        expect(getMinSumOfArray2(input)).toBe(expected);
     });
+});
 
-    test('input [1] output [1]', () => {
-        const input = [1];
-        const expected = [1];
-        mergeSort2(input);
+describe('getInversionPairCount', () => {
+    const testData = [
+        { input: [4, 1, 2, 3, 5, 2], expected: 6 },
+        { input: [4, 1, 2, 3, 5, 2, 1], expected: 11 },
+        { input: [4, 1, 2, 3, 5, 2, 1, 0], expected: 18 },
+        { input: [4, 1, 2, 3, 5, 2, 1, 0, 10], expected: 18 },
+    ];
 
-        expect(input).toEqual(expected);
-    });
-
-    test('input 3,5,2,1,4,3 output 1,2,3,3,4,5', () => {
-        const input = [3, 5, 2, 1, 4, 3];
-        const expected = [1, 2, 3, 3, 4, 5];
-        mergeSort2(input);
-
-        expect(input).toEqual(expected);
-    });
-
-    test('input 3,5,2,1,4,3,10,2,11,24,0 output 0,1,2,2,3,3,4,5,10,11,24', () => {
-        const input = [3, 5, 2, 1, 4, 3, 10, 2, 11, 24, 0];
-        const expected = [0, 1, 2, 2, 3, 3, 4, 5, 10, 11, 24];
-        mergeSort2(input);
-
-        expect(input).toEqual(expected);
+    it.each(testData)('getInversionPairCount %j', ({ input, expected }) => {
+        expect(getInversionPairCount(input)).toBe(expected);
+        expect(getInversionPairCount2(input)).toBe(expected);
     });
 });
--- src/algorithm/merge-sort.ts
+++ src/algorithm/merge-sort.ts
@@ -24,23 +24,15 @@ function merge(arr: number[], left: number, mid: number, right: number) {
     let leftIndex = left;
     let rightIndex = mid + 1;
     while (leftIndex <= mid && rightIndex <= right) {
-        const leftValue = arr[leftIndex];
-        const rightValue = arr[rightIndex];
-        if (leftValue <= rightValue) {
-            leftIndex++;
-            temp.push(leftValue);
-        } else {
-            rightIndex++;
-            temp.push(rightValue);
-        }
+        temp.push(arr[leftIndex] <= arr[rightIndex] ? arr[leftIndex++] : arr[rightIndex++]);
     }
 
-    for (let i = leftIndex; i <= mid; i++) {
-        temp.push(arr[i]);
+    while (leftIndex <= mid) {
+        temp.push(arr[leftIndex++]);
     }
 
-    for (let i = rightIndex; i <= right; i++) {
-        temp.push(arr[i]);
+    while (rightIndex <= right) {
+        temp.push(arr[rightIndex++]);
     }
 
     for (let i = 0; i < temp.length; i++) {
@@ -66,4 +58,155 @@ export function mergeSort2(arr: number[]) {
             merge(arr, left, mid, right);
         }
     }
+}
+
+/* 
+在一个数组中，一个数左边比它小的数的总和叫做数的小和，所有数的小和累加起来叫做数组小和。
+比如说：对于数组 [1,3,4,2,5]
+1左边比1小的数字：没有
+3左边比3小的数字：1
+4左边比4小的数字：1,3
+2左边比2小的数字：1
+5左边比5小的数字：1,3,4,2
+所以数组小和就是1+1+3+1+1+3+4+2=16
+ */
+export function getMinSumOfArray(arr: number[]) {
+    let sum = 0;
+    for (let i = 1; i < arr.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if (arr[j] < arr[i]) {
+                sum += arr[j];
+            }
+        }
+    }
+
+    return sum;
+}
+
+export function getMinSumOfArray2(arr: number[]) {
+    if (!arr || arr.length === 1) {
+        return 0;
+    }
+
+    return recursiveMinSumOfArray(arr, 0, arr.length - 1);
+}
+
+function recursiveMinSumOfArray(arr: number[], left: number, right: number): number {
+    if (left === right) {
+        return 0;
+    }
+
+    const mid = left + ((right - left) >> 1);
+    const leftSum = recursiveMinSumOfArray(arr, left, mid);
+    const rightSum = recursiveMinSumOfArray(arr, mid + 1, right);
+    const mergeSum = mergeGetMinSumOfArray(arr, left, mid, right);
+
+    return leftSum + rightSum + mergeSum;
+}
+
+function mergeGetMinSumOfArray(arr: number[], left: number, mid: number, right: number) {
+    const temp = [];
+    let sum = 0;
+
+    let leftIndex = left;
+    let rightIndex = mid + 1;
+    while (leftIndex <= mid && rightIndex <= right) {
+        if (arr[leftIndex] < arr[rightIndex]) {
+            sum += arr[leftIndex] * (right - rightIndex + 1);
+            temp.push(arr[leftIndex++]);
+        } else {
+            temp.push(arr[rightIndex++]);
+        }
+    }
+
+    while (leftIndex <= mid) {
+        temp.push(arr[leftIndex++]);
+    }
+
+    while (rightIndex <= right) {
+        temp.push(arr[rightIndex++]);
+    }
+
+    for (let i = 0; i < temp.length; i++) {
+        arr[left + i] = temp[i];
+    }
+
+    return sum;
+}
+
+/* 逆序对问题
+给定数组 [4,1,2,3,5,2]
+4可以组合的逆序对是 [4,1],[4,2],[4,3],[4,2] 也就是4的右边有多少数比4小
+1可以组合的逆序对是 无
+2可以组合的逆序对是 无
+3可以组合的逆序对是 [3,2]
+5可以组合的逆序对是 [5,2]
+总的逆序对就是 6
+*/
+export function getInversionPairCount(arr: number[]) {
+    if (!arr || arr.length < 2) {
+        return 0;
+    }
+
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] > arr[j]) {
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
+
+export function getInversionPairCount2(arr: number[]) {
+    if (!arr || arr.length < 2) {
+        return 0;
+    }
+
+    return recursiveGetInversionPairCount(arr, 0, arr.length - 1);
+}
+
+function recursiveGetInversionPairCount(arr: number[], left: number, right: number): number {
+    if (left === right) {
+        return 0;
+    }
+
+    const mid = left + ((right - left) >> 1);
+    const leftCount = recursiveGetInversionPairCount(arr, left, mid);
+    const rightCount = recursiveGetInversionPairCount(arr, mid + 1, right);
+    const mergeCount = mergeGetInversionPairCount(arr, left, mid, right);
+
+    return leftCount + rightCount + mergeCount;
+}
+
+function mergeGetInversionPairCount(arr: number[], left: number, mid: number, right: number) {
+    const temp = [];
+    let sum = 0;
+
+    let leftIndex = left;
+    let rightIndex = mid + 1;
+    while (leftIndex <= mid && rightIndex <= right) {
+        if (arr[leftIndex] > arr[rightIndex]) {
+            sum += mid - leftIndex + 1;
+            temp.push(arr[rightIndex++]);
+        } else {
+            temp.push(arr[leftIndex++]);
+        }
+    }
+
+    while (leftIndex <= mid) {
+        temp.push(arr[leftIndex++]);
+    }
+
+    while (rightIndex <= right) {
+        temp.push(arr[rightIndex++]);
+    }
+
+    for (let i = 0; i < temp.length; i++) {
+        arr[left + i] = temp[i];
+    }
+
+    return sum;
 }
