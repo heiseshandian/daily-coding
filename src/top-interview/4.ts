8647f9bc83f7c91ebf968c9f731a9259acbf58ce
baymax <476803711@qq.com>
2022-12-26 20:49:25 +0800

    从左到右的尝试模型

---
+++ src/top-interview/4.ts
@@ -0,0 +1,60 @@
+/* 
+给定两个有序数组，arr1,arr2，长度分别是m和n，如果m+n是奇数直接返回中位数，如果m+n返回两个中位数 / 2
+例：
+arr1 = [1,3], arr2 = [2]
+return 2
+
+arr1 = [1,2], arr2 = [3,4]
+return 2.5
+*/
+export function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
+    const sum = nums1.length + nums2.length;
+    const target = Math.ceil(sum / 2);
+    const isEven = target === sum / 2;
+
+    let count = 0;
+    let i1 = 0;
+    let i2 = 0;
+    while (count < target - 1) {
+        if (i1 === nums1.length) {
+            count++;
+            i2++;
+            continue;
+        }
+
+        if (i2 === nums2.length) {
+            count++;
+            i1++;
+            continue;
+        }
+
+        if (nums1[i1] <= nums2[i2]) {
+            count++;
+            i1++;
+        } else {
+            count++;
+            i2++;
+        }
+    }
+
+    if (i1 === nums1.length) {
+        if (isEven) {
+            return (nums2[i2] + nums2[i2 + 1]) / 2;
+        }
+        return nums2[i2];
+    }
+
+    if (i2 === nums2.length) {
+        if (isEven) {
+            return (nums1[i1] + nums1[i1 + 1]) / 2;
+        }
+        return nums1[i1];
+    }
+    if (!isEven) {
+        return nums1[i1] <= nums2[i2] ? nums1[i1] : nums2[i2];
+    }
+    const [a, b] = [nums1[i1], nums2[i2], nums1[i1 + 1] || Infinity, nums2[i2 + 1] || Infinity]
+        .sort((a, b) => a - b)
+        .slice(0, 2);
+    return (a + b) / 2;
+}
---
+++ src/top-interview/__tests__/4.spec.ts
@@ -0,0 +1,45 @@
+import { findMedianSortedArrays } from '../4';
+
+describe('findMedianSortedArrays', () => {
+    const testData = [
+        {
+            input: {
+                nums1: [1, 3],
+                nums2: [2],
+            },
+            expected: 2,
+        },
+        {
+            input: {
+                nums1: [0, 1, 9],
+                nums2: [1, 2, 3],
+            },
+            expected: 1.5,
+        },
+        {
+            input: {
+                nums1: [1, 3],
+                nums2: [2, 4],
+            },
+            expected: 2.5,
+        },
+        {
+            input: {
+                nums1: [1, 2],
+                nums2: [3, 4],
+            },
+            expected: 2.5,
+        },
+        {
+            input: {
+                nums1: [10],
+                nums2: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+            },
+            expected: 5.5,
+        },
+    ];
+
+    it.each(testData)('findMedianSortedArrays %j', ({ input: { nums1, nums2 }, expected }) => {
+        expect(findMedianSortedArrays(nums1, nums2)).toBe(expected);
+    });
+});
