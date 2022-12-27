2a56e272f468e5f6532c6ef7f34e52ae6e44a97a
baymax <476803711@qq.com>
2022-12-27 10:24:32 +0800

    整数取反

---
+++ src/top-interview/7.ts
@@ -0,0 +1,17 @@
+// 整数逆序 [-2^31, 2^31 - 1] 逆序之后超出范围返回0
+export function reverseInteger(x: number): number {
+    const MAX_VALUE = Math.pow(2, 31) - 1;
+    const MIN_VALUE = -Math.pow(2, 31);
+    let reversed = 0;
+    while (x != 0) {
+        const pop = x % 10;
+        // js除10取整需要特殊处理
+        x = parseInt(`${x / 10}`);
+
+        // Simply because biggest int32 is 2147483647 and least is -2147483648
+        if (reversed > MAX_VALUE / 10 || (reversed == MAX_VALUE / 10 && pop > 7)) return 0;
+        if (reversed < MIN_VALUE / 10 || (reversed == MIN_VALUE / 10 && pop < -8)) return 0;
+        reversed = reversed * 10 + pop;
+    }
+    return reversed;
+}
---
+++ src/top-interview/__tests__/7.spec.ts
@@ -0,0 +1,13 @@
+import { reverseInteger } from '../7';
+
+describe('reverseInteger', () => {
+    const testData = [
+        { input: 123, expected: 321 },
+        { input: -123, expected: -321 },
+        { input: 120, expected: 21 },
+    ];
+
+    it.each(testData)('reverseInteger %j', ({ input, expected }) => {
+        expect(reverseInteger(input)).toBe(expected);
+    });
+});
