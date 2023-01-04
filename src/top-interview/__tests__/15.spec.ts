2381a14d6091221898ebeba91e6a7b9123642c41
baymax <476803711@qq.com>
2023-01-05 00:04:17 +0800

    三元组问题

---
+++ src/top-interview/__tests__/15.spec.ts
@@ -0,0 +1,30 @@
+import { threeSum, threeSum2 } from '../15';
+
+describe('threeSum', () => {
+    const testData = [
+        {
+            input: [-1, 0, 1, 2, -1, -4],
+            expected: [
+                [-1, 0, 1],
+                [-1, -1, 2],
+            ],
+        },
+        {
+            input: [0, 1, 1],
+            expected: [],
+        },
+    ];
+    const comparator = (a: number[], b: number[]) => {
+        for (let i = 0; i < a.length; i++) {
+            if (a[i] !== b[i]) {
+                return a[i] - b[i];
+            }
+        }
+        return 0;
+    };
+
+    it.each(testData)('threeSum %j', ({ input, expected }) => {
+        expect(threeSum(input).sort(comparator)).toEqual(expected.sort(comparator));
+        expect(threeSum2(input).sort(comparator)).toEqual(expected.sort(comparator));
+    });
+});
