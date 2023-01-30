09aad600adbc31cfbc7e68a1f56438f01691ebef
baymax <476803711@qq.com>
2023-01-30 15:05:16 +0800

    滑动窗口

---
+++ src/algorithm/__tests__/sliding-window.spec.ts
@@ -0,0 +1,42 @@
+import { SlidingWindow } from '../sliding-window';
+
+describe('SlidingWindow', () => {
+    const arr = [4, 3, 5, 4, 3, 3, 6, 7];
+
+    test('moveRight moveLeft getMax', () => {
+        const slidingWindow = new SlidingWindow(arr);
+
+        slidingWindow.moveRight();
+        slidingWindow.moveRight();
+
+        expect(slidingWindow.doubleQueue).toEqual([0, 1]);
+        expect(slidingWindow.getMax()).toBe(4);
+
+        slidingWindow.moveLeft();
+        expect(slidingWindow.doubleQueue).toEqual([1]);
+        expect(slidingWindow.getMax()).toBe(3);
+    });
+
+    test('moveRight moveLeft getMax 2', () => {
+        const slidingWindow = new SlidingWindow(arr);
+
+        slidingWindow.moveRight();
+        slidingWindow.moveRight();
+        slidingWindow.moveRight();
+        slidingWindow.moveRight();
+
+        expect(slidingWindow.doubleQueue).toEqual([2, 3]);
+        expect(slidingWindow.getMax()).toBe(5);
+
+        slidingWindow.moveLeft();
+
+        expect(slidingWindow.doubleQueue).toEqual([2, 3]);
+        expect(slidingWindow.getMax()).toBe(5);
+
+        slidingWindow.moveLeft();
+        slidingWindow.moveLeft();
+
+        expect(slidingWindow.doubleQueue).toEqual([3]);
+        expect(slidingWindow.getMax()).toBe(4);
+    });
+});
---
+++ src/algorithm/sliding-window.ts
@@ -0,0 +1,45 @@
+export class SlidingWindow {
+    arr: number[];
+
+    left = -1;
+    right = -1;
+
+    // 双端队列，严格从大到小
+    doubleQueue: number[] = [];
+
+    constructor(arr: number[]) {
+        this.arr = arr;
+    }
+
+    public moveRight() {
+        if (this.right + 1 >= this.arr.length) {
+            return;
+        }
+        this.right++;
+
+        while (this.doubleQueue.length !== 0) {
+            const last = this.doubleQueue[this.doubleQueue.length - 1];
+            if (this.arr[last] > this.arr[this.right]) {
+                break;
+            }
+            this.doubleQueue.length--;
+        }
+        this.doubleQueue.push(this.right);
+    }
+
+    public moveLeft() {
+        // 滑动窗口左边界不可以超过右边界
+        if (this.left + 1 > this.right) {
+            return;
+        }
+        this.left++;
+
+        if (this.left >= this.doubleQueue[0]) {
+            this.doubleQueue.shift();
+        }
+    }
+
+    public getMax() {
+        return this.arr[this.doubleQueue[0]];
+    }
+}
