eb850813ad2e83c96f064cb40e1b5826a59cc950
baymax <476803711@qq.com>
2023-02-26 14:26:33 +0800

    基数排序

--- src/algorithm/__tests__/bucket-sort.spec.ts
+++ src/algorithm/__tests__/bucket-sort.spec.ts
@@ -1,7 +1,11 @@
-import { countSort } from '../bucket-sort';
-import { countSortTestData } from './bucket-sort.testdata';
+import { baseSort, countSort } from '../bucket-sort';
+import { baseSortTestData, countSortTestData } from './bucket-sort.testdata';
 describe('bucket-sort', () => {
     it.each(countSortTestData)('countSort', ({ input, expected }) => {
         expect(countSort(input)).toEqual(expected);
+    });
+
+    it.each(baseSortTestData)('baseSort', ({ input, expected }) => {
+        expect(baseSort(input)).toEqual(expected);
     });
 });
--- src/algorithm/bucket-sort.ts
+++ src/algorithm/bucket-sort.ts
@@ -22,3 +22,42 @@ export function countSort(arr: number[]): number[] {
 
     return arr;
 }
+
+// 基数排序
+// 非负数，十进制
+export function baseSort(arr: number[]): number[] {
+    let max = Math.max(...arr);
+    let count = 0;
+    while (max !== 0) {
+        count++;
+
+        max = Math.floor(max / 10);
+    }
+
+    // 10个桶，桶中的内容先进先出
+    const buckets: number[][] = new Array(10).fill(0).map((_) => []);
+
+    let k = 1;
+    while (k <= count) {
+        const base = Math.pow(10, k);
+        const divisor = base / 10;
+
+        // 个位数，十位数，百位数分别入桶出桶
+        for (let i = 0; i < arr.length; i++) {
+            const cur = Math.floor((arr[i] % base) / divisor);
+            buckets[cur].push(arr[i]);
+        }
+
+        let j = 0;
+        // 出桶
+        for (let i = 0; i < buckets.length; i++) {
+            while (buckets[i].length > 0) {
+                arr[j++] = buckets[i].shift()!;
+            }
+        }
+
+        k++;
+    }
+
+    return arr;
+}
