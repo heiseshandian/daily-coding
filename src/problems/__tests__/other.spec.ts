2fc7243648e5d8c512a1c6994d966c00fd1763a5
baymax <476803711@qq.com>
2023-02-27 21:19:13 +0800

    算法优化

--- src/problems/__tests__/other.spec.ts
+++ src/problems/__tests__/other.spec.ts
@@ -264,7 +264,11 @@ import {
     getMinChangesTestData,
     getLengthOfLongestSubArrayWithSumK2TestData,
 } from './other.testdata';
-import { getLongestIncreasingSubsequence, findSubstringInWrapRoundString } from '../other';
+import {
+    getLongestIncreasingSubsequence,
+    findSubstringInWrapRoundString,
+    findSubstringInWrapRoundString2,
+} from '../other';
 import { maxRunTimeTestData, findSubstringInWrapRoundStringTestData } from './other.testdata';
 
 describe('other', () => {
@@ -298,14 +302,11 @@ describe('other', () => {
         expect(getMaxRopePoints2(arr, rope)).toBe(expected);
     });
 
-    it.each(getPlusOrMinusCountTestData)(
-        'getPlusOrMinusCount',
-        ({ input: { arr, target }, expected }) => {
-            expect(getPlusOrMinusCount(arr, target)).toBe(expected);
-            expect(getPlusOrMinusCountDp(arr, target)).toBe(expected);
-            expect(getPlusOrMinusCountDp2(arr, target)).toBe(expected);
-        }
-    );
+    it.each(getPlusOrMinusCountTestData)('getPlusOrMinusCount', ({ input: { arr, target }, expected }) => {
+        expect(getPlusOrMinusCount(arr, target)).toBe(expected);
+        expect(getPlusOrMinusCountDp(arr, target)).toBe(expected);
+        expect(getPlusOrMinusCountDp2(arr, target)).toBe(expected);
+    });
 
     it.each(getMaxMoneyTestData)('getMaxMoney', ({ input, expected }) => {
         expect(getMaxMoney(input)).toBe(expected);
@@ -420,19 +421,13 @@ describe('other', () => {
         expect(getMinSubArrThatShouldBeSorted(input)).toEqual(expected);
     });
 
-    it.each(getMinSumThatCanNotBeComposedTestData)(
-        'getMinSumThatCanNotBeComposed',
-        ({ input, expected }) => {
-            expect(getMinSumThatCanNotBeComposed(input)).toBe(expected);
-        }
-    );
+    it.each(getMinSumThatCanNotBeComposedTestData)('getMinSumThatCanNotBeComposed', ({ input, expected }) => {
+        expect(getMinSumThatCanNotBeComposed(input)).toBe(expected);
+    });
 
-    it.each(getMinSumThatCanNotBeComposed1TestData)(
-        'getMinSumThatCanNotBeComposed1',
-        ({ input, expected }) => {
-            expect(getMinSumThatCanNotBeComposed1(input)).toEqual(expected);
-        }
-    );
+    it.each(getMinSumThatCanNotBeComposed1TestData)('getMinSumThatCanNotBeComposed1', ({ input, expected }) => {
+        expect(getMinSumThatCanNotBeComposed1(input)).toEqual(expected);
+    });
 
     it.each(countLostNumbersTestData)('countLostNumbers', ({ input: { arr, range }, expected }) => {
         expect(countLostNumbers(arr, range)).toBe(expected);
@@ -454,12 +449,9 @@ describe('other', () => {
         }
     );
 
-    it.each(getMaxLenOfComposableSubArrTestData)(
-        'getMaxLenOfComposableSubArr',
-        ({ input, expected }) => {
-            expect(getMaxLenOfComposableSubArr(input)).toBe(expected);
-        }
-    );
+    it.each(getMaxLenOfComposableSubArrTestData)('getMaxLenOfComposableSubArr', ({ input, expected }) => {
+        expect(getMaxLenOfComposableSubArr(input)).toBe(expected);
+    });
 
     it.each(getMinStrCountTestData)('getMinStrCount', ({ input, expected }) => {
         expect(getMinStrCount(input)).toBe(expected);
@@ -501,15 +493,7 @@ describe('other', () => {
             [3, 2, 4, 1, 6, 7, 5].forEach((val) => message.receive(val, val));
 
             expect(console.log).toHaveBeenCalledTimes(7);
-            expect((console.log as jest.Mock).mock.calls).toEqual([
-                [1],
-                [2],
-                [3],
-                [4],
-                [5],
-                [6],
-                [7],
-            ]);
+            expect((console.log as jest.Mock).mock.calls).toEqual([[1], [2], [3], [4], [5], [6], [7]]);
         });
     });
 
@@ -529,12 +513,9 @@ describe('other', () => {
         expect(getClosestSumK(arr, k)).toBe(expected);
     });
 
-    it.each(getClosestSumKOfMatrixTestData)(
-        'getClosestSumKOfMatrix',
-        ({ input: { matrix, k }, expected }) => {
-            expect(getClosestSumKOfMatrix(matrix, k)).toBe(expected);
-        }
-    );
+    it.each(getClosestSumKOfMatrixTestData)('getClosestSumKOfMatrix', ({ input: { matrix, k }, expected }) => {
+        expect(getClosestSumKOfMatrix(matrix, k)).toBe(expected);
+    });
 
     it.each(findWordsTestData)('findWords', ({ input: { board, words }, expected }) => {
         expect(findWords(board, words)).toEqual(expected);
@@ -562,13 +543,10 @@ describe('other', () => {
         expect(countSubsequence(s, t)).toBe(expected);
     });
 
-    it.each(getCalculateMethodsTestData)(
-        'getCalculateMethods',
-        ({ input: { str, target }, expected }) => {
-            expect(getCalculateMethods(str, target).sort()).toEqual(expected.sort());
-            expect(getCalculateMethods2(str, target).sort()).toEqual(expected.sort());
-        }
-    );
+    it.each(getCalculateMethodsTestData)('getCalculateMethods', ({ input: { str, target }, expected }) => {
+        expect(getCalculateMethods(str, target).sort()).toEqual(expected.sort());
+        expect(getCalculateMethods2(str, target).sort()).toEqual(expected.sort());
+    });
 
     test('zigzagLevelOrder', () => {
         const head = new TreeNode(3);
@@ -590,14 +568,11 @@ describe('other', () => {
         expect(countJointMethodsDp2(str, arr)).toBe(expected);
     });
 
-    it.each(reverseBetweenTestData)(
-        'reverseBetween',
-        ({ input: { arr, left, right }, expected }) => {
-            const head = SingleLinkedList.from(arr);
+    it.each(reverseBetweenTestData)('reverseBetween', ({ input: { arr, left, right }, expected }) => {
+        const head = SingleLinkedList.from(arr);
 
-            expect(SingleLinkedList.toArray(reverseBetween(head, left, right))).toEqual(expected);
-        }
-    );
+        expect(SingleLinkedList.toArray(reverseBetween(head, left, right))).toEqual(expected);
+    });
 
     it.each(updatePathsTestData)('updatePaths', ({ input, expected }) => {
         updatePaths(input);
@@ -645,9 +620,7 @@ describe('other', () => {
     });
 
     it.each(getValidParenthesesTestData)('getValidParentheses', ({ input, expected }) => {
-        expect(getValidParentheses(input).sort(parenthesesComparator)).toEqual(
-            expected.sort(parenthesesComparator)
-        );
+        expect(getValidParentheses(input).sort(parenthesesComparator)).toEqual(expected.sort(parenthesesComparator));
     });
 
     it.each(getMaxLengthOfIncreasingSubsequenceTestData)(
@@ -672,12 +645,9 @@ describe('other', () => {
         expect(findSubstring(s, words)).toEqual(expected);
     });
 
-    it.each(evaluationMethodsTestData)(
-        'evaluationMethods',
-        ({ input: { str, expectedResult }, expected }) => {
-            expect(evaluationMethods(str, expectedResult)).toBe(expected);
-        }
-    );
+    it.each(evaluationMethodsTestData)('evaluationMethods', ({ input: { str, expectedResult }, expected }) => {
+        expect(evaluationMethods(str, expectedResult)).toBe(expected);
+    });
 
     it.each(findMinMovesTestData)('findMinMoves', ({ input, expected }) => {
         expect(findMinMoves(input)).toBe(expected);
@@ -744,12 +714,9 @@ describe('other', () => {
         expect(getSumOfMatrix(input)).toEqual(expected);
     });
 
-    it.each(getSumOfi1j1i2j2TestData)(
-        'getSumOfi1j1i2j2',
-        ({ input: { sumOfMatrix, i1, i2, j1, j2 }, expected }) => {
-            expect(getSumOfi1j1i2j2(sumOfMatrix, i1, j1, i2, j2)).toBe(expected);
-        }
-    );
+    it.each(getSumOfi1j1i2j2TestData)('getSumOfi1j1i2j2', ({ input: { sumOfMatrix, i1, i2, j1, j2 }, expected }) => {
+        expect(getSumOfi1j1i2j2(sumOfMatrix, i1, j1, i2, j2)).toBe(expected);
+    });
 
     test('getSumOfi1j1i2j2 will throw error if the provided index is invalid', () => {
         expect(() => getSumOfi1j1i2j2([[]], 0, 0, 1, 1)).toThrowError('Invalid index');
@@ -792,19 +759,13 @@ describe('other', () => {
         expect(getMaxValueOfS(input)).toBe(expected);
     });
 
-    it.each(getLongestIncreasingSubsequenceTestData)(
-        'getLongestIncreasingSubsequence',
-        ({ input, expected }) => {
-            expect(getLongestIncreasingSubsequence(input)).toEqual(expected);
-        }
-    );
+    it.each(getLongestIncreasingSubsequenceTestData)('getLongestIncreasingSubsequence', ({ input, expected }) => {
+        expect(getLongestIncreasingSubsequence(input)).toEqual(expected);
+    });
 
-    it.each(getMaxRemovableSubsequenceTestData)(
-        'getMaxRemovableSubsequence',
-        ({ input, expected }) => {
-            expect(getMaxRemovableSubsequence(input)).toBe(expected);
-        }
-    );
+    it.each(getMaxRemovableSubsequenceTestData)('getMaxRemovableSubsequence', ({ input, expected }) => {
+        expect(getMaxRemovableSubsequence(input)).toBe(expected);
+    });
 
     it.each(getMinChangesTestData)('getMinChanges', ({ input, expected }) => {
         expect(getMinChanges(input)).toBe(expected);
@@ -839,12 +800,9 @@ describe('other', () => {
         expect(minEatingSpeed(arr, h)).toBe(expected);
     });
 
-    it.each(maxUncrossedLinesTestData)(
-        'maxUncrossedLines',
-        ({ input: { nums1, nums2 }, expected }) => {
-            expect(maxUncrossedLines(nums1, nums2)).toBe(expected);
-        }
-    );
+    it.each(maxUncrossedLinesTestData)('maxUncrossedLines', ({ input: { nums1, nums2 }, expected }) => {
+        expect(maxUncrossedLines(nums1, nums2)).toBe(expected);
+    });
 
     it.each(avoidFloodTestData)('avoidFlood', ({ input, expected }) => {
         expect(avoidFlood(input)).toEqual(expected);
@@ -874,10 +832,8 @@ describe('other', () => {
         expect(leastWaitingTime2(arr, n)).toBe(expected);
     });
 
-    it.each(findSubstringInWrapRoundStringTestData)(
-        'findSubstringInWrapRoundString',
-        ({ input, expected }) => {
-            expect(findSubstringInWrapRoundString(input)).toBe(expected);
-        }
-    );
+    it.each(findSubstringInWrapRoundStringTestData)('findSubstringInWrapRoundString', ({ input, expected }) => {
+        expect(findSubstringInWrapRoundString(input)).toBe(expected);
+        expect(findSubstringInWrapRoundString2(input)).toBe(expected);
+    });
 });
