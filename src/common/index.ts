256816d1c7ab81850ef467ecece298c9888eaa98
baymax <476803711@qq.com>
2023-02-27 21:33:02 +0800

    代码优化

--- src/common/index.ts
+++ src/common/index.ts
@@ -20,14 +20,14 @@ export function maxCommonFactor(a: number, b: number): number {
 // 打印二进制位
 export function printBinary(n: number): string {
     let result: string[] = [];
-    for (let i = 0; i < 32; i++) {
+    for (let i = 31; i >= 0; i--) {
         // n & 000...0001
         // n & 000...0010
         // n & 000...0100 与完之后的结果只有某一位不是1结果才是0，否则那一位必然是1
         result.push((n & (1 << i)) === 0 ? '0' : '1');
     }
 
-    return result.reverse().join('');
+    return result.join('');
 }
 
 export function flipACoin(): boolean {
