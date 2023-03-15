6986248cf24e57520fe4bd9c68d3a8667718200c
baymax <476803711@qq.com>
2023-03-15 09:19:46 +0800

    重新开始系统学习vue3设计原理

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
     return {
-        root: './src/design-pattern/',
+        // root: './src/design-pattern/',
+        root: './src/vue3-study/',
     };
 });
