a3774e9381ac076e6082f0d8d98e46ff726fe371
baymax <476803711@qq.com>
2023-10-04 09:07:18 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -211,3 +211,29 @@ export function threeSumClosest(nums: number[], target: number): number {
 
     return closestSum;
 }
+
+/* 
+https://leetcode.com/problems/minimum-time-visiting-all-points/
+
+On a 2D plane, there are n points with integer coordinates points[i] = [xi, yi]. Return the minimum time in seconds to visit all the points in the order given by points.
+
+You can move according to these rules:
+
+In 1 second, you can either:
+    move vertically by one unit,
+    move horizontally by one unit, or
+    move diagonally sqrt(2) units (in other words, move one unit vertically then one unit horizontally in 1 second).
+You have to visit the points in the same order as they appear in the array.
+You are allowed to pass through points that appear later in the order, but these do not count as visits.
+*/
+export function minTimeToVisitAllPoints(points: number[][]): number {
+    let minTime = 0;
+    for (let i = 1; i < points.length; i++) {
+        const prev = points[i - 1];
+        const cur = points[i];
+        const xDiff = Math.abs(cur[0] - prev[0]);
+        const yDiff = Math.abs(cur[1] - prev[1]);
+        minTime += Math.max(xDiff, yDiff);
+    }
+    return minTime;
+}
