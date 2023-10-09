95a05cf7c4c2c18352a84258de7cbaa361fe1be8
baymax <476803711@qq.com>
2023-10-09 16:11:16 +0800

    feat: 回溯法解决数独问题

--- src/problems/backtracking.ts
+++ src/problems/backtracking.ts
@@ -101,3 +101,82 @@ export function generateParenthesis(n: number): string[] {
 
     return result;
 }
+
+/* 
+https://leetcode.com/problems/sudoku-solver/
+
+Write a program to solve a Sudoku puzzle by filling the empty cells.
+
+A sudoku solution must satisfy all of the following rules:
+
+Each of the digits 1-9 must occur exactly once in each row.
+Each of the digits 1-9 must occur exactly once in each column.
+Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+The '.' character indicates empty cells.
+*/
+export function solveSudoku(board: string[][]): void {
+    // row[i][num] (i:0-8,num:1-9) row[0][9]=true 表示0行9这个数字已经出现过（也就是说第0行不能再放9了）
+    const row: boolean[][] = new Array(9).fill(0).map((_) => new Array(10));
+    // 某一列某个数字是否出现过
+    const col: boolean[][] = new Array(9).fill(0).map((_) => new Array(10));
+    // 某个桶里某个数字是否出现过
+    const bucket: boolean[][] = new Array(9).fill(0).map((_) => new Array(10));
+    initFlags(board, row, col, bucket);
+
+    const backtracking = (i: number, j: number): boolean => {
+        if (i === 9) {
+            return true;
+        }
+
+        // j到8之后i需要换行，也就是加1
+        const nexti = j < 8 ? i : i + 1;
+        // j到8之后需要跳到下一行从0开始
+        const nextj = j < 8 ? j + 1 : 0;
+
+        // 如果当前位置是数字则直接跳到下一个位置
+        if (board[i][j] !== '.') {
+            return backtracking(nexti, nextj);
+        }
+        const bucketId = 3 * parseInt(`${i / 3}`) + parseInt(`${j / 3}`);
+        // 否则从1-9开始尝试
+        for (let num = 1; num <= 9; num++) {
+            if (!row[i][num] && !col[j][num] && !bucket[bucketId][num]) {
+                row[i][num] = true;
+                col[j][num] = true;
+                bucket[bucketId][num] = true;
+                board[i][j] = `${num}`;
+
+                // 后续可以成功直接返回
+                if (backtracking(nexti, nextj)) {
+                    return true;
+                }
+
+                // 恢复现场用下一个数字来尝试
+                row[i][num] = false;
+                col[j][num] = false;
+                bucket[bucketId][num] = false;
+                board[i][j] = '.';
+            }
+        }
+
+        return false;
+    };
+
+    backtracking(0, 0);
+}
+
+function initFlags(board: string[][], row: boolean[][], col: boolean[][], bucket: boolean[][]): void {
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[0].length; j++) {
+            const bucketId = 3 * parseInt(`${i / 3}`) + parseInt(`${j / 3}`);
+            if (board[i][j] === '.') {
+                continue;
+            }
+
+            const num = parseInt(board[i][j]);
+            row[i][num] = true;
+            col[j][num] = true;
+            bucket[bucketId][num] = true;
+        }
+    }
+}
