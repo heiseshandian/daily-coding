ffa4ea13d706eea1e8b2f36c76f2ce3094a630b7
baymax <476803711@qq.com>
2023-10-11 21:34:16 +0800

    feat: 每日一题

--- src/problems/backtracking.ts
+++ src/problems/backtracking.ts
@@ -209,3 +209,31 @@ export function subsets(nums: number[]): number[][] {
 
     return result;
 }
+
+/* 
+https://leetcode.com/problems/combinations/
+
+Given two integers n and k, return all possible combinations 
+of k numbers chosen from the range [1, n].
+
+You may return the answer in any order.
+*/
+export function combine(n: number, k: number): number[][] {
+    const result: number[][] = [];
+
+    const backtracking = (index: number, path: number[]) => {
+        if (path.length === k) {
+            result.push(path.slice());
+            return;
+        }
+
+        for (let i = index; i <= n; i++) {
+            path.push(i);
+            backtracking(i + 1, path);
+            path.pop();
+        }
+    };
+    backtracking(1, []);
+
+    return result;
+}
