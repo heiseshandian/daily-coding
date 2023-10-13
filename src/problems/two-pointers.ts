45c671e7f35961824cf7c8876a38207fe112795b
baymax <476803711@qq.com>
2023-10-13 11:41:28 +0800

    feat: 每日一题

---
+++ src/problems/two-pointers.ts
@@ -0,0 +1,31 @@
+/* 
+https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
+Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, 
+find two numbers such that they add up to a specific target number. 
+Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 < numbers.length.
+
+Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+
+The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+Your solution must use only constant extra space.
+*/
+export function twoSum(numbers: number[], target: number): number[] {
+    let left = 0;
+    let right = numbers.length - 1;
+    while (left < right) {
+        const sum = numbers[left] + numbers[right];
+        if (sum === target) {
+            return [left + 1, right + 1];
+        }
+
+        if (sum > target) {
+            right--;
+        } else {
+            left++;
+        }
+    }
+
+    return [];
+}
