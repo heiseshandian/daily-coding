35cfbf33b304a7fbb4b46e8ccad9897964f24571
baymax <476803711@qq.com>
2023-10-16 13:22:13 +0800

    feat: 二维接雨水问题

--- src/problems/two-pointers.ts
+++ src/problems/two-pointers.ts
@@ -29,3 +29,33 @@ export function twoSum(numbers: number[], target: number): number[] {
 
     return [];
 }
+
+/* 
+https://leetcode.com/problems/container-with-most-water/description/
+
+You are given an integer array height of length n. There are n vertical lines drawn such 
+that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+
+Notice that you may not slant the container.
+*/
+export function maxArea(height: number[]): number {
+    let max = 0;
+
+    let left = 0;
+    let right = height.length - 1;
+    while (left < right) {
+        max = Math.max(max, (right - left) * Math.min(height[left], height[right]));
+
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return max;
+}
