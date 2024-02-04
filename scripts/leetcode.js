3fb41b60ef261c9056192d1ed56bd71283b0623a
baymax <476803711@qq.com>
2024-02-04 10:31:22 +0800

    feat: 优化leetcode脚本能力

--- scripts/leetcode.js
+++ scripts/leetcode.js
@@ -1,6 +1,8 @@
 // 自动复制leetcode上的链接地址、标题、描述等
-function addCopyBtn() {
-    const btnContainer = document.querySelector('.flexlayout__tab .text-title-large');
+addCopyBtn();
+
+async function addCopyBtn() {
+    const btnContainer = await getBtnContainer();
     if (btnContainer.hasAddedCopyBtn) {
         return;
     }
@@ -14,10 +16,33 @@ function addCopyBtn() {
     btnContainer.hasAddedCopyBtn = true;
 }
 
-function reduceMultipleEmptyLines(text) {
-    return text.replace(/(\n\s*\n)+/g, '\n\n');
+/**
+ * 由于 btnContainer 是异步加载的，这里使用 raf 来获取
+ *
+ * @returns {Promise<HTMLDivElement>} btnContainer
+ */
+function getBtnContainer() {
+    let btnContainer;
+    let resolveFn;
+
+    const updateBtnContainer = () => {
+        btnContainer = document.querySelector('.flexlayout__tab .text-title-large');
+        if (!btnContainer) {
+            requestAnimationFrame(updateBtnContainer);
+        } else {
+            resolveFn(btnContainer);
+        }
+    };
+    updateBtnContainer();
+
+    return new Promise((resolve) => {
+        resolveFn = resolve;
+    });
 }
 
+/**
+ * 点击复制按钮后将内容复制到剪贴板
+ */
 function handleClickCopyBtn() {
     const container = document.querySelector('.flexlayout__tab');
 
@@ -30,13 +55,46 @@ function handleClickCopyBtn() {
     navigator.clipboard
         .writeText(toCopy)
         .then(() => {
-            console.log('Text successfully copied to clipboard');
+            showSuccessMessage('Text successfully copied to clipboard');
         })
         .catch((err) => {
-            console.error('Unable to copy text to clipboard', err);
+            console.error(err);
+            showErrorMessage('Unable to copy text to clipboard');
         });
 }
 
-setTimeout(() => {
-    addCopyBtn();
-}, 4000);
+function reduceMultipleEmptyLines(text) {
+    return text.replace(/(\n\s*\n)+/g, '\n\n');
+}
+
+function showMessage(message, type) {
+    const messageContainer = document.createElement('div');
+    messageContainer.className = 'message-container';
+    document.body.appendChild(messageContainer);
+
+    const messageElement = document.createElement('div');
+    messageElement.className = `message ${type}`;
+    messageElement.textContent = message;
+    messageContainer.appendChild(messageElement);
+
+    messageContainer.classList.remove('hidden');
+
+    setTimeout(function () {
+        hideMessage(messageContainer);
+    }, 3000);
+}
+
+function hideMessage(messageContainer) {
+    messageContainer.classList.add('hidden');
+    setTimeout(function () {
+        document.body.removeChild(messageContainer);
+    }, 500);
+}
+
+function showSuccessMessage(message) {
+    showMessage(message, 'success');
+}
+
+function showErrorMessage(message) {
+    showMessage(message, 'error');
+}
