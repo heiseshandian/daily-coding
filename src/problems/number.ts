6c013e676f72483c42dcbb47d6270262eb42dc4f
baymax <476803711@qq.com>
2024-02-08 16:42:11 +0800

    feat: 每日一题

--- src/problems/number.ts
+++ src/problems/number.ts
@@ -74,3 +74,54 @@ export function subtractProductAndSum(n: number): number {
         digits.reduce((acc, cur) => acc + cur)
     );
 }
+
+/*
+https://leetcode.com/problems/find-n-unique-integers-sum-up-to-zero/
+1304. Find N Unique Integers Sum up to Zero
+Given an integer n, return any array containing n unique integers such that they add up to 0.
+
+Example 1:
+
+Input: n = 5
+Output: [-7,-1,1,3,4]
+Explanation: These arrays also are accepted [-5,-1,1,2,3] , [-3,-1,2,-2,4].
+
+Example 2:
+
+Input: n = 3
+Output: [-1,0,1]
+
+Example 3:
+
+Input: n = 1
+Output: [0]
+
+Constraints:
+
+	1 <= n <= 1000
+*/
+export function sumZero(n: number): number[] {
+    const mid = n >> 1;
+    const left = new Array(mid).fill(0).map((_, i) => -(i + 1));
+    const right = left.map((v) => -v);
+
+    if (n & 1) {
+        return left.concat(0).concat(right);
+    } else {
+        return left.concat(right);
+    }
+}
+
+export function sumZero2(n: number): number[] {
+    const mid = n >> 1;
+    const result: number[] = [];
+    for (let i = 1; i <= mid; i++) {
+        result.push(i, -i);
+    }
+
+    if (n & 1) {
+        result.push(0);
+    }
+
+    return result;
+}
