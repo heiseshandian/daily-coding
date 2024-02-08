3bef151804dd538b00dd471719daf015bacb3b23
baymax <476803711@qq.com>
2024-02-08 16:31:20 +0800

    feat: 每日一题

--- src/problems/number.ts
+++ src/problems/number.ts
@@ -34,3 +34,43 @@ export function judgeSquareSum(c: number): boolean {
 
     return false;
 }
+
+/*
+https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/description/
+1281. Subtract the Product and Sum of Digits of an Integer
+Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+
+Example 1:
+
+Input: n = 234
+Output: 15 
+Explanation: 
+Product of digits = 2 * 3 * 4 = 24 
+Sum of digits = 2 + 3 + 4 = 9 
+Result = 24 - 9 = 15
+
+Example 2:
+
+Input: n = 4421
+Output: 21
+Explanation: 
+Product of digits = 4 * 4 * 2 * 1 = 32 
+Sum of digits = 4 + 4 + 2 + 1 = 11 
+Result = 32 - 11 = 21
+
+Constraints:
+
+	1 <= n <= 10^5
+*/
+export function subtractProductAndSum(n: number): number {
+    const digits: number[] = [];
+    while (n) {
+        digits.push(n % 10);
+        n = Math.floor(n / 10);
+    }
+
+    return (
+        digits.reduce((acc, cur) => acc * cur, 1) -
+        digits.reduce((acc, cur) => acc + cur)
+    );
+}
