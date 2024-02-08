87dbc8601c7716b7a519cc3fbc640a12065a3c21
baymax <476803711@qq.com>
2024-02-08 14:09:12 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -800,3 +800,55 @@ export function countMatches(
     const index = ['type', 'color', 'name'].indexOf(ruleKey);
     return items.filter((item) => item[index] === ruleValue).length;
 }
+
+/* 
+https://leetcode.com/problems/monotonic-array/description/
+896. Monotonic Array
+An array is monotonic if it is either monotone increasing or monotone decreasing.
+
+An array nums is monotone increasing if for all i <= j, nums[i] <= nums[j]. An array nums is monotone decreasing if for all i <= j, nums[i] >= nums[j].
+
+Given an integer array nums, return true if the given array is monotonic, or false otherwise.
+
+Example 1:
+
+Input: nums = [1,2,2,3]
+Output: true
+
+Example 2:
+
+Input: nums = [6,5,4,4]
+Output: true
+
+Example 3:
+
+Input: nums = [1,3,2]
+Output: false
+
+Constraints:
+
+	1 <= nums.length <= 10^5
+	-10^5 <= nums[i] <= 10^5
+*/
+export function isMonotonic(nums: number[]): boolean {
+    let i = 0;
+    let isIncreasing = false;
+    let isDecreasing = false;
+    while (i < nums.length - 1) {
+        if (nums[i] === nums[i + 1]) {
+            // do nonthing
+        } else if (nums[i] < nums[i + 1]) {
+            isIncreasing = true;
+        } else {
+            isDecreasing = true;
+        }
+
+        if (isIncreasing && isDecreasing) {
+            return false;
+        }
+
+        i++;
+    }
+
+    return isIncreasing === false || isDecreasing === false;
+}
