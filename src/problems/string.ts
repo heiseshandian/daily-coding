9c899a37a2e260ad44e0cd21f65a281e9ee61ae6
baymax <476803711@qq.com>
2024-02-08 15:35:07 +0800

    feat: 每日一题

---
+++ src/problems/string.ts
@@ -0,0 +1,47 @@
+/*
+https://leetcode.com/problems/construct-k-palindrome-strings/
+1400. Construct K Palindrome Strings
+Given a string s and an integer k, return true if you can use all the characters in s to construct k palindrome strings or false otherwise.
+
+Example 1:
+
+Input: s = "annabelle", k = 2
+Output: true
+Explanation: You can construct two palindromes using all characters in s.
+Some possible constructions "anna" + "elble", "anbna" + "elle", "anellena" + "b"
+
+Example 2:
+
+Input: s = "leetcode", k = 3
+Output: false
+Explanation: It is impossible to construct 3 palindromes using all the characters of s.
+
+Example 3:
+
+Input: s = "true", k = 4
+Output: true
+Explanation: The only possible solution is to put each character in a separate string.
+
+Constraints:
+
+	1 <= s.length <= 10^5
+	s consists of lowercase English letters.
+	1 <= k <= 10^5
+*/
+export function canConstruct(s: string, k: number): boolean {
+    if (s.length < k) {
+        return false;
+    }
+
+    const map: Record<string, number> = {};
+    for (let i = 0; i < s.length; i++) {
+        const prev = map[s[i]] || 0;
+        map[s[i]] = prev + 1;
+    }
+
+    const oddCount = Object.keys(map)
+        .map((k) => map[k])
+        .filter((times) => times & 1).length;
+
+    return oddCount <= k;
+}
