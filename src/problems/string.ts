87995cf5668863167f0de8c58fcc90a93fdc0066
baymax <476803711@qq.com>
2024-02-10 12:22:29 +0800

    refactor: 将countSubstrings移动到string中

--- src/problems/string.ts
+++ src/problems/string.ts
@@ -43,3 +43,70 @@ export function canConstruct(s: string, k: number): boolean {
 
     return oddCount <= k;
 }
+
+/*
+https://leetcode.com/problems/palindromic-substrings/description/?envType=daily-question&envId=2024-02-10
+647. Palindromic Substrings
+Given a string s, return the number of palindromic substrings in it.
+
+A string is a palindrome when it reads the same backward as forward.
+
+A substring is a contiguous sequence of characters within the string.
+
+Example 1:
+
+Input: s = "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+
+Example 2:
+
+Input: s = "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+
+Constraints:
+
+	1 <= s.length <= 1000
+	s consists of lowercase English letters.
+
+Hints:
+- How can we reuse a previously computed palindrome to compute a larger palindrome?
+- If “aba” is a palindrome, is “xabax” a palindrome? Similarly is “xabay” a palindrome?
+- Complexity based hint:
+If we use brute force and check whether for every start and end position a substring is a palindrome we have O(n^2) start - end pairs 
+and O(n) palindromic checks. Can we reduce the time for palindromic checks to O(1) by reusing some previous computation?
+*/
+export function countSubstrings(s: string): number {
+    let count = 0;
+
+    for (let center = 0; center < s.length; center++) {
+        // odd
+        for (
+            let start = center, end = center;
+            start >= 0 && end < s.length;
+            start--, end++
+        ) {
+            if (s[start] === s[end]) {
+                count++;
+            } else {
+                break;
+            }
+        }
+
+        // even
+        for (
+            let start = center, end = center + 1;
+            start >= 0 && end < s.length;
+            start--, end++
+        ) {
+            if (s[start] === s[end]) {
+                count++;
+            } else {
+                break;
+            }
+        }
+    }
+
+    return count;
+}
