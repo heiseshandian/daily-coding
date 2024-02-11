9886864e3cafd14adf8de4970a70d1f3f49b121b
baymax <476803711@qq.com>
2024-02-12 00:03:43 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -1,4 +1,5 @@
 import { cache } from '../design-pattern/proxy';
+import { GenericHeap } from '../algorithm/generic-heap';
 /* 
 https://leetcode.com/problems/find-the-maximum-number-of-marked-indices/description/
 
@@ -1067,4 +1068,77 @@ export function findWinners2(matches: number[][]): number[][] {
     });
 
     return [answer1, answer2];
+}
+
+/*
+https://leetcode.com/problems/find-the-kth-largest-integer-in-the-array/
+1985. Find the Kth Largest Integer in the Array
+You are given an array of strings nums and an integer k. Each string in nums represents an integer without leading zeros.
+
+Return the string that represents the kth largest integer in nums.
+
+Note: Duplicate numbers should be counted distinctly. For example, if nums is ["1","2","2"], "2" is the first largest integer, "2" is the second-largest integer, and "1" is the third-largest integer.
+
+Example 1:
+
+Input: nums = ["3","6","7","10"], k = 4
+Output: "3"
+Explanation:
+The numbers in nums sorted in non-decreasing order are ["3","6","7","10"].
+The 4th largest integer in nums is "3".
+
+Example 2:
+
+Input: nums = ["2","21","12","1"], k = 3
+Output: "2"
+Explanation:
+The numbers in nums sorted in non-decreasing order are ["1","2","12","21"].
+The 3rd largest integer in nums is "2".
+
+Example 3:
+
+Input: nums = ["0","0"], k = 2
+Output: "0"
+Explanation:
+The numbers in nums sorted in non-decreasing order are ["0","0"].
+The 2nd largest integer in nums is "0".
+
+Constraints:
+
+	1 <= k <= nums.length <= 10^4
+	1 <= nums[i].length <= 100
+	nums[i] consists of only digits.
+	nums[i] will not have any leading zeros.
+*/
+export function kthLargestNumber(nums: string[], k: number): string {
+    const minHeap = new GenericHeap<string>((a, b) => {
+        const aBigInt = BigInt(a);
+        const bBigInt = BigInt(b);
+
+        if (aBigInt < bBigInt) {
+            return -1;
+        }
+        if (aBigInt === bBigInt) {
+            return 0;
+        }
+        return 1;
+    });
+    minHeap.push(nums[0]);
+
+    for (let i = 1; i < nums.length; i++) {
+        if (minHeap.size() < k) {
+            minHeap.push(nums[i]);
+            continue;
+        }
+        if (BigInt(minHeap.peek()) > BigInt(nums[i])) {
+            continue;
+        }
+
+        if (minHeap.size() >= k) {
+            minHeap.pop();
+        }
+        minHeap.push(nums[i]);
+    }
+
+    return String(minHeap.pop());
 }
