a9b59e34bc5e67b21b43262305719abf8e0ec542
baymax <476803711@qq.com>
2024-02-13 00:21:49 +0800

    feat: 每日一题

--- src/problems/string.ts
+++ src/problems/string.ts
@@ -209,3 +209,51 @@ export function winnerOfGame2(colors: string): boolean {
 
     return aliceCount > bobCount;
 }
+
+/*
+https://leetcode.com/problems/longest-palindromic-subsequence/description/?utm_source=LCUS&utm_medium=ip_redirect&utm_campaign=transfer2china
+516. Longest Palindromic Subsequence
+Given a string s, find the longest palindromic subsequence's length in s.
+
+A subsequence is a sequence that can be derived from another sequence by deleting some or no elements without changing the order of the remaining elements.
+
+Example 1:
+
+Input: s = "bbbab"
+Output: 4
+Explanation: One possible longest palindromic subsequence is "bbbb".
+
+Example 2:
+
+Input: s = "cbbd"
+Output: 2
+Explanation: One possible longest palindromic subsequence is "bb".
+
+Constraints:
+
+	1 <= s.length <= 1000
+	s consists only of lowercase English letters.
+
+思路分析：
+- 范围内的尝试模型
+*/
+export function longestPalindromeSubseq(s: string): number {
+    const n: number = s.length;
+    // dp[i][j] s[i-j] 范围内最长回文串长度
+    const dp: number[][] = Array.from({ length: n }, () => Array(n).fill(0));
+
+    // 从下到上-从左到右填表
+    for (let i = n - 1; i >= 0; i--) {
+        dp[i][i] = 1;
+
+        for (let j = i + 1; j < n; j++) {
+            if (s[i] === s[j]) {
+                dp[i][j] = dp[i + 1][j - 1] + 2;
+            } else {
+                dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    return dp[0][n - 1];
+}
