791cf7a21acd76757e1926a180616244755c1fb4
baymax <476803711@qq.com>
2024-02-14 21:57:51 +0800

    feat: 每日一题

--- src/problems/string.ts
+++ src/problems/string.ts
@@ -311,3 +311,52 @@ export function longestPalindrome(s: string): string {
 
     return max;
 }
+
+/*
+https://leetcode.com/problems/check-if-one-string-swap-can-make-strings-equal/description/
+1790. Check if One String Swap Can Make Strings Equal
+You are given two strings s1 and s2 of equal length. A string swap is an operation where you choose two indices in a string (not necessarily different) and swap the characters at these indices.
+
+Return true if it is possible to make both strings equal by performing at most one string swap on exactly one of the strings. Otherwise, return false.
+
+Example 1:
+
+Input: s1 = "bank", s2 = "kanb"
+Output: true
+Explanation: For example, swap the first character with the last character of s2 to make "bank".
+
+Example 2:
+
+Input: s1 = "attack", s2 = "defend"
+Output: false
+Explanation: It is impossible to make them equal with one string swap.
+
+Example 3:
+
+Input: s1 = "kelb", s2 = "kelb"
+Output: true
+Explanation: The two strings are already equal, so no string swap operation is required.
+
+Constraints:
+
+	1 <= s1.length, s2.length <= 100
+	s1.length == s2.length
+	s1 and s2 consist of only lowercase English letters.
+*/
+export function areAlmostEqual(s1: string, s2: string): boolean {
+    const diff: number[] = [];
+    for (let i = 0; i < s1.length; i++) {
+        if (s1[i] !== s2[i]) {
+            diff.push(i);
+        }
+    }
+
+    if (diff.length === 0) {
+        return true;
+    }
+    if (diff.length === 2) {
+        const [first, last] = diff;
+        return s1[first] === s2[last] && s1[last] === s2[first];
+    }
+    return false;
+}
