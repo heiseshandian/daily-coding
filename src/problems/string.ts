59088b8af75f26a1c69082290e75197f8d2050e6
baymax <476803711@qq.com>
2024-02-16 17:07:53 +0800

    feat: 每日一题

--- src/problems/string.ts
+++ src/problems/string.ts
@@ -360,3 +360,68 @@ export function areAlmostEqual(s1: string, s2: string): boolean {
     }
     return false;
 }
+
+/*
+https://leetcode.com/problems/unique-length-3-palindromic-subsequences/description/
+1930. Unique Length-3 Palindromic Subsequences
+Given a string s, return the number of unique palindromes of length three that are a subsequence of s.
+
+Note that even if there are multiple ways to obtain the same subsequence, it is still only counted once.
+
+A palindrome is a string that reads the same forwards and backwards.
+
+A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+
+	For example, "ace" is a subsequence of "abcde".
+
+Example 1:
+
+Input: s = "aabca"
+Output: 3
+Explanation: The 3 palindromic subsequences of length 3 are:
+- "aba" (subsequence of "aabca")
+- "aaa" (subsequence of "aabca")
+- "aca" (subsequence of "aabca")
+
+Example 2:
+
+Input: s = "adc"
+Output: 0
+Explanation: There are no palindromic subsequences of length 3 in "adc".
+
+Example 3:
+
+Input: s = "bbcbaba"
+Output: 4
+Explanation: The 4 palindromic subsequences of length 3 are:
+- "bbb" (subsequence of "bbcbaba")
+- "bcb" (subsequence of "bbcbaba")
+- "bab" (subsequence of "bbcbaba")
+- "aba" (subsequence of "bbcbaba")
+
+Constraints:
+
+	3 <= s.length <= 10^5
+	s consists of only lowercase English letters.
+*/
+export function countPalindromicSubsequence(s: string): number {
+    const map: Record<string, number> = {};
+    for (let i = 0; i < s.length; i++) {
+        map[s[i]] = i;
+    }
+
+    const countUniqueAlhas = (l: number, r: number): number => {
+        return new Set(s.substring(l + 1, r)).size;
+    };
+
+    let count = 0;
+    const set = new Set();
+    for (let i = 0; i < s.length; i++) {
+        if (map[s[i]] > i && !set.has(s[i])) {
+            set.add(s[i]);
+            count += countUniqueAlhas(i, map[s[i]]);
+        }
+    }
+
+    return count;
+}
