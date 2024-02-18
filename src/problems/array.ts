acc4f747e32bd8e9785dd9ca41983c25d5c15dff
baymax <476803711@qq.com>
2024-02-18 11:26:48 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -1630,3 +1630,106 @@ export function minOperations(nums: number[]): number {
 
     return minOps;
 }
+
+/*
+https://leetcode.com/problems/meeting-rooms-iii/description/?envType=daily-question&envId=2024-02-18
+2402. Meeting Rooms III
+You are given an integer n. There are n rooms numbered from 0 to n - 1.
+
+You are given a 2D integer array meetings where meetings[i] = [starti, endi] means that a meeting will be held during the half-closed time interval [starti, endi). All the values of starti are unique.
+
+Meetings are allocated to rooms in the following manner:
+
+	Each meeting will take place in the unused room with the lowest number.
+	If there are no available rooms, the meeting will be delayed until a room becomes free. The delayed meeting should have the same duration as the original meeting.
+	When a room becomes unused, meetings that have an earlier original start time should be given the room.
+
+Return the number of the room that held the most meetings. If there are multiple rooms, return the room with the lowest number.
+
+A half-closed interval [a, b) is the interval between a and b including a and not including b.
+
+Example 1:
+
+Input: n = 2, meetings = [[0,10],[1,5],[2,7],[3,4]]
+Output: 0
+Explanation:
+- At time 0, both rooms are not being used. The first meeting starts in room 0.
+- At time 1, only room 1 is not being used. The second meeting starts in room 1.
+- At time 2, both rooms are being used. The third meeting is delayed.
+- At time 3, both rooms are being used. The fourth meeting is delayed.
+- At time 5, the meeting in room 1 finishes. The third meeting starts in room 1 for the time period [5,10).
+- At time 10, the meetings in both rooms finish. The fourth meeting starts in room 0 for the time period [10,11).
+Both rooms 0 and 1 held 2 meetings, so we return 0. 
+
+Example 2:
+
+Input: n = 3, meetings = [[1,20],[2,10],[3,5],[4,9],[6,8]]
+Output: 1
+Explanation:
+- At time 1, all three rooms are not being used. The first meeting starts in room 0.
+- At time 2, rooms 1 and 2 are not being used. The second meeting starts in room 1.
+- At time 3, only room 2 is not being used. The third meeting starts in room 2.
+- At time 4, all three rooms are being used. The fourth meeting is delayed.
+- At time 5, the meeting in room 2 finishes. The fourth meeting starts in room 2 for the time period [5,10).
+- At time 6, all three rooms are being used. The fifth meeting is delayed.
+- At time 10, the meetings in rooms 1 and 2 finish. The fifth meeting starts in room 1 for the time period [10,12).
+Room 0 held 1 meeting while rooms 1 and 2 each held 2 meetings, so we return 1. 
+
+Constraints:
+
+	1 <= n <= 100
+	1 <= meetings.length <= 10^5
+	meetings[i].length == 2
+	0 <= starti < endi <= 10^5
+	All the values of starti are unique.
+
+Hints:
+- Sort meetings based on start times.
+- Use two min heaps, the first one keeps track of the numbers of all the rooms that are free. 
+The second heap keeps track of the end times of all the meetings that are happening and the room that they are in.
+- Keep track of the number of times each room is used in an array.
+- With each meeting, check if there are any free rooms. If there are, then use the room with the smallest number. 
+Otherwise, assign the meeting to the room whose meeting will end the soonest.
+*/
+export function mostBooked(n: number, meetings: number[][]): number {
+    meetings.sort(([startA], [startB]) => startA - startB);
+
+    const usedTimes = new Array(n).fill(0);
+    const freeRooms = new GenericHeap((a, b) => a - b);
+    const endTimes = new GenericHeap<[endTime: number, room: number]>(
+        ([endTimeA, roomA], [endTimeB, roomB]) =>
+            endTimeA - endTimeB || roomA - roomB
+    );
+    for (let i = 0; i < n; i++) {
+        freeRooms.push(i);
+    }
+
+    for (let i = 0; i < meetings.length; i++) {
+        const [starti, endi] = meetings[i];
+        while (endTimes.size() > 0 && endTimes.peek()[0] <= starti) {
+            const [, room] = endTimes.pop();
+            freeRooms.push(room);
+        }
+
+        if (freeRooms.size() > 0) {
+            const firstFreeRoom = freeRooms.pop();
+            endTimes.push([endi, firstFreeRoom]);
+            usedTimes[firstFreeRoom]++;
+        } else {
+            const [endTime, room] = endTimes.pop();
+            endTimes.push([endTime + endi - starti, room]);
+            usedTimes[room]++;
+        }
+    }
+
+    let max = -Infinity;
+    let maxIndex = -1;
+    for (let i = 0; i < usedTimes.length; i++) {
+        if (max < usedTimes[i]) {
+            max = usedTimes[i];
+            maxIndex = i;
+        }
+    }
+
+    return maxIndex;
+}
