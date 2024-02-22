d883285af6ef277060da1f591b3a8b902d95ef29
baymax <476803711@qq.com>
2024-02-23 00:29:52 +0800

    feat: 每日一题

--- src/problems/string.ts
+++ src/problems/string.ts
@@ -489,3 +489,140 @@ export function wordPattern(pattern: string, s: string): boolean {
 
     return true;
 }
+
+/*
+https://leetcode.com/problems/shortest-common-supersequence/
+1092. Shortest Common Supersequence 
+Given two strings str1 and str2, return the shortest string that has both str1 and str2 as subsequences. If there are multiple valid strings, return any of them.
+
+A string s is a subsequence of string t if deleting some number of characters from t (possibly 0) results in the string s.
+
+Example 1:
+
+Input: str1 = "abac", str2 = "cab"
+Output: "cabac"
+Explanation: 
+str1 = "abac" is a subsequence of "cabac" because we can delete the first "c".
+str2 = "cab" is a subsequence of "cabac" because we can delete the last "ac".
+The answer provided is the shortest such string that satisfies these properties.
+
+Example 2:
+
+Input: str1 = "aaaaaaaa", str2 = "aaaaaaaa"
+Output: "aaaaaaaa"
+
+Constraints:
+
+	1 <= str1.length, str2.length <= 1000
+	str1 and str2 consist of lowercase English letters.
+*/
+export function shortestCommonSupersequence(
+    str1: string,
+    str2: string
+): string {
+    const m = str1.length,
+        n = str2.length;
+    // dp array to store the length of LCS.
+    const dp: number[][] = Array.from({ length: m + 1 }, () =>
+        Array(n + 1).fill(0)
+    );
+
+    // Populate the dp array with lengths of LCS.
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (str1[i - 1] === str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    // Construct the SCS from the dp array.
+    let i = m,
+        j = n;
+    let scs = '';
+
+    while (i > 0 && j > 0) {
+        if (str1[i - 1] === str2[j - 1]) {
+            scs = str1[i - 1] + scs;
+            i--;
+            j--;
+        } else if (dp[i - 1][j] > dp[i][j - 1]) {
+            scs = str1[i - 1] + scs;
+            i--;
+        } else {
+            scs = str2[j - 1] + scs;
+            j--;
+        }
+    }
+
+    // Add remaining characters from str1 or str2.
+    while (i > 0) {
+        scs = str1[i - 1] + scs;
+        i--;
+    }
+    while (j > 0) {
+        scs = str2[j - 1] + scs;
+        j--;
+    }
+
+    return scs;
+}
+
+// 此方法在某些场景下会产生大量数组操作，数组扩容，可能会非常影响性能，
+// leetcode 上会出现 Time Limit Exceeded
+export function shortestCommonSupersequence2(
+    str1: string,
+    str2: string
+): string {
+    const dp: [i: number, j: number][][][] = new Array(str1.length)
+        .fill(0)
+        .map((_) => new Array(str2.length).fill([]));
+
+    for (let i = 0; i < str1.length; i++) {
+        for (let j = 0; j < str2.length; j++) {
+            if (str1[i] === str2[j]) {
+                dp[i][j] =
+                    i - 1 >= 0 && j - 1 >= 0
+                        ? dp[i - 1][j - 1].concat([[i, j]])
+                        : [[i, j]];
+                continue;
+            }
+
+            if (
+                i - 1 >= 0 &&
+                (j === 0 ||
+                    (j - 1 >= 0 && dp[i - 1][j].length >= dp[i][j - 1].length))
+            ) {
+                dp[i][j] = dp[i - 1][j];
+                continue;
+            }
+
+            if (
+                j - 1 >= 0 &&
+                (i === 0 ||
+                    (i - 1 >= 0 && dp[i][j - 1].length > dp[i - 1][j].length))
+            ) {
+                dp[i][j] = dp[i][j - 1];
+            }
+        }
+    }
+
+    let result = '';
+    const maxCommonSubsequence = dp[str1.length - 1][str2.length - 1];
+    let prevI = 0;
+    let prevJ = 0;
+    for (let i = 0; i < maxCommonSubsequence.length; i++) {
+        const [endI, endJ] = maxCommonSubsequence[i];
+        result += str1.slice(prevI, endI);
+        result += str2.slice(prevJ, endJ);
+        result += str1[endI];
+        prevI = endI + 1;
+        prevJ = endJ + 1;
+    }
+    result += str1.slice(prevI);
+    result += str2.slice(prevJ);
+
+    return result;
+}
