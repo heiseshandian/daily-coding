d61033ba54881e4c1cbf3758d6245db5db9429d7
baymax <476803711@qq.com>
2024-02-27 09:43:58 +0800

    feat: 每日一题

--- src/problems/string.ts
+++ src/problems/string.ts
@@ -761,3 +761,60 @@ export function minMutation(
         }
     }
 }
+
+/*
+https://leetcode.com/problems/count-number-of-homogenous-substrings/?envType=daily-question&envId=2024-02-27
+1759. Count Number of Homogenous Substrings
+Given a string s, return the number of homogenous substrings of s. Since the answer may be too large, return it modulo 10^9 + 7.
+
+A string is homogenous if all the characters of the string are the same.
+
+A substring is a contiguous sequence of characters within a string.
+
+Example 1:
+
+Input: s = "abbcccaa"
+Output: 13
+Explanation: The homogenous substrings are listed as below:
+"a"   appears 3 times.
+"aa"  appears 1 time.
+"b"   appears 2 times.
+"bb"  appears 1 time.
+"c"   appears 3 times.
+"cc"  appears 2 times.
+"ccc" appears 1 time.
+3 + 1 + 2 + 1 + 3 + 2 + 1 = 13.
+
+Example 2:
+
+Input: s = "xy"
+Output: 2
+Explanation: The homogenous substrings are "x" and "y".
+
+Example 3:
+
+Input: s = "zzzzz"
+Output: 15
+
+Constraints:
+
+	1 <= s.length <= 10^5
+	s consists of lowercase letters.
+*/
+export function countHomogenous(s: string): number {
+    let count = 0;
+    let prevIndex = 0;
+    const updateCount = (n: number) => {
+        count += ((n + 1) * n) / 2;
+    };
+
+    for (let i = 1; i < s.length; i++) {
+        if (s[prevIndex] !== s[i]) {
+            updateCount(i - prevIndex);
+            prevIndex = i;
+        }
+    }
+    updateCount(s.length - prevIndex);
+
+    return count % (Math.pow(10, 9) + 7);
+}
