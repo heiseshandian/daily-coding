2bc8c7e949cb5b266e8978f90069772da2154a95
baymax <476803711@qq.com>
2024-02-27 09:23:42 +0800

    feat: 每日一题

--- src/problems/tree.ts
+++ src/problems/tree.ts
@@ -1,4 +1,4 @@
-import { TreeNode } from '../algorithm/tree';
+import { TreeNode, getMaxDistance } from '../algorithm/tree';
 import { Queue } from '../algorithm/queue';
 import { cache } from '../design-pattern/proxy';
 /* 
@@ -1081,4 +1081,33 @@ export function isSameTree(
         isSameTree(p?.left, q?.left) &&
         isSameTree(p?.right, q?.right)
     );
+}
+
+/*
+https://leetcode.com/problems/diameter-of-binary-tree/description/?envType=daily-question&envId=2024-02-27
+543. Diameter of Binary Tree
+Given the root of a binary tree, return the length of the diameter of the tree.
+
+The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+
+The length of a path between two nodes is represented by the number of edges between them.
+
+Example 1:
+
+Input: root = [1,2,3,4,5]
+Output: 3
+Explanation: 3 is the length of the path [4,2,1,3] or [5,2,1,3].
+
+Example 2:
+
+Input: root = [1,2]
+Output: 1
+
+Constraints:
+
+	The number of nodes in the tree is in the range [1, 10^4].
+	-100 <= Node.val <= 100
+*/
+export function diameterOfBinaryTree(root: TreeNode | null): number {
+    return getMaxDistance(root).maxDistance - 1;
 }
