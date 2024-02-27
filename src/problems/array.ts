ccfca873027ab64ab4ca1887aab1e1e19705ee21
baymax <476803711@qq.com>
2024-02-28 00:13:29 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -2457,3 +2457,62 @@ export function maxOperations(nums: number[], k: number): number {
 
     return count;
 }
+
+/*
+https://leetcode.com/problems/combination-sum-iv/description/
+377. Combination Sum IV
+Given an array of distinct integers nums and a target integer target, return the number of possible 
+combinations that add up to target.
+
+The test cases are generated so that the answer can fit in a 32-bit integer.
+
+Example 1:
+
+Input: nums = [1,2,3], target = 4
+Output: 7
+Explanation:
+The possible combination ways are:
+(1, 1, 1, 1)
+(1, 1, 2)
+(1, 2, 1)
+(1, 3)
+(2, 1, 1)
+(2, 2)
+(3, 1)
+Note that different sequences are counted as different combinations.
+
+Example 2:
+
+Input: nums = [9], target = 3
+Output: 0
+
+Constraints:
+
+	1 <= nums.length <= 200
+	1 <= nums[i] <= 1000
+	All the elements of nums are unique.
+	1 <= target <= 1000
+
+Follow up: What if negative numbers are allowed in the given array? How does it change the problem? 
+What limitation we need to add to the question to allow negative numbers?
+
+注：此处实际是个排列问题，不是组合问题，dfs 不可以从i开始取，一直到 nums.length-1 ，相反，每次都能从头开始取数
+*/
+export function combinationSum4(nums: number[], target: number): number {
+    const dfs = cache((rest: number): number => {
+        if (rest === 0) {
+            return 1;
+        }
+
+        let count = 0;
+        for (const num of nums) {
+            if (rest - num >= 0) {
+                count += dfs(rest - num);
+            }
+        }
+
+        return count;
+    });
+
+    return dfs(target);
+}
