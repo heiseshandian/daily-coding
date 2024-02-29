e99d1f229ad8a3d72f32061d3f7e4aeb944e636a
baymax <476803711@qq.com>
2024-02-29 09:44:42 +0800

    feat: 每日一题

--- src/problems/tree.ts
+++ src/problems/tree.ts
@@ -1230,3 +1230,111 @@ export function levelOrderBottom(root: TreeNode | null): number[][] {
 
     return result.reverse();
 }
+
+/*
+https://leetcode.com/problems/even-odd-tree/description/
+1609. Even Odd Tree
+A binary tree is named Even-Odd if it meets the following conditions:
+
+	The root of the binary tree is at level index 0, its children are at level index 1, their children are at level index 2, etc.
+	For every even-indexed level, all nodes at the level have odd integer values in strictly increasing order (from left to right).
+	For every odd-indexed level, all nodes at the level have even integer values in strictly decreasing order (from left to right).
+
+Given the root of a binary tree, return true if the binary tree is Even-Odd, otherwise return false.
+
+Example 1:
+
+Input: root = [1,10,4,3,null,7,9,12,8,6,null,null,2]
+Output: true
+Explanation: The node values on each level are:
+Level 0: [1]
+Level 1: [10,4]
+Level 2: [3,7,9]
+Level 3: [12,8,6,2]
+Since levels 0 and 2 are all odd and increasing and levels 1 and 3 are all even and decreasing, the tree is Even-Odd.
+
+Example 2:
+
+Input: root = [5,4,2,3,3,7]
+Output: false
+Explanation: The node values on each level are:
+Level 0: [5]
+Level 1: [4,2]
+Level 2: [3,3,7]
+Node values in level 2 must be in strictly increasing order, so the tree is not Even-Odd.
+
+Example 3:
+
+Input: root = [5,9,1,3,5,7]
+Output: false
+Explanation: Node values in the level 1 should be even integers.
+
+Constraints:
+
+	The number of nodes in the tree is in the range [1, 10^5].
+	1 <= Node.val <= 10^6
+*/
+export function isEvenOddTree(root: TreeNode | null): boolean {
+    if (!root) {
+        return true;
+    }
+
+    let isEven = true;
+    const nodes = [root];
+    const level: number[] = [];
+    const checkCurrentLevel = () => {
+        if (isEven) {
+            for (let i = 1; i < level.length; i++) {
+                if (level[i] <= level[i - 1]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        for (let i = 1; i < level.length; i++) {
+            if (level[i] >= level[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    };
+
+    let curEnd = root;
+    let nextEnd = root;
+    while (nodes.length) {
+        const node = nodes.shift()!;
+        if (isEven && (node.val & 1) === 0) {
+            return false;
+        }
+        if (!isEven && (node.val & 1) === 1) {
+            return false;
+        }
+
+        level.push(node.val);
+
+        if (node.left) {
+            nodes.push(node.left);
+            nextEnd = node.left;
+        }
+        if (node.right) {
+            nodes.push(node.right);
+            nextEnd = node.right;
+        }
+
+        if (node === curEnd) {
+            if (!checkCurrentLevel()) {
+                return false;
+            }
+
+            level.length = 0;
+            curEnd = nextEnd;
+            isEven = !isEven;
+        }
+    }
+    if (!checkCurrentLevel()) {
+        return false;
+    }
+
+    return true;
+}
