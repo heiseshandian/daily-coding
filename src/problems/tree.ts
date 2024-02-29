639e9c8d70aa73a2a8d071aba7cff654e3b886ca
baymax <476803711@qq.com>
2024-02-29 22:48:04 +0800

    feat: 每日一题

--- src/problems/tree.ts
+++ src/problems/tree.ts
@@ -1,5 +1,9 @@
-import { TreeNode, getMaxDistance } from '../algorithm/tree';
 import { Queue } from '../algorithm/queue';
+import {
+    TreeNode,
+    deserializeByLevel,
+    getMaxDistance,
+} from '../algorithm/tree';
 import { cache } from '../design-pattern/proxy';
 /* 
 https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
@@ -1337,4 +1341,58 @@ export function isEvenOddTree(root: TreeNode | null): boolean {
     }
 
     return true;
+}
+
+/*
+https://leetcode.com/problems/sum-of-nodes-with-even-valued-grandparent/description/
+1315. Sum of Nodes with Even-Valued Grandparent
+Given the root of a binary tree, return the sum of values of nodes with an even-valued grandparent. If there are no nodes with an even-valued grandparent, return 0.
+
+A grandparent of a node is the parent of its parent if it exists.
+
+Example 1:
+
+Input: root = [6,7,8,2,7,1,3,9,null,1,4,null,null,null,5]
+Output: 18
+Explanation: The red nodes are the nodes with even-value grandparent while the blue nodes are the even-value grandparents.
+
+Example 2:
+
+Input: root = [1]
+Output: 0
+
+Constraints:
+
+	The number of nodes in the tree is in the range [1, 10^4].
+	1 <= Node.val <= 100
+
+Hints
+- Traverse the tree keeping the parent and the grandparent.
+- If the grandparent of the current node is even-valued, add the value of this node to the answer.
+*/
+export function sumEvenGrandparent(root: TreeNode | null): number {
+    if (!root) {
+        return 0;
+    }
+
+    const parent = new Map<TreeNode, TreeNode>();
+    let result = 0;
+    const dfs = (node: TreeNode) => {
+        const grandParent = parent.get(parent.get(node)!);
+        if (grandParent && (grandParent.val & 1) === 0) {
+            result += node.val;
+        }
+
+        if (node.left) {
+            parent.set(node.left, node);
+            dfs(node.left);
+        }
+        if (node.right) {
+            parent.set(node.right, node);
+            dfs(node.right);
+        }
+    };
+    dfs(root);
+
+    return result;
 }
