466441e5148777f4387635bef77259691aff70e7
baymax <476803711@qq.com>
2024-02-29 23:07:06 +0800

    feat: 每日一题

--- src/problems/string.ts
+++ src/problems/string.ts
@@ -877,3 +877,58 @@ export function reverseParentheses(s: string): string {
 
     return chars.filter((v) => v !== '(' && v !== ')').join('');
 }
+
+/*
+https://leetcode.com/problems/add-strings/
+415. Add Strings
+Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
+
+You must solve the problem without using any built-in library for handling large integers (such as BigInteger). You must also not convert the inputs to integers directly.
+
+Example 1:
+
+Input: num1 = "11", num2 = "123"
+Output: "134"
+
+Example 2:
+
+Input: num1 = "456", num2 = "77"
+Output: "533"
+
+Example 3:
+
+Input: num1 = "0", num2 = "0"
+Output: "0"
+
+Constraints:
+
+	1 <= num1.length, num2.length <= 10^4
+	num1 and num2 consist of only digits.
+	num1 and num2 don't have any leading zeros except for the zero itself.
+*/
+export function addStrings(num1: string, num2: string): string {
+    let result = '';
+    let carry = 0;
+    let end1 = num1.length - 1;
+    let end2 = num2.length - 1;
+    while (end1 >= 0 && end2 >= 0) {
+        const cur = Number(num1[end1--]) + Number(num2[end2--]) + carry;
+        result = (cur % 10) + result;
+        carry = Math.floor(cur / 10);
+    }
+    while (end1 >= 0) {
+        const cur = Number(num1[end1--]) + carry;
+        result = (cur % 10) + result;
+        carry = Math.floor(cur / 10);
+    }
+    while (end2 >= 0) {
+        const cur = Number(num2[end2--]) + carry;
+        result = (cur % 10) + result;
+        carry = Math.floor(cur / 10);
+    }
+    if (carry) {
+        result = carry + result;
+    }
+
+    return result;
+}
