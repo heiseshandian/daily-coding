2e403251502eb834f652dca07f89ff5e59df1710
baymax <476803711@qq.com>
2024-03-01 09:49:10 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -2611,3 +2611,43 @@ export function findLeastNumOfUniqueInts(arr: number[], k: number): number {
 
     return 0;
 }
+
+/*
+https://leetcode.com/problems/maximum-odd-binary-number/description/?envType=daily-question&envId=2024-03-01
+2864. Maximum Odd Binary Number
+You are given a binary string s that contains at least one '1'.
+
+You have to rearrange the bits in such a way that the resulting binary number is the maximum odd binary number that can be created from this combination.
+
+Return a string representing the maximum odd binary number that can be created from the given combination.
+
+Note that the resulting string can have leading zeros.
+
+Example 1:
+
+Input: s = "010"
+Output: "001"
+Explanation: Because there is just one '1', it must be in the last position. So the answer is "001".
+
+Example 2:
+
+Input: s = "0101"
+Output: "1001"
+Explanation: One of the '1's must be in the last position. The maximum number that can be made with the remaining digits is "100". So the answer is "1001".
+
+Constraints:
+
+	1 <= s.length <= 100
+	s consists only of '0' and '1'.
+	s contains at least one '1'.
+*/
+export function maximumOddBinaryNumber(s: string): string {
+    let oneCount = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '1') {
+            oneCount++;
+        }
+    }
+
+    return '1'.repeat(oneCount - 1) + '0'.repeat(s.length - oneCount) + '1';
+}
