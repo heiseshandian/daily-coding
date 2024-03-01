8ab0c25d6bea50487033642c2d7ce89b4116f338
baymax <476803711@qq.com>
2024-03-01 22:26:09 +0800

    feat: 每日一题

--- src/problems/matrix.ts
+++ src/problems/matrix.ts
@@ -1,3 +1,4 @@
+import { cache } from '../design-pattern/proxy';
 // 锯齿形打印矩阵
 export function zigzagMatrix(matrix: number[][]) {
     if (!matrix || matrix.length === 0) {
@@ -377,4 +378,54 @@ export function maximalSquare2(matrix: string[][]): number {
         }
     }
     return max * max;
+}
+
+/*
+https://leetcode.com/problems/minimum-falling-path-sum/description/
+931. Minimum Falling Path Sum
+Given an n x n array of integers matrix, return the minimum sum of any falling path through matrix.
+
+A falling path starts at any element in the first row and chooses the element in the next row that is either directly below or diagonally left/right. Specifically, the next element from position (row, col) will be (row + 1, col - 1), (row + 1, col), or (row + 1, col + 1).
+
+Example 1:
+
+Input: matrix = [[2,1,3],[6,5,4],[7,8,9]]
+Output: 13
+Explanation: There are two falling paths with a minimum sum as shown.
+
+Example 2:
+
+Input: matrix = [[-19,57],[-40,-5]]
+Output: -59
+Explanation: The falling path with a minimum sum is shown.
+
+Constraints:
+
+	n == matrix.length == matrix[i].length
+	1 <= n <= 100
+	-100 <= matrix[i][j] <= 100
+*/
+export function minFallingPathSum(matrix: number[][]): number {
+    const dfs = cache((row: number, col: number) => {
+        if (row === matrix.length) {
+            return 0;
+        }
+        const cur = matrix[row][col];
+
+        let min = Infinity;
+        // left
+        if (col - 1 >= 0) {
+            min = Math.min(min, cur + dfs(row + 1, col - 1));
+        }
+        // center
+        min = Math.min(min, cur + dfs(row + 1, col));
+        // right
+        if (col + 1 < matrix[0].length) {
+            min = Math.min(min, cur + dfs(row + 1, col + 1));
+        }
+
+        return min;
+    });
+
+    return Math.min(...matrix[0].map((_, j) => dfs(0, j)));
 }
