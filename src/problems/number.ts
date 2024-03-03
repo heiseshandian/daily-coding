6f65a1398e19d9902bf27bc59b91c3fb1ab3b366
baymax <476803711@qq.com>
2024-03-03 21:25:03 +0800

    feat: 每日一题

--- src/problems/number.ts
+++ src/problems/number.ts
@@ -252,3 +252,47 @@ export function countLargestGroup(n: number): number {
 
     return Object.values(map).filter((v) => v === max).length;
 }
+
+/*
+https://leetcode.com/problems/water-bottles/description/
+1518. Water Bottles
+There are numBottles water bottles that are initially full of water. You can exchange numExchange empty water bottles from the market with one full water bottle.
+
+The operation of drinking a full water bottle turns it into an empty bottle.
+
+Given the two integers numBottles and numExchange, return the maximum number of water bottles you can drink.
+
+Example 1:
+
+Input: numBottles = 9, numExchange = 3
+Output: 13
+Explanation: You can exchange 3 empty bottles to get 1 full water bottle.
+Number of water bottles you can drink: 9 + 3 + 1 = 13.
+
+Example 2:
+
+Input: numBottles = 15, numExchange = 4
+Output: 19
+Explanation: You can exchange 4 empty bottles to get 1 full water bottle. 
+Number of water bottles you can drink: 15 + 3 + 1 = 19.
+
+Constraints:
+
+	1 <= numBottles <= 100
+	2 <= numExchange <= 100
+*/
+export function numWaterBottles(
+    numBottles: number,
+    numExchange: number
+): number {
+    let count = numBottles;
+    let prev = 0;
+    while (numBottles >= numExchange) {
+        const newBottles = Math.floor(numBottles / numExchange);
+        prev = numBottles - newBottles * numExchange;
+        count += newBottles;
+        numBottles = newBottles + prev;
+    }
+
+    return count;
+}
