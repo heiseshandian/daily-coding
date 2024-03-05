0cade59dc810e1d6cfe5c615c6334db8b8fd2832
baymax <476803711@qq.com>
2024-03-05 09:29:06 +0800

    feat: 每日一题

--- src/problems/string.ts
+++ src/problems/string.ts
@@ -986,3 +986,67 @@ export function addMinimum(word: string): number {
 
     return min;
 }
+
+/*
+https://leetcode.com/problems/minimum-length-of-string-after-deleting-similar-ends/description/
+1750. Minimum Length of String After Deleting Similar Ends
+Given a string s consisting only of characters 'a', 'b', and 'c'. You are asked to apply the following algorithm on the string any number of times:
+
+	Pick a non-empty prefix from the string s where all the characters in the prefix are equal.
+	Pick a non-empty suffix from the string s where all the characters in this suffix are equal.
+	The prefix and the suffix should not intersect at any index.
+	The characters from the prefix and suffix must be the same.
+	Delete both the prefix and the suffix.
+
+Return the minimum length of s after performing the above operation any number of times (possibly zero times).
+
+Example 1:
+
+Input: s = "ca"
+Output: 2
+Explanation: You can't remove any characters, so the string stays as is.
+
+Example 2:
+
+Input: s = "cabaabac"
+Output: 0
+Explanation: An optimal sequence of operations is:
+- Take prefix = "c" and suffix = "c" and remove them, s = "abaaba".
+- Take prefix = "a" and suffix = "a" and remove them, s = "baab".
+- Take prefix = "b" and suffix = "b" and remove them, s = "aa".
+- Take prefix = "a" and suffix = "a" and remove them, s = "".
+
+Example 3:
+
+Input: s = "aabccabba"
+Output: 3
+Explanation: An optimal sequence of operations is:
+- Take prefix = "aa" and suffix = "a" and remove them, s = "bccabb".
+- Take prefix = "b" and suffix = "bb" and remove them, s = "cca".
+
+Constraints:
+
+	1 <= s.length <= 10^5
+	s only consists of characters 'a', 'b', and 'c'.
+*/
+export function minimumLength(s: string): number {
+    let left = 0;
+    let right = s.length - 1;
+    while (left < right) {
+        if (s[left] !== s[right]) {
+            return right - left + 1;
+        }
+
+        while (left + 1 < right && s[left] === s[left + 1]) {
+            left++;
+        }
+        while (right - 1 > left && s[right] === s[right - 1]) {
+            right--;
+        }
+
+        left++;
+        right--;
+    }
+
+    return left === right ? 1 : 0;
+}
