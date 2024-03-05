ae72af0ab71ae267cb31b9909080e460e45a2b6c
baymax <476803711@qq.com>
2024-03-05 19:24:12 +0800

    feat: 每日一题

--- src/problems/string.ts
+++ src/problems/string.ts
@@ -1050,3 +1050,57 @@ export function minimumLength(s: string): number {
 
     return left === right ? 1 : 0;
 }
+
+/*
+https://leetcode.com/problems/string-without-aaa-or-bbb/description/
+984. String Without AAA or BBB
+Given two integers a and b, return any string s such that:
+
+	s has length a + b and contains exactly a 'a' letters, and exactly b 'b' letters,
+	The substring 'aaa' does not occur in s, and
+	The substring 'bbb' does not occur in s.
+
+Example 1:
+
+Input: a = 1, b = 2
+Output: "abb"
+Explanation: "abb", "bab" and "bba" are all correct answers.
+
+Example 2:
+
+Input: a = 4, b = 1
+Output: "aabaa"
+
+Constraints:
+
+	0 <= a, b <= 100
+	It is guaranteed such an s exists for the given a and b.
+*/
+export function strWithout3a3b(a: number, b: number): string {
+    let bigger = Math.max(a, b);
+    let smaller = Math.min(a, b);
+    const bigChar = a === bigger ? 'a' : 'b';
+    const smallChar = bigChar === 'a' ? 'b' : 'a';
+
+    if (bigger - smaller < 3) {
+        return (
+            `${bigChar}${smallChar}`.repeat(smaller) +
+            bigChar.repeat(bigger - smaller)
+        );
+    }
+
+    const len = Math.min(bigger >> 1, smaller);
+    const result = new Array(len).fill(`${bigChar}${bigChar}${smallChar}`);
+    if (bigger > len << 1) {
+        result.push(bigChar.repeat(bigger - (len << 1)));
+    }
+
+    let left = smaller - len;
+    let i = 0;
+    while (left > 0) {
+        result[i++] = `${bigChar}${smallChar}${bigChar}${smallChar}`;
+        left--;
+    }
+
+    return result.join('');
+}
