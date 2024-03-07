103ff4ab58737c2fc1de728f7520d1503badead7
baymax <476803711@qq.com>
2024-03-07 18:18:01 +0800

    wip: daily problems

--- src/problems/number.ts
+++ src/problems/number.ts
@@ -296,3 +296,25 @@ export function numWaterBottles(
 
     return count;
 }
+
+/*
+https://leetcode.com/problems/lexicographical-numbers/description/
+386. Lexicographical Numbers
+Given an integer n, return all the numbers in the range [1, n] sorted in lexicographical order.
+
+You must write an algorithm that runs in O(n) time and uses O(1) extra space. 
+
+Example 1:
+Input: n = 13
+Output: [1,10,11,12,13,2,3,4,5,6,7,8,9]
+Example 2:
+Input: n = 2
+Output: [1,2]
+
+Constraints:
+
+	1 <= n <= 10^4
+*/
+export function lexicalOrder(n: number): number[] {
+    const backtracking = () => {};
+}
