d45a15f8167ae4f7adb0309d6ab820f9834d32af
baymax <476803711@qq.com>
2024-03-08 16:33:05 +0800

    feat: 每日一题

--- src/problems/number.ts
+++ src/problems/number.ts
@@ -373,3 +373,47 @@ export function maxFrequencyElements(nums: number[]): number {
 
     return repeatTimes.reduce((acc, cur) => (cur === max ? acc + cur : acc), 0);
 }
+
+/*
+https://leetcode.com/problems/valid-triangle-number/
+611. Valid Triangle Number
+Given an integer array nums, return the number of triplets chosen from the array that can make triangles if we take them as side lengths of a triangle.
+
+Example 1:
+
+Input: nums = [2,2,3,4]
+Output: 3
+Explanation: Valid combinations are: 
+2,3,4 (using the first 2)
+2,3,4 (using the second 2)
+2,2,3
+
+Example 2:
+
+Input: nums = [4,2,3,4]
+Output: 4
+
+Constraints:
+
+	1 <= nums.length <= 1000
+	0 <= nums[i] <= 1000
+*/
+export function triangleNumber(nums: number[]): number {
+    nums.sort((a, b) => b - a);
+
+    let count = 0;
+    for (let i = 0; i < nums.length - 2; i++) {
+        let left = i + 1;
+        let right = nums.length - 1;
+        while (left < right) {
+            if (nums[left] + nums[right] <= nums[i]) {
+                right--;
+            } else {
+                count += right - left;
+                left++;
+            }
+        }
+    }
+
+    return count;
+}
