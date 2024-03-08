711542ea02b3d6b4fa4a1b5c84732e92a7362f28
baymax <476803711@qq.com>
2024-03-08 09:15:48 +0800

    feat: 每日一题

--- src/problems/number.ts
+++ src/problems/number.ts
@@ -333,3 +333,43 @@ export function lexicalOrder(n: number): number[] {
 
     return result.slice(1);
 }
+
+/*
+https://leetcode.com/problems/count-elements-with-maximum-frequency/description/
+3005. Count Elements With Maximum Frequency
+You are given an array nums consisting of positive integers.
+
+Return the total frequencies of elements in nums such that those elements all have the maximum frequency.
+
+The frequency of an element is the number of occurrences of that element in the array.
+
+Example 1:
+
+Input: nums = [1,2,2,3,1,4]
+Output: 4
+Explanation: The elements 1 and 2 have a frequency of 2 which is the maximum frequency in the array.
+So the number of elements in the array with maximum frequency is 4.
+
+Example 2:
+
+Input: nums = [1,2,3,4,5]
+Output: 5
+Explanation: All elements of the array have a frequency of 1 which is the maximum.
+So the number of elements in the array with maximum frequency is 5.
+
+Constraints:
+
+	1 <= nums.length <= 100
+	1 <= nums[i] <= 100
+*/
+export function maxFrequencyElements(nums: number[]): number {
+    const map: Record<number, number> = {};
+    nums.forEach((v) => {
+        map[v] = (map[v] || 0) + 1;
+    });
+
+    const repeatTimes = Object.values(map);
+    const max = Math.max(...repeatTimes);
+
+    return repeatTimes.reduce((acc, cur) => (cur === max ? acc + cur : acc), 0);
+}
