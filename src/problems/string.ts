6f85f0cca39dc53c1878eb4ce26e2da102ee2e0d
baymax <476803711@qq.com>
2024-03-08 10:40:30 +0800

    feat: 每日一题

--- src/problems/string.ts
+++ src/problems/string.ts
@@ -1174,3 +1174,39 @@ function canSplit(prefix: string, suffix: string): boolean {
         isPalindrome(prefix, left, right) || isPalindrome(suffix, left, right)
     );
 }
+
+/*
+https://leetcode.com/problems/check-if-binary-string-has-at-most-one-segment-of-ones/description/
+1784. Check if Binary String Has at Most One Segment of Ones
+Given a binary string s ​​​​​without leading zeros, return true​​​ if s contains at most one contiguous segment of ones. Otherwise, return false.
+
+Example 1:
+
+Input: s = "1001"
+Output: false
+Explanation: The ones do not form a contiguous segment.
+
+Example 2:
+
+Input: s = "110"
+Output: true
+
+Constraints:
+
+	1 <= s.length <= 100
+	s[i]​​​​ is either '0' or '1'.
+	s[0] is '1'.
+*/
+export function checkOnesSegment(s: string): boolean {
+    let hasZero = false;
+    for (let i = 1; i < s.length; i++) {
+        if (s[i] === '0') {
+            hasZero = true;
+        }
+        if (s[i] === '1' && hasZero) {
+            return false;
+        }
+    }
+
+    return true;
+}
