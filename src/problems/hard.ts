1da1c40dc7fe54c8e28c3fab6e811259291e783d
baymax <476803711@qq.com>
2024-03-09 16:30:20 +0800

    feat: 每日一题

--- src/problems/hard.ts
+++ src/problems/hard.ts
@@ -1,3 +1,4 @@
+import { GenericHeap } from '../algorithm/generic-heap';
 /*
 https://leetcode.com/problems/find-the-longest-valid-obstacle-course-at-each-position/description/
 1964. Find the Longest Valid Obstacle Course at Each Position
@@ -75,4 +76,67 @@ export function longestObstacleCourseAtEachPosition(
     }
 
     return result;
+}
+
+/*
+https://leetcode.com/problems/minimum-number-of-taps-to-open-to-water-a-garden/description/
+1326. Minimum Number of Taps to Open to Water a Garden
+There is a one-dimensional garden on the x-axis. The garden starts at the point 0 and ends at the point n. (i.e., the length of the garden is n).
+
+There are n + 1 taps located at points [0, 1, ..., n] in the garden.
+
+Given an integer n and an integer array ranges of length n + 1 where ranges[i] (0-indexed) means the i-th tap 
+can water the area [i - ranges[i], i + ranges[i]] if it was open.
+
+Return the minimum number of taps that should be open to water the whole garden, If the garden cannot be watered return -1.
+
+Example 1:
+
+Input: n = 5, ranges = [3,4,1,1,0,0]
+Output: 1
+Explanation: The tap at point 0 can cover the interval [-3,3]
+The tap at point 1 can cover the interval [-3,5]
+The tap at point 2 can cover the interval [1,3]
+The tap at point 3 can cover the interval [2,4]
+The tap at point 4 can cover the interval [4,4]
+The tap at point 5 can cover the interval [5,5]
+Opening Only the second tap will water the whole garden [0,5]
+
+Example 2:
+
+Input: n = 3, ranges = [0,0,0,0]
+Output: -1
+Explanation: Even if you activate all the four taps you cannot water the whole garden.
+
+Constraints:
+
+	1 <= n <= 10^4
+	ranges.length == n + 1
+	0 <= ranges[i] <= 100
+*/
+export function minTaps(n: number, ranges: number[]): number {
+    const areas = ranges
+        .map((v, i) => [i - v, i + v])
+        .sort(([startA], [startB]) => startA - startB);
+    const maxHeap = new GenericHeap<number[]>(
+        ([, endA], [, endB]) => endB - endA
+    );
+
+    let min = 0;
+    let currentEnd = 0;
+    let i = 0;
+    while (currentEnd < n) {
+        while (i < areas.length && areas[i][0] <= currentEnd) {
+            maxHeap.push(areas[i++]);
+        }
+
+        if (maxHeap.size() === 0) {
+            return -1;
+        }
+
+        currentEnd = maxHeap.pop()[1];
+        min++;
+    }
+
+    return min;
 }
