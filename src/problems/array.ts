5639ce64836c687a270619383209a1bdf7cd21a7
baymax <476803711@qq.com>
2024-03-14 11:21:51 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -3539,3 +3539,45 @@ export function timeRequiredToBuy(tickets: number[], k: number): number {
         getSeconds(k + 1, tickets.length - 1, tickets[k] - 1)
     );
 }
+
+/*
+https://leetcode.com/problems/binary-subarrays-with-sum/description/
+930. Binary Subarrays With Sum
+Given a binary array nums and an integer goal, return the number of non-empty subarrays with a sum goal.
+
+A subarray is a contiguous part of the array.
+
+Example 1:
+
+Input: nums = [1,0,1,0,1], goal = 2
+Output: 4
+Explanation: The 4 subarrays are bolded and underlined below:
+[1,0,1,0,1]
+[1,0,1,0,1]
+[1,0,1,0,1]
+[1,0,1,0,1]
+
+Example 2:
+
+Input: nums = [0,0,0,0,0], goal = 0
+Output: 15
+
+Constraints:
+
+	1 <= nums.length <= 10^4
+	nums[i] is either 0 or 1.
+	0 <= goal <= nums.length
+*/
+export function numSubarraysWithSum(nums: number[], goal: number): number {
+    let prefix = 0;
+    const repeatTimes: Record<number, number> = { 0: 1 };
+    let count = 0;
+
+    nums.forEach((v) => {
+        prefix += v;
+        count += repeatTimes[prefix - goal] || 0;
+        repeatTimes[prefix] = (repeatTimes[prefix] || 0) + 1;
+    });
+
+    return count;
+}
