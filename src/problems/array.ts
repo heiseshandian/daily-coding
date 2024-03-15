864a24ccc001fbc285d31a177e4de8980321ce5a
baymax <476803711@qq.com>
2024-03-15 09:33:03 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -3581,3 +3581,50 @@ export function numSubarraysWithSum(nums: number[], goal: number): number {
 
     return count;
 }
+
+/*
+https://leetcode.com/problems/product-of-array-except-self/description/?envType=daily-question&envId=2024-03-15
+238. Product of Array Except Self
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+Example 1:
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+Example 2:
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+
+Constraints:
+
+	2 <= nums.length <= 10^5
+	-30 <= nums[i] <= 30
+	The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not count as extra space for space complexity analysis.)
+*/
+export function productExceptSelf(nums: number[]): number[] {
+    const prefix: number[] = new Array(nums.length);
+    const suffix: number[] = new Array(nums.length);
+    prefix[0] = nums[0];
+    suffix[nums.length - 1] = nums[nums.length - 1];
+
+    for (let i = 1; i < nums.length; i++) {
+        prefix[i] = prefix[i - 1] * nums[i];
+    }
+    for (let i = nums.length - 2; i >= 0; i--) {
+        suffix[i] = suffix[i + 1] * nums[i];
+    }
+
+    const result: number[] = new Array(nums.length);
+    for (let i = 0; i < nums.length; i++) {
+        const prev = i - 1 >= 0 ? prefix[i - 1] : 1;
+        const next = i + 1 < nums.length ? suffix[i + 1] : 1;
+        result[i] = prev * next;
+    }
+
+    return result;
+}
