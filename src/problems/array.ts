8b82f6271062f1cb3ed541df68a4216eb185c8ea
baymax <476803711@qq.com>
2024-03-19 22:20:23 +0800

    feat： 每日一題

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -3723,3 +3723,53 @@ export function findMinArrowShots(points: number[][]): number {
 
     return count;
 }
+
+/*
+https://leetcode.com/problems/arithmetic-slices/description/
+413. Arithmetic Slices
+An integer array is called arithmetic if it consists of at least three elements and if the difference 
+between any two consecutive elements is the same.
+
+	For example, [1,3,5,7,9], [7,7,7,7], and [3,-1,-5,-9] are arithmetic sequences.
+
+Given an integer array nums, return the number of arithmetic subarrays of nums.
+
+A subarray is a contiguous subsequence of the array.
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: 3
+Explanation: We have 3 arithmetic slices in nums: [1, 2, 3], [2, 3, 4] and [1,2,3,4] itself.
+
+Example 2:
+
+Input: nums = [1]
+Output: 0
+
+Constraints:
+
+	1 <= nums.length <= 5000
+	-1000 <= nums[i] <= 1000
+*/
+export function numberOfArithmeticSlices(nums: number[]): number {
+    let count = 0;
+    let i = 0;
+    while (i < nums.length - 2) {
+        if (nums[i + 2] - nums[i + 1] !== nums[i + 1] - nums[i]) {
+            i++;
+            continue;
+        }
+
+        const diff = nums[i + 1] - nums[i];
+        let k = i + 3;
+        while (k < nums.length && nums[k] - nums[k - 1] === diff) {
+            k++;
+        }
+        const n = k - i;
+        count += ((n - 1) * (n - 2)) / 2;
+        i = k - 1;
+    }
+
+    return count;
+}
