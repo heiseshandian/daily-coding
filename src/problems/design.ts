49485d2433e977106f8f25518b24f5af189bc253
baymax <476803711@qq.com>
2024-03-20 19:08:02 +0800

    feat: 每日一题

--- src/problems/design.ts
+++ src/problems/design.ts
@@ -523,3 +523,61 @@ export function cancellable(
         clearInterval(interval);
     };
 }
+
+/*
+https://leetcode.com/problems/random-pick-index/description/
+398. Random Pick Index
+Given an integer array nums with possible duplicates, randomly output the index of a given target number. You can assume that 
+the given target number must exist in the array.
+
+Implement the Solution class:
+
+	Solution(int[] nums) Initializes the object with the array nums.
+	int pick(int target) Picks a random index i from nums where nums[i] == target. If there are multiple valid i's, then each 
+    index should have an equal probability of returning.
+
+Example 1:
+
+Input
+["Solution", "pick", "pick", "pick"]
+[[[1, 2, 3, 3, 3]], [3], [1], [3]]
+Output
+[null, 4, 0, 2]
+
+Explanation
+Solution solution = new Solution([1, 2, 3, 3, 3]);
+solution.pick(3); // It should return either index 2, 3, or 4 randomly. Each index should have equal probability of returning.
+solution.pick(1); // It should return 0. Since in the array only nums[0] is equal to 1.
+solution.pick(3); // It should return either index 2, 3, or 4 randomly. Each index should have equal probability of returning.
+
+Constraints:
+
+	1 <= nums.length <= 10^4
+	2^31 <= nums[i] <= 2^31 - 1
+	target is an integer from nums.
+	At most 104 calls will be made to pick.
+*/
+export class Solution {
+    private numIndexes: Record<number, number[]> = {};
+
+    constructor(nums: number[]) {
+        nums.forEach((v, i) => {
+            if (!this.numIndexes[v]) {
+                this.numIndexes[v] = [];
+            }
+
+            this.numIndexes[v].push(i);
+        });
+    }
+
+    pick(target: number): number {
+        const indexes = this.numIndexes[target];
+        const len = indexes.length;
+        if (len === 1) {
+            return indexes[0];
+        }
+
+        const index = Math.floor(Math.random() * len);
+        return indexes[index];
+    }
+}
