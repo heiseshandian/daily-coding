252106b8bf84812cc6ee539ab69e1161b5cd81dc
baymax <476803711@qq.com>
2024-03-20 19:29:50 +0800

    feat: 每日一题

--- src/problems/tree.ts
+++ src/problems/tree.ts
@@ -1,9 +1,5 @@
 import { Queue } from '../algorithm/queue';
-import {
-    TreeNode,
-    deserializeByLevel,
-    getMaxDistance,
-} from '../algorithm/tree';
+import { TreeNode, getMaxDistance } from '../algorithm/tree';
 import { cache } from '../design-pattern/proxy';
 /* 
 https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
@@ -1457,4 +1453,58 @@ export function longestUnivaluePath(root: TreeNode | null): number {
     };
 
     return dfs(root)[1] - 1;
+}
+
+/*
+https://leetcode.com/problems/binary-tree-right-side-view/
+199. Binary Tree Right Side View
+Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+Example 1:
+
+Input: root = [1,2,3,null,5,null,4]
+Output: [1,3,4]
+
+Example 2:
+
+Input: root = [1,null,3]
+Output: [1,3]
+
+Example 3:
+
+Input: root = []
+Output: []
+
+Constraints:
+
+	The number of nodes in the tree is in the range [0, 100].
+	-100 <= Node.val <= 100
+*/
+export function rightSideView(root: TreeNode | null): number[] {
+    if (!root) {
+        return [];
+    }
+
+    let curEnd = root;
+    let nextEnd = root;
+    const result: number[] = [];
+    const queue: TreeNode[] = [root];
+    while (queue.length) {
+        const node = queue.shift()!;
+        if (node.left) {
+            queue.push(node.left);
+            nextEnd = node.left;
+        }
+        if (node.right) {
+            queue.push(node.right);
+            nextEnd = node.right;
+        }
+
+        if (node === curEnd) {
+            result.push(node.val);
+            curEnd = nextEnd;
+        }
+    }
+
+    return result;
 }
