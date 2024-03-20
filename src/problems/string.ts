93bb549f8c4f1530858bbda0ed0a4bc139b84914
baymax <476803711@qq.com>
2024-03-20 18:33:17 +0800

    feat: 每日一题

--- src/problems/string.ts
+++ src/problems/string.ts
@@ -1420,3 +1420,54 @@ export function smallestString(s: string): string {
     }
     return s.slice(0, i) + convert(s.slice(i, end)) + s.slice(end);
 }
+
+/*
+https://leetcode.com/problems/minimum-moves-to-convert-string/description/
+2027. Minimum Moves to Convert String
+You are given a string s consisting of n characters which are either 'X' or 'O'.
+
+A move is defined as selecting three consecutive characters of s and converting them to 'O'. Note that if a move is 
+applied to the character 'O', it will stay the same.
+
+Return the minimum number of moves required so that all the characters of s are converted to 'O'.
+
+Example 1:
+
+Input: s = "XXX"
+Output: 1
+Explanation: XXX -> OOO
+We select all the 3 characters and convert them in one move.
+
+Example 2:
+
+Input: s = "XXOX"
+Output: 2
+Explanation: XXOX -> OOOX -> OOOO
+We select the first 3 characters in the first move, and convert them to 'O'.
+Then we select the last 3 characters and convert them so that the final string contains all 'O's.
+
+Example 3:
+
+Input: s = "OOOO"
+Output: 0
+Explanation: There are no 'X's in s to convert.
+
+Constraints:
+
+	3 <= s.length <= 1000
+	s[i] is either 'X' or 'O'.
+*/
+export function minimumMoves(s: string): number {
+    let i = 0;
+    let count = 0;
+    while (i < s.length) {
+        if (s[i] === 'X') {
+            count++;
+            i += 3;
+        } else {
+            i++;
+        }
+    }
+
+    return count;
+}
