26f8241f8329584137161053d3fb983b73a76be6
baymax <476803711@qq.com>
2024-03-21 19:24:21 +0800

    feat: 每日一题

--- src/problems/greedy.ts
+++ src/problems/greedy.ts
@@ -196,3 +196,83 @@ export function minimumCost(s: string): number {
         getLeftCount('1', '0') + getRightCount('1', '0')
     );
 }
+
+/*
+https://leetcode.com/problems/maximum-score-from-removing-substrings/
+1717. Maximum Score From Removing Substrings
+You are given a string s and two integers x and y. You can perform two types of operations any number of times.
+
+	Remove substring "ab" and gain x points.
+
+		For example, when removing "ab" from "cabxbae" it becomes "cxbae".
+
+	Remove substring "ba" and gain y points.
+
+		For example, when removing "ba" from "cabxbae" it becomes "cabxe".
+
+Return the maximum points you can gain after applying the above operations on s.
+
+Example 1:
+
+Input: s = "cdbcbbaaabab", x = 4, y = 5
+Output: 19
+Explanation:
+- Remove the "ba" underlined in "cdbcbbaaabab". Now, s = "cdbcbbaaab" and 5 points are added to the score.
+- Remove the "ab" underlined in "cdbcbbaaab". Now, s = "cdbcbbaa" and 4 points are added to the score.
+- Remove the "ba" underlined in "cdbcbbaa". Now, s = "cdbcba" and 5 points are added to the score.
+- Remove the "ba" underlined in "cdbcba". Now, s = "cdbc" and 5 points are added to the score.
+Total score = 5 + 4 + 5 + 5 = 19.
+
+Example 2:
+
+Input: s = "aabbaaxybbaabb", x = 5, y = 4
+Output: 20
+
+Constraints:
+
+	1 <= s.length <= 10^5
+	1 <= x, y <= 10^4
+	s consists of lowercase English letters.
+*/
+export function maximumGain(s: string, x: number, y: number): number {
+    const biggerChar = x >= y ? 'ab' : 'ba';
+    const smallerChar = biggerChar === 'ab' ? 'ba' : 'ab';
+    const bigger = Math.max(x, y);
+    const smaller = Math.min(x, y);
+
+    const getPoints = (
+        str: string | string[],
+        char: string,
+        point: number
+    ): [points: number, s: string[]] => {
+        const stack = [str[0]];
+        let i = 1;
+        let points = 0;
+        while (i < str.length) {
+            if (stack.length === 0) {
+                stack.push(str[i++]);
+                continue;
+            }
+
+            while (stack.length > 0) {
+                const top = stack.pop()!;
+
+                if (top + str[i] === char) {
+                    points += point;
+                    i++;
+                } else {
+                    stack.push(top);
+                    stack.push(str[i++]);
+                    break;
+                }
+            }
+        }
+
+        return [points, stack];
+    };
+
+    const [biggerPoints, rest] = getPoints(s, biggerChar, bigger);
+    const [smallerPoints] = getPoints(rest, smallerChar, smaller);
+
+    return biggerPoints + smallerPoints;
+}
