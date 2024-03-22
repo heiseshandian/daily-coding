78abbdf027bbe56beca2e68703631643499cf033
baymax <476803711@qq.com>
2024-03-22 10:15:42 +0800

    feat: 每日一题

--- src/problems/string.ts
+++ src/problems/string.ts
@@ -1540,3 +1540,57 @@ export function findMinimumOperations(
 
     return i === 0 ? -1 : s1.length + s2.length + s3.length - i * 3;
 }
+
+/*
+https://leetcode.com/problems/minimum-string-length-after-removing-substrings/
+2696. Minimum String Length After Removing Substrings
+You are given a string s consisting only of uppercase English letters.
+
+You can apply some operations to this string where, in one operation, you can remove any occurrence of one of the substrings "AB" or "CD" from s.
+
+Return the minimum possible length of the resulting string that you can obtain.
+
+Note that the string concatenates after removing the substring and could produce new "AB" or "CD" substrings.
+
+Example 1:
+
+Input: s = "ABFCACDB"
+Output: 2
+Explanation: We can do the following operations:
+- Remove the substring "ABFCACDB", so s = "FCACDB".
+- Remove the substring "FCACDB", so s = "FCAB".
+- Remove the substring "FCAB", so s = "FC".
+So the resulting length of the string is 2.
+It can be shown that it is the minimum length that we can obtain.
+
+Example 2:
+
+Input: s = "ACBBD"
+Output: 5
+Explanation: We cannot do any operations on the string so the length remains the same.
+
+Constraints:
+
+	1 <= s.length <= 100
+	s consists only of uppercase English letters.
+*/
+export function minLength(s: string): number {
+    const stack = [s[0]];
+    let i = 1;
+    let min = s.length;
+    while (i < s.length) {
+        if (
+            stack.length &&
+            ((stack[stack.length - 1] === 'A' && s[i] === 'B') ||
+                (stack[stack.length - 1] === 'C' && s[i] === 'D'))
+        ) {
+            min -= 2;
+            stack.pop();
+            i++;
+        } else {
+            stack.push(s[i++]);
+        }
+    }
+
+    return min;
+}
