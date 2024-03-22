6d0b658c493e6a29434fd8f233350783d5adb5f0
baymax <476803711@qq.com>
2024-03-22 09:03:31 +0800

    feat: 每日一题

--- src/problems/string.ts
+++ src/problems/string.ts
@@ -1493,3 +1493,50 @@ Constraints:
 export function defangIPaddr(address: string): string {
     return address.replace(/\./g, '[.]');
 }
+
+/*
+https://leetcode.com/problems/make-three-strings-equal/description/
+2937. Make Three Strings Equal
+You are given three strings: s1, s2, and s3. In one operation you can choose one of these strings and delete its rightmost character. 
+Note that you cannot completely empty a string.
+
+Return the minimum number of operations required to make the strings equal. If it is impossible to make them equal, return -1.
+
+Example 1:
+
+Input: s1 = "abc", s2 = "abb", s3 = "ab"
+
+Output: 2
+
+Explanation: Deleting the rightmost character from both s1 and s2 will result in three equal strings.
+
+Example 2:
+
+Input: s1 = "dac", s2 = "bac", s3 = "cac"
+
+Output: -1
+
+Explanation: Since the first letters of s1 and s2 differ, they cannot be made equal.
+
+Constraints:
+
+	1 <= s1.length, s2.length, s3.length <= 100
+	s1, s2 and s3 consist only of lowercase English letters.
+*/
+export function findMinimumOperations(
+    s1: string,
+    s2: string,
+    s3: string
+): number {
+    const minLen = Math.min(s1.length, s2.length, s3.length);
+    let i = 0;
+    while (i < minLen) {
+        if (s1[i] === s2[i] && s1[i] === s3[i]) {
+            i++;
+        } else {
+            break;
+        }
+    }
+
+    return i === 0 ? -1 : s1.length + s2.length + s3.length - i * 3;
+}
