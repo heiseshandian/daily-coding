b22e3fb58157dbcc9c85e5e2a83650f1d0a3ba9d
baymax <476803711@qq.com>
2024-03-27 13:33:58 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -3973,3 +3973,50 @@ export function findDuplicates(nums: number[]): number[] {
 
     return result;
 }
+
+/*
+https://leetcode.com/problems/subarray-product-less-than-k/description/
+713. Subarray Product Less Than K
+Given an array of integers nums and an integer k, return the number of contiguous subarrays where the product 
+of all the elements in the subarray is strictly less than k.
+
+Example 1:
+
+Input: nums = [10,5,2,6], k = 100
+Output: 8
+Explanation: The 8 subarrays that have product less than 100 are:
+[10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6]
+Note that [10, 5, 2] is not included as the product of 100 is not strictly less than k.
+
+Example 2:
+
+Input: nums = [1,2,3], k = 0
+Output: 0
+
+Constraints:
+
+    1 <= nums.length <= 10^4
+    1 <= nums[i] <= 1000
+    0 <= k <= 10^6
+*/
+export function numSubarrayProductLessThanK(nums: number[], k: number): number {
+    let left = 0;
+    let right = 0;
+    let product = 1;
+    let count = 0;
+    while (left < nums.length) {
+        while (right < nums.length && product < k) {
+            product *= nums[right++];
+        }
+
+        if (left < right) {
+            count += product < k ? right - left : right - left - 1;
+            product /= nums[left++];
+        } else {
+            product = 1;
+            right = ++left;
+        }
+    }
+
+    return count;
+}
