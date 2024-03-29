f61119dc8d108e4fa4f337357e03796f4e80107c
baymax <476803711@qq.com>
2024-03-29 10:53:26 +0800

    feat: 每日一题

--- src/problems/tree.ts
+++ src/problems/tree.ts
@@ -1,5 +1,9 @@
 import { Queue } from '../algorithm/queue';
-import { TreeNode, getMaxDistance } from '../algorithm/tree';
+import {
+    TreeNode,
+    deserializeByLevel,
+    getMaxDistance,
+} from '../algorithm/tree';
 import { cache } from '../design-pattern/proxy';
 /* 
 https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
@@ -1560,4 +1564,95 @@ export function largestValues(root: TreeNode | null): number[] {
     }
 
     return result;
+}
+
+/*
+https://leetcode.com/problems/reverse-odd-levels-of-binary-tree/description/?envType=list&envId=mhgl61ev
+2415. Reverse Odd Levels of Binary Tree
+Given the root of a perfect binary tree, reverse the node values at each odd level of the tree.
+
+	For example, suppose the node values at level 3 are [2,1,3,4,7,11,29,18], then it should become [18,29,11,7,4,3,1,2].
+
+Return the root of the reversed tree.
+
+A binary tree is perfect if all parent nodes have two children and all leaves are on the same level.
+
+The level of a node is the number of edges along the path between it and the root node.
+
+Example 1:
+
+Input: root = [2,3,5,8,13,21,34]
+Output: [2,5,3,8,13,21,34]
+Explanation: 
+The tree has only one odd level.
+The nodes at level 1 are 3, 5 respectively, which are reversed and become 5, 3.
+
+Example 2:
+
+Input: root = [7,13,11]
+Output: [7,11,13]
+Explanation: 
+The nodes at level 1 are 13, 11, which are reversed and become 11, 13.
+
+Example 3:
+
+Input: root = [0,1,2,0,0,0,0,1,1,1,1,2,2,2,2]
+Output: [0,2,1,0,0,0,0,2,2,2,2,1,1,1,1]
+Explanation: 
+The odd levels have non-zero values.
+The nodes at level 1 were 1, 2, and are 2, 1 after the reversal.
+The nodes at level 3 were 1, 1, 1, 1, 2, 2, 2, 2, and are 2, 2, 2, 2, 1, 1, 1, 1 after the reversal.
+
+Constraints:
+
+	The number of nodes in the tree is in the range [1, 2^14].
+	0 <= Node.val <= 10^5
+	root is a perfect binary tree.
+*/
+export function reverseOddLevels(root: TreeNode): TreeNode | null {
+    let curEnd = root;
+    let nextEnd = root;
+    const queue: TreeNode[] = [root];
+    let isOdd = false;
+    const level: TreeNode[] = [];
+    const reverseLevel = () => {
+        let l = 0;
+        let r = level.length - 1;
+        while (l < r) {
+            let t = level[l].val;
+            level[l].val = level[r].val;
+            level[r].val = t;
+
+            l++;
+            r--;
+        }
+    };
+
+    while (queue.length > 0) {
+        const node = queue.shift()!;
+        if (isOdd) {
+            level.push(node);
+        }
+
+        if (node.left) {
+            queue.push(node.left);
+            nextEnd = node.left;
+        }
+        if (node.right) {
+            queue.push(node.right);
+            nextEnd = node.right;
+        }
+
+        if (node === curEnd) {
+            if (isOdd) {
+                reverseLevel();
+                level.length = 0;
+            }
+
+            isOdd = !isOdd;
+            curEnd = nextEnd;
+        }
+    }
+
+    return root;
 }
