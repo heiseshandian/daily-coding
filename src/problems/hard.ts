228cf5deb7272cf614b365eac54f88185420e73a
baymax <476803711@qq.com>
2024-03-31 23:37:17 +0800

    feat: 每日一题

--- src/problems/hard.ts
+++ src/problems/hard.ts
@@ -542,3 +542,64 @@ export function countSubarrays(
 
     return count;
 }
+
+/*
+https://leetcode.com/problems/sum-of-floored-pairs/description/?envType=list&envId=o5cftq05
+1862. Sum of Floored Pairs
+Given an integer array nums, return the sum of floor(nums[i] / nums[j]) for all pairs of indices 0 <= i, j < nums.length in the array. Since the answer may be too large, return it modulo 109 + 7.
+
+The floor() function returns the integer part of the division.
+
+Example 1:
+
+Input: nums = [2,5,9]
+Output: 10
+Explanation:
+floor(2 / 5) = floor(2 / 9) = floor(5 / 9) = 0
+floor(2 / 2) = floor(5 / 5) = floor(9 / 9) = 1
+floor(5 / 2) = 2
+floor(9 / 2) = 4
+floor(9 / 5) = 1
+We calculate the floor of the division for every pair of indices in the array then sum them up.
+
+Example 2:
+
+Input: nums = [7,7,7,7,7,7,7]
+Output: 49
+
+Constraints:
+
+	1 <= nums.length <= 10^5
+	1 <= nums[i] <= 10^5
+*/
+export function sumOfFlooredPairs(nums: number[]) {
+    const MAX = Math.max(...nums);
+
+    const countsGreaterOrEqualTo = new Array(MAX + 1).fill(0);
+
+    nums.forEach((num) => (countsGreaterOrEqualTo[num] += 1));
+
+    for (let num = MAX - 1; num >= 0; num -= 1) {
+        countsGreaterOrEqualTo[num] += countsGreaterOrEqualTo[num + 1];
+    }
+
+    const numCounts = nums.reduce((counts, num) => {
+        counts.set(num, (counts.get(num) || 0) + 1);
+        return counts;
+    }, new Map());
+
+    const MOD = 10 ** 9 + 7;
+    let totalCount = 0;
+
+    numCounts.forEach((count, num) => {
+        let current = num;
+
+        while (current <= MAX) {
+            totalCount =
+                (totalCount + countsGreaterOrEqualTo[current] * count) % MOD;
+            current += num;
+        }
+    });
+
+    return totalCount;
+}
