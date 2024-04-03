816294da58bdbbe2ccd6432f561ad94b6b014474
baymax <476803711@qq.com>
2024-04-03 09:38:17 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -4565,3 +4565,68 @@ export function peakIndexInMountainArray(arr: number[]): number {
 
     return -1;
 }
+
+/*
+https://leetcode.com/problems/pairs-of-songs-with-total-durations-divisible-by-60/description/?envType=list&envId=o5cftq05
+1010. Pairs of Songs With Total Durations Divisible by 60
+You are given a list of songs where the ith song has a duration of time[i] seconds.
+
+Return the number of pairs of songs for which their total duration in seconds is divisible by 60. Formally, we want the number of indices i, j such that i < j with (time[i] + time[j]) % 60 == 0.
+
+Example 1:
+
+Input: time = [30,20,150,100,40]
+Output: 3
+Explanation: Three pairs have a total duration divisible by 60:
+(time[0] = 30, time[2] = 150): total duration 180
+(time[1] = 20, time[3] = 100): total duration 120
+(time[1] = 20, time[4] = 40): total duration 60
+
+Example 2:
+
+Input: time = [60,60,60]
+Output: 3
+Explanation: All three pairs have a total duration of 120, which is divisible by 60.
+
+Constraints:
+
+	1 <= time.length <= 10^4
+	1 <= time[i] <= 500
+*/
+export function numPairsDivisibleBy60(time: number[]): number {
+    const freqMap: Record<number, number> = {};
+    let max = -Infinity;
+    time.forEach((t) => {
+        freqMap[t] = (freqMap[t] ?? 0) + 1;
+        max = Math.max(max, t);
+    });
+
+    let count = 0;
+    const upperBound = Math.floor((max << 1) / 60);
+    time.forEach((t) => {
+        let left = 1;
+        let right = upperBound;
+        let closest = upperBound;
+        while (left <= right) {
+            const mid = left + ((right - left) >> 1);
+            if (mid * 60 > t) {
+                closest = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        while (closest <= upperBound) {
+            const match = closest * 60 - t;
+            if (freqMap[match]) {
+                count += match === t ? freqMap[match] - 1 : freqMap[match];
+            }
+
+            closest++;
+        }
+    });
+
+    // here we should use >>> or we will get negtive result if count is bigger than Math.pow(2,31)-1
+    return count >>> 1;
+}
