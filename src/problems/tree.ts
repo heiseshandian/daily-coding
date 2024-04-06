089a9c500a48cca18be58c71504d89aac55b21ae
baymax <476803711@qq.com>
2024-04-06 18:42:07 +0800

    feat: 每日一题

--- src/problems/tree.ts
+++ src/problems/tree.ts
@@ -1711,3 +1711,39 @@ export function averageOfLevels(root: TreeNode): number[] {
 
     return result;
 }
+
+/*
+https://leetcode.com/problems/binary-tree-postorder-traversal/
+145. Binary Tree Postorder Traversal
+Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+Example 1:
+
+Input: root = [1,null,2,3]
+Output: [3,2,1]
+
+Example 2:
+
+Input: root = []
+Output: []
+
+Example 3:
+
+Input: root = [1]
+Output: [1]
+
+Constraints:
+
+	The number of the nodes in the tree is in the range [0, 100].
+	-100 <= Node.val <= 100
+
+Follow up: Recursive solution is trivial, could you do it iteratively?
+*/
+export function postorderTraversal(root: TreeNode | null): number[] {
+    if (!root) {
+        return [];
+    }
+    const left = postorderTraversal(root.left);
+    const right = postorderTraversal(root.right);
+    return left.concat(right).concat(root.val);
+}
