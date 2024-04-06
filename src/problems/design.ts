bec90e60bc85172e3e8e057acb068ad45f167af5
baymax <476803711@qq.com>
2024-04-06 16:39:42 +0800

    feat: 每日一题

--- src/problems/design.ts
+++ src/problems/design.ts
@@ -677,3 +677,160 @@ export class Calculator {
         return this.val;
     }
 }
+
+/*
+https://leetcode.com/problems/design-linked-list/description/?envType=list&envId=o5cftq05
+707. Design Linked List
+Design your implementation of the linked list. You can choose to use a singly or doubly linked list.
+A node in a singly linked list should have two attributes: val and next. val is the value of the current node, and next is a pointer/reference to the next node.
+If you want to use the doubly linked list, you will need one more attribute prev to indicate the previous node in the linked list. Assume all nodes in the linked list are 0-indexed.
+
+Implement the MyLinkedList class:
+
+	MyLinkedList() Initializes the MyLinkedList object.
+	int get(int index) Get the value of the indexth node in the linked list. If the index is invalid, return -1.
+	void addAtHead(int val) Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+	void addAtTail(int val) Append a node of value val as the last element of the linked list.
+	void addAtIndex(int index, int val) Add a node of value val before the indexth node in the linked list. If index equals the length of the linked list, the node will be appended to the end of the linked list. If index is greater than the length, the node will not be inserted.
+	void deleteAtIndex(int index) Delete the indexth node in the linked list, if the index is valid.
+
+Example 1:
+
+Input
+["MyLinkedList", "addAtHead", "addAtTail", "addAtIndex", "get", "deleteAtIndex", "get"]
+[[], [1], [3], [1, 2], [1], [1], [1]]
+Output
+[null, null, null, null, 2, null, 3]
+
+Explanation
+MyLinkedList myLinkedList = new MyLinkedList();
+myLinkedList.addAtHead(1);
+myLinkedList.addAtTail(3);
+myLinkedList.addAtIndex(1, 2);    // linked list becomes 1->2->3
+myLinkedList.get(1);              // return 2
+myLinkedList.deleteAtIndex(1);    // now the linked list is 1->3
+myLinkedList.get(1);              // return 3
+
+Constraints:
+
+	0 <= index, val <= 1000
+	Please do not use the built-in LinkedList library.
+	At most 2000 calls will be made to get, addAtHead, addAtTail, addAtIndex and deleteAtIndex.
+*/
+class MyLinkedListNode {
+    val: number;
+    next: MyLinkedListNode | null;
+
+    constructor(val: number) {
+        this.val = val;
+    }
+}
+
+export class MyLinkedList {
+    private head: MyLinkedListNode | null;
+    private tail: MyLinkedListNode | null;
+    private size: number;
+
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    get(index: number): number {
+        let cur = this.head;
+        let i = 0;
+        while (i < index && cur) {
+            cur = cur.next;
+            i++;
+        }
+
+        return cur?.val ?? -1;
+    }
+
+    addAtHead(val: number): void {
+        const node = new MyLinkedListNode(val);
+        if (!this.head) {
+            this.head = node;
+            this.tail = node;
+            this.size++;
+            return;
+        }
+
+        node.next = this.head;
+        this.head = node;
+        this.size++;
+    }
+
+    addAtTail(val: number): void {
+        const node = new MyLinkedListNode(val);
+        if (!this.tail) {
+            this.head = node;
+            this.tail = node;
+            this.size++;
+            return;
+        }
+
+        this.tail.next = node;
+        this.tail = node;
+        this.size++;
+    }
+
+    addAtIndex(index: number, val: number): void {
+        if (index > this.size) {
+            return;
+        }
+        if (index === 0) {
+            this.addAtHead(val);
+            return;
+        }
+        if (index === this.size) {
+            this.addAtTail(val);
+            return;
+        }
+
+        const node = new MyLinkedListNode(val);
+
+        let cur = this.head;
+        let i = 1;
+        while (i < index && cur) {
+            cur = cur.next;
+            i++;
+        }
+
+        const next = cur!.next;
+        cur!.next = node;
+        node.next = next;
+        this.size++;
+    }
+
+    deleteAtIndex(index: number): void {
+        if (index < 0 || index >= this.size) {
+            return;
+        }
+        if (this.size === 1) {
+            this.head = null;
+            this.tail = null;
+            this.size = 0;
+            return;
+        }
+        if (index === 0) {
+            this.head = this.head!.next;
+            this.size--;
+            return;
+        }
+
+        let i = 1;
+        let cur = this.head;
+        while (i < index && cur) {
+            cur = cur.next;
+            i++;
+        }
+
+        if (index === this.size - 1) {
+            this.tail = cur;
+        }
+        cur!.next = cur!.next!.next;
+        this.size--;
+    }
+}
