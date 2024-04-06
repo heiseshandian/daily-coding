c463c507ee5f04dc61aa813f4830045fc7f8a3b1
baymax <476803711@qq.com>
2024-04-06 16:16:25 +0800

    feat: 每日一题

--- src/problems/matrix.ts
+++ src/problems/matrix.ts
@@ -778,3 +778,83 @@ export function diagonalSum(mat: number[][]): number {
     }
     return sum;
 }
+
+/*
+https://leetcode.com/problems/find-a-peak-element-ii/description/?envType=list&envId=o5cftq05
+1901. Find a Peak Element II
+A peak element in a 2D grid is an element that is strictly greater than all of its adjacent neighbors to the left, right, top, and bottom.
+
+Given a 0-indexed m x n matrix mat where no two adjacent cells are equal, find any peak element mat[i][j] and return the length 2 array [i,j].
+
+You may assume that the entire matrix is surrounded by an outer perimeter with the value -1 in each cell.
+
+You must write an algorithm that runs in O(m log(n)) or O(n log(m)) time.
+
+Example 1:
+
+Input: mat = [[1,4],[3,2]]
+Output: [0,1]
+Explanation: Both 3 and 4 are peak elements so [1,0] and [0,1] are both acceptable answers.
+
+Example 2:
+
+Input: mat = [[10,20,15],[21,30,14],[7,16,32]]
+Output: [1,1]
+Explanation: Both 30 and 32 are peak elements so [1,1] and [2,2] are both acceptable answers.
+
+Constraints:
+
+	m == mat.length
+	n == mat[i].length
+	1 <= m, n <= 500
+	1 <= mat[i][j] <= 10^5
+	No two adjacent cells are equal.
+
+Hints
+- Let's assume that the width of the array is bigger than the height, otherwise, we will split in another direction.
+- Split the array into three parts: central column left side and right side.
+- Go through the central column and two neighbor columns and look for maximum.
+- If it's in the central column - this is our peak.
+- If it's on the left side, run this algorithm on subarray left_side + central_column.
+- If it's on the right side, run this algorithm on subarray right_side + central_column
+*/
+export function findPeakGrid(mat: number[][]): number[] {
+    const m = mat.length;
+    const n = mat[0].length;
+
+    const getMaxIndex = (column: number) => {
+        let max = -Infinity;
+        let maxIndex = -1;
+
+        for (let i = 0; i < m; i++) {
+            if (max < mat[i][column]) {
+                max = mat[i][column];
+                maxIndex = i;
+            }
+        }
+
+        return [max, maxIndex];
+    };
+
+    let left = 0;
+    let right = n - 1;
+    while (left <= right) {
+        const mid = left + ((right - left) >> 1);
+        const [leftMax] = mid - 1 >= 0 ? getMaxIndex(mid - 1) : [-Infinity, -1];
+        const [midMax, midMaxIndex] = getMaxIndex(mid);
+        const [rightMax] = mid + 1 < n ? getMaxIndex(mid + 1) : [-Infinity, -1];
+
+        const max = Math.max(leftMax, midMax, rightMax);
+        if (max === midMax) {
+            return [midMaxIndex, mid];
+        }
+
+        if (max === leftMax) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return [-1, -1];
+}
