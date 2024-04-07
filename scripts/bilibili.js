84f01d5b02d0be2f8d0c872caf1298b1bc0b52b8
baymax <476803711@qq.com>
2024-04-08 00:01:35 +0800

    feat: 优化哔哩哔哩小助手

--- scripts/bilibili.js
+++ scripts/bilibili.js
@@ -1,68 +1,16 @@
-addBtns();
+init();
 
 const observer = new MutationObserver(function () {
-    addBtns();
+    init();
 });
 observer.observe(document, { childList: true, subtree: true });
 
-function addBtns() {
-    addChangeSpeedBtn();
-    setVideoPlaybackRate(); // 确保视频播放速度被设置
+function init() {
+    setVideoPlaybackRate();
+    bindEvents();
 }
 
-const CHANGE_SPEED = '倍速';
-
-async function addChangeSpeedBtn() {
-    const btnContainer = await getBtnContainer();
-    if (btnContainer.hasAddedChangeSpeedBtn) {
-        return;
-    }
-
-    const btn = document.createElement('div');
-    btn.textContent = CHANGE_SPEED;
-    btn.classList.add('bpx-player-ctrl-btn');
-    btn.classList.add('change-speed-btn');
-
-    const speedListContainer = document.createElement('div');
-    speedListContainer.classList.add('speed-list-container');
-    speedListContainer.classList.add('bpx-player-ctrl-playbackrate-menu');
-
-    const speeds = [0.5, 0.75, 1, 1.25, 1.5, 1.75, 2.0];
-    speeds.forEach((speed) => {
-        const speedOption = document.createElement('div');
-        speedOption.textContent = `${speed}`;
-        speedOption.classList.add('speed-option');
-        speedOption.classList.add('bpx-player-ctrl-playbackrate-menu-item');
-        speedOption.onclick = function () {
-            localStorage.setItem('videoPlaybackRate', speed);
-            setVideoPlaybackRate(speed); // 更新视频播放速度
-            speedListContainer.style.display = 'none';
-        };
-        speedListContainer.appendChild(speedOption);
-    });
-
-    btn.onmouseover = function () {
-        // 直接显示speedListContainer，无需再动态计算位置
-        speedListContainer.style.display = 'block';
-    };
-
-    document.querySelector(
-        '#bilibili-player .bpx-player-control-wrap'
-    ).onmouseleave = speedListContainer.onmouseleave = function (event) {
-        if (
-            !btn.contains(event.relatedTarget) &&
-            !speedListContainer.contains(event.relatedTarget)
-        ) {
-            speedListContainer.style.display = 'none';
-        }
-    };
-
-    btn.appendChild(speedListContainer);
-    btnContainer.appendChild(btn);
-    btnContainer.hasAddedChangeSpeedBtn = true;
-}
-
-function getBtnContainer(
+function getElement(
     selectors = '#bilibili-player .bpx-player-control-bottom-center'
 ) {
     let btnContainer;
@@ -81,6 +29,27 @@ function getBtnContainer(
         resolveFn = resolve;
         updateBtnContainer();
     });
+}
+
+async function bindEvents() {
+    const speedMenu = await getElement(
+        '#bilibili-player .bpx-player-ctrl-playbackrate-menu'
+    );
+    if (!speedMenu || speedMenu.hasBoundEvents) {
+        return;
+    }
+
+    Array.from(
+        speedMenu.querySelectorAll('.bpx-player-ctrl-playbackrate-menu-item')
+    ).forEach((node) => {
+        node.addEventListener('click', () => {
+            const speed = parseFloat(node.textContent.trim());
+            localStorage.setItem('videoPlaybackRate', speed);
+            setVideoPlaybackRate();
+        });
+    });
+
+    speedMenu.hasBoundEvents = true;
 }
 
 function setVideoPlaybackRate(
