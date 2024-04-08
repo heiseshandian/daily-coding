53ef371e1a936fb82c13036efd7647f3762b9c9c
baymax <476803711@qq.com>
2024-04-08 23:00:05 +0800

    feat: 每日一题

--- src/problems/heap.ts
+++ src/problems/heap.ts
@@ -234,3 +234,57 @@ export function minStoneSum(piles: number[], k: number): number {
 
     return maxHeap.container.reduce((acc, cur) => acc + cur);
 }
+
+/*
+https://leetcode.com/problems/distant-barcodes/description/?envType=list&envId=o5cftq05
+1054. Distant Barcodes
+In a warehouse, there is a row of barcodes, where the ith barcode is barcodes[i].
+
+Rearrange the barcodes so that no two adjacent barcodes are equal. You may return any answer, and it is guaranteed an answer exists.
+
+Example 1:
+Input: barcodes = [1,1,1,2,2,2]
+Output: [2,1,2,1,2,1]
+Example 2:
+Input: barcodes = [1,1,1,1,2,2,3,3]
+Output: [1,3,1,3,1,2,1,2]
+
+Constraints:
+
+	1 <= barcodes.length <= 10000
+	1 <= barcodes[i] <= 10000
+*/
+type BarcodePair = [barcode: number, count: number];
+
+export function rearrangeBarcodes(barcodes: number[]): number[] {
+    const map: Record<number, number> = {};
+    barcodes.forEach((v) => {
+        map[v] = (map[v] || 0) + 1;
+    });
+
+    const heap = new GenericHeap<BarcodePair>(([, a], [, b]) => b - a);
+    heap.initHeap(Object.keys(map).map((v) => [+v, map[v]]));
+
+    const result: number[] = Array(barcodes.length);
+    let i = 0;
+    let cycle = 2;
+    while (heap.size() > 0) {
+        cycle = 2;
+        const remainingPairs: BarcodePair[] = [];
+        while (cycle && heap.size() > 0) {
+            const [v, count] = heap.pop();
+            if (count - 1 > 0) {
+                remainingPairs.push([v, count - 1]);
+            }
+
+            cycle--;
+            result[i++] = v;
+        }
+
+        remainingPairs.forEach((t) => {
+            heap.push(t);
+        });
+    }
+
+    return result;
+}
