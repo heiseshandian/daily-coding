68cad5c198c99a7f020f346211443c16b01d625f
baymax <476803711@qq.com>
2024-04-10 23:06:02 +0800

    feat: 每日一题

--- src/problems/hard.ts
+++ src/problems/hard.ts
@@ -603,3 +603,117 @@ export function sumOfFlooredPairs(nums: number[]) {
 
     return totalCount;
 }
+
+/*
+https://leetcode.com/problems/insert-delete-getrandom-o1-duplicates-allowed/description/?envType=list&envId=o5cftq05
+381. Insert Delete GetRandom O(1) - Duplicates allowed
+RandomizedCollection is a data structure that contains a collection of numbers, possibly duplicates (i.e., a multiset). It should support inserting and removing specific elements and also reporting a random element.
+
+Implement the RandomizedCollection class:
+
+	RandomizedCollection() Initializes the empty RandomizedCollection object.
+	bool insert(int val) Inserts an item val into the multiset, even if the item is already present. Returns true if the item is not present, false otherwise.
+	bool remove(int val) Removes an item val from the multiset if present. Returns true if the item is present, false otherwise. Note that if val has multiple occurrences in the multiset, we only remove one of them.
+	int getRandom() Returns a random element from the current multiset of elements. The probability of each element being returned is linearly related to the number of the same values the multiset contains.
+
+You must implement the functions of the class such that each function works on average O(1) time complexity.
+
+Note: The test cases are generated such that getRandom will only be called if there is at least one item in the RandomizedCollection.
+
+Example 1:
+
+Input
+["RandomizedCollection", "insert", "insert", "insert", "getRandom", "remove", "getRandom"]
+[[], [1], [1], [2], [], [1], []]
+Output
+[null, true, false, true, 2, true, 1]
+
+Explanation
+RandomizedCollection randomizedCollection = new RandomizedCollection();
+randomizedCollection.insert(1);   // return true since the collection does not contain 1.
+                                  // Inserts 1 into the collection.
+randomizedCollection.insert(1);   // return false since the collection contains 1.
+                                  // Inserts another 1 into the collection. Collection now contains [1,1].
+randomizedCollection.insert(2);   // return true since the collection does not contain 2.
+                                  // Inserts 2 into the collection. Collection now contains [1,1,2].
+randomizedCollection.getRandom(); // getRandom should:
+                                  // - return 1 with probability 2/3, or
+                                  // - return 2 with probability 1/3.
+randomizedCollection.remove(1);   // return true since the collection contains 1.
+                                  // Removes 1 from the collection. Collection now contains [1,2].
+randomizedCollection.getRandom(); // getRandom should return 1 or 2, both equally likely.
+
+Constraints:
+
+	2^31 <= val <= 2^31 - 1
+	At most 2 * 105 calls in total will be made to insert, remove, and getRandom.
+	There will be at least one element in the data structure when getRandom is called.
+*/
+export class RandomizedCollection {
+    private index2ValMap: Map<number, number>;
+    private val2IndexMap: Map<number, Set<number>>;
+    private size: number;
+
+    constructor() {
+        this.index2ValMap = new Map();
+        this.val2IndexMap = new Map();
+        this.size = 0;
+    }
+
+    insert(val: number): boolean {
+        const inserted = this.val2IndexMap.has(val);
+        if (!inserted) {
+            this.index2ValMap.set(this.size, val);
+            this.val2IndexMap.set(val, new Set([this.size]));
+        } else {
+            const indexSet = this.val2IndexMap.get(val)!;
+            this.index2ValMap.set(this.size, val);
+            indexSet.add(this.size);
+        }
+        this.size++;
+
+        return !inserted;
+    }
+
+    remove(val: number): boolean {
+        const indexSet = this.val2IndexMap.get(val);
+        if (indexSet === undefined) {
+            return false;
+        }
+
+        const toDeleteIndex = indexSet.values().next().value;
+        this.swap(toDeleteIndex, this.size - 1);
+        indexSet.delete(this.size - 1);
+        if (indexSet.size === 0) {
+            this.val2IndexMap.delete(val);
+        }
+
+        this.index2ValMap.delete(this.size - 1);
+        this.size--;
+
+        return true;
+    }
+
+    private swap(i: number, j: number) {
+        if (i === j) {
+            return;
+        }
+
+        const valI = this.index2ValMap.get(i)!;
+        const valJ = this.index2ValMap.get(j)!;
+        this.index2ValMap.set(i, valJ);
+        this.index2ValMap.set(j, valI);
+
+        const indexSetI = this.val2IndexMap.get(valI)!;
+        const indexSetJ = this.val2IndexMap.get(valJ)!;
+        indexSetI.delete(i);
+        indexSetI.add(j);
+        indexSetJ.delete(j);
+        indexSetJ.add(i);
+    }
+
+    getRandom(): number {
+        const random = Math.floor(Math.random() * this.size);
+        return this.index2ValMap.get(random)!;
+    }
+}
