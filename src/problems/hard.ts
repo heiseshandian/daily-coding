f551a9d3c50212673bb03d01ada6cf295fec9f3a
baymax <476803711@qq.com>
2024-04-11 12:16:54 +0800

    feat:每日一题

--- src/problems/hard.ts
+++ src/problems/hard.ts
@@ -1,6 +1,7 @@
 import { cache } from '../design-pattern/proxy';
 import { swap } from '../common/index';
 import { SlidingWindow } from '../algorithm/sliding-window';
+import { GenericHeap } from '../algorithm/generic-heap';
 
 /*
 https://leetcode.com/problems/find-the-longest-valid-obstacle-course-at-each-position/description/
@@ -826,4 +827,64 @@ export function findInMountainArray(
     }
 
     return -1;
+}
+
+/*
+https://leetcode.com/problems/number-of-flowers-in-full-bloom/description/?envType=list&envId=o5cftq05
+2251. Number of Flowers in Full Bloom
+You are given a 0-indexed 2D integer array flowers, where flowers[i] = [starti, endi] means the ith flower will be in 
+full bloom from starti to endi (inclusive). You are also given a 0-indexed integer array people of size n, where people[i] 
+is the time that the ith person will arrive to see the flowers.
+
+Return an integer array answer of size n, where answer[i] is the number of flowers that are in full bloom when the ith person arrives.
+
+Example 1:
+
+Input: flowers = [[1,6],[3,7],[9,12],[4,13]], people = [2,3,7,11]
+Output: [1,2,2,2]
+Explanation: The figure above shows the times when the flowers are in full bloom and when the people arrive.
+For each person, we return the number of flowers in full bloom during their arrival.
+
+Example 2:
+
+Input: flowers = [[1,10],[3,3]], people = [3,3,2]
+Output: [2,2,1]
+Explanation: The figure above shows the times when the flowers are in full bloom and when the people arrive.
+For each person, we return the number of flowers in full bloom during their arrival.
+
+Constraints:
+
+	1 <= flowers.length <= 10^4
+	flowers[i].length == 2
+	1 <= starti <= endi <= 10^9
+	1 <= people.length <= 10^4
+	1 <= people[i] <= 10^9
+*/
+export function fullBloomFlowers(
+    flowers: number[][],
+    people: number[]
+): number[] {
+    flowers.sort(([startA], [startB]) => startA - startB);
+    const sorted = people.map((t, i) => [t, i]).sort(([tA], [tB]) => tA - tB);
+    const result = Array(people.length);
+
+    const heap = new GenericHeap((a, b) => a - b);
+
+    let prev = 0;
+    sorted.forEach(([t, index]) => {
+        let i = prev;
+        while (i < flowers.length && flowers[i][0] <= t) {
+            heap.push(flowers[i][1]);
+            i++;
+        }
+        prev = i;
+
+        while (heap.size() > 0 && heap.peek() < t) {
+            heap.pop();
+        }
+
+        result[index] = heap.size();
+    });
+
+    return result;
 }
