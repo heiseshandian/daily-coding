a47acb8b315c891d53f09c0d5555ef57169066e4
baymax <476803711@qq.com>
2024-04-11 22:56:08 +0800

    feat: 每日一题

--- src/problems/math.ts
+++ src/problems/math.ts
@@ -591,3 +591,50 @@ export function integerBreak(n: number): number {
 
     return rest === 0 ? threeProduct : threeProduct * rest;
 }
+
+/*
+https://leetcode.com/problems/excel-sheet-column-title/description/?envType=list&envId=o5cftq05
+168. Excel Sheet Column Title
+Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
+
+For example:
+
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28 
+...
+
+Example 1:
+
+Input: columnNumber = 1
+Output: "A"
+
+Example 2:
+
+Input: columnNumber = 28
+Output: "AB"
+
+Example 3:
+
+Input: columnNumber = 701
+Output: "ZY"
+
+Constraints:
+
+	1 <= columnNumber <= 2^31 - 1
+*/
+export function convertToTitle(columnNumber: number): string {
+    let str: string = '';
+    let num: number;
+
+    while (columnNumber > 0) {
+        num = (columnNumber - 1) % 26;
+        str = String.fromCharCode(num + 65) + str;
+        columnNumber = Math.floor((columnNumber - num) / 26);
+    }
+    return str;
+}
