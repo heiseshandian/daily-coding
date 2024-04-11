327f85ea3e1664502031d6e4b57463f0a4c8dd10
baymax <476803711@qq.com>
2024-04-11 09:24:12 +0800

    feat: 每日一题

--- src/problems/hard.ts
+++ src/problems/hard.ts
@@ -717,3 +717,113 @@ export class RandomizedCollection {
         return this.index2ValMap.get(random)!;
     }
 }
+
+/*
+https://leetcode.com/problems/find-in-mountain-array/description/?envType=list&envId=o5cftq05
+1095. Find in Mountain Array
+(This problem is an interactive problem.)
+
+You may recall that an array arr is a mountain array if and only if:
+
+	arr.length >= 3
+	There exists some i with 0 < i < arr.length - 1 such that:
+
+		arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
+		arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+
+Given a mountain array mountainArr, return the minimum index such that mountainArr.get(index) == target. If such an index does not exist, return -1.
+
+You cannot access the mountain array directly. You may only access the array using a MountainArray interface:
+
+	MountainArray.get(k) returns the element of the array at index k (0-indexed).
+	MountainArray.length() returns the length of the array.
+
+Submissions making more than 100 calls to MountainArray.get will be judged Wrong Answer. Also, any solutions that attempt to circumvent the judge will result in disqualification.
+
+Example 1:
+
+Input: array = [1,2,3,4,5,3,1], target = 3
+Output: 2
+Explanation: 3 exists in the array, at index=2 and index=5. Return the minimum index, which is 2.
+
+Example 2:
+
+Input: array = [0,1,2,4,2,1], target = 3
+Output: -1
+Explanation: 3 does not exist in the array, so we return -1.
+
+Constraints:
+
+	3 <= mountain_arr.length() <= 10^4
+	0 <= target <= 10^9
+	0 <= mountain_arr.get(index) <= 10^9
+*/
+
+// This is the MountainArray's API interface.
+// You should not implement it, or speculate about its implementation
+class MountainArray {
+    // @ts-expect-error
+    get(index: number): number {}
+    // @ts-expect-error
+    length(): number {}
+}
+
+export function findInMountainArray(
+    target: number,
+    mountainArr: MountainArray
+) {
+    const len = mountainArr.length();
+    const get = cache((index: number) => mountainArr.get(index));
+
+    // find peak
+    let left = 0;
+    let right = len - 1;
+    let peak: number = 0;
+    while (left <= right) {
+        const mid = left + ((right - left) >> 1);
+        if (get(mid) > get(mid - 1) && get(mid) > get(mid + 1)) {
+            peak = mid;
+            break;
+        }
+
+        if (get(mid - 1) < get(mid) && get(mid) < get(mid + 1)) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    // find target from left side
+    left = 0;
+    right = peak;
+    while (left <= right) {
+        const mid = left + ((right - left) >> 1);
+        if (get(mid) === target) {
+            return mid;
+        }
+
+        if (get(mid) > target) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    // find target from right side
+    left = peak + 1;
+    right = len - 1;
+    while (left <= right) {
+        const mid = left + ((right - left) >> 1);
+        if (get(mid) === target) {
+            return mid;
+        }
+
+        if (get(mid) > target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+}
