03f154b58917ad679714447d5d56f83a3bc281d6
baymax <476803711@qq.com>
2024-04-12 09:13:40 +0800

    feat: 每日一题

--- src/problems/design.ts
+++ src/problems/design.ts
@@ -1060,3 +1060,61 @@ export function once(fn: Function): OnceFn {
         return undefined;
     };
 }
+
+/*
+https://leetcode.com/problems/array-wrapper/description/
+2695. Array Wrapper
+Create a class ArrayWrapper that accepts an array of integers in its constructor. This class should have two features:
+
+	When two instances of this class are added together with the + operator, the resulting value is the sum of all the elements in both arrays.
+	When the String() function is called on the instance, it will return a comma separated string surrounded by brackets. For example, [1,2,3].
+
+Example 1:
+
+Input: nums = [[1,2],[3,4]], operation = "Add"
+Output: 10
+Explanation:
+const obj1 = new ArrayWrapper([1,2]);
+const obj2 = new ArrayWrapper([3,4]);
+obj1 + obj2; // 10
+
+Example 2:
+
+Input: nums = [[23,98,42,70]], operation = "String"
+Output: "[23,98,42,70]"
+Explanation:
+const obj = new ArrayWrapper([23,98,42,70]);
+String(obj); // "[23,98,42,70]"
+
+Example 3:
+
+Input: nums = [[],[]], operation = "Add"
+Output: 0
+Explanation:
+const obj1 = new ArrayWrapper([]);
+const obj2 = new ArrayWrapper([]);
+obj1 + obj2; // 0
+
+Constraints:
+
+	0 <= nums.length <= 1000
+	0 <= nums[i] <= 1000
+	Note: nums is the array passed to the constructor
+*/
+export class ArrayWrapper {
+    private nums: number[];
+    private sum: number;
+
+    constructor(nums: number[]) {
+        this.nums = nums;
+        this.sum = nums.reduce((acc, cur) => acc + cur, 0);
+    }
+
+    valueOf(): number {
+        return this.sum;
+    }
+
+    toString(): string {
+        return JSON.stringify(this.nums);
+    }
+}
