e32db6ec7698a02dc718da2ce94a6129fedcfce4
baymax <476803711@qq.com>
2024-04-13 22:55:40 +0800

    feat: 每日一题

--- src/problems/binary-search.ts
+++ src/problems/binary-search.ts
@@ -29,10 +29,20 @@ export function medianSlidingWindow(nums: number[], k: number): number[] {
             continue;
         }
 
-        const toDeletePosition = getClosestMaxOrEqual(window, toDelete, 0, k - 1);
+        const toDeletePosition = getClosestMaxOrEqual(
+            window,
+            toDelete,
+            0,
+            k - 1
+        );
         window.splice(toDeletePosition, 1);
 
-        const toInsertPosition = getClosestMaxOrEqual(window, nums[i], 0, k - 2);
+        const toInsertPosition = getClosestMaxOrEqual(
+            window,
+            nums[i],
+            0,
+            k - 2
+        );
         window.splice(toInsertPosition, 0, nums[i]);
 
         result.push(getMedian());
@@ -52,7 +62,11 @@ An integer a is closer to x than an integer b if:
 |a - x| < |b - x|, or
 |a - x| == |b - x| and a < b
 */
-export function findClosestElements(arr: number[], k: number, x: number): number[] {
+export function findClosestElements(
+    arr: number[],
+    k: number,
+    x: number
+): number[] {
     const result: number[] = [];
     const closestIndex = getClosestIndex(arr, x);
     result.push(arr[closestIndex]);
@@ -101,9 +115,55 @@ function getClosestIndex(arr: number[], x: number): number {
         }
     }
 
-    if (closestIndex > 0 && Math.abs(arr[closestIndex] - x) === Math.abs(arr[closestIndex - 1] - x)) {
+    if (
+        closestIndex > 0 &&
+        Math.abs(arr[closestIndex] - x) === Math.abs(arr[closestIndex - 1] - x)
+    ) {
         closestIndex--;
     }
 
     return closestIndex;
+}
+
+/*
+https://leetcode.com/problems/arranging-coins/description/
+441. Arranging Coins
+You have n coins and you want to build a staircase with these coins. The staircase consists of k rows where the ith row has exactly i coins. The last row of the staircase may be incomplete.
+
+Given the integer n, return the number of complete rows of the staircase you will build.
+
+Example 1:
+
+Input: n = 5
+Output: 2
+Explanation: Because the 3rd row is incomplete, we return 2.
+
+Example 2:
+
+Input: n = 8
+Output: 3
+Explanation: Because the 4th row is incomplete, we return 3.
+
+Constraints:
+
+	1 <= n <= 2^31 - 1
+*/
+export function arrangeCoins(n: number): number {
+    let left = 1;
+    let right = n;
+    while (left <= right) {
+        const mid = left + ((right - left) >> 1);
+        const sum = ((1 + mid) * mid) / 2;
+        if (sum <= n && sum + mid + 1 > n) {
+            return mid;
+        }
+
+        if (sum <= n) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
 }
