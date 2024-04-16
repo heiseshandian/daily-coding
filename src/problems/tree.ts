9505b5eab64f003eae030965e014010101749445
baymax <476803711@qq.com>
2024-04-16 08:30:28 +0800

    feat: 每日一题

--- src/problems/tree.ts
+++ src/problems/tree.ts
@@ -1852,3 +1852,85 @@ export function sumNumbers(root: TreeNode | null): number {
 
     return sum;
 }
+
+/*
+https://leetcode.com/problems/add-one-row-to-tree/description/
+623. Add One Row to Tree
+Given the root of a binary tree and two integers val and depth, add a row of nodes with value val at the given depth depth.
+
+Note that the root node is at depth 1.
+
+The adding rule is:
+
+	Given the integer depth, for each not null tree node cur at the depth depth - 1, create two tree nodes with value val as cur's left subtree root and right subtree root.
+	cur's original left subtree should be the left subtree of the new left subtree root.
+	cur's original right subtree should be the right subtree of the new right subtree root.
+	If depth == 1 that means there is no depth depth - 1 at all, then create a tree node with value val as the new root of the whole original tree, and the original tree is the new root's left subtree.
+
+Example 1:
+
+Input: root = [4,2,6,3,1,5], val = 1, depth = 2
+Output: [4,1,1,2,null,null,6,3,1,5]
+
+Example 2:
+
+Input: root = [4,2,null,3,1], val = 1, depth = 3
+Output: [4,2,null,1,1,3,null,null,1]
+
+Constraints:
+
+	The number of nodes in the tree is in the range [1, 10^4].
+	The depth of the tree is in the range [1, 10^4].
+	-100 <= Node.val <= 100
+	-10^5 <= val <= 10^5
+	1 <= depth <= the depth of tree + 1
+*/
+export function addOneRow(
+    root: TreeNode | null,
+    val: number,
+    depth: number
+): TreeNode | null {
+    if (!root) {
+        return root;
+    }
+    if (depth === 1) {
+        const newRoot = new TreeNode(val);
+        newRoot.left = root;
+        return newRoot;
+    }
+
+    const queue: TreeNode[] = [root];
+    let curEnd = root;
+    let nextEnd = root;
+    const parents: TreeNode[] = [];
+    let i = 0;
+    while (queue.length > 0) {
+        const node = queue.shift()!;
+        if (i === depth - 2) {
+            parents.push(node);
+        }
+
+        if (node.left) {
+            queue.push(node.left);
+            nextEnd = node.left;
+        }
+        if (node.right) {
+            queue.push(node.right);
+            nextEnd = node.right;
+        }
+        if (node === curEnd) {
+            i++;
+            curEnd = nextEnd;
+
+            if (parents.length) {
+                parents.forEach((p) => {
+                    p.left = new TreeNode(val, p.left);
+                    p.right = new TreeNode(val, null, p.right);
+                });
+                break;
+            }
+        }
+    }
+
+    return root;
+}
