a975417f711fb9406714f4fbf1a7be8d2a23f7f3
baymax <476803711@qq.com>
2024-04-18 23:55:31 +0800

    feat: 每日一题

--- src/problems/matrix.ts
+++ src/problems/matrix.ts
@@ -921,3 +921,61 @@ export function maxMoves(grid: number[][]): number {
 
     return max;
 }
+
+/*
+https://leetcode.com/problems/island-perimeter/description/?envType=daily-question&envId=2024-04-18
+463. Island Perimeter
+You are given row x col grid representing a map where grid[i][j] = 1 represents land and grid[i][j] = 0 represents water.
+
+Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, 
+and there is exactly one island (i.e., one or more connected land cells).
+
+The island doesn't have "lakes", meaning the water inside isn't connected to the water around the island. 
+One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
+
+Example 1:
+
+Input: grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
+Output: 16
+Explanation: The perimeter is the 16 yellow stripes in the image above.
+
+Example 2:
+
+Input: grid = [[1]]
+Output: 4
+
+Example 3:
+
+Input: grid = [[1,0]]
+Output: 4
+
+Constraints:
+
+	row == grid.length
+	col == grid[i].length
+	1 <= row, col <= 100
+	grid[i][j] is 0 or 1.
+	There is exactly one island in grid.
+*/
+export function islandPerimeter(grid: number[][]): number {
+    let perimeters = 0;
+
+    const m = grid.length;
+    const n = grid[0].length;
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] === 1) {
+                perimeters += 4;
+
+                if (j + 1 < n && grid[i][j + 1] === 1) {
+                    perimeters -= 2;
+                }
+                if (i + 1 < m && grid[i + 1][j] === 1) {
+                    perimeters -= 2;
+                }
+            }
+        }
+    }
+
+    return perimeters;
+}
