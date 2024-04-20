8214a5f11279d2134c8f1f497605e596c8d0dbf1
baymax <476803711@qq.com>
2024-04-20 09:41:37 +0800

    feat: 每日一题

--- src/problems/matrix.ts
+++ src/problems/matrix.ts
@@ -979,3 +979,91 @@ export function islandPerimeter(grid: number[][]): number {
 
     return perimeters;
 }
+
+/*
+https://leetcode.com/problems/find-all-groups-of-farmland/description/
+1992. Find All Groups of Farmland
+You are given a 0-indexed m x n binary matrix land where a 0 represents a hectare of forested land and a 1 represents a hectare of farmland.
+
+To keep the land organized, there are designated rectangular areas of hectares that consist entirely of farmland. 
+These rectangular areas are called groups. No two groups are adjacent, meaning farmland in one group is not four-directionally 
+adjacent to another farmland in a different group.
+
+land can be represented by a coordinate system where the top left corner of land is (0, 0) and the bottom right corner of land is (m-1, n-1). 
+Find the coordinates of the top left and bottom right corner of each group of farmland. A group of farmland with a top left corner at (r1, c1) 
+and a bottom right corner at (r2, c2) is represented by the 4-length array [r1, c1, r2, c2].
+
+Return a 2D array containing the 4-length arrays described above for each group of farmland in land. 
+If there are no groups of farmland, return an empty array. You may return the answer in any order.
+
+Example 1:
+
+Input: land = [[1,0,0],[0,1,1],[0,1,1]]
+Output: [[0,0,0,0],[1,1,2,2]]
+Explanation:
+The first group has a top left corner at land[0][0] and a bottom right corner at land[0][0].
+The second group has a top left corner at land[1][1] and a bottom right corner at land[2][2].
+
+Example 2:
+
+Input: land = [[1,1],[1,1]]
+Output: [[0,0,1,1]]
+Explanation:
+The first group has a top left corner at land[0][0] and a bottom right corner at land[1][1].
+
+Example 3:
+
+Input: land = [[0]]
+Output: []
+Explanation:
+There are no groups of farmland.
+
+Constraints:
+
+	m == land.length
+	n == land[i].length
+	1 <= m, n <= 300
+	land consists of only 0's and 1's.
+	Groups of farmland are rectangular in shape.
+*/
+export function findFarmland(land: number[][]): number[][] {
+    const m = land.length;
+    const n = land[0].length;
+    const visited: boolean[][] = Array.from({ length: m }, () =>
+        Array(n).fill(false)
+    );
+
+    const markVisited = (positions: number[]) => {
+        const [i1, j1, i2, j2] = positions;
+        for (let i = i1; i <= i2; i++) {
+            for (let j = j1; j <= j2; j++) {
+                visited[i][j] = true;
+            }
+        }
+    };
+
+    const result: number[][] = [];
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (visited[i][j] || land[i][j] === 0) {
+                continue;
+            }
+            visited[i][j] = true;
+
+            const leftTop = [i, j];
+            while (j < n && land[i][j] === 1) {
+                j++;
+            }
+            let k = i;
+            while (k < m && land[k][j - 1] === 1) {
+                k++;
+            }
+
+            const cur = leftTop.concat([k - 1, j - 1]);
+            markVisited(cur);
+            result.push(cur);
+        }
+    }
+
+    return result;
+}
