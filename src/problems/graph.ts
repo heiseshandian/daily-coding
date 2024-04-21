72516507afb613d9adffdc3b1f8c3d81be3d6560
baymax <476803711@qq.com>
2024-04-21 09:13:43 +0800

    feat: 每日一题

--- src/problems/graph.ts
+++ src/problems/graph.ts
@@ -199,3 +199,70 @@ export function findCheapestPrice(
     // 如果最终目的地的价格没有被更新，返回-1，否则返回最小价格
     return dp[k + 1][dst] === Number.MAX_SAFE_INTEGER ? -1 : dp[k + 1][dst];
 }
+
+/*
+https://leetcode.com/problems/find-if-path-exists-in-graph/description/?envType=daily-question&envId=2024-04-21
+1971. Find if Path Exists in Graph
+There is a bi-directional graph with n vertices, where each vertex is labeled from 0 to n - 1 (inclusive). 
+The edges in the graph are represented as a 2D integer array edges, where each edges[i] = [ui, vi] denotes a bi-directional 
+edge between vertex ui and vertex vi. Every vertex pair is connected by at most one edge, and no vertex has an edge to itself.
+
+You want to determine if there is a valid path that exists from vertex source to vertex destination.
+
+Given edges and the integers n, source, and destination, return true if there is a valid path from source to destination, or false otherwise.
+
+Example 1:
+
+Input: n = 3, edges = [[0,1],[1,2],[2,0]], source = 0, destination = 2
+Output: true
+Explanation: There are two paths from vertex 0 to vertex 2:
+- 0 → 1 → 2
+- 0 → 2
+
+Example 2:
+
+Input: n = 6, edges = [[0,1],[0,2],[3,5],[5,4],[4,3]], source = 0, destination = 5
+Output: false
+Explanation: There is no path from vertex 0 to vertex 5.
+
+Constraints:
+
+	1 <= n <= 10^5
+	0 <= edges.length <= 10^5
+	edges[i].length == 2
+	0 <= ui, vi <= n - 1
+	ui != vi
+	0 <= source, destination <= n - 1
+	There are no duplicate edges.
+	There are no self edges.
+*/
+export function validPath(
+    n: number,
+    edges: number[][],
+    source: number,
+    destination: number
+): boolean {
+    const edgeMap = new Map<number, number[]>();
+    edges.forEach(([from, to]) => {
+        edgeMap.set(from, (edgeMap.get(from) ?? []).concat(to));
+        edgeMap.set(to, (edgeMap.get(to) ?? []).concat(from));
+    });
+
+    const visited = Array(n).fill(false);
+    const queue = [source];
+    while (queue.length > 0) {
+        const p = queue.shift()!;
+        if (p === destination) {
+            return true;
+        }
+
+        (edgeMap.get(p) ?? []).forEach((to) => {
+            if (!visited[to]) {
+                visited[to] = true;
+                queue.push(to);
+            }
+        });
+    }
+
+    return false;
+}
