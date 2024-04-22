d4b2805a20c3b9d52369b61f7274f275cc65e916
baymax <476803711@qq.com>
2024-04-22 21:31:37 +0800

    feat: 每日一题

--- src/problems/math.ts
+++ src/problems/math.ts
@@ -638,3 +638,47 @@ export function convertToTitle(columnNumber: number): string {
     }
     return str;
 }
+
+/*
+https://leetcode.com/problems/pascals-triangle-ii/description/?envType=list&envId=o5cftq05
+119. Pascal's Triangle II
+Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+Example 1:
+Input: rowIndex = 3
+Output: [1,3,3,1]
+Example 2:
+Input: rowIndex = 0
+Output: [1]
+Example 3:
+Input: rowIndex = 1
+Output: [1,1]
+
+Constraints:
+
+	0 <= rowIndex <= 33
+
+Follow up: Could you optimize your algorithm to use only O(rowIndex) extra space?
+*/
+export function getRow(rowIndex: number): number[] {
+    if (rowIndex < 2) {
+        return Array(rowIndex + 1).fill(1);
+    }
+
+    let i = 1;
+    let result = [1, 1];
+    while (i < rowIndex) {
+        const next = Array(result.length + 1).fill(1);
+        let j = 1;
+        while (j < next.length - 1) {
+            next[j] = result[j - 1] + result[j++];
+        }
+
+        result = next;
+        i++;
+    }
+
+    return result;
+}
