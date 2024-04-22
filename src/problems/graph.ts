bff243c023a92642d8b5034883e6c2808d6cee7b
baymax <476803711@qq.com>
2024-04-22 19:39:17 +0800

    feat: 每日一题

--- src/problems/graph.ts
+++ src/problems/graph.ts
@@ -266,3 +266,101 @@ export function validPath(
 
     return false;
 }
+
+/*
+https://leetcode.com/problems/open-the-lock/description/
+752. Open the Lock
+You have a lock in front of you with 4 circular wheels. Each wheel has 10 slots: '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'. 
+The wheels can rotate freely and wrap around: for example we can turn '9' to be '0', or '0' to be '9'. Each move consists of turning 
+one wheel one slot.
+
+The lock initially starts at '0000', a string representing the state of the 4 wheels.
+
+You are given a list of deadends dead ends, meaning if the lock displays any of these codes, the wheels of the lock will stop turning and you will be unable to open it.
+
+Given a target representing the value of the wheels that will unlock the lock, return the minimum total number of turns required to open the lock, or -1 if it is impossible.
+
+Example 1:
+
+Input: deadends = ["0201","0101","0102","1212","2002"], target = "0202"
+Output: 6
+Explanation: 
+A sequence of valid moves would be "0000" -> "1000" -> "1100" -> "1200" -> "1201" -> "1202" -> "0202".
+Note that a sequence like "0000" -> "0001" -> "0002" -> "0102" -> "0202" would be invalid,
+because the wheels of the lock become stuck after the display becomes the dead end "0102".
+
+Example 2:
+
+Input: deadends = ["8888"], target = "0009"
+Output: 1
+Explanation: We can turn the last wheel in reverse to move from "0000" -> "0009".
+
+Example 3:
+
+Input: deadends = ["8887","8889","8878","8898","8788","8988","7888","9888"], target = "8888"
+Output: -1
+Explanation: We cannot reach the target without getting stuck.
+
+Constraints:
+
+	1 <= deadends.length <= 500
+	deadends[i].length == 4
+	target.length == 4
+	target will not be in the list deadends.
+	target and deadends[i] consist of digits only.
+*/
+export function openLock(deadends: string[], target: string): number {
+    const deadendSet = new Set(deadends);
+    if (deadendSet.has('0000')) {
+        return -1;
+    }
+
+    let queue: string[] = ['0000'];
+    const visited = new Set<string>();
+    let minStep = 0;
+    while (queue.length) {
+        const curLevel = queue.slice();
+        const nexts: string[] = [];
+
+        for (let i = 0; i < curLevel.length; i++) {
+            if (curLevel[i] === target) {
+                return minStep;
+            }
+
+            getNextNums(curLevel[i], deadendSet).forEach((v) => {
+                if (!visited.has(v)) {
+                    visited.add(v);
+                    nexts.push(v);
+                }
+            });
+        }
+
+        if (nexts.length === 0) {
+            return -1;
+        }
+        queue = nexts;
+
+        minStep++;
+    }
+
+    return -1;
+}
+
+function getNextNums(char: string, deadendSet: Set<string>): string[] {
+    const nexts: string[] = [];
+
+    const len = char.length;
+    for (let i = 0; i < len; i++) {
+        const before = char[i] === '0' ? '9' : `${Number(char[i]) - 1}`;
+        const after = char[i] === '9' ? '0' : `${Number(char[i]) + 1}`;
+
+        [before, after].forEach((v) => {
+            const n = char.slice(0, i) + v + char.slice(i + 1);
+            if (!deadendSet.has(n)) {
+                nexts.push(n);
+            }
+        });
+    }
+
+    return nexts;
+}
