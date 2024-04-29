23c63c89bb65dd977f96d22184979377c8981f1e
baymax <476803711@qq.com>
2024-04-29 10:15:51 +0800

    feat: 每日一题

--- src/problems/backtracking.ts
+++ src/problems/backtracking.ts
@@ -1,3 +1,4 @@
+import { cache } from '../design-pattern/proxy';
 /* 
 https://leetcode.com/problems/combination-sum-iii/
 
@@ -397,4 +398,54 @@ export function beautifulSubsets(nums: number[], k: number): number {
     backtracking(0, [], []);
 
     return count;
+}
+
+/*
+https://leetcode.com/problems/maximum-strength-of-a-group/description/
+2708. Maximum Strength of a Group
+You are given a 0-indexed integer array nums representing the score of students in an exam. The teacher would like 
+to form one non-empty group of students with maximal strength, where the strength of a group of students of indices i0, i1, i2, ... , 
+ik is defined as nums[i0] * nums[i1] * nums[i2] * ... * nums[ik​].
+
+Return the maximum strength of a group the teacher can create.
+
+Example 1:
+
+Input: nums = [3,-1,-5,2,5,-9]
+Output: 1350
+Explanation: One way to form a group of maximal strength is to group the students at indices [0,2,3,4,5]. 
+Their strength is 3 * (-5) * 2 * 5 * (-9) = 1350, which we can show is optimal.
+
+Example 2:
+
+Input: nums = [-4,-5,-4]
+Output: 20
+Explanation: Group the students at indices [0, 1] . Then, we’ll have a resulting strength of 20. We cannot achieve greater strength.
+
+Constraints:
+
+	1 <= nums.length <= 13
+	-9 <= nums[i] <= 9
+*/
+export function maxStrength(nums: number[]): number {
+    let max = nums[0];
+
+    const backtracking = cache((i: number, product: number | null) => {
+        if (product === 0) {
+            max = Math.max(max, 0);
+            return;
+        }
+        if (i === nums.length) {
+            if (product !== null) {
+                max = Math.max(max, product);
+            }
+            return;
+        }
+
+        backtracking(i + 1, product === null ? nums[i] : product * nums[i]);
+        backtracking(i + 1, product);
+    });
+    backtracking(0, null);
+
+    return max;
 }
