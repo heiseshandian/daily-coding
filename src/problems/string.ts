20364aca076b053bc7acd93f863bb23cc3d726bb
baymax <476803711@qq.com>
2024-04-30 10:23:56 +0800

    feat: 每日一题

--- src/problems/string.ts
+++ src/problems/string.ts
@@ -2018,3 +2018,93 @@ export function sortVowels(s: string): string {
         })
         .join('');
 }
+
+/*
+https://leetcode.com/problems/number-of-wonderful-substrings/description/?envType=daily-question&envId=2024-04-30
+1915. Number of Wonderful Substrings
+A wonderful string is a string where at most one letter appears an odd number of times.
+
+	For example, "ccjjc" and "abab" are wonderful, but "ab" is not.
+
+Given a string word that consists of the first ten lowercase English letters ('a' through 'j'), return the number of 
+wonderful non-empty substrings in word. If the same substring appears multiple times in word, then count each occurrence separately.
+
+A substring is a contiguous sequence of characters in a string.
+
+Example 1:
+
+Input: word = "aba"
+Output: 4
+Explanation: The four wonderful substrings are underlined below:
+- "aba" -> "a"
+- "aba" -> "b"
+- "aba" -> "a"
+- "aba" -> "aba"
+
+Example 2:
+
+Input: word = "aabb"
+Output: 9
+Explanation: The nine wonderful substrings are underlined below:
+- "aabb" -> "a"
+- "aabb" -> "aa"
+- "aabb" -> "aab"
+- "aabb" -> "aabb"
+- "aabb" -> "a"
+- "aabb" -> "abb"
+- "aabb" -> "b"
+- "aabb" -> "bb"
+- "aabb" -> "b"
+
+Example 3:
+
+Input: word = "he"
+Output: 2
+Explanation: The two wonderful substrings are underlined below:
+- "he" -> "h"
+- "he" -> "e"
+
+Constraints:
+
+	1 <= word.length <= 10^5
+	word consists of lowercase English letters from 'a' to 'j'.
+*/
+export function wonderfulSubstrings(word: string): number {
+    const bitMask = {
+        a: 0b0000000001,
+        b: 0b0000000010,
+        c: 0b0000000100,
+        d: 0b0000001000,
+        e: 0b0000010000,
+        f: 0b0000100000,
+        g: 0b0001000000,
+        h: 0b0010000000,
+        i: 0b0100000000,
+        j: 0b1000000000,
+    };
+
+    const prefix: number[] = [bitMask[word[0]]];
+    const freqMap = new Map<number, number>();
+    freqMap.set(prefix[0], 1);
+    for (let i = 1; i < word.length; i++) {
+        prefix[i] = prefix[i - 1] ^ bitMask[word[i]];
+        freqMap.set(prefix[i], (freqMap.get(prefix[i]) ?? 0) + 1);
+    }
+
+    const expectedXors = Object.values(bitMask).concat(0);
+    let count = 0;
+    for (let i = 0; i < word.length; i++) {
+        const prev = i === 0 ? 0 : prefix[i - 1];
+        expectedXors.forEach((xor) => {
+            count += freqMap.get(xor ^ prev) ?? 0;
+        });
+
+        if (freqMap.get(prefix[i])! > 1) {
+            freqMap.set(prefix[i], freqMap.get(prefix[i])! - 1);
+        } else {
+            freqMap.delete(prefix[i]);
+        }
+    }
+
+    return count;
+}
