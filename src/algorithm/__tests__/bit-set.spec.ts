9f600a4e76289abae84907e8fb6206a24f7e6dae
baymax <476803711@qq.com>
2024-05-01 11:20:43 +0800

    feat: 位图

---
+++ src/algorithm/__tests__/bit-set.spec.ts
@@ -0,0 +1,28 @@
+import { BitSet } from '../bitSet';
+
+describe('bit-set', () => {
+    const len = 10000;
+    const nums1 = Array(len)
+        .fill(0)
+        .map(() => (Math.random() * len) >>> 0);
+    const nums2 = Array(len)
+        .fill(0)
+        .map(() => (Math.random() * len) >>> 0);
+
+    const set = new Set(nums1);
+    const bitSet = new BitSet(len);
+    nums1.forEach((n) => {
+        bitSet.add(n);
+    });
+
+    it.each(nums2)('has', (n) => {
+        expect(set.has(n)).toEqual(bitSet.has(n));
+    });
+
+    it.each(nums2)('delete', (n) => {
+        expect(set.has(n)).toEqual(bitSet.has(n));
+        set.delete(n);
+        bitSet.delete(n);
+        expect(set.has(n)).toEqual(bitSet.has(n));
+    });
+});
