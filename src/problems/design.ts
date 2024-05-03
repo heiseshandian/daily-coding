4dff4c4d0f95d4a4676a38359e5ee13ab611668c
baymax <476803711@qq.com>
2024-05-03 14:47:59 +0800

    feat: 每日一题

--- src/problems/design.ts
+++ src/problems/design.ts
@@ -1344,3 +1344,78 @@ export function compose(functions: F[]): F {
         return x;
     };
 }
+
+/*
+https://leetcode.com/problems/maximum-frequency-stack/description/
+895. Maximum Frequency Stack
+Design a stack-like data structure to push elements to the stack and pop the most frequent element from the stack.
+
+Implement the FreqStack class:
+
+	FreqStack() constructs an empty frequency stack.
+	void push(int val) pushes an integer val onto the top of the stack.
+	int pop() removes and returns the most frequent element in the stack.
+
+		If there is a tie for the most frequent element, the element closest to the stack's top is removed and returned.
+
+Example 1:
+
+Input
+["FreqStack", "push", "push", "push", "push", "push", "push", "pop", "pop", "pop", "pop"]
+[[], [5], [7], [5], [7], [4], [5], [], [], [], []]
+Output
+[null, null, null, null, null, null, null, 5, 7, 5, 4]
+
+Explanation
+FreqStack freqStack = new FreqStack();
+freqStack.push(5); // The stack is [5]
+freqStack.push(7); // The stack is [5,7]
+freqStack.push(5); // The stack is [5,7,5]
+freqStack.push(7); // The stack is [5,7,5,7]
+freqStack.push(4); // The stack is [5,7,5,7,4]
+freqStack.push(5); // The stack is [5,7,5,7,4,5]
+freqStack.pop();   // return 5, as 5 is the most frequent. The stack becomes [5,7,5,7,4].
+freqStack.pop();   // return 7, as 5 and 7 is the most frequent, but 7 is closest to the top. The stack becomes [5,7,5,4].
+freqStack.pop();   // return 5, as 5 is the most frequent. The stack becomes [5,7,4].
+freqStack.pop();   // return 4, as 4, 5 and 7 is the most frequent, but 4 is closest to the top. The stack becomes [5,7].
+
+Constraints:
+
+	0 <= val <= 10^9
+	At most 2 * 104 calls will be made to push and pop.
+	It is guaranteed that there will be at least one element in the stack before calling pop.
+*/
+export class FreqStack {
+    freqMap: Map<number, number>;
+    valuesMap: Map<number, number[]>;
+    constructor() {
+        this.freqMap = new Map();
+        this.valuesMap = new Map();
+    }
+
+    push(val: number): void {
+        const times = (this.freqMap.get(val) ?? 0) + 1;
+        this.freqMap.set(val, times);
+        if (this.valuesMap.has(times)) {
+            this.valuesMap.get(times)?.push(val);
+        } else {
+            this.valuesMap.set(times, [val]);
+        }
+    }
+
+    pop(): number {
+        const maxTimes = this.valuesMap.size;
+        const val = this.valuesMap.get(maxTimes)?.pop()!;
+        if (this.valuesMap.get(maxTimes)?.length === 0) {
+            this.valuesMap.delete(maxTimes);
+        }
+
+        if (this.freqMap.get(val) === 1) {
+            this.freqMap.delete(val);
+        } else {
+            this.freqMap.set(val, this.freqMap.get(val)! - 1);
+        }
+
+        return val;
+    }
+}
