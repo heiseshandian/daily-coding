1cd821e177b53d24ac76170114ae4e87c725f382
baymax <476803711@qq.com>
2024-05-04 21:34:24 +0800

    feat: 每日一题

--- src/problems/tree.ts
+++ src/problems/tree.ts
@@ -2131,3 +2131,55 @@ export function pathSum(root: TreeNode | null, targetSum: number): number[][] {
 
     return result;
 }
+
+/*
+https://leetcode.com/problems/binary-tree-level-order-traversal/description/
+102. Binary Tree Level Order Traversal
+Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+Example 1:
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[9,20],[15,7]]
+
+Example 2:
+
+Input: root = [1]
+Output: [[1]]
+
+Example 3:
+
+Input: root = []
+Output: []
+
+Constraints:
+
+	The number of nodes in the tree is in the range [0, 2000].
+	-1000 <= Node.val <= 1000
+*/
+export function levelOrder(root: TreeNode | null): number[][] {
+    if (!root) {
+        return [];
+    }
+
+    const result: number[][] = [];
+    const queue: TreeNode[] = [root];
+    while (queue.length) {
+        const len = queue.length;
+        const level: number[] = Array(len);
+        for (let i = 0; i < len; i++) {
+            const node = queue.shift()!;
+            level[i] = node.val;
+
+            if (node.left) {
+                queue.push(node.left);
+            }
+            if (node.right) {
+                queue.push(node.right);
+            }
+        }
+        result.push(level);
+    }
+
+    return result;
+}
