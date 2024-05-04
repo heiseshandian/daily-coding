7697affb824a1314d43682392ea90d16c0c1e1f2
baymax <476803711@qq.com>
2024-05-04 23:12:32 +0800

    feat: 每日一题

--- src/problems/tree.ts
+++ src/problems/tree.ts
@@ -2264,3 +2264,53 @@ Constraints:
 export function maxDepth(root: TreeNode | null): number {
     return !root ? 0 : Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
 }
+
+/*
+https://leetcode.com/problems/check-completeness-of-a-binary-tree/description/
+958. Check Completeness of a Binary Tree
+Given the root of a binary tree, determine if it is a complete binary tree.
+
+In a complete binary tree, every level, except possibly the last, is completely filled, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+
+Example 1:
+
+Input: root = [1,2,3,4,5,6]
+Output: true
+Explanation: Every level before the last is full (ie. levels with node-values {1} and {2, 3}), and all nodes in the last level ({4, 5, 6}) are as far left as possible.
+
+Example 2:
+
+Input: root = [1,2,3,4,5,null,7]
+Output: false
+Explanation: The node with value 7 isn't as far left as possible.
+
+Constraints:
+
+	The number of nodes in the tree is in the range [1, 100].
+	1 <= Node.val <= 1000
+*/
+export function isCompleteTree(root: TreeNode | null): boolean {
+    if (!root) {
+        return true;
+    }
+
+    const queue: TreeNode[] = [root];
+    let leaf = false;
+    while (queue.length) {
+        const node = queue.shift()!;
+        if ((node.right && !node.left) || (leaf && !isLeaf(node))) {
+            return false;
+        }
+        if (node.left) {
+            queue.push(node.left);
+        }
+        if (node.right) {
+            queue.push(node.right);
+        }
+        if (!node.left || !node.right) {
+            leaf = true;
+        }
+    }
+
+    return true;
+}
