caad79da1e30b987659ef977a45ac648eeed68ce
baymax <476803711@qq.com>
2024-05-04 22:34:07 +0800

    feat: 每日一题

--- src/problems/tree.ts
+++ src/problems/tree.ts
@@ -1,9 +1,5 @@
 import { Queue } from '../algorithm/queue';
-import {
-    TreeNode,
-    deserializeByLevel,
-    getMaxDistance,
-} from '../algorithm/tree';
+import { TreeNode, getMaxDistance } from '../algorithm/tree';
 import { cache } from '../design-pattern/proxy';
 /* 
 https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
@@ -2182,4 +2178,63 @@ export function levelOrder(root: TreeNode | null): number[][] {
     }
 
     return result;
+}
+
+/*
+https://leetcode.com/problems/maximum-width-of-binary-tree/description/
+662. Maximum Width of Binary Tree
+Given the root of a binary tree, return the maximum width of the given tree.
+
+The maximum width of a tree is the maximum width among all levels.
+
+The width of one level is defined as the length between the end-nodes (the leftmost and rightmost non-null nodes), where the null nodes between the end-nodes that would be present in a complete binary tree extending down to that level are also counted into the length calculation.
+
+It is guaranteed that the answer will in the range of a 32-bit signed integer.
+
+Example 1:
+
+Input: root = [1,3,2,5,3,null,9]
+Output: 4
+Explanation: The maximum width exists in the third level with length 4 (5,3,null,9).
+
+Example 2:
+
+Input: root = [1,3,2,5,null,null,9,6,null,7]
+Output: 7
+Explanation: The maximum width exists in the fourth level with length 7 (6,null,null,null,null,null,7).
+
+Example 3:
+
+Input: root = [1,3,2,5]
+Output: 2
+Explanation: The maximum width exists in the second level with length 2 (3,2).
+
+Constraints:
+
+	The number of nodes in the tree is in the range [1, 3000].
+	-100 <= Node.val <= 100
+*/
+export function widthOfBinaryTree(root: TreeNode | null): number {
+    if (!root) {
+        return 0;
+    }
+
+    // index 使用 bitint 避免越界
+    const queue: Array<[node: TreeNode, index: bigint]> = [[root, 1n]];
+    let max = 0;
+    while (queue.length) {
+        const len = queue.length;
+        max = Math.max(max, Number(queue[len - 1][1] - queue[0][1]) + 1);
+        for (let i = 0; i < len; i++) {
+            const [node, index] = queue.shift()!;
+            if (node.left) {
+                queue.push([node.left, index * 2n]);
+            }
+            if (node.right) {
+                queue.push([node.right, index * 2n + 1n]);
+            }
+        }
+    }
+
+    return max;
 }
