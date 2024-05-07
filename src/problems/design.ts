a03df37d139064f964cf6cea7e37759a3efe8055
baymax <476803711@qq.com>
2024-05-07 09:30:41 +0800

    feat: 每日一题

--- src/problems/design.ts
+++ src/problems/design.ts
@@ -1419,3 +1419,49 @@ export class FreqStack {
         return val;
     }
 }
+
+/*
+https://leetcode.com/problems/generate-fibonacci-sequence/description/
+2648. Generate Fibonacci Sequence
+Write a generator function that returns a generator object which yields the fibonacci sequence.
+
+The fibonacci sequence is defined by the relation Xn = Xn-1 + Xn-2.
+
+The first few numbers of the series are 0, 1, 1, 2, 3, 5, 8, 13.
+
+Example 1:
+
+Input: callCount = 5
+Output: [0,1,1,2,3]
+Explanation:
+const gen = fibGenerator();
+gen.next().value; // 0
+gen.next().value; // 1
+gen.next().value; // 1
+gen.next().value; // 2
+gen.next().value; // 3
+
+Example 2:
+
+Input: callCount = 0
+Output: []
+Explanation: gen.next() is never called so nothing is outputted
+
+Constraints:
+
+	0 <= callCount <= 50
+*/
+export function* fibGenerator(): Generator<number, any, number> {
+    yield 0;
+    yield 1;
+
+    let first = 0;
+    let second = 1;
+    while (true) {
+        const cur = first + second;
+        first = second;
+        second = cur;
+
+        yield cur;
+    }
+}
