1f64dd1857262b6621fe590f2b6882b9353b69a8
baymax <476803711@qq.com>
2024-05-09 23:32:56 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -5409,3 +5409,62 @@ export function longestWPI(hours: number[]): number {
 
     return max;
 }
+
+/*
+https://leetcode.com/problems/make-sum-divisible-by-p/description/
+1590. Make Sum Divisible by P
+Given an array of positive integers nums, remove the smallest subarray (possibly empty) such that the sum of the remaining elements is divisible by p. It is not allowed to remove the whole array.
+
+Return the length of the smallest subarray that you need to remove, or -1 if it's impossible.
+
+A subarray is defined as a contiguous block of elements in the array.
+
+Example 1:
+
+Input: nums = [3,1,4,2], p = 6
+Output: 1
+Explanation: The sum of the elements in nums is 10, which is not divisible by 6. We can remove the subarray [4], and the sum of the remaining elements is 6, which is divisible by 6.
+
+Example 2:
+
+Input: nums = [6,3,5,2], p = 9
+Output: 2
+Explanation: We cannot remove a single element to get a sum divisible by 9. The best way is to remove the subarray [5,2], leaving us with [6,3] with sum 9.
+
+Example 3:
+
+Input: nums = [1,2,3], p = 3
+Output: 0
+Explanation: Here the sum is 6. which is already divisible by 3. Thus we do not need to remove anything.
+
+Constraints:
+
+	1 <= nums.length <= 10^5
+	1 <= nums[i] <= 10^9
+	1 <= p <= 10^9
+*/
+export function minSubarray(nums: number[], p: number): number {
+    const mod = nums.reduce((sum, cur) => sum + cur) % p;
+    // 整体余数为 0 说明整个数组就满足需求，不需要删除任何子数组
+    if (mod === 0) {
+        return 0;
+    }
+
+    const map = new Map();
+    map.set(0, -1);
+
+    let sum = 0;
+    // 此处最大值需要用 nums.length, 不能用 Infinity
+    let min = nums.length;
+    nums.forEach((n, i) => {
+        sum += n;
+        const prev = ((sum % p) - mod + p) % p;
+
+        if (map.has(prev)) {
+            min = Math.min(min, i - map.get(prev));
+        }
+        map.set(sum % p, i);
+    });
+
+    return min === nums.length ? -1 : min;
+}
