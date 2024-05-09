7d85ab9f6beba1df8bb24504785d0f07cb82f592
baymax <476803711@qq.com>
2024-05-10 00:03:12 +0800

    feat: 每日一题

--- src/problems/string.ts
+++ src/problems/string.ts
@@ -1,4 +1,4 @@
-import { getCharIndex, swap } from '../common/index';
+import { getCharIndex, isEven, swap } from '../common/index';
 /*
 https://leetcode.com/problems/construct-k-palindrome-strings/
 1400. Construct K Palindrome Strings
@@ -2148,4 +2148,61 @@ export function removeDuplicates(s: string): string {
     }
 
     return stack.join('');
+}
+
+/*
+https://leetcode.com/problems/find-the-longest-substring-containing-vowels-in-even-counts/description/?envType=problem-list-v2&envId=o5cftq05
+1371. Find the Longest Substring Containing Vowels in Even Counts
+Given the string s, return the size of the longest substring containing each vowel an even number of times. That is, 'a', 'e', 'i', 'o', and 'u' must appear an even number of times.
+
+Example 1:
+
+Input: s = "eleetminicoworoep"
+Output: 13
+Explanation: The longest substring is "leetminicowor" which contains two each of the vowels: e, i and o and zero of the vowels: a and u.
+
+Example 2:
+
+Input: s = "leetcodeisgreat"
+Output: 5
+Explanation: The longest substring is "leetc" which contains two e's.
+
+Example 3:
+
+Input: s = "bcbcbc"
+Output: 6
+Explanation: In this case, the given string "bcbcbc" is the longest because all vowels: a, e, i, o and u appear zero times.
+
+Constraints:
+
+	1 <= s.length <= 5 x 10^5
+	s contains only lowercase English letters.
+*/
+export function findTheLongestSubstring(s: string): number {
+    // a e i o u
+    // 0 0 0 0 0
+    const vowels = {
+        a: 0,
+        e: 1,
+        i: 2,
+        o: 3,
+        u: 4,
+    };
+    const map = Array(32);
+    map[0] = -1;
+    let max = 0;
+    for (let i = 0, status = 0; i < s.length; i++) {
+        const index = vowels[s[i]];
+        if (index !== undefined) {
+            status ^= 1 << index;
+        }
+
+        if (map[status] !== undefined) {
+            max = Math.max(max, i - map[status]);
+        } else {
+            map[status] = i;
+        }
+    }
+
+    return max;
 }
