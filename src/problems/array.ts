284bb04d5a11eddae0a7f5dfb90b0cd4143e24dc
baymax <476803711@qq.com>
2024-05-09 23:06:34 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -5359,3 +5359,53 @@ export function maximumHappinessSum(happiness: number[], k: number): number {
 
     return max;
 }
+
+/*
+https://leetcode.com/problems/longest-well-performing-interval/description/
+1124. Longest Well-Performing Interval
+We are given hours, a list of the number of hours worked per day for a given employee.
+
+A day is considered to be a tiring day if and only if the number of hours worked is (strictly) greater than 8.
+
+A well-performing interval is an interval of days for which the number of tiring days is strictly larger than the number of non-tiring days.
+
+Return the length of the longest well-performing interval.
+
+Example 1:
+
+Input: hours = [9,9,6,0,6,6,9]
+Output: 3
+Explanation: The longest well-performing interval is [9,9,6].
+
+Example 2:
+
+Input: hours = [6,6,6]
+Output: 0
+
+Constraints:
+
+	1 <= hours.length <= 10^4
+	0 <= hours[i] <= 16
+*/
+export function longestWPI(hours: number[]): number {
+    const map = new Map();
+    map.set(0, -1);
+
+    let max = 0;
+    let sum = 0;
+    hours.forEach((h, i) => {
+        sum += h > 8 ? 1 : -1;
+
+        if (sum > 0) {
+            max = Math.max(max, i + 1);
+        } else if (map.has(sum - 1)) {
+            max = Math.max(max, i - map.get(sum - 1));
+        }
+
+        if (!map.has(sum)) {
+            map.set(sum, i);
+        }
+    });
+
+    return max;
+}
