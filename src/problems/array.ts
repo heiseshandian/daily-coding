e96a350309c9ad46e76a31b5b70f19d976d11c4c
baymax <476803711@qq.com>
2024-05-10 09:58:02 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -5468,3 +5468,57 @@ export function minSubarray(nums: number[], p: number): number {
 
     return min === nums.length ? -1 : min;
 }
+
+/*
+https://leetcode.com/problems/k-th-smallest-prime-fraction/description/
+786. K-th Smallest Prime Fraction
+You are given a sorted integer array arr containing 1 and prime numbers, where all the integers of arr are unique. You are also given an integer k.
+
+For every i and j where 0 <= i < j < arr.length, we consider the fraction arr[i] / arr[j].
+
+Return the kth smallest fraction considered. Return your answer as an array of integers of size 2, where answer[0] == arr[i] and answer[1] == arr[j].
+
+Example 1:
+
+Input: arr = [1,2,3,5], k = 3
+Output: [2,5]
+Explanation: The fractions to be considered in sorted order are:
+1/5, 1/3, 2/5, 1/2, 3/5, and 2/3.
+The third fraction is 2/5.
+
+Example 2:
+
+Input: arr = [1,7], k = 1
+Output: [1,7]
+
+Constraints:
+
+	2 <= arr.length <= 1000
+	1 <= arr[i] <= 10^4
+	arr[0] == 1
+	arr[i] is a prime number for i > 0.
+	All the numbers of arr are unique and sorted in strictly increasing order.
+	1 <= k <= arr.length * (arr.length - 1) / 2
+
+Follow up: Can you solve the problem with better than O(n2) complexity?
+*/
+export function kthSmallestPrimeFraction(arr: number[], k: number): number[] {
+    arr.sort((a, b) => a - b);
+    const heap = new GenericHeap<[val: number, i: number, j: number]>(
+        ([valA], [valB]) => valB - valA
+    );
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (heap.size() < k) {
+                heap.push([arr[i] / arr[j], i, j]);
+            } else if (heap.peek()[0] > arr[i] / arr[j]) {
+                heap.pop();
+                heap.push([arr[i] / arr[j], i, j]);
+            }
+        }
+    }
+
+    const [, i, j] = heap.peek();
+    return [arr[i], arr[j]];
+}
