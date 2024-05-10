2837ae7c4b51dd6f88b8c614bce27c3fd6d1bae2
baymax <476803711@qq.com>
2024-05-10 10:00:58 +0800

    feat: 每日一题

--- src/problems/matrix.ts
+++ src/problems/matrix.ts
@@ -1114,3 +1114,56 @@ export function matrixSum(nums: number[][]): number {
 
     return score;
 }
+
+/*
+https://leetcode.com/problems/row-with-maximum-ones/description/
+2643. Row With Maximum Ones
+Given a m x n binary matrix mat, find the 0-indexed position of the row that contains the maximum count of ones, and the number of ones in that row.
+
+In case there are multiple rows that have the maximum count of ones, the row with the smallest row number should be selected.
+
+Return an array containing the index of the row, and the number of ones in it.
+
+Example 1:
+
+Input: mat = [[0,1],[1,0]]
+Output: [0,1]
+Explanation: Both rows have the same number of 1's. So we return the index of the smaller row, 0, and the maximum count of ones (1). So, the answer is [0,1]. 
+
+Example 2:
+
+Input: mat = [[0,0,0],[0,1,1]]
+Output: [1,2]
+Explanation: The row indexed 1 has the maximum count of ones (2). So we return its index, 1, and the count. So, the answer is [1,2].
+
+Example 3:
+
+Input: mat = [[0,0],[1,1],[0,0]]
+Output: [1,2]
+Explanation: The row indexed 1 has the maximum count of ones (2). So the answer is [1,2].
+
+Constraints:
+
+	m == mat.length 
+	n == mat[i].length 
+	1 <= m, n <= 100 
+	mat[i][j] is either 0 or 1.
+*/
+export function rowAndMaximumOnes(mat: number[][]): number[] {
+    let maxIndex = 0;
+    let maxOnes = -Infinity;
+
+    for (let i = 0; i < mat.length; i++) {
+        let ones = 0;
+        for (let j = 0; j < mat[0].length; j++) {
+            ones += mat[i][j];
+        }
+
+        if (ones > maxOnes) {
+            maxOnes = ones;
+            maxIndex = i;
+        }
+    }
+
+    return [maxIndex, maxOnes];
+}
