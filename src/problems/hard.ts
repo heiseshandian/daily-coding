c6f2005a0cd7187b9e1528ce2ce09562dd8a6aa9
baymax <476803711@qq.com>
2024-05-11 11:27:00 +0800

    feat: 每日一题

--- src/problems/hard.ts
+++ src/problems/hard.ts
@@ -1043,3 +1043,73 @@ export function nthMagicalNumber(n: number, a: number, b: number): number {
 
     return closest % modulo;
 }
+
+/*
+https://leetcode.com/problems/minimum-cost-to-hire-k-workers/description/?envType=daily-question&envId=2024-05-11
+857. Minimum Cost to Hire K Workers
+There are n workers. You are given two integer arrays quality and wage where quality[i] is the quality of the ith worker and wage[i] is the minimum wage expectation for the ith worker.
+
+We want to hire exactly k workers to form a paid group. To hire a group of k workers, we must pay them according to the following rules:
+
+	Every worker in the paid group must be paid at least their minimum wage expectation.
+	In the group, each worker's pay must be directly proportional to their quality. This means if a worker’s quality is double that of another worker in the group, then they must be paid twice as much as the other worker.
+
+Given the integer k, return the least amount of money needed to form a paid group satisfying the above conditions. Answers within 10-5 of the actual answer will be accepted.
+
+Example 1:
+
+Input: quality = [10,20,5], wage = [70,50,30], k = 2
+Output: 105.00000
+Explanation: We pay 70 to 0th worker and 35 to 2nd worker.
+
+Example 2:
+
+Input: quality = [3,1,10,10,1], wage = [4,8,2,2,7], k = 3
+Output: 30.66667
+Explanation: We pay 4 to 0th worker, 13.33333 to 2nd and 3rd workers separately.
+
+Constraints:
+
+	n == quality.length == wage.length
+	1 <= k <= n <= 10^4
+	1 <= quality[i], wage[i] <= 10^4
+
+FIXME: TLE 待优化性能
+*/
+export function mincostToHireWorkers(
+    quality: number[],
+    wage: number[],
+    k: number
+): number {
+    const sorted = quality
+        .map((q, i) => [q, wage[i]])
+        .sort(([qA, wA], [qB, wB]) => wB / qB - wA / qA);
+
+    const heap = new GenericHeap((a, b) => b - a);
+    let min = Infinity;
+
+    let baseQuality = 0;
+    let baseWage = 0;
+    let baseRatio = 0;
+    for (let i = 0; i <= sorted.length - k; i++) {
+        [baseQuality, baseWage] = sorted[i];
+        baseRatio = baseWage / baseQuality;
+
+        for (let j = i; j < sorted.length; j++) {
+            if (heap.size() < k) {
+                heap.push(sorted[j][0]);
+            } else if (heap.peek() > sorted[j][0]) {
+                heap.pop();
+                heap.push(sorted[j][0]);
+            }
+        }
+
+        min = Math.min(
+            min,
+            heap.container.reduce((sum, cur) => sum + cur) * baseRatio
+        );
+        heap.container.length = 0;
+    }
+
+    return min;
+}
