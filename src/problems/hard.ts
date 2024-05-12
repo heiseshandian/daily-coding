8459377324bd3750e14af487138f591efb7dc29b
baymax <476803711@qq.com>
2024-05-12 20:53:04 +0800

    feat: 每日一题

--- src/problems/hard.ts
+++ src/problems/hard.ts
@@ -1169,3 +1169,86 @@ function canSplit(nums: number[], k: number, range: number): boolean {
 
     return parts <= k;
 }
+
+/*
+https://leetcode.com/problems/find-k-th-smallest-pair-distance/description/
+719. Find K-th Smallest Pair Distance
+The distance of a pair of integers a and b is defined as the absolute difference between a and b.
+
+Given an integer array nums and an integer k, return the kth smallest distance among all the pairs nums[i] and nums[j] where 0 <= i < j < nums.length.
+
+Example 1:
+
+Input: nums = [1,3,1], k = 1
+Output: 0
+Explanation: Here are all the pairs:
+(1,3) -> 2
+(1,1) -> 0
+(3,1) -> 2
+Then the 1st smallest distance pair is (1,1), and its distance is 0.
+
+Example 2:
+
+Input: nums = [1,1,1], k = 2
+Output: 0
+
+Example 3:
+
+Input: nums = [1,6,1], k = 3
+Output: 5
+
+Constraints:
+
+	n == nums.length
+	2 <= n <= 10^4
+	0 <= nums[i] <= 10^6
+	1 <= k <= n * (n - 1) / 2
+*/
+export function smallestDistancePair(nums: number[], k: number): number {
+    nums.sort((a, b) => a - b);
+
+    let l = 0;
+    let r = nums[nums.length - 1] - nums[0];
+    let distance = -1;
+    while (l <= r) {
+        const m = l + ((r - l) >> 1);
+        if (countPairsWithSmallerDistance(nums, m) >= k) {
+            distance = m;
+            r = m - 1;
+        } else {
+            l = m + 1;
+        }
+    }
+
+    return distance;
+}
+
+function countPairsWithSmallerDistance(
+    nums: number[],
+    distance: number
+): number {
+    let count = 0;
+    const n = nums.length;
+    for (let i = 0; i < n - 1; i++) {
+        if (nums[i + 1] - nums[i] > distance) {
+            continue;
+        }
+
+        let l = i + 1;
+        let r = n - 1;
+        let rightBound = i + 1;
+        while (l <= r) {
+            const m = l + ((r - l) >> 1);
+            if (nums[m] - nums[i] <= distance) {
+                rightBound = m;
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+
+        count += rightBound - i;
+    }
+
+    return count;
+}
