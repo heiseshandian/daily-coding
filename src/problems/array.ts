bdc0274da5d96d8ed8f1655bc70798de056e93c0
baymax <476803711@qq.com>
2024-05-12 15:02:36 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -5522,3 +5522,49 @@ export function kthSmallestPrimeFraction(arr: number[], k: number): number[] {
     const [, i, j] = heap.peek();
     return [arr[i], arr[j]];
 }
+
+/*
+https://leetcode.com/problems/sort-array-by-parity-ii/description/
+922. Sort Array By Parity II
+Given an array of integers nums, half of the integers in nums are odd, and the other half are even.
+
+Sort the array so that whenever nums[i] is odd, i is odd, and whenever nums[i] is even, i is even.
+
+Return any answer array that satisfies this condition.
+
+Example 1:
+
+Input: nums = [4,2,5,7]
+Output: [4,5,2,7]
+Explanation: [4,7,2,5], [2,5,4,7], [2,7,4,5] would also have been accepted.
+
+Example 2:
+
+Input: nums = [2,3]
+Output: [2,3]
+
+Constraints:
+
+	2 <= nums.length <= 10^4
+	nums.length is even.
+	Half of the integers in nums are even.
+	0 <= nums[i] <= 1000
+
+Follow Up: Could you solve it in-place?
+*/
+export function sortArrayByParityII(nums: number[]): number[] {
+    let even = 0;
+    let odd = 1;
+    const n = nums.length;
+    while (even < n && odd < n) {
+        if (isEven(nums[n - 1])) {
+            swap(nums, even, n - 1);
+            even += 2;
+        } else {
+            swap(nums, odd, n - 1);
+            odd += 2;
+        }
+    }
+
+    return nums;
+}
