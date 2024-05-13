d2cbbb9a045bcd77f4fd8539709f3118b6b4cda7
baymax <476803711@qq.com>
2024-05-13 23:09:39 +0800

    feat: 每日一题

--- src/problems/matrix.ts
+++ src/problems/matrix.ts
@@ -1279,3 +1279,67 @@ export function largestLocal(grid: number[][]): number[][] {
 
     return local;
 }
+
+/*
+https://leetcode.com/problems/score-after-flipping-matrix/description/?envType=daily-question&envId=2024-05-13
+861. Score After Flipping Matrix
+You are given an m x n binary matrix grid.
+
+A move consists of choosing any row or column and toggling each value in that row or column (i.e., changing all 0's to 1's, and all 1's to 0's).
+
+Every row of the matrix is interpreted as a binary number, and the score of the matrix is the sum of these numbers.
+
+Return the highest possible score after making any number of moves (including zero moves).
+
+Example 1:
+
+Input: grid = [[0,0,1,1],[1,0,1,0],[1,1,0,0]]
+Output: 39
+Explanation: 0b1111 + 0b1001 + 0b1111 = 15 + 9 + 15 = 39
+
+Example 2:
+
+Input: grid = [[0]]
+Output: 1
+
+Constraints:
+
+	m == grid.length
+	n == grid[i].length
+	1 <= m, n <= 20
+	grid[i][j] is either 0 or 1.
+
+贪心
+- 横向第一个数字为 0 才 flip
+- 纵向 0 的个数比 1 多才 flip
+*/
+export function matrixScore(grid: number[][]): number {
+    for (let i = 0; i < grid.length; i++) {
+        if (grid[i][0] === 0) {
+            for (let j = 0; j < grid[i].length; j++) {
+                grid[i][j] ^= 1;
+            }
+        }
+    }
+
+    for (let j = 0; j < grid[0].length; j++) {
+        let zeroCount = 0;
+        for (let i = 0; i < grid.length; i++) {
+            if (grid[i][j] === 0) {
+                zeroCount++;
+            }
+        }
+        if (zeroCount > grid.length - zeroCount) {
+            for (let i = 0; i < grid.length; i++) {
+                grid[i][j] ^= 1;
+            }
+        }
+    }
+
+    return grid.reduce(
+        (sum, row) =>
+            sum +
+            row.reduce((s, cur, i) => s + (cur << (row.length - i - 1)), 0),
+        0
+    );
+}
