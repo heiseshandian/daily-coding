865b78df014d5f065c0501c7a5b00acbda20cc68
baymax <476803711@qq.com>
2024-05-14 22:39:32 +0800

    feat: 每日一题

--- src/problems/stack.ts
+++ src/problems/stack.ts
@@ -429,3 +429,55 @@ function down(stack: number[], max: number, k: number, depth: number) {
         stack.push(num);
     }
 }
+
+/*
+https://leetcode.com/problems/maximum-width-ramp/description/
+962. Maximum Width Ramp
+A ramp in an integer array nums is a pair (i, j) for which i < j and nums[i] <= nums[j]. The width of such a ramp is j - i.
+
+Given an integer array nums, return the maximum width of a ramp in nums. If there is no ramp in nums, return 0.
+
+Example 1:
+
+Input: nums = [6,0,8,2,1,5]
+Output: 4
+Explanation: The maximum width ramp is achieved at (i, j) = (1, 5): nums[1] = 0 and nums[5] = 5.
+
+Example 2:
+
+Input: nums = [9,8,1,0,1,9,4,0,4,1]
+Output: 7
+Explanation: The maximum width ramp is achieved at (i, j) = (2, 9): nums[2] = 1 and nums[9] = 1.
+
+Constraints:
+
+	2 <= nums.length <= 10^4
+	0 <= nums[i] <= 10^4
+*/
+export function maxWidthRamp(nums: number[]): number {
+    // 严格从大到小
+    const stack: number[] = [0];
+    nums.forEach((v, i) => {
+        const prev = nums[stack[stack.length - 1]];
+        if (v < prev) {
+            stack.push(i);
+        }
+    });
+
+    let max = 0;
+    for (let i = nums.length - 1; i >= 0; ) {
+        if (stack.length === 0) {
+            break;
+        }
+
+        const prev = nums[stack[stack.length - 1]];
+        if (prev <= nums[i]) {
+            max = Math.max(max, i - stack[stack.length - 1]);
+            stack.pop();
+        } else {
+            i--;
+        }
+    }
+
+    return max;
+}
