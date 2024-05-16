4e1d84a4bc9abaff56e9b4bbbb9bb4885a68a60d
baymax <476803711@qq.com>
2024-05-16 23:37:40 +0800

    feat: 每日一题

--- src/problems/greedy.ts
+++ src/problems/greedy.ts
@@ -428,3 +428,36 @@ export function checkValidString(s: string): boolean {
 
     return leftMin === 0;
 }
+
+/*
+https://leetcode.com/problems/largest-number/description/
+179. Largest Number
+Given a list of non-negative integers nums, arrange them such that they form the largest number and return it.
+
+Since the result may be very large, so you need to return a string instead of an integer.
+
+Example 1:
+
+Input: nums = [10,2]
+Output: "210"
+
+Example 2:
+
+Input: nums = [3,30,34,5,9]
+Output: "9534330"
+
+Constraints:
+
+	1 <= nums.length <= 100
+	0 <= nums[i] <= 10^9
+*/
+export function largestNumber(nums: number[]): string {
+    const comparator = (a: number, b: number) => {
+        return Number(String(b) + String(a)) - Number(String(a) + String(b));
+    };
+    const num = nums
+        .sort((a, b) => comparator(a, b))
+        .reduce((s, c) => s + c, '');
+
+    return num[0] === '0' ? '0' : num;
+}
