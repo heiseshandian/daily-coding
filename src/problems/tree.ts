7c69ae53ee5f687fad2e41f400d77c86cba7c64f
baymax <476803711@qq.com>
2024-05-17 10:02:40 +0800

    feat: 每日一题

--- src/problems/tree.ts
+++ src/problems/tree.ts
@@ -1,5 +1,9 @@
 import { Queue } from '../algorithm/queue';
-import { TreeNode, getMaxDistance } from '../algorithm/tree';
+import {
+    TreeNode,
+    deserializeByLevel,
+    getMaxDistance,
+} from '../algorithm/tree';
 import { cache } from '../design-pattern/proxy';
 /* 
 https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
@@ -2362,4 +2366,59 @@ export function trimBST(
     root.left = l;
     root.right = r;
     return root;
+}
+
+/*
+https://leetcode.com/problems/delete-leaves-with-a-given-value/description/?envType=daily-question&envId=2024-05-17
+1325. Delete Leaves With a Given Value
+Given a binary tree root and an integer target, delete all the leaf nodes with value target.
+
+Note that once you delete a leaf node with value target, if its parent node becomes a leaf node and has the value target, it should also be deleted (you need to continue doing that until you cannot).
+
+Example 1:
+
+Input: root = [1,2,3,2,null,2,4], target = 2
+Output: [1,null,3,null,4]
+Explanation: Leaf nodes in green with value (target = 2) are removed (Picture in left). 
+After removing, new nodes become leaf nodes with value (target = 2) (Picture in center).
+
+Example 2:
+
+Input: root = [1,3,3,3,2], target = 3
+Output: [1,3,null,null,2]
+
+Example 3:
+
+Input: root = [1,2,null,2,null,2], target = 2
+Output: [1]
+Explanation: Leaf nodes in green with value (target = 2) are removed at each step.
+
+Constraints:
+
+	The number of nodes in the tree is in the range [1, 3000].
+	1 <= Node.val, target <= 1000
+*/
+export function removeLeafNodes(
+    root: TreeNode | null,
+    target: number
+): TreeNode | null {
+    const dfs = (root: TreeNode | null) => {
+        if (!root) {
+            return null;
+        }
+
+        const left = dfs(root.left);
+        const right = dfs(root.right);
+        if (left && isLeaf(left) && left.val === target) {
+            root.left = null;
+        }
+        if (right && isLeaf(right) && right.val === target) {
+            root.right = null;
+        }
+
+        return root;
+    };
+    const newRoot = dfs(root);
+
+    return newRoot && isLeaf(newRoot) && newRoot.val === target ? null : root;
 }
