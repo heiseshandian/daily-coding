9d0c407597cc09ed919f9cc0cee8a9dc574d2462
baymax <476803711@qq.com>
2024-05-18 20:10:07 +0800

    feat: 每日一题

--- src/problems/tree.ts
+++ src/problems/tree.ts
@@ -2422,3 +2422,51 @@ export function removeLeafNodes(
 
     return newRoot && isLeaf(newRoot) && newRoot.val === target ? null : root;
 }
+
+/*
+https://leetcode.com/problems/distribute-coins-in-binary-tree/description/?envType=daily-question&envId=2024-05-18
+979. Distribute Coins in Binary Tree
+You are given the root of a binary tree with n nodes where each node in the tree has node.val coins. There are n coins in total throughout the whole tree.
+
+In one move, we may choose two adjacent nodes and move one coin from one node to another. A move may be from parent to child, or from child to parent.
+
+Return the minimum number of moves required to make every node have exactly one coin.
+
+Example 1:
+
+Input: root = [3,0,0]
+Output: 2
+Explanation: From the root of the tree, we move one coin to its left child, and one coin to its right child.
+
+Example 2:
+
+Input: root = [0,3,0]
+Output: 3
+Explanation: From the left child of the root, we move two coins to the root [taking two moves]. Then, we move one coin from the root of the tree to the right child.
+
+Constraints:
+
+	The number of nodes in the tree is n.
+	1 <= n <= 100
+	0 <= Node.val <= n
+	The sum of all Node.val is n.
+*/
+export function distributeCoins(root: TreeNode | null): number {
+    let moves = 0;
+
+    const postOrder = (node: TreeNode | null): number => {
+        if (!node) {
+            return 0;
+        }
+
+        const left = postOrder(node.left);
+        const right = postOrder(node.right);
+
+        moves += Math.abs(left) + Math.abs(right);
+
+        return node.val + left + right - 1;
+    };
+    postOrder(root);
+
+    return moves;
+}
