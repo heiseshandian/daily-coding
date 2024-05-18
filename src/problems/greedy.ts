27fd81afa121046bd8fde5672537eaf3c9e08a58
baymax <476803711@qq.com>
2024-05-18 13:36:07 +0800

    feat: 每日一题

--- src/problems/greedy.ts
+++ src/problems/greedy.ts
@@ -1,5 +1,6 @@
 import { isEven, isOdd } from '../common';
 import { cache } from '../design-pattern/proxy';
+import { GenericHeap } from '../algorithm/generic-heap';
 
 /* 
 https://leetcode.com/problems/non-overlapping-intervals/
@@ -563,3 +564,60 @@ export const minDays = cache((n: number) => {
         (n % 3) + 1 + minDays(Math.floor(n / 3))
     );
 });
+
+/*
+https://leetcode.com/problems/course-schedule-iii/description/
+630. Course Schedule III
+There are n different online courses numbered from 1 to n. You are given an array courses where courses[i] = [durationi, lastDayi] 
+indicate that the ith course should be taken continuously for durationi days and must be finished before or on lastDayi.
+
+You will start on the 1st day and you cannot take two or more courses simultaneously.
+
+Return the maximum number of courses that you can take.
+
+Example 1:
+
+Input: courses = [[100,200],[200,1300],[1000,1250],[2000,3200]]
+Output: 3
+Explanation: 
+There are totally 4 courses, but you can take 3 courses at most:
+First, take the 1st course, it costs 100 days so you will finish it on the 100th day, and ready to take the next course on the 101st day.
+Second, take the 3rd course, it costs 1000 days so you will finish it on the 1100th day, and ready to take the next course on the 1101st day. 
+Third, take the 2nd course, it costs 200 days so you will finish it on the 1300th day. 
+The 4th course cannot be taken now, since you will finish it on the 3300th day, which exceeds the closed date.
+
+Example 2:
+
+Input: courses = [[1,2]]
+Output: 1
+
+Example 3:
+
+Input: courses = [[3,2],[4,3]]
+Output: 0
+
+Constraints:
+
+	1 <= courses.length <= 10^4
+	1 <= durationi, lastDayi <= 10^4
+*/
+export function scheduleCourse(courses: number[][]): number {
+    courses.sort(([, endA], [, endB]) => endA - endB);
+
+    const heap = new GenericHeap((a, b) => b - a);
+    let time = 0;
+    let count = 0;
+    courses.forEach(([duration, end]) => {
+        if (time + duration <= end) {
+            heap.push(duration);
+            time += duration;
+            count++;
+        } else if (heap.size() > 0 && heap.peek() > duration) {
+            time -= heap.pop();
+            heap.push(duration);
+            time += duration;
+        }
+    });
+
+    return count;
+}
