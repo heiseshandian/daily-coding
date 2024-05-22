b502edc31d61594b2282cda48a8eedf60204d74c
baymax <476803711@qq.com>
2024-05-22 23:22:31 +0800

    feat: 每日一题

--- src/problems/hard.ts
+++ src/problems/hard.ts
@@ -1355,3 +1355,72 @@ export function minSwapsCouples(row: number[]): number {
 
     return couples - sets;
 }
+
+/*
+https://leetcode.com/problems/similar-string-groups/description/
+839. Similar String Groups
+Two strings, X and Y, are considered similar if either they are identical or we can make them 
+equivalent by swapping at most two letters (in distinct positions) within the string X.
+
+For example, "tars" and "rats" are similar (swapping at positions 0 and 2), and "rats" 
+and "arts" are similar, but "star" is not similar to "tars", "rats", or "arts".
+
+Together, these form two connected groups by similarity: {"tars", "rats", "arts"} and {"star"}.  
+Notice that "tars" and "arts" are in the same group even though they are not similar.  
+Formally, each group is such that a word is in the group if and only if it is similar to at 
+least one other word in the group.
+
+We are given a list strs of strings where every string in strs is an anagram of 
+every other string in strs. How many groups are there?
+
+Example 1:
+
+Input: strs = ["tars","rats","arts","star"]
+Output: 2
+
+Example 2:
+
+Input: strs = ["omv","ovm"]
+Output: 1
+
+Constraints:
+
+	1 <= strs.length <= 300
+	1 <= strs[i].length <= 300
+	strs[i] consists of lowercase letters only.
+	All words in strs have the same length and are anagrams of each other.
+*/
+export function numSimilarGroups(strs: string[]): number {
+    const n = strs.length;
+    const unionFind = new UnionFind(n);
+    let sets = n;
+
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            if (!unionFind.isSameSet(i, j) && isSimilar(strs[i], strs[j])) {
+                unionFind.union(i, j);
+                sets--;
+            }
+        }
+    }
+
+    return sets;
+}
+
+function isSimilar(a: string, b: string): boolean {
+    let i = 0;
+    let n = a.length;
+    let diff = 0;
+
+    while (i < n) {
+        if (a[i] !== b[i]) {
+            diff++;
+        }
+        if (diff >= 3) {
+            return false;
+        }
+        i++;
+    }
+
+    return true;
+}
