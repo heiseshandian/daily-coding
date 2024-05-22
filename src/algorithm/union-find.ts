0eb1de08e5b4d806919ecdb9fb8b689f444d91cc
baymax <476803711@qq.com>
2024-05-22 23:12:55 +0800

    feat: 每日一题

---
+++ src/algorithm/union-find.ts
@@ -0,0 +1,32 @@
+// Simplified version of union find
+export class UnionFind {
+    private father: number[];
+
+    constructor(n: number) {
+        this.father = Array(n);
+
+        for (let i = 0; i < n; i++) {
+            this.father[i] = i;
+        }
+    }
+
+    public find(i: number) {
+        if (i !== this.father[i]) {
+            this.father[i] = this.find(this.father[i]);
+        }
+
+        return this.father[i];
+    }
+
+    public isSameSet(a: number, b: number) {
+        return this.find(a) === this.find(b);
+    }
+
+    public union(a: number, b: number) {
+        const fa = this.find(a);
+        const fb = this.find(b);
+        if (fa !== fb) {
+            this.father[fa] = fb;
+        }
+    }
+}
--- src/problems/hard.ts
+++ src/problems/hard.ts
@@ -2,6 +2,7 @@ import { cache } from '../design-pattern/proxy';
 import { lcm, swap } from '../common/index';
 import { SlidingWindow } from '../algorithm/sliding-window';
 import { GenericHeap } from '../algorithm/generic-heap';
+import { UnionFind } from '../algorithm/union-find';
 
 /*
 https://leetcode.com/problems/find-the-longest-valid-obstacle-course-at-each-position/description/
@@ -1303,4 +1304,54 @@ export function shortestSubarray(nums: number[], k: number): number {
     }
 
     return min === Infinity ? -1 : min;
+}
+
+/*
+https://leetcode.com/problems/couples-holding-hands/description/
+765. Couples Holding Hands
+There are n couples sitting in 2n seats arranged in a row and want to hold hands.
+
+The people and seats are represented by an integer array row where row[i] is the ID of 
+the person sitting in the ith seat. The couples are numbered in order, the first couple being (0, 1), 
+the second couple being (2, 3), and so on with the last couple being (2n - 2, 2n - 1).
+
+Return the minimum number of swaps so that every couple is sitting side by side. 
+A swap consists of choosing any two people, then they stand up and switch seats.
+
+Example 1:
+
+Input: row = [0,2,1,3]
+Output: 1
+Explanation: We only need to swap the second (row[1]) and third (row[2]) person.
+
+Example 2:
+
+Input: row = [3,2,0,1]
+Output: 0
+Explanation: All couples are already seated side by side.
+
+Constraints:
+
+	2n == row.length
+	2 <= n <= 30
+	n is even.
+	0 <= row[i] < 2n
+	All the elements of row are unique.
+*/
+export function minSwapsCouples(row: number[]): number {
+    const n = row.length;
+    const couples = n >> 1;
+    const unionFind = new UnionFind(couples);
+    let sets = couples;
+
+    for (let i = 0; i < n; i += 2) {
+        const a = row[i] >> 1;
+        const b = row[i + 1] >> 1;
+        if (a !== b && !unionFind.isSameSet(a, b)) {
+            unionFind.union(a, b);
+            sets--;
+        }
+    }
+
+    return couples - sets;
 }
