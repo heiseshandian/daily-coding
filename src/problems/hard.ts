648ab24b0ea08c52e58be0e9717ef1da8bbc769d
baymax <476803711@qq.com>
2024-05-25 10:05:52 +0800

    feat: 每日一题

--- src/problems/hard.ts
+++ src/problems/hard.ts
@@ -1523,3 +1523,64 @@ function getAllSubsets(words: string[]) {
 
     return subsets;
 }
+
+/*
+https://leetcode.com/problems/word-break-ii/description/
+140. Word Break II
+Given a string s and a dictionary of strings wordDict, add spaces in s to construct a sentence where each word is a valid dictionary word. Return all such possible sentences in any order.
+
+Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+Example 1:
+
+Input: s = "catsanddog", wordDict = ["cat","cats","and","sand","dog"]
+Output: ["cats and dog","cat sand dog"]
+
+Example 2:
+
+Input: s = "pineapplepenapple", wordDict = ["apple","pen","applepen","pine","pineapple"]
+Output: ["pine apple pen apple","pineapple pen apple","pine applepen apple"]
+Explanation: Note that you are allowed to reuse a dictionary word.
+
+Example 3:
+
+Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+Output: []
+
+Constraints:
+
+	1 <= s.length <= 20
+	1 <= wordDict.length <= 1000
+	1 <= wordDict[i].length <= 10
+	s and wordDict[i] consist of only lowercase English letters.
+	All the strings of wordDict are unique.
+	Input is generated in a way that the length of the answer doesn't exceed 105.
+*/
+export function wordBreak(s: string, wordDict: string[]): string[] {
+    const set = new Set<string>();
+    let min = 0;
+    let max = 0;
+    wordDict.forEach((w) => {
+        min = Math.min(min, w.length);
+        max = Math.max(max, w.length);
+        set.add(w);
+    });
+
+    const result: string[] = [];
+    const backgracking = (index: number, path: string) => {
+        if (index === s.length) {
+            result.push(path.trim());
+            return;
+        }
+
+        for (let l = min; l <= max; l++) {
+            const w = s.slice(index, index + l);
+            if (set.has(w)) {
+                backgracking(index + l, path + ' ' + w);
+            }
+        }
+    };
+    backgracking(0, '');
+
+    return result;
+}
