fc6244c8d3303210757d87094744be20ec33def7
baymax <476803711@qq.com>
2024-05-25 15:56:15 +0800

    feat: 每日一题

--- src/problems/union-set.ts
+++ src/problems/union-set.ts
@@ -1,3 +1,4 @@
+import { UnionFind } from '../algorithm/union-find';
 import { UnionSet } from '../algorithm/union-set';
 /*
 https://leetcode.com/problems/minimum-score-of-a-path-between-two-cities/description/?envType=list&envId=mhgl61ev
@@ -58,4 +59,74 @@ export function minScore(n: number, roads: number[][]): number {
     });
 
     return minDistance;
+}
+
+/*
+https://leetcode.com/problems/most-stones-removed-with-same-row-or-column/description/
+947. Most Stones Removed with Same Row or Column
+On a 2D plane, we place n stones at some integer coordinate points. Each coordinate point may have at most one stone.
+
+A stone can be removed if it shares either the same row or the same column as another stone that has not been removed.
+
+Given an array stones of length n where stones[i] = [xi, yi] represents the location of the ith stone, return the largest possible number of stones that can be removed.
+
+Example 1:
+
+Input: stones = [[0,0],[0,1],[1,0],[1,2],[2,1],[2,2]]
+Output: 5
+Explanation: One way to remove 5 stones is as follows:
+1. Remove stone [2,2] because it shares the same row as [2,1].
+2. Remove stone [2,1] because it shares the same column as [0,1].
+3. Remove stone [1,2] because it shares the same row as [1,0].
+4. Remove stone [1,0] because it shares the same column as [0,0].
+5. Remove stone [0,1] because it shares the same row as [0,0].
+Stone [0,0] cannot be removed since it does not share a row/column with another stone still on the plane.
+
+Example 2:
+
+Input: stones = [[0,0],[0,2],[1,1],[2,0],[2,2]]
+Output: 3
+Explanation: One way to make 3 moves is as follows:
+1. Remove stone [2,2] because it shares the same row as [2,0].
+2. Remove stone [2,0] because it shares the same column as [0,0].
+3. Remove stone [0,2] because it shares the same row as [0,0].
+Stones [0,0] and [1,1] cannot be removed since they do not share a row/column with another stone still on the plane.
+
+Example 3:
+
+Input: stones = [[0,0]]
+Output: 0
+Explanation: [0,0] is the only stone on the plane, so you cannot remove it.
+
+Constraints:
+
+	1 <= stones.length <= 1000
+	0 <= xi, yi <= 10^4
+	No two stones are at the same coordinate point.
+*/
+export function removeStones(stones: number[][]): number {
+    const unionFind = new UnionFind(stones.length);
+    const rows = new Map<number, number>();
+    const cols = new Map<number, number>();
+    let sets = stones.length;
+
+    stones.forEach(([x, y], i) => {
+        if (rows.has(x) && !unionFind.isSameSet(rows.get(x)!, i)) {
+            unionFind.union(rows.get(x)!, i);
+            sets--;
+        }
+        if (cols.has(y) && !unionFind.isSameSet(cols.get(y)!, i)) {
+            unionFind.union(cols.get(y)!, i);
+            sets--;
+        }
+
+        if (!rows.has(x)) {
+            rows.set(x, i);
+        }
+        if (!cols.has(y)) {
+            cols.set(y, i);
+        }
+    });
+
+    return stones.length - sets;
 }
