b13e18474e115421d8427e81387294c1bacb6e2a
baymax <476803711@qq.com>
2024-05-29 09:33:44 +0800

    feat: 每日一题

--- src/problems/bit.ts
+++ src/problems/bit.ts
@@ -502,3 +502,83 @@ export function minOperations2(nums: number[], k: number): number {
 
     return minOp;
 }
+
+/*
+https://leetcode.com/problems/number-of-steps-to-reduce-a-number-in-binary-representation-to-one/description/
+1404. Number of Steps to Reduce a Number in Binary Representation to One
+Given the binary representation of an integer as a string s, return the number of steps to reduce it to 1 under the following rules:
+
+	If the current number is even, you have to divide it by 2.
+
+	If the current number is odd, you have to add 1 to it.
+
+It is guaranteed that you can always reach one for all test cases.
+
+Example 1:
+
+Input: s = "1101"
+Output: 6
+Explanation: "1101" corressponds to number 13 in their decimal representation.
+Step 1) 13 is odd, add 1 and obtain 14. 
+Step 2) 14 is even, divide by 2 and obtain 7.
+Step 3) 7 is odd, add 1 and obtain 8.
+Step 4) 8 is even, divide by 2 and obtain 4.  
+Step 5) 4 is even, divide by 2 and obtain 2. 
+Step 6) 2 is even, divide by 2 and obtain 1.  
+
+Example 2:
+
+Input: s = "10"
+Output: 1
+Explanation: "10" corressponds to number 2 in their decimal representation.
+Step 1) 2 is even, divide by 2 and obtain 1.  
+
+Example 3:
+
+Input: s = "1"
+Output: 0
+
+Constraints:
+
+	1 <= s.length <= 500
+	s consists of characters '0' or '1'
+	s[0] == '1'
+*/
+export function numSteps(s: string): number {
+    const bits = s.split('');
+    let steps = 0;
+
+    while (bits.length > 1) {
+        const last = bits[bits.length - 1];
+
+        if (last === '0') {
+            bits.pop();
+            steps++;
+        } else {
+            addOneToBinaryArrayInPlace(bits);
+            steps++;
+        }
+    }
+
+    return steps;
+}
+
+function addOneToBinaryArrayInPlace(bits: string[]): void {
+    let carry = 1;
+
+    for (let i = bits.length - 1; i >= 0; i--) {
+        if (bits[i] === '1' && carry === 1) {
+            bits[i] = '0';
+        } else if (bits[i] === '0' && carry === 1) {
+            bits[i] = '1';
+            carry = 0;
+            break;
+        } else {
+            break;
+        }
+    }
+
+    if (carry === 1) {
+        bits.unshift('1');
+    }
+}
