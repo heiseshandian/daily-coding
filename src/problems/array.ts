097c2521942d661b25077db5ea12a5cf38732d7d
baymax <476803711@qq.com>
2024-05-30 09:39:56 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -5768,3 +5768,60 @@ export function specialArray(nums: number[]): number {
 
     return -1;
 }
+
+/*
+https://leetcode.com/problems/count-triplets-that-can-form-two-arrays-of-equal-xor/description/
+1442. Count Triplets That Can Form Two Arrays of Equal XOR
+Given an array of integers arr.
+
+We want to select three indices i, j and k where (0 <= i < j <= k < arr.length).
+
+Let's define a and b as follows:
+
+	a = arr[i] ^ arr[i + 1] ^ ... ^ arr[j - 1]
+	b = arr[j] ^ arr[j + 1] ^ ... ^ arr[k]
+
+Note that ^ denotes the bitwise-xor operation.
+
+Return the number of triplets (i, j and k) Where a == b.
+
+Example 1:
+
+Input: arr = [2,3,1,6,7]
+Output: 4
+Explanation: The triplets are (0,1,2), (0,2,2), (2,3,4) and (2,4,4)
+
+Example 2:
+
+Input: arr = [1,1,1,1,1]
+Output: 10
+
+Constraints:
+
+	1 <= arr.length <= 300
+	1 <= arr[i] <= 10^8
+*/
+export function countTriplets(arr: number[]): number {
+    const n = arr.length;
+    const prefix = Array(n + 1).fill(0);
+    arr.forEach((v, i) => {
+        prefix[i + 1] = prefix[i] ^ v;
+    });
+
+    let triples = 0;
+    let a = 0;
+    let b = 0;
+    for (let i = 1; i <= n; i++) {
+        for (let j = i + 1; j <= n; j++) {
+            for (let k = j; k <= n; k++) {
+                a = prefix[j - 1] ^ prefix[i - 1];
+                b = prefix[k] ^ prefix[j - 1];
+                if (a === b) {
+                    triples++;
+                }
+            }
+        }
+    }
+
+    return triples;
+}
