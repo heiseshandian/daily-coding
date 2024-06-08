9cd0ed1eb02d3113847f61192a9c6964aef275fb
baymax <476803711@qq.com>
2024-06-08 13:03:31 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -5825,3 +5825,64 @@ export function countTriplets(arr: number[]): number {
 
     return triples;
 }
+
+/*
+https://leetcode.com/problems/continuous-subarray-sum/description/
+523. Continuous Subarray Sum
+Given an integer array nums and an integer k, return true if nums has a good subarray or false otherwise.
+
+A good subarray is a subarray where:
+
+	its length is at least two, and
+	the sum of the elements of the subarray is a multiple of k.
+
+Note that:
+
+	A subarray is a contiguous part of the array.
+	An integer x is a multiple of k if there exists an integer n such that x = n * k. 0 is always a multiple of k.
+
+Example 1:
+
+Input: nums = [23,2,4,6,7], k = 6
+Output: true
+Explanation: [2, 4] is a continuous subarray of size 2 whose elements sum up to 6.
+
+Example 2:
+
+Input: nums = [23,2,6,4,7], k = 6
+Output: true
+Explanation: [23, 2, 6, 4, 7] is an continuous subarray of size 5 whose elements sum up to 42.
+42 is a multiple of 6 because 42 = 7 * 6 and 7 is an integer.
+
+Example 3:
+
+Input: nums = [23,2,6,4,7], k = 13
+Output: false
+
+Constraints:
+
+	1 <= nums.length <= 10^5
+	0 <= nums[i] <= 10^9
+	0 <= sum(nums[i]) <= 2^31 - 1
+	1 <= k <= 2^31 - 1
+*/
+export function checkSubarraySum(nums: number[], k: number): boolean {
+    let sum = 0;
+    // for cases like [3, 3]
+    const map = { '0': -1 };
+    let remainder = 0;
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        remainder = sum % k;
+
+        if (map[remainder] < i - 1) {
+            return true;
+        }
+
+        if (map[remainder] === undefined) {
+            map[remainder] = i;
+        }
+    }
+
+    return false;
+}
