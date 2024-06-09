65c5c735c0801a4d2eb3d97954058586e07c0c85
baymax <476803711@qq.com>
2024-06-09 21:13:49 +0800

    feat: 每日一题

--- src/review/dp.ts
+++ src/review/dp.ts
@@ -187,3 +187,61 @@ export function longestPalindrome(s: string): string {
 
     return s.slice(l, r + 1);
 }
+
+/*
+https://leetcode.com/problems/ones-and-zeroes/description/
+474. Ones and Zeroes
+You are given an array of binary strings strs and two integers m and n.
+
+Return the size of the largest subset of strs such that there are at most m 0's and n 1's in the subset.
+
+A set x is a subset of a set y if all elements of x are also elements of y.
+
+Example 1:
+
+Input: strs = ["10","0001","111001","1","0"], m = 5, n = 3
+Output: 4
+Explanation: The largest subset with at most 5 0's and 3 1's is {"10", "0001", "1", "0"}, so the answer is 4.
+Other valid but smaller subsets include {"0001", "1"} and {"10", "1", "0"}.
+{"111001"} is an invalid subset because it contains 4 1's, greater than the maximum of 3.
+
+Example 2:
+
+Input: strs = ["10","0","1"], m = 1, n = 1
+Output: 2
+Explanation: The largest subset is {"0", "1"}, so the answer is 2.
+
+Constraints:
+
+	1 <= strs.length <= 600
+	1 <= strs[i].length <= 100
+	strs[i] consists only of digits '0' and '1'.
+	1 <= m, n <= 100
+*/
+export function findMaxForm(strs: string[], m: number, n: number): number {
+    const countZeros = (str: string) => {
+        let zeros = 0;
+        for (let i = 0; i < str.length; i++) {
+            if (str[i] === '0') {
+                zeros++;
+            }
+        }
+
+        return zeros;
+    };
+
+    const dp: number[][] = Array.from({ length: m + 1 }, () =>
+        Array(n + 1).fill(0)
+    );
+    for (let i = strs.length - 1; i >= 0; i--) {
+        const zeros = countZeros(strs[i]);
+        const ones = strs[i].length - zeros;
+        for (let j = m; j >= zeros; j--) {
+            for (let k = n; k >= ones; k--) {
+                dp[j][k] = Math.max(dp[j][k], 1 + dp[j - zeros][k - ones]);
+            }
+        }
+    }
+
+    return dp[m][n];
+}
