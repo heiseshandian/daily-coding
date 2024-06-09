1a768eaa627301be439a4cfe3509e260610da5cb
baymax <476803711@qq.com>
2024-06-09 22:08:02 +0800

    feat: 每日一题

--- src/review/dp.ts
+++ src/review/dp.ts
@@ -1,3 +1,4 @@
+import { cache } from '../design-pattern/proxy';
 /*
 https://leetcode.com/problems/minimum-path-sum/description/
 64. Minimum Path Sum
@@ -244,4 +245,63 @@ export function findMaxForm(strs: string[], m: number, n: number): number {
     }
 
     return dp[m][n];
+}
+
+/*
+https://leetcode.com/problems/profitable-schemes/description/
+879. Profitable Schemes
+There is a group of n members, and a list of various crimes they could commit. The ith crime generates a profit[i] and requires group[i] members to participate in it. If a member participates in one crime, that member can't participate in another crime.
+
+Let's call a profitable scheme any subset of these crimes that generates at least minProfit profit, and the total number of members participating in that subset of crimes is at most n.
+
+Return the number of schemes that can be chosen. Since the answer may be very large, return it modulo 109 + 7.
+
+Example 1:
+
+Input: n = 5, minProfit = 3, group = [2,2], profit = [2,3]
+Output: 2
+Explanation: To make a profit of at least 3, the group could either commit crimes 0 and 1, or just crime 1.
+In total, there are 2 schemes.
+
+Example 2:
+
+Input: n = 10, minProfit = 5, group = [2,3,5], profit = [6,7,8]
+Output: 7
+Explanation: To make a profit of at least 5, the group could commit any crimes, as long as they commit one.
+There are 7 possible schemes: (0), (1), (2), (0,1), (0,2), (1,2), and (0,1,2).
+
+Constraints:
+
+	1 <= n <= 100
+	0 <= minProfit <= 100
+	1 <= group.length <= 100
+	1 <= group[i] <= 100
+	profit.length == group.length
+	0 <= profit[i] <= 100
+*/
+export function profitableSchemes(
+    n: number,
+    minProfit: number,
+    group: number[],
+    profit: number[]
+): number {
+    const MOD = Math.pow(10, 9) + 7;
+
+    const dp: number[][] = Array.from({ length: minProfit + 1 }, () =>
+        Array(n + 1).fill(0)
+    );
+    for (let k = 0; k <= n; k++) {
+        dp[0][k] = 1;
+    }
+
+    for (let i = group.length - 1; i >= 0; i--) {
+        for (let j = minProfit; j >= 0; j--) {
+            const tmp = Math.max(0, j - profit[i]);
+            for (let k = n; k >= group[i]; k--) {
+                dp[j][k] = (dp[j][k] + dp[tmp][k - group[i]]) % MOD;
+            }
+        }
+    }
+
+    return dp[minProfit][n];
 }
