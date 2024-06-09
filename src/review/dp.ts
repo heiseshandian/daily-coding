a6ac28861c7ca37cf147b6b689ad26b0f40433ed
baymax <476803711@qq.com>
2024-06-09 22:44:35 +0800

    feat: 每日一题

--- src/review/dp.ts
+++ src/review/dp.ts
@@ -305,3 +305,75 @@ export function profitableSchemes(
 
     return dp[minProfit][n];
 }
+
+/*
+https://leetcode.com/problems/knight-probability-in-chessboard/description/
+688. Knight Probability in Chessboard
+On an n x n chessboard, a knight starts at the cell (row, column) and attempts to make exactly k moves. The rows and columns are 0-indexed, so the top-left cell is (0, 0), and the bottom-right cell is (n - 1, n - 1).
+
+A chess knight has eight possible moves it can make, as illustrated below. Each move is two cells in a cardinal direction, then one cell in an orthogonal direction.
+
+Each time the knight is to move, it chooses one of eight possible moves uniformly at random (even if the piece would go off the chessboard) and moves there.
+
+The knight continues moving until it has made exactly k moves or has moved off the chessboard.
+
+Return the probability that the knight remains on the board after it has stopped moving.
+
+Example 1:
+
+Input: n = 3, k = 2, row = 0, column = 0
+Output: 0.06250
+Explanation: There are two moves (to (1,2), (2,1)) that will keep the knight on the board.
+From each of those positions, there are also two moves that will keep the knight on the board.
+The total probability the knight stays on the board is 0.0625.
+
+Example 2:
+
+Input: n = 1, k = 0, row = 0, column = 0
+Output: 1.00000
+
+Constraints:
+
+	1 <= n <= 25
+	0 <= k <= 100
+	0 <= row, column <= n - 1
+*/
+export function knightProbability(
+    n: number,
+    k: number,
+    row: number,
+    column: number
+): number {
+    const dirs: number[][] = [
+        [1, 2],
+        [2, 1],
+        [2, -1],
+        [1, -2],
+        [-1, -2],
+        [-2, -1],
+        [-2, 1],
+        [-1, 2],
+    ];
+    const dp: number[][][] = Array.from({ length: k + 1 }, () =>
+        Array.from({ length: n }, () => Array(n).fill(1))
+    );
+
+    for (let i = 1; i <= k; i++) {
+        for (let r = 0; r < n; r++) {
+            for (let c = 0; c < n; c++) {
+                let p = 0;
+                dirs.forEach(([x, y]) => {
+                    const nextR = r + x;
+                    const nextC = c + y;
+                    if (nextR >= 0 && nextR < n && nextC >= 0 && nextC < n) {
+                        p += dp[i][nextR][nextC];
+                    }
+                });
+
+                dp[i][r][c] = p / 8;
+            }
+        }
+    }
+
+    return dp[k][row][column];
+}
