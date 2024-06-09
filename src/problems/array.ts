7fe5ca58392db1ccc507e0b98a41101fb51ae0b5
baymax <476803711@qq.com>
2024-06-09 14:09:00 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -5886,3 +5886,52 @@ export function checkSubarraySum(nums: number[], k: number): boolean {
 
     return false;
 }
+
+/*
+https://leetcode.com/problems/subarray-sums-divisible-by-k/description/
+974. Subarray Sums Divisible by K
+Given an integer array nums and an integer k, return the number of non-empty subarrays that have a sum divisible by k.
+
+A subarray is a contiguous part of an array.
+
+Example 1:
+
+Input: nums = [4,5,0,-2,-3,1], k = 5
+Output: 7
+Explanation: There are 7 subarrays with a sum divisible by k = 5:
+[4, 5, 0, -2, -3, 1], [5], [5, 0], [5, 0, -2, -3], [0], [0, -2, -3], [-2, -3]
+
+Example 2:
+
+Input: nums = [5], k = 9
+Output: 0
+
+Constraints:
+
+	1 <= nums.length <= 10^4
+	-10^4 <= nums[i] <= 10^4
+	2 <= k <= 10^4
+*/
+export function subarraysDivByK(nums: number[], k: number): number {
+    const times = new Map<number, number>();
+    times.set(0, 1);
+
+    let sum = 0;
+    let tmp = 0;
+    nums.forEach((v) => {
+        sum += v;
+        tmp = sum % k;
+        if (tmp < 0) {
+            tmp += k;
+        }
+
+        times.set(tmp, (times.get(tmp) ?? 0) + 1);
+    });
+
+    let count = 0;
+    for (const t of times.values()) {
+        count += (t * (t - 1)) / 2;
+    }
+
+    return count;
+}
