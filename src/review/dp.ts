f8b626a3975a1e0a26f48a11ceb818777574f270
baymax <476803711@qq.com>
2024-06-10 21:49:04 +0800

    feat: 每日一题

--- src/review/dp.ts
+++ src/review/dp.ts
@@ -1,4 +1,3 @@
-import { cache } from '../design-pattern/proxy';
 /*
 https://leetcode.com/problems/minimum-path-sum/description/
 64. Minimum Path Sum
@@ -459,4 +458,57 @@ export function rob(nums: number[]): number {
     }
 
     return dp[n - 1];
+}
+
+/*
+https://leetcode.com/problems/maximum-sum-circular-subarray/description/
+918. Maximum Sum Circular Subarray
+Given a circular integer array nums of length n, return the maximum possible sum of a non-empty subarray of nums.
+
+A circular array means the end of the array connects to the beginning of the array. Formally, 
+the next element of nums[i] is nums[(i + 1) % n] and the previous element of nums[i] is nums[(i - 1 + n) % n].
+
+A subarray may only include each element of the fixed buffer nums at most once. Formally, for a subarray nums[i], 
+nums[i + 1], ..., nums[j], there does not exist i <= k1, k2 <= j with k1 % n == k2 % n.
+
+Example 1:
+
+Input: nums = [1,-2,3,-2]
+Output: 3
+Explanation: Subarray [3] has maximum sum 3.
+
+Example 2:
+
+Input: nums = [5,-3,5]
+Output: 10
+Explanation: Subarray [5,5] has maximum sum 5 + 5 = 10.
+
+Example 3:
+
+Input: nums = [-3,-2,-3]
+Output: -2
+Explanation: Subarray [-2] has maximum sum -2.
+
+Constraints:
+
+	n == nums.length
+	1 <= n <= 10^4
+	-3 * 10^4 <= nums[i] <= 10^4
+*/
+export function maxSubarraySumCircular(nums: number[]): number {
+    let sum = nums[0];
+    let prevMax = nums[0];
+    let prevMin = Math.min(0, nums[0]);
+    let max = prevMax;
+    let min = prevMin;
+    for (let i = 1; i < nums.length; i++) {
+        sum += nums[i];
+
+        prevMax = Math.max(prevMax + nums[i], nums[i]);
+        prevMin = Math.min(prevMin + nums[i], nums[i]);
+        max = Math.max(max, prevMax);
+        min = Math.min(min, prevMin);
+    }
+
+    return sum === min ? max : Math.max(max, sum - min);
 }
