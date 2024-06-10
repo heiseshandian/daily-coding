be256b51490d318472eca82fff172013f42b2ffb
baymax <476803711@qq.com>
2024-06-10 22:18:31 +0800

    feat: 每日一题

--- src/review/dp.ts
+++ src/review/dp.ts
@@ -512,3 +512,73 @@ export function maxSubarraySumCircular(nums: number[]): number {
 
     return sum === min ? max : Math.max(max, sum - min);
 }
+
+/*
+https://leetcode.com/problems/house-robber-iv/description/
+2560. House Robber IV
+There are several consecutive houses along a street, each of which has some money inside. There is also a robber, 
+who wants to steal money from the homes, but he refuses to steal from adjacent homes.
+
+The capability of the robber is the maximum amount of money he steals from one house of all the houses he robbed.
+
+You are given an integer array nums representing how much money is stashed in each house. More formally, the ith house from the left has nums[i] dollars.
+
+You are also given an integer k, representing the minimum number of houses the robber will steal from. It is always possible to steal at least k houses.
+
+Return the minimum capability of the robber out of all the possible ways to steal at least k houses.
+
+Example 1:
+
+Input: nums = [2,3,5,9], k = 2
+Output: 5
+Explanation: 
+There are three ways to rob at least 2 houses:
+- Rob the houses at indices 0 and 2. Capability is max(nums[0], nums[2]) = 5.
+- Rob the houses at indices 0 and 3. Capability is max(nums[0], nums[3]) = 9.
+- Rob the houses at indices 1 and 3. Capability is max(nums[1], nums[3]) = 9.
+Therefore, we return min(5, 9, 9) = 5.
+
+Example 2:
+
+Input: nums = [2,7,9,3,1], k = 2
+Output: 2
+Explanation: There are 7 ways to rob the houses. The way which leads to minimum capability is to rob the house at index 0 and 4.
+Return max(nums[0], nums[4]) = 2.
+
+Constraints:
+
+	1 <= nums.length <= 10^5
+	1 <= nums[i] <= 10^9
+	1 <= k <= (nums.length + 1)/2
+*/
+export function minCapability(nums: number[], k: number): number {
+    const canSteal = (m: number): boolean => {
+        let sum = 0;
+        for (let i = 0; i < nums.length; i++) {
+            if (nums[i] <= m) {
+                sum++;
+                i++;
+            }
+            if (sum >= k) {
+                return true;
+            }
+        }
+
+        return sum >= k;
+    };
+
+    let l = 0;
+    let r = Math.max(...nums);
+    let min = r;
+    while (l <= r) {
+        const m = l + ((r - l) >> 1);
+        if (canSteal(m)) {
+            r = m - 1;
+            min = m;
+        } else {
+            l = m + 1;
+        }
+    }
+
+    return min;
+}
