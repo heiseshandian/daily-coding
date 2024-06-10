ab705a3b7a2653d3e2fea49d824db77aeeb381a7
baymax <476803711@qq.com>
2024-06-10 13:42:36 +0800

    feat: 每日一题

--- src/problems/greedy.ts
+++ src/problems/greedy.ts
@@ -886,3 +886,70 @@ export function isNStraightHand(hand: number[], groupSize: number): boolean {
 
     return true;
 }
+
+/*
+https://leetcode.com/problems/minimize-the-maximum-difference-of-pairs/description/
+2616. Minimize the Maximum Difference of Pairs
+You are given a 0-indexed integer array nums and an integer p. Find p pairs of indices of nums such that the maximum difference amongst all the pairs is minimized. Also, ensure no index appears more than once amongst the p pairs.
+
+Note that for a pair of elements at the index i and j, the difference of this pair is |nums[i] - nums[j]|, where |x| represents the absolute value of x.
+
+Return the minimum maximum difference among all p pairs. We define the maximum of an empty set to be zero.
+
+Example 1:
+
+Input: nums = [10,1,2,7,1,3], p = 2
+Output: 1
+Explanation: The first pair is formed from the indices 1 and 4, and the second pair is formed from the indices 2 and 5. 
+The maximum difference is max(|nums[1] - nums[4]|, |nums[2] - nums[5]|) = max(0, 1) = 1. Therefore, we return 1.
+
+Example 2:
+
+Input: nums = [4,2,1,2], p = 1
+Output: 0
+Explanation: Let the indices 1 and 3 form a pair. The difference of that pair is |2 - 2| = 0, which is the minimum we can attain.
+
+Constraints:
+
+	1 <= nums.length <= 10^5
+	0 <= nums[i] <= 10^9
+	0 <= p <= (nums.length)/2
+*/
+export function minimizeMax(nums: number[], p: number): number {
+    if (p === 0) {
+        return 0;
+    }
+
+    const n = nums.length;
+    nums.sort((a, b) => a - b);
+
+    const checkDiff = (d: number): boolean => {
+        let i = 0;
+        let count = 0;
+        while (i < n - 1 && count < p) {
+            if (nums[i + 1] - nums[i] <= d) {
+                count++;
+                i += 2;
+            } else {
+                i++;
+            }
+        }
+
+        return count >= p;
+    };
+
+    let l = 0;
+    let r = nums[n - 1] - nums[0];
+    let diff = 0;
+    while (l <= r) {
+        const m = l + ((r - l) >> 1);
+        if (checkDiff(m)) {
+            diff = m;
+            r = m - 1;
+        } else {
+            l = m + 1;
+        }
+    }
+
+    return diff;
+}
