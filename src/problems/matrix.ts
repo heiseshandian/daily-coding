db72f22dbd8ac6de1d2c6ab79234227def48435a
baymax <476803711@qq.com>
2024-06-11 22:00:10 +0800

    feat: 每日一题

--- src/problems/matrix.ts
+++ src/problems/matrix.ts
@@ -1,4 +1,5 @@
 import { cache } from '../design-pattern/proxy';
+import { UnionFind } from '../algorithm/union-find';
 // 锯齿形打印矩阵
 export function zigzagMatrix(matrix: number[][]) {
     if (!matrix || matrix.length === 0) {
@@ -1566,4 +1567,51 @@ export function maximumSafenessFactor(grid: number[][]): number {
     }
 
     return max;
+}
+
+/*
+https://leetcode.com/problems/number-of-provinces/
+547. Number of Provinces
+There are n cities. Some of them are connected, while some are not. If city a is connected directly with city b, and city b is connected directly with city c, then city a is connected indirectly with city c.
+
+A province is a group of directly or indirectly connected cities and no other cities outside of the group.
+
+You are given an n x n matrix isConnected where isConnected[i][j] = 1 if the ith city and the jth city are directly connected, and isConnected[i][j] = 0 otherwise.
+
+Return the total number of provinces.
+
+Example 1:
+
+Input: isConnected = [[1,1,0],[1,1,0],[0,0,1]]
+Output: 2
+
+Example 2:
+
+Input: isConnected = [[1,0,0],[0,1,0],[0,0,1]]
+Output: 3
+
+Constraints:
+
+	1 <= n <= 200
+	n == isConnected.length
+	n == isConnected[i].length
+	isConnected[i][j] is 1 or 0.
+	isConnected[i][i] == 1
+	isConnected[i][j] == isConnected[j][i]
+*/
+export function findCircleNum(isConnected: number[][]): number {
+    const n = isConnected.length;
+    const unionFind = new UnionFind(n);
+    let province = n;
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (isConnected[i][j] && !unionFind.isSameSet(i, j)) {
+                unionFind.union(i, j);
+                province--;
+            }
+        }
+    }
+
+    return province;
 }
