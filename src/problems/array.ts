6de935c6d4972889da72a2729a52749371b4778a
baymax <476803711@qq.com>
2024-06-12 23:14:32 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -5984,3 +5984,46 @@ export function nextGreaterElement(nums1: number[], nums2: number[]): number[] {
         return -1;
     });
 }
+
+/*
+https://leetcode.com/problems/sort-colors/description/
+75. Sort Colors
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function.
+
+Example 1:
+
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+
+Example 2:
+
+Input: nums = [2,0,1]
+Output: [0,1,2]
+
+Constraints:
+
+	n == nums.length
+	1 <= n <= 300
+	nums[i] is either 0, 1, or 2.
+
+Follow up: Could you come up with a one-pass algorithm using only constant extra space?
+*/
+export function sortColors(nums: number[]): void {
+    const n = nums.length;
+    let left = -1;
+    let right = n;
+    let i = 0;
+    while (i < right) {
+        if (nums[i] === 1) {
+            i++;
+        } else if (nums[i] === 0) {
+            swap(nums, i++, ++left);
+        } else {
+            swap(nums, i, --right);
+        }
+    }
+}
