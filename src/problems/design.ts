b74814fe0b8410dab470ca5c5b8f7ca1b6467eef
baymax <476803711@qq.com>
2024-06-13 09:12:03 +0800

    feat: 每日一题

--- src/problems/design.ts
+++ src/problems/design.ts
@@ -1589,3 +1589,36 @@ export function checkIfInstanceOf2(obj: any, classFunction: any) {
 
     return false;
 }
+
+/*
+https://leetcode.com/problems/array-prototype-last/description/
+2619. Array Prototype Last
+Write code that enhances all arrays such that you can call the array.last() method on any array and it will return the last element. If there are no elements in the array, it should return -1.
+
+You may assume the array is the output of JSON.parse.
+
+Example 1:
+
+Input: nums = [null, {}, 3]
+Output: 3
+Explanation: Calling nums.last() should return the last element: 3.
+
+Example 2:
+
+Input: nums = []
+Output: -1
+Explanation: Because there are no elements, return -1.
+
+Constraints:
+
+	arr is a valid JSON array
+	0 <= arr.length <= 1000
+*/
+export interface Array<T> {
+    last(): T | -1;
+}
+
+// @ts-expect-error
+Array.prototype.last = function () {
+    return this.length > 0 ? this[this.length - 1] : -1;
+};
