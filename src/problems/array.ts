c96e95a32d3d9c62eb9181d9149c83e04a5046fe
baymax <476803711@qq.com>
2024-06-20 09:48:17 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -6374,3 +6374,65 @@ export function minDays(bloomDay: number[], m: number, k: number): number {
 
     return min;
 }
+
+/*
+https://leetcode.com/problems/magnetic-force-between-two-balls/description/?envType=daily-question&envId=2024-06-20
+1552. Magnetic Force Between Two Balls
+In the universe Earth C-137, Rick discovered a special form of magnetic force between two balls if they are put in his new invented basket. Rick has n empty baskets, the ith basket is at position[i], Morty has m balls and needs to distribute the balls into the baskets such that the minimum magnetic force between any two balls is maximum.
+
+Rick stated that magnetic force between two different balls at positions x and y is |x - y|.
+
+Given the integer array position and the integer m. Return the required force.
+
+Example 1:
+
+Input: position = [1,2,3,4,7], m = 3
+Output: 3
+Explanation: Distributing the 3 balls into baskets 1, 4 and 7 will make the magnetic force between ball pairs [3, 3, 6]. The minimum magnetic force is 3. We cannot achieve a larger minimum magnetic force than 3.
+
+Example 2:
+
+Input: position = [5,4,3,2,1,1000000000], m = 2
+Output: 999999999
+Explanation: We can use baskets 1 and 1000000000.
+
+Constraints:
+
+	n == position.length
+	2 <= n <= 10^5
+	1 <= position[i] <= 10^9
+	All integers in position are distinct.
+	2 <= m <= position.length
+*/
+export function maxDistance(position: number[], m: number): number {
+    const n = position.length;
+    position.sort((a, b) => a - b);
+
+    const canPlace = (distance: number): boolean => {
+        let count = 1;
+        let start = 0;
+        for (let i = 0; i < n && count < m; i++) {
+            if (position[i] - position[start] >= distance) {
+                count++;
+                start = i;
+            }
+        }
+
+        return count >= m;
+    };
+
+    let l = 0;
+    let r = position[n - 1] - position[0];
+    let max = 0;
+    while (l <= r) {
+        const m = l + ((r - l) >> 1);
+        if (canPlace(m)) {
+            max = m;
+            l = m + 1;
+        } else {
+            r = m - 1;
+        }
+    }
+
+    return max;
+}
