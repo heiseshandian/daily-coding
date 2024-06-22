2f1484a96e603cb2e09b74d0cef081d0644042d8
baymax <476803711@qq.com>
2024-06-22 10:10:28 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -6436,3 +6436,52 @@ export function maxDistance(position: number[], m: number): number {
 
     return max;
 }
+
+/*
+https://leetcode.com/problems/count-number-of-nice-subarrays/description/
+1248. Count Number of Nice Subarrays
+Given an array of integers nums and an integer k. A continuous subarray is called nice if there are k odd numbers on it.
+
+Return the number of nice sub-arrays.
+
+Example 1:
+
+Input: nums = [1,1,2,1,1], k = 3
+Output: 2
+Explanation: The only sub-arrays with 3 odd numbers are [1,1,2,1] and [1,2,1,1].
+
+Example 2:
+
+Input: nums = [2,4,6], k = 1
+Output: 0
+Explanation: There are no odd numbers in the array.
+
+Example 3:
+
+Input: nums = [2,2,2,1,2,2,1,2,2,2], k = 2
+Output: 16
+
+Constraints:
+
+	1 <= nums.length <= 50000
+	1 <= nums[i] <= 10^5
+	1 <= k <= nums.length
+*/
+export function numberOfSubarrays(nums: number[], k: number): number {
+    const freqMap = new Map<number, number>();
+    freqMap.set(0, 1);
+
+    let sum = 0;
+    let count = 0;
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i] & 1;
+        const target = sum - k;
+        if (freqMap.has(target)) {
+            count += freqMap.get(target)!;
+        }
+
+        freqMap.set(sum, (freqMap.get(sum) ?? 0) + 1);
+    }
+
+    return count;
+}
