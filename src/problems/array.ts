7bb86bdf3e15967d15164e4107aa0646e57900d8
baymax <476803711@qq.com>
2024-06-22 15:55:11 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -6533,3 +6533,52 @@ export function lastStoneWeight(stones: number[]): number {
 
     return heap.pop() || 0;
 }
+
+/*
+https://leetcode.com/problems/last-stone-weight-ii/description/
+1049. Last Stone Weight II
+You are given an array of integers stones where stones[i] is the weight of the ith stone.
+
+We are playing a game with the stones. On each turn, we choose any two stones and smash them together. Suppose the stones have weights x and y with x <= y. The result of this smash is:
+
+	If x == y, both stones are destroyed, and
+	If x != y, the stone of weight x is destroyed, and the stone of weight y has new weight y - x.
+
+At the end of the game, there is at most one stone left.
+
+Return the smallest possible weight of the left stone. If there are no stones left, return 0.
+
+Example 1:
+
+Input: stones = [2,7,4,1,8,1]
+Output: 1
+Explanation:
+We can combine 2 and 4 to get 2, so the array converts to [2,7,1,8,1] then,
+we can combine 7 and 8 to get 1, so the array converts to [2,1,1,1] then,
+we can combine 2 and 1 to get 1, so the array converts to [1,1,1] then,
+we can combine 1 and 1 to get 0, so the array converts to [1], then that's the optimal value.
+
+Example 2:
+
+Input: stones = [31,26,33,21,40]
+Output: 5
+
+Constraints:
+
+	1 <= stones.length <= 30
+	1 <= stones[i] <= 100
+*/
+export function lastStoneWeightII(stones: number[]): number {
+    const sum = stones.reduce((s, c) => s + c, 0);
+    const half = sum >> 1;
+    const n = stones.length;
+    const dp = Array(half + 1).fill(0);
+
+    for (let i = 1; i <= n; i++) {
+        for (let j = half; j >= stones[i - 1]; j--) {
+            dp[j] = Math.max(dp[j], dp[j - stones[i - 1]] + stones[i - 1]);
+        }
+    }
+
+    return sum - (dp[half] << 1);
+}
