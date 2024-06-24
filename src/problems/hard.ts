92860c089d9520ca34d0b392bac38072c90daebc
baymax <476803711@qq.com>
2024-06-24 09:49:24 +0800

    feat: 每日一题

--- src/problems/hard.ts
+++ src/problems/hard.ts
@@ -1900,3 +1900,75 @@ export function maxEnvelopes(envelopes: number[][]): number {
 
     return max;
 }
+
+/*
+https://leetcode.com/problems/minimum-number-of-k-consecutive-bit-flips/description/
+995. Minimum Number of K Consecutive Bit Flips
+You are given a binary array nums and an integer k.
+
+A k-bit flip is choosing a subarray of length k from nums and simultaneously changing every 0 in the subarray to 1, and every 1 in the subarray to 0.
+
+Return the minimum number of k-bit flips required so that there is no 0 in the array. If it is not possible, return -1.
+
+A subarray is a contiguous part of an array.
+
+Example 1:
+
+Input: nums = [0,1,0], k = 1
+Output: 2
+Explanation: Flip nums[0], then flip nums[2].
+
+Example 2:
+
+Input: nums = [1,1,0], k = 2
+Output: -1
+Explanation: No matter how we flip subarrays of size 2, we cannot make the array become [1,1,1].
+
+Example 3:
+
+Input: nums = [0,0,0,1,0,1,1,0], k = 3
+Output: 3
+Explanation: 
+Flip nums[0],nums[1],nums[2]: nums becomes [1,1,1,1,0,1,1,0]
+Flip nums[4],nums[5],nums[6]: nums becomes [1,1,1,1,1,0,0,0]
+Flip nums[5],nums[6],nums[7]: nums becomes [1,1,1,1,1,1,1,1]
+
+Constraints:
+
+	1 <= nums.length <= 10^5
+	1 <= k <= nums.length
+
+There is only one way to filp A[0],
+and A[0] will tell us if we need to filp the range A[0] ~ A[K -1].
+So we start from the leftmost one by one using a greedy idea to solve this problem.
+
+Create a new array isFlipped[n].
+isFlipped[i] = 1 if we flip K consecutive bits starting at A[i].
+
+We maintain a variable flipped and flipped = 1 if the current bit is flipped.
+
+If flipped = 0 and A[i] = 0, we need to flip at A[i].
+If flipped = 1 and A[i] = 1, we need to flip at A[i].
+*/
+export function minKBitFlips(nums: number[], k: number): number {
+    const n = nums.length;
+    let flipped = 0;
+    const isFlipped = Array(n).fill(0);
+
+    let count = 0;
+    for (let i = 0; i < n; i++) {
+        if (i >= k) {
+            flipped ^= isFlipped[i - k];
+        }
+        if (flipped === nums[i]) {
+            if (i + k > n) {
+                return -1;
+            }
+            isFlipped[i] = 1;
+            flipped ^= 1;
+            count++;
+        }
+    }
+
+    return count;
+}
