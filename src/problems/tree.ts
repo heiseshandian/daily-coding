b8edba0bb99af1de314f71143e80bbe04da3538c
baymax <476803711@qq.com>
2024-06-25 09:28:11 +0800

    feat: 每日一题

--- src/problems/tree.ts
+++ src/problems/tree.ts
@@ -2540,3 +2540,59 @@ export function findMinHeightTrees(n: number, edges: number[][]): number[] {
 
     return leafs;
 }
+
+/*
+https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree/description/
+1038. Binary Search Tree to Greater Sum Tree
+Given the root of a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed to the original key plus the sum of all keys greater than the original key in BST.
+
+As a reminder, a binary search tree is a tree that satisfies these constraints:
+
+	The left subtree of a node contains only nodes with keys less than the node's key.
+	The right subtree of a node contains only nodes with keys greater than the node's key.
+	Both the left and right subtrees must also be binary search trees.
+
+Example 1:
+
+Input: root = [4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]
+Output: [30,36,21,36,35,26,15,null,null,null,33,null,null,null,8]
+
+Example 2:
+
+Input: root = [0,null,1]
+Output: [1,null,1]
+
+Constraints:
+
+	The number of nodes in the tree is in the range [1, 100].
+	0 <= Node.val <= 100
+	All the values in the tree are unique.
+
+Note: This question is the same as 538: https://leetcode.com/problems/convert-bst-to-greater-tree/
+*/
+export function bstToGst(root: TreeNode | null): TreeNode | null {
+    const nodes: TreeNode[] = [];
+    const pre = (node: TreeNode | null) => {
+        if (!node) {
+            return;
+        }
+
+        pre(node.left);
+        nodes.push(node);
+        pre(node.right);
+    };
+    pre(root);
+
+    const n = nodes.length;
+    const suffix: number[] = Array(n);
+    suffix[n - 1] = nodes[n - 1].val;
+    for (let i = n - 2; i >= 0; i--) {
+        suffix[i] = suffix[i + 1] + nodes[i].val;
+    }
+
+    for (let i = 0; i < n - 1; i++) {
+        nodes[i].val += suffix[i + 1];
+    }
+
+    return root;
+}
