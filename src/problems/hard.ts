184419f63b7a9ff84d2406cd4287d88f7a91545b
baymax <476803711@qq.com>
2024-06-25 23:30:28 +0800

    feat: 每日一题

--- src/problems/hard.ts
+++ src/problems/hard.ts
@@ -1972,3 +1972,55 @@ export function minKBitFlips(nums: number[], k: number): number {
 
     return count;
 }
+
+/*
+https://leetcode.com/problems/minimum-falling-path-sum-ii/description/?envType=daily-question&envId=2024-04-26
+1289. Minimum Falling Path Sum II
+Given an n x n integer matrix grid, return the minimum sum of a falling path with non-zero shifts.
+
+A falling path with non-zero shifts is a choice of exactly one element from each row of grid such that no two elements chosen in adjacent rows are in the same column.
+
+Example 1:
+
+Input: grid = [[1,2,3],[4,5,6],[7,8,9]]
+Output: 13
+Explanation: 
+The possible falling paths are:
+[1,5,9], [1,5,7], [1,6,7], [1,6,8],
+[2,4,8], [2,4,9], [2,6,7], [2,6,8],
+[3,4,8], [3,4,9], [3,5,7], [3,5,9]
+The falling path with the smallest sum is [1,5,7], so the answer is 13.
+
+Example 2:
+
+Input: grid = [[7]]
+Output: 7
+
+Constraints:
+
+	n == grid.length == grid[i].length
+	1 <= n <= 200
+	-99 <= grid[i][j] <= 99
+*/
+export function minFallingPathSum(grid: number[][]): number {
+    const n = grid.length;
+    const dp: number[][] = Array.from({ length: n }, () => Array(n));
+    for (let j = 0; j < n; j++) {
+        dp[0][j] = grid[0][j];
+    }
+
+    for (let i = 1; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            let min = Infinity;
+            for (let k = 0; k < n; k++) {
+                if (k !== j) {
+                    min = Math.min(min, dp[i - 1][k]);
+                }
+            }
+
+            dp[i][j] = min + grid[i][j];
+        }
+    }
+
+    return Math.min(...dp[n - 1]);
+}
