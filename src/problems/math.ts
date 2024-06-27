1efed61d9d920b7b63c666ff2a6886d9aec02c45
baymax <476803711@qq.com>
2024-06-27 22:56:30 +0800

    feat: 每日一题

--- src/problems/math.ts
+++ src/problems/math.ts
@@ -682,3 +682,33 @@ export function getRow(rowIndex: number): number[] {
 
     return result;
 }
+
+/*
+https://leetcode.com/problems/count-numbers-with-unique-digits/description/
+357. Count Numbers with Unique Digits
+Given an integer n, return the count of all numbers with unique digits, x, where 0 <= x < 10n.
+
+Example 1:
+
+Input: n = 2
+Output: 91
+Explanation: The answer should be the total numbers in the range of 0 ≤ x < 100, excluding 11,22,33,44,55,66,77,88,99
+
+Example 2:
+
+Input: n = 0
+Output: 1
+
+Constraints:
+
+	0 <= n <= 8
+*/
+export function countNumbersWithUniqueDigits(n: number): number {
+    let prev = 1;
+    const tmp: number[] = [9, 9, 8, 7, 6, 5, 4, 3];
+    for (let i = 1; i <= n; i++) {
+        prev = tmp.slice(0, i).reduce((acc, cur) => acc * cur, 1) + prev;
+    }
+
+    return prev;
+}
