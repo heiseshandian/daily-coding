0bf6c384fb812b513b9769296f49fde937d8a1e5
baymax <476803711@qq.com>
2024-06-30 00:40:56 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -6636,3 +6636,49 @@ export function makeConnected(n: number, connections: number[][]): number {
 
     return set - 1;
 }
+
+/*
+https://leetcode.com/problems/intersection-of-multiple-arrays/description/
+2248. Intersection of Multiple Arrays
+Given a 2D integer array nums where nums[i] is a non-empty array of distinct positive integers, return the list of integers that are present in each array of nums sorted in ascending order.
+
+Example 1:
+
+Input: nums = [[3,1,2,4,5],[1,2,3,4],[3,4,5,6]]
+Output: [3,4]
+Explanation: 
+The only integers present in each of nums[0] = [3,1,2,4,5], nums[1] = [1,2,3,4], and nums[2] = [3,4,5,6] are 3 and 4, so we return [3,4].
+
+Example 2:
+
+Input: nums = [[1,2,3],[4,5,6]]
+Output: []
+Explanation: 
+There does not exist any integer present both in nums[0] and nums[1], so we return an empty list [].
+
+Constraints:
+
+	1 <= nums.length <= 1000
+	1 <= sum(nums[i].length) <= 1000
+	1 <= nums[i][j] <= 1000
+	All the values of nums[i] are unique.
+*/
+export function intersection(nums: number[][]): number[] {
+    const times = nums[0].reduce((acc, v) => {
+        acc[v] = 1;
+        return acc;
+    }, {});
+
+    const n = nums.length;
+    for (let i = 1; i < n; i++) {
+        nums[i].forEach((v) => {
+            if (times[v]) {
+                times[v]++;
+            }
+        });
+    }
+
+    return Object.keys(times)
+        .filter((k) => times[k] === n)
+        .map((k) => +k);
+}
