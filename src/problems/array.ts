b5414c62dbfc044fad3fc9ea6d629b556f4f17d5
baymax <476803711@qq.com>
2024-06-29 11:00:11 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -3,6 +3,7 @@ import { GenericHeap } from '../algorithm/generic-heap';
 import { getClosestMaxOrEqual, isEven, swap } from '../common';
 import { UnionSet } from '../algorithm/union-set';
 import { countBits } from '../common/bit';
+import { UnionFind } from '../algorithm/union-find';
 /* 
 https://leetcode.com/problems/find-the-maximum-number-of-marked-indices/description/
 
@@ -6581,4 +6582,57 @@ export function lastStoneWeightII(stones: number[]): number {
     }
 
     return sum - (dp[half] << 1);
+}
+
+/*
+https://leetcode.com/problems/number-of-operations-to-make-network-connected/description/
+1319. Number of Operations to Make Network Connected
+There are n computers numbered from 0 to n - 1 connected by ethernet cables connections forming a network where connections[i] = [ai, bi] represents a connection between computers ai and bi. Any computer can reach any other computer directly or indirectly through the network.
+
+You are given an initial computer network connections. You can extract certain cables between two directly connected computers, and place them between any pair of disconnected computers to make them directly connected.
+
+Return the minimum number of times you need to do this in order to make all the computers connected. If it is not possible, return -1.
+
+Example 1:
+
+Input: n = 4, connections = [[0,1],[0,2],[1,2]]
+Output: 1
+Explanation: Remove cable between computer 1 and 2 and place between computers 1 and 3.
+
+Example 2:
+
+Input: n = 6, connections = [[0,1],[0,2],[0,3],[1,2],[1,3]]
+Output: 2
+
+Example 3:
+
+Input: n = 6, connections = [[0,1],[0,2],[0,3],[1,2]]
+Output: -1
+Explanation: There are not enough cables.
+
+Constraints:
+
+	1 <= n <= 10^5
+	1 <= connections.length <= min(n * (n - 1) / 2, 105)
+	connections[i].length == 2
+	0 <= ai, bi < n
+	ai != bi
+	There are no repeated connections.
+	No two computers are connected by more than one cable.
+*/
+export function makeConnected(n: number, connections: number[][]): number {
+    if (connections.length < n - 1) {
+        return -1;
+    }
+
+    const unionFind = new UnionFind(n);
+    let set = n;
+    connections.forEach(([from, to]) => {
+        if (!unionFind.isSameSet(from, to)) {
+            unionFind.union(from, to);
+            set--;
+        }
+    });
+
+    return set - 1;
 }
