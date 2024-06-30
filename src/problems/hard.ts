644141d93ec0abfb72335c5b1897f4cc1152279c
baymax <476803711@qq.com>
2024-06-30 10:03:16 +0800

    feat: 每日一题

--- src/problems/hard.ts
+++ src/problems/hard.ts
@@ -2030,3 +2030,87 @@ export function minFallingPathSum(grid: number[][]): number {
 
     return Math.min(...dp);
 }
+
+/*
+https://leetcode.com/problems/remove-max-number-of-edges-to-keep-graph-fully-traversable/description/?envType=daily-question&envId=2024-06-30
+1579. Remove Max Number of Edges to Keep Graph Fully Traversable
+Alice and Bob have an undirected graph of n nodes and three types of edges:
+
+	Type 1: Can be traversed by Alice only.
+	Type 2: Can be traversed by Bob only.
+	Type 3: Can be traversed by both Alice and Bob.
+
+Given an array edges where edges[i] = [typei, ui, vi] represents a bidirectional edge of type typei between nodes ui and vi, find the maximum number of edges you can remove so that after removing the edges, the graph can still be fully traversed by both Alice and Bob. The graph is fully traversed by Alice and Bob if starting from any node, they can reach all other nodes.
+
+Return the maximum number of edges you can remove, or return -1 if Alice and Bob cannot fully traverse the graph.
+
+Example 1:
+
+Input: n = 4, edges = [[3,1,2],[3,2,3],[1,1,3],[1,2,4],[1,1,2],[2,3,4]]
+Output: 2
+Explanation: If we remove the 2 edges [1,1,2] and [1,1,3]. The graph will still be fully traversable by Alice and Bob. Removing any additional edge will not make it so. So the maximum number of edges we can remove is 2.
+
+Example 2:
+
+Input: n = 4, edges = [[3,1,2],[3,2,3],[1,1,4],[2,1,4]]
+Output: 0
+Explanation: Notice that removing any edge will not make the graph fully traversable by Alice and Bob.
+
+Example 3:
+
+Input: n = 4, edges = [[3,2,3],[1,1,2],[2,3,4]]
+Output: -1
+Explanation: In the current graph, Alice cannot reach node 4 from the other nodes. Likewise, Bob cannot reach 1. Therefore it's impossible to make the graph fully traversable.
+
+Constraints:
+
+	1 <= n <= 10^5
+	1 <= edges.length <= min(105, 3 * n * (n - 1) / 2)
+	edges[i].length == 3
+	1 <= typei <= 3
+	1 <= ui < vi <= n
+	All tuples (typei, ui, vi) are distinct.
+*/
+export function maxNumEdgesToRemove(n: number, edges: number[][]): number {
+    const alice = new UnionFind(n);
+    const bob = new UnionFind(n);
+    let set = n;
+    let max = 0;
+    for (const [type, from, to] of edges) {
+        if (type !== 3) {
+            continue;
+        }
+
+        if (alice.isSameSet(from, to)) {
+            max++;
+        } else {
+            alice.union(from, to);
+            bob.union(from, to);
+            set--;
+        }
+    }
+
+    let aliceSet = set;
+    let bobSet = set;
+    for (const [type, from, to] of edges) {
+        if (type === 1) {
+            if (alice.isSameSet(from, to)) {
+                max++;
+            } else {
+                alice.union(from, to);
+                aliceSet--;
+            }
+        }
+
+        if (type === 2) {
+            if (bob.isSameSet(from, to)) {
+                max++;
+            } else {
+                bob.union(from, to);
+                bobSet--;
+            }
+        }
+    }
+
+    return aliceSet === 1 && bobSet === 1 ? max : -1;
+}
