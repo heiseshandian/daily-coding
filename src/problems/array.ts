a799563ed192e60b6538c80acbee0bb0a1685f2d
baymax <476803711@qq.com>
2024-07-01 10:02:32 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -6682,3 +6682,41 @@ export function intersection(nums: number[][]): number[] {
         .filter((k) => times[k] === n)
         .map((k) => +k);
 }
+
+/*
+https://leetcode.com/problems/three-consecutive-odds/description/?envType=daily-question&envId=2024-07-01
+1550. Three Consecutive Odds
+Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
+
+Example 1:
+
+Input: arr = [2,6,4,1]
+Output: false
+Explanation: There are no three consecutive odds.
+
+Example 2:
+
+Input: arr = [1,2,34,3,4,5,7,23,12]
+Output: true
+Explanation: [5,7,23] are three consecutive odds.
+
+Constraints:
+
+	1 <= arr.length <= 1000
+	1 <= arr[i] <= 1000
+*/
+export function threeConsecutiveOdds(arr: number[]): boolean {
+    let odd = 0;
+    for (const v of arr) {
+        if (v & 1) {
+            odd++;
+            if (odd >= 3) {
+                return true;
+            }
+        } else {
+            odd = 0;
+        }
+    }
+
+    return false;
+}
