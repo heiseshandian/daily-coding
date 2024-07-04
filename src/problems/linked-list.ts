6cf53d319f294dbdea6a6ec8119f51312c9ce54f
baymax <476803711@qq.com>
2024-07-04 09:29:46 +0800

    feat: 每日一题

--- src/problems/linked-list.ts
+++ src/problems/linked-list.ts
@@ -926,3 +926,60 @@ function reverse(head: ListNode): ListNode {
 
     return prev!;
 }
+
+/*
+https://leetcode.com/problems/merge-nodes-in-between-zeros/description/
+2181. Merge Nodes in Between Zeros
+You are given the head of a linked list, which contains a series of integers separated by 0's. The beginning and end of the linked list will have Node.val == 0.
+
+For every two consecutive 0's, merge all the nodes lying in between them into a single node whose value is the sum of all the merged nodes. The modified list should not contain any 0's.
+
+Return the head of the modified linked list.
+
+Example 1:
+
+Input: head = [0,3,1,0,4,5,2,0]
+Output: [4,11]
+Explanation: 
+The above figure represents the given linked list. The modified list contains
+- The sum of the nodes marked in green: 3 + 1 = 4.
+- The sum of the nodes marked in red: 4 + 5 + 2 = 11.
+
+Example 2:
+
+Input: head = [0,1,0,3,0,2,2,0]
+Output: [1,3,4]
+Explanation: 
+The above figure represents the given linked list. The modified list contains
+- The sum of the nodes marked in green: 1 = 1.
+- The sum of the nodes marked in red: 3 = 3.
+- The sum of the nodes marked in yellow: 2 + 2 = 4.
+
+Constraints:
+
+	The number of nodes in the list is in the range [3, 2 * 10^5].
+	0 <= Node.val <= 1000
+	There are no two consecutive nodes with Node.val == 0.
+	The beginning and end of the linked list have Node.val == 0.
+*/
+export function mergeNodes(head: ListNode): ListNode {
+    const newHead = head.next!;
+
+    let left: ListNode = head.next!;
+    let sum = 0;
+    let cur: ListNode | null = left;
+    while (cur) {
+        if (cur.val !== 0) {
+            sum += cur.val;
+            cur = cur.next;
+        } else {
+            left.val = sum;
+            left.next = cur.next;
+            sum = 0;
+            cur = cur.next;
+            left = cur!;
+        }
+    }
+
+    return newHead;
+}
