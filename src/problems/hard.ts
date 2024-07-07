9391b0a56a0acd799aa310bb4d7cda68d261c605
baymax <476803711@qq.com>
2024-07-07 11:05:48 +0800

    feat: 每日一题

--- src/problems/hard.ts
+++ src/problems/hard.ts
@@ -2255,3 +2255,56 @@ export function sumOfDistancesInTree(n: number, edges: number[][]): number[] {
 
     return res;
 }
+
+/*
+https://leetcode.com/problems/maximum-value-of-k-coins-from-piles/description/
+2218. Maximum Value of K Coins From Piles
+There are n piles of coins on a table. Each pile consists of a positive number of coins of assorted denominations.
+
+In one move, you can choose any coin on top of any pile, remove it, and add it to your wallet.
+
+Given a list piles, where piles[i] is a list of integers denoting the composition of the ith pile from top to bottom, and a positive integer k, return the maximum total value of coins you can have in your wallet if you choose exactly k coins optimally.
+
+Example 1:
+
+Input: piles = [[1,100,3],[7,8,9]], k = 2
+Output: 101
+Explanation:
+The above diagram shows the different ways we can choose k coins.
+The maximum total we can obtain is 101.
+
+Example 2:
+
+Input: piles = [[100],[100],[100],[100],[100],[100],[1,1,1,1,1,1,700]], k = 7
+Output: 706
+Explanation:
+The maximum total can be obtained if we choose all coins from the last pile.
+
+Constraints:
+
+	n == piles.length
+	1 <= n <= 1000
+	1 <= piles[i][j] <= 10^5
+	1 <= k <= sum(piles[i].length) <= 2000
+*/
+export function maxValueOfCoins(piles: number[][], k: number): number {
+    const n = piles.length;
+    const dp: number[] = Array(k + 1).fill(0);
+
+    for (let i = 1; i <= n; i++) {
+        const pile = piles[i - 1];
+        const max = Math.min(k, pile.length);
+        const prefix: number[] = [pile[0]];
+        for (let j = 1; j < max; j++) {
+            prefix[j] = prefix[j - 1] + pile[j];
+        }
+
+        for (let j = k; j >= 0; j--) {
+            for (let l = Math.min(max - 1, j - 1); l >= 0; l--) {
+                dp[j] = Math.max(dp[j], dp[j - l - 1] + prefix[l]);
+            }
+        }
+    }
+
+    return dp[k];
+}
