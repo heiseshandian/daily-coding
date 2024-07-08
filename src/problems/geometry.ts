529338225283f90f4cfd8036e4eb250e412115e8
baymax <476803711@qq.com>
2024-07-08 12:22:02 +0800

    feat: 每日一题

--- src/problems/geometry.ts
+++ src/problems/geometry.ts
@@ -158,8 +158,13 @@ class MaxHeapWithDelete {
         let left = (i << 1) | 1;
         while (left < this.arr.length) {
             const right = left + 1;
-            let biggestIndex = right < this.arr.length && this.arr[right][0] > this.arr[left][0] ? right : left;
-            biggestIndex = this.arr[i][0] >= this.arr[biggestIndex][0] ? i : biggestIndex;
+            let biggestIndex =
+                right < this.arr.length &&
+                this.arr[right][0] > this.arr[left][0]
+                    ? right
+                    : left;
+            biggestIndex =
+                this.arr[i][0] >= this.arr[biggestIndex][0] ? i : biggestIndex;
             if (biggestIndex === i) {
                 break;
             }
@@ -176,4 +181,55 @@ class MaxHeapWithDelete {
 
         swap(this.arr, i, j);
     }
+}
+
+/*
+https://leetcode.com/problems/determine-if-a-cell-is-reachable-at-a-given-time/description/?envType=problem-list-v2&envId=o5cftq05
+2849. Determine if a Cell Is Reachable at a Given Time
+You are given four integers sx, sy, fx, fy, and a non-negative integer t.
+
+In an infinite 2D grid, you start at the cell (sx, sy). Each second, you must move to any of its adjacent cells.
+
+Return true if you can reach cell (fx, fy) after exactly t seconds, or false otherwise.
+
+A cell's adjacent cells are the 8 cells around it that share at least one corner with it. You can visit the same cell several times.
+
+Example 1:
+
+Input: sx = 2, sy = 4, fx = 7, fy = 7, t = 6
+Output: true
+Explanation: Starting at cell (2, 4), we can reach cell (7, 7) in exactly 6 seconds by going through the cells depicted in the picture above. 
+
+Example 2:
+
+Input: sx = 3, sy = 1, fx = 7, fy = 3, t = 3
+Output: false
+Explanation: Starting at cell (3, 1), it takes at least 4 seconds to reach cell (7, 3) by going through the cells depicted in the picture above. Hence, we cannot reach cell (7, 3) at the third second.
+
+Constraints:
+
+	1 <= sx, sy, fx, fy <= 10^9
+	0 <= t <= 10^9
+*/
+export function isReachableAtTime(
+    sx: number,
+    sy: number,
+    fx: number,
+    fy: number,
+    t: number
+): boolean {
+    const distanceX = Math.abs(fx - sx);
+    const distanceY = Math.abs(fy - sy);
+    const minSeconds =
+        Math.min(distanceX, distanceY) + Math.abs(distanceX - distanceY);
+    if (t < minSeconds) {
+        return false;
+    }
+
+    const rest = t - minSeconds;
+
+    if (rest & 1) {
+        return Math.max(distanceX, distanceY) > 0 || rest > 2;
+    }
+    return true;
 }
