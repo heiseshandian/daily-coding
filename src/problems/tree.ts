9b43eeceb51e9b2512453cf9b637f1c4f3c5c9c3
baymax <476803711@qq.com>
2024-07-08 10:40:48 +0800

    feat: 每日一题

--- src/problems/tree.ts
+++ src/problems/tree.ts
@@ -6,6 +6,7 @@ import {
     serializeByLevel,
 } from '../algorithm/tree';
 import { cache } from '../design-pattern/proxy';
+import { fullPermutationTestData } from './__tests__/recursion.testdata';
 /* 
 https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
 
@@ -2649,4 +2650,64 @@ export function balanceBST(root: TreeNode | null): TreeNode | null {
     };
 
     return build(0, values.length - 1);
+}
+
+/*
+https://leetcode.com/problems/count-complete-tree-nodes/description/
+222. Count Complete Tree Nodes
+Given the root of a complete binary tree, return the number of the nodes in the tree.
+
+According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+
+Design an algorithm that runs in less than O(n) time complexity.
+
+Example 1:
+
+Input: root = [1,2,3,4,5,6]
+Output: 6
+
+Example 2:
+
+Input: root = []
+Output: 0
+
+Example 3:
+
+Input: root = [1]
+Output: 1
+
+Constraints:
+
+	The number of nodes in the tree is in the range [0, 5 * 10^4].
+	0 <= Node.val <= 10^4
+	The tree is guaranteed to be complete.
+*/
+export function countNodes(root: TreeNode | null): number {
+    const l = getLeftHeight(root);
+    const r = getRightHeight(root);
+    if (l === r) {
+        return (1 << l) - 1;
+    }
+
+    return countNodes(root!.left) + countNodes(root!.right) + 1;
+}
+
+function getLeftHeight(node: TreeNode | null) {
+    let h = 0;
+    while (node) {
+        node = node.left;
+        h++;
+    }
+
+    return h;
+}
+
+function getRightHeight(node: TreeNode | null) {
+    let h = 0;
+    while (node) {
+        node = node.right;
+        h++;
+    }
+
+    return h;
 }
