008fa8015abacffd90573100cb9056f667378473
baymax <476803711@qq.com>
2024-07-09 22:32:18 +0800

    feat: 每日一题

--- src/problems/string.ts
+++ src/problems/string.ts
@@ -1,4 +1,5 @@
 import { getCharIndex, swap } from '../common/index';
+import { cache } from '../design-pattern/proxy';
 /*
 https://leetcode.com/problems/construct-k-palindrome-strings/
 1400. Construct K Palindrome Strings
@@ -2387,4 +2388,91 @@ export function longestIdealString(s: string, k: number): number {
     }
 
     return max;
+}
+
+/*
+https://leetcode.com/problems/count-number-of-texts/description/?envType=problem-list-v2&envId=o5cftq05
+2266. Count Number of Texts
+Alice is texting Bob using her phone. The mapping of digits to letters is shown in the figure below.
+
+In order to add a letter, Alice has to press the key of the corresponding digit i times, where i is the position of the letter in the key.
+
+	For example, to add the letter 's', Alice has to press '7' four times. Similarly, to add the letter 'k', Alice has to press '5' twice.
+	Note that the digits '0' and '1' do not map to any letters, so Alice does not use them.
+
+However, due to an error in transmission, Bob did not receive Alice's text message but received a string of pressed keys instead.
+
+	For example, when Alice sent the message "bob", Bob received the string "2266622".
+
+Given a string pressedKeys representing the string received by Bob, return the total number of possible text messages Alice could have sent.
+
+Since the answer may be very large, return it modulo 109 + 7.
+
+Example 1:
+
+Input: pressedKeys = "22233"
+Output: 8
+Explanation:
+The possible text messages Alice could have sent are:
+"aaadd", "abdd", "badd", "cdd", "aaae", "abe", "bae", and "ce".
+Since there are 8 possible messages, we return 8.
+
+Example 2:
+
+Input: pressedKeys = "222222222222222222222222222222222222"
+Output: 82876089
+Explanation:
+There are 2082876103 possible text messages Alice could have sent.
+Since we need to return the answer modulo 109 + 7, we return 2082876103 % (109 + 7) = 82876089.
+
+Constraints:
+
+	1 <= pressedKeys.length <= 10^5
+	pressedKeys only consists of digits from '2' - '9'.
+*/
+export function countTexts(pressedKeys: string): number {
+    const modulo = 10 ** 9 + 7;
+
+    const isAllEqual = (start: number, end: number): boolean => {
+        for (let i = start; i <= end; i++) {
+            if (pressedKeys[i] !== pressedKeys[start]) {
+                return false;
+            }
+        }
+
+        return true;
+    };
+
+    const dfs = cache((i: number): number => {
+        if (i === pressedKeys.length) {
+            return 1;
+        }
+
+        let ret = 0;
+
+        ret = (ret + dfs(i + 1)) % modulo;
+
+        if (
+            i + 1 < pressedKeys.length &&
+            pressedKeys[i + 1] === pressedKeys[i]
+        ) {
+            ret = (ret + dfs(i + 2)) % modulo;
+        }
+
+        if (i + 2 < pressedKeys.length && isAllEqual(i, i + 2)) {
+            ret = (ret + dfs(i + 3)) % modulo;
+        }
+
+        if (
+            (pressedKeys[i] === '7' || pressedKeys[i] === '9') &&
+            i + 3 < pressedKeys.length &&
+            isAllEqual(i, i + 3)
+        ) {
+            ret = (ret + dfs(i + 4)) % modulo;
+        }
+
+        return ret % modulo;
+    });
+
+    return dfs(0);
 }
