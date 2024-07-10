c12fea61c1db4cabe9faf5c5cfbb1bbdf24e6974
baymax <476803711@qq.com>
2024-07-10 09:26:13 +0800

    feat: 每日一题

--- src/problems/greedy.ts
+++ src/problems/greedy.ts
@@ -953,3 +953,57 @@ export function minimizeMax(nums: number[], p: number): number {
 
     return diff;
 }
+
+/*
+https://leetcode.com/problems/crawler-log-folder/description/
+1598. Crawler Log Folder
+The Leetcode file system keeps a log each time some user performs a change folder operation.
+
+The operations are described below:
+
+	"../" : Move to the parent folder of the current folder. (If you are already in the main folder, remain in the same folder).
+	"./" : Remain in the same folder.
+	"x/" : Move to the child folder named x (This folder is guaranteed to always exist).
+
+You are given a list of strings logs where logs[i] is the operation performed by the user at the ith step.
+
+The file system starts in the main folder, then the operations in logs are performed.
+
+Return the minimum number of operations needed to go back to the main folder after the change folder operations.
+
+Example 1:
+
+Input: logs = ["d1/","d2/","../","d21/","./"]
+Output: 2
+Explanation: Use this change folder operation "../" 2 times and go back to the main folder.
+
+Example 2:
+
+Input: logs = ["d1/","d2/","./","d3/","../","d31/"]
+Output: 3
+
+Example 3:
+
+Input: logs = ["d1/","../","../","../"]
+Output: 0
+
+Constraints:
+
+	1 <= logs.length <= 10^3
+	2 <= logs[i].length <= 10
+	logs[i] contains lowercase English letters, digits, '.', and '/'.
+	logs[i] follows the format described in the statement.
+	Folder names consist of lowercase English letters and digits.
+*/
+export function minOperations(logs: string[]): number {
+    const map = {
+        './': 0,
+        '../': -1,
+        default: 1,
+    };
+
+    return Math.max(
+        0,
+        logs.reduce((s, c) => s + (map[c] ?? map.default), 0)
+    );
+}
