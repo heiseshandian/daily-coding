2018b2eb92ce931267501485855c3fc869a57d38
baymax <476803711@qq.com>
2024-07-15 09:49:45 +0800

    feat: 每日一题

--- src/problems/tree.ts
+++ src/problems/tree.ts
@@ -2794,3 +2794,83 @@ export function replaceValueInTree(root: TreeNode): TreeNode {
 
     return root;
 }
+
+/*
+https://leetcode.com/problems/create-binary-tree-from-descriptions/description/
+2196. Create Binary Tree From Descriptions
+You are given a 2D integer array descriptions where descriptions[i] = [parenti, childi, isLefti] indicates that parenti is the parent of childi in a binary tree of unique values. Furthermore,
+
+	If isLefti == 1, then childi is the left child of parenti.
+	If isLefti == 0, then childi is the right child of parenti.
+
+Construct the binary tree described by descriptions and return its root.
+
+The test cases will be generated such that the binary tree is valid.
+
+Example 1:
+
+Input: descriptions = [[20,15,1],[20,17,0],[50,20,1],[50,80,0],[80,19,1]]
+Output: [50,20,80,15,17,19]
+Explanation: The root node is the node with value 50 since it has no parent.
+The resulting binary tree is shown in the diagram.
+
+Example 2:
+
+Input: descriptions = [[1,2,1],[2,3,0],[3,4,1]]
+Output: [1,2,null,null,3,4]
+Explanation: The root node is the node with value 1 since it has no parent.
+The resulting binary tree is shown in the diagram.
+
+Constraints:
+
+	1 <= descriptions.length <= 10^4
+	descriptions[i].length == 3
+	1 <= parenti, childi <= 10^5
+	0 <= isLefti <= 1
+	The binary tree described by descriptions is valid.
+*/
+export function createBinaryTree(descriptions: number[][]): TreeNode | null {
+    const map: Record<number, { left: number | null; right: number | null }> =
+        {};
+    const childSet = new Set<number>();
+
+    descriptions.forEach(([parent, child, isLeft]) => {
+        if (!map[parent]) {
+            map[parent] = {
+                left: null,
+                right: null,
+            };
+        }
+        childSet.add(child);
+
+        if (isLeft) {
+            map[parent].left = child;
+        } else {
+            map[parent].right = child;
+        }
+    });
+
+    const p = Object.keys(map).find((p) => !childSet.has(Number(p)))!;
+    const root = new TreeNode(Number(p));
+
+    const queue: TreeNode[] = [root];
+    while (queue.length) {
+        const node = queue.shift()!;
+
+        if (map[node.val]) {
+            const { left, right } = map[node.val];
+            if (left !== null) {
+                const n = new TreeNode(left);
+                node.left = n;
+                queue.push(n);
+            }
+            if (right !== null) {
+                const n = new TreeNode(right);
+                node.right = n;
+                queue.push(n);
+            }
+        }
+    }
+
+    return root;
+}
