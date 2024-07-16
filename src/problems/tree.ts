597fac205dc668bb83b8b3d466ade8bba54f1af8
baymax <476803711@qq.com>
2024-07-16 10:40:36 +0800

    feat: 每日一题

--- src/problems/tree.ts
+++ src/problems/tree.ts
@@ -2874,3 +2874,86 @@ export function createBinaryTree(descriptions: number[][]): TreeNode | null {
 
     return root;
 }
+
+/*
+https://leetcode.com/problems/step-by-step-directions-from-a-binary-tree-node-to-another/description/
+2096. Step-By-Step Directions From a Binary Tree Node to Another
+You are given the root of a binary tree with n nodes. Each node is uniquely assigned a value from 1 to n. You are also given an integer startValue representing the value of the start node s, and a different integer destValue representing the value of the destination node t.
+
+Find the shortest path starting from node s and ending at node t. Generate step-by-step directions of such path as a string consisting of only the uppercase letters 'L', 'R', and 'U'. Each letter indicates a specific direction:
+
+	'L' means to go from a node to its left child node.
+	'R' means to go from a node to its right child node.
+	'U' means to go from a node to its parent node.
+
+Return the step-by-step directions of the shortest path from node s to node t.
+
+Example 1:
+
+Input: root = [5,1,2,3,null,6,4], startValue = 3, destValue = 6
+Output: "UURL"
+Explanation: The shortest path is: 3 → 1 → 5 → 2 → 6.
+
+Example 2:
+
+Input: root = [2,1], startValue = 2, destValue = 1
+Output: "L"
+Explanation: The shortest path is: 2 → 1.
+
+Constraints:
+
+	The number of nodes in the tree is n.
+	2 <= n <= 10^5
+	1 <= Node.val <= n
+	All the values in the tree are unique.
+	1 <= startValue, destValue <= n
+	startValue != destValue
+*/
+export function getDirections(
+    root: TreeNode,
+    startValue: number,
+    destValue: number
+): string {
+    type Path = Array<[n: number, dir: 'L' | 'R' | 'U']>;
+
+    let start: Path = [];
+    let dest: Path = [];
+
+    const dfs = (node: TreeNode, path: Path): void => {
+        if (node.val === startValue) {
+            start = path.slice();
+        }
+        if (node.val === destValue) {
+            dest = path.slice();
+        }
+
+        if (node.left) {
+            path.push([node.left.val, 'L']);
+            dfs(node.left, path);
+            path.pop();
+        }
+        if (node.right) {
+            path.push([node.right.val, 'R']);
+            dfs(node.right, path);
+            path.pop();
+        }
+    };
+    dfs(root, [[root.val, 'U']]);
+
+    let lca = root.val;
+    let i = 0;
+    const len = Math.min(start.length, dest.length);
+    while (i < len) {
+        if (start[i][0] === dest[i][0]) {
+            lca = start[i++][0];
+        } else {
+            break;
+        }
+    }
+
+    return start
+        .slice(i)
+        .map(() => 'U')
+        .concat(dest.slice(i).map(([, dir]) => dir))
+        .join('');
+}
