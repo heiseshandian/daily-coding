f2b7f3df769bb4920357980fbf18cee9f98511d2
baymax <476803711@qq.com>
2024-07-17 12:34:26 +0800

    feat: 每日一题

--- src/problems/tree.ts
+++ src/problems/tree.ts
@@ -2998,3 +2998,73 @@ export function mergeTrees(
 
     return newRoot;
 }
+
+/*
+https://leetcode.com/problems/delete-nodes-and-return-forest/description/
+1110. Delete Nodes And Return Forest
+Given the root of a binary tree, each node in the tree has a distinct value.
+
+After deleting all nodes with a value in to_delete, we are left with a forest (a disjoint union of trees).
+
+Return the roots of the trees in the remaining forest. You may return the result in any order.
+
+Example 1:
+
+Input: root = [1,2,3,4,5,6,7], to_delete = [3,5]
+Output: [[1,2,null,4],[6],[7]]
+
+Example 2:
+
+Input: root = [1,2,4,null,3], to_delete = [3]
+Output: [[1,2,4]]
+
+Constraints:
+
+	The number of nodes in the given tree is at most 1000.
+	Each node has a distinct value between 1 and 1000.
+	to_delete.length <= 1000
+	to_delete contains distinct values between 1 and 1000.
+*/
+export function delNodes(
+    root: TreeNode | null,
+    to_delete: number[]
+): Array<TreeNode | null> {
+    if (!root) {
+        return [];
+    }
+
+    const parent = new Map<TreeNode, [TreeNode | null, 'left' | 'right']>();
+    parent.set(root, [null, 'left']);
+    const set = new Set(to_delete);
+    const res = new Set([root]);
+
+    const queue: TreeNode[] = [root];
+    while (queue.length) {
+        const node = queue.shift()!;
+        if (set.has(node.val)) {
+            const [p, dir] = parent.get(node)!;
+            if (p) {
+                p[dir] = null;
+            }
+            if (node.left) {
+                res.add(node.left);
+            }
+            if (node.right) {
+                res.add(node.right);
+            }
+
+            res.delete(node);
+        }
+
+        if (node.left) {
+            parent.set(node.left, [node, 'left']);
+            queue.push(node.left);
+        }
+        if (node.right) {
+            parent.set(node.right, [node, 'right']);
+            queue.push(node.right);
+        }
+    }
+
+    return Array.from(res);
+}
