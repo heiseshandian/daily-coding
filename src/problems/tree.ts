93d144c86a6136279b96e8231709e14c76e4b863
baymax <476803711@qq.com>
2024-07-18 12:32:25 +0800

    feat: 每日一题

--- src/problems/tree.ts
+++ src/problems/tree.ts
@@ -1,12 +1,6 @@
 import { Queue } from '../algorithm/queue';
-import {
-    TreeNode,
-    deserializeByLevel,
-    getMaxDistance,
-    serializeByLevel,
-} from '../algorithm/tree';
+import { TreeNode, getMaxDistance } from '../algorithm/tree';
 import { cache } from '../design-pattern/proxy';
-import { fullPermutationTestData } from './__tests__/recursion.testdata';
 /* 
 https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
 
@@ -2940,12 +2934,11 @@ export function getDirections(
     };
     dfs(root, [[root.val, 'U']]);
 
-    let lca = root.val;
     let i = 0;
     const len = Math.min(start.length, dest.length);
     while (i < len) {
         if (start[i][0] === dest[i][0]) {
-            lca = start[i++][0];
+            i++;
         } else {
             break;
         }
@@ -3125,4 +3118,77 @@ export function countPairs(root: TreeNode | null, distance: number): number {
     getPairs(root);
 
     return count;
+}
+
+/*
+https://leetcode.com/problems/count-the-number-of-complete-components/description/?envType=problem-list-v2&envId=o5cftq05
+2685. Count the Number of Complete Components
+You are given an integer n. There is an undirected graph with n vertices, numbered from 0 to n - 1. You are given a 2D integer array edges where edges[i] = [ai, bi] denotes that there exists an undirected edge connecting vertices ai and bi.
+
+Return the number of complete connected components of the graph.
+
+A connected component is a subgraph of a graph in which there exists a path between any two vertices, and no vertex of the subgraph shares an edge with a vertex outside of the subgraph.
+
+A connected component is said to be complete if there exists an edge between every pair of its vertices.
+
+Example 1:
+
+Input: n = 6, edges = [[0,1],[0,2],[1,2],[3,4]]
+Output: 3
+Explanation: From the picture above, one can see that all of the components of this graph are complete.
+
+Example 2:
+
+Input: n = 6, edges = [[0,1],[0,2],[1,2],[3,4],[3,5]]
+Output: 1
+Explanation: The component containing vertices 0, 1, and 2 is complete since there is an edge between every pair of two vertices. On the other hand, the component containing vertices 3, 4, and 5 is not complete since there is no edge between vertices 4 and 5. Thus, the number of complete components in this graph is 1.
+
+Constraints:
+
+	1 <= n <= 50
+	0 <= edges.length <= n * (n - 1) / 2
+	edges[i].length == 2
+	0 <= ai, bi <= n - 1
+	ai != bi
+	There are no repeated edges.
+*/
+export function countCompleteComponents(n: number, edges: number[][]): number {
+    const tables: number[][] = Array.from({ length: n }, () => []);
+    edges.forEach(([from, to]) => {
+        tables[from].push(to);
+        tables[to].push(from);
+    });
+
+    const visited = new Set<number>();
+
+    const bfs = (i: number) => {
+        let vertices = 0;
+        let edgeCount = 0;
+        const queue = [i];
+        visited.add(i);
+
+        while (queue.length) {
+            const node = queue.shift()!;
+            vertices++;
+
+            tables[node].forEach((t) => {
+                if (!visited.has(t)) {
+                    visited.add(t);
+                    queue.push(t);
+                }
+                edgeCount++;
+            });
+        }
+
+        return vertices * (vertices - 1) === edgeCount;
+    };
+
+    let res = 0;
+    for (let i = 0; i < n; i++) {
+        if (!visited.has(i) && bfs(i)) {
+            res++;
+        }
+    }
+
+    return res;
 }
