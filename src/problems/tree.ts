ceb1a6ab9fb0996535ca633278ed22c4be0145ef
baymax <476803711@qq.com>
2024-07-18 09:47:50 +0800

    feat: 每日一题

--- src/problems/tree.ts
+++ src/problems/tree.ts
@@ -3068,3 +3068,61 @@ export function delNodes(
 
     return Array.from(res);
 }
+
+/*
+https://leetcode.com/problems/number-of-good-leaf-nodes-pairs/description/
+1530. Number of Good Leaf Nodes Pairs
+You are given the root of a binary tree and an integer distance. A pair of two different leaf nodes of a binary tree is said to be good if the length of the shortest path between them is less than or equal to distance.
+
+Return the number of good leaf node pairs in the tree.
+
+Example 1:
+
+Input: root = [1,2,3,null,4], distance = 3
+Output: 1
+Explanation: The leaf nodes of the tree are 3 and 4 and the length of the shortest path between them is 3. This is the only good pair.
+
+Example 2:
+
+Input: root = [1,2,3,4,5,6,7], distance = 3
+Output: 2
+Explanation: The good pairs are [4,5] and [6,7] with shortest path = 2. The pair [4,6] is not good because the length of ther shortest path between them is 4.
+
+Example 3:
+
+Input: root = [7,1,4,6,null,5,3,null,null,null,null,null,2], distance = 3
+Output: 1
+Explanation: The only good pair is [2,5].
+
+Constraints:
+
+	The number of nodes in the tree is in the range [1, 210].
+	1 <= Node.val <= 100
+	1 <= distance <= 10
+*/
+export function countPairs(root: TreeNode | null, distance: number): number {
+    // Strategy:
+    // Aggregate distance to child at each parent
+    // Add one for each parent
+    // Compare right and left hand side of leaf nodes, if any of the distances
+    // add up to the target or smaller we increment a counter and continue to aggregate
+    let count = 0;
+    function getPairs(node: TreeNode | null): number[] {
+        if (node === null) return [];
+
+        const lhs = getPairs(node.left);
+        const rhs = getPairs(node.right);
+
+        for (const lDist of lhs) {
+            count += rhs.filter((rDist) => rDist + lDist <= distance).length;
+        }
+
+        if (node.left === null && node.right === null) return [1];
+
+        return [...lhs, ...rhs].map((h) => h + 1);
+    }
+
+    getPairs(root);
+
+    return count;
+}
