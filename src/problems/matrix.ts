ceb0501b99679eb632e55296fc4f786ec0f53bb6
baymax <476803711@qq.com>
2024-07-19 09:22:49 +0800

    feat: 每日一题

--- src/problems/matrix.ts
+++ src/problems/matrix.ts
@@ -1615,3 +1615,66 @@ export function findCircleNum(isConnected: number[][]): number {
 
     return province;
 }
+
+/*
+https://leetcode.com/problems/lucky-numbers-in-a-matrix/description/
+1380. Lucky Numbers in a Matrix
+Given an m x n matrix of distinct numbers, return all lucky numbers in the matrix in any order.
+
+A lucky number is an element of the matrix such that it is the minimum element in its row and maximum in its column.
+
+Example 1:
+
+Input: matrix = [[3,7,8],[9,11,13],[15,16,17]]
+Output: [15]
+Explanation: 15 is the only lucky number since it is the minimum in its row and the maximum in its column.
+
+Example 2:
+
+Input: matrix = [[1,10,4,2],[9,3,8,7],[15,16,17,12]]
+Output: [12]
+Explanation: 12 is the only lucky number since it is the minimum in its row and the maximum in its column.
+
+Example 3:
+
+Input: matrix = [[7,8],[1,2]]
+Output: [7]
+Explanation: 7 is the only lucky number since it is the minimum in its row and the maximum in its column.
+
+Constraints:
+
+	m == mat.length
+	n == mat[i].length
+	1 <= n, m <= 50
+	1 <= matrix[i][j] <= 10^5.
+	All elements in the matrix are distinct.
+*/
+export function luckyNumbers(matrix: number[][]): number[] {
+    const res: number[] = [];
+    const m = matrix.length;
+    const n = matrix[0].length;
+    for (let i = 0; i < m; i++) {
+        let min = Infinity;
+        let minIndex = -1;
+        for (let j = 0; j < n; j++) {
+            if (min > matrix[i][j]) {
+                min = matrix[i][j];
+                minIndex = j;
+            }
+        }
+
+        let found = true;
+        for (let k = 0; k < m; k++) {
+            if (matrix[k][minIndex] > min) {
+                found = false;
+                break;
+            }
+        }
+
+        if (found) {
+            res.push(min);
+        }
+    }
+
+    return res;
+}
