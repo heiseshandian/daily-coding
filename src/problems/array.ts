6600452707b4dd8030213e640392a1999e1173ac
baymax <476803711@qq.com>
2024-07-29 09:27:20 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -7033,3 +7033,66 @@ export function sortJumbled(mapping: number[], nums: number[]): number[] {
 
     return nums.sort((a, b) => getRealNum(a) - getRealNum(b));
 }
+
+/*
+https://leetcode.com/problems/count-number-of-teams/description/
+1395. Count Number of Teams
+There are n soldiers standing in a line. Each soldier is assigned a unique rating value.
+
+You have to form a team of 3 soldiers amongst them under the following rules:
+
+	Choose 3 soldiers with index (i, j, k) with rating (rating[i], rating[j], rating[k]).
+	A team is valid if: (rating[i] < rating[j] < rating[k]) or (rating[i] > rating[j] > rating[k]) where (0 <= i < j < k < n).
+
+Return the number of teams you can form given the conditions. (soldiers can be part of multiple teams).
+
+Example 1:
+
+Input: rating = [2,5,3,4,1]
+Output: 3
+Explanation: We can form three teams given the conditions. (2,3,4), (5,4,1), (5,3,1). 
+
+Example 2:
+
+Input: rating = [2,1,3]
+Output: 0
+Explanation: We can't form any team given the conditions.
+
+Example 3:
+
+Input: rating = [1,2,3,4]
+Output: 4
+
+Constraints:
+
+	n == rating.length
+	3 <= n <= 1000
+	1 <= rating[i] <= 10^5
+	All the integers in rating are unique.
+*/
+export function numTeams(rating: number[]): number {
+    let count = 0;
+    for (let i = 0; i < rating.length - 2; i++) {
+        for (let j = i + 1; j < rating.length - 1; j++) {
+            if (rating[i] === rating[j]) {
+                continue;
+            }
+
+            if (rating[i] < rating[j]) {
+                for (let k = j + 1; k < rating.length; k++) {
+                    if (rating[j] < rating[k]) {
+                        count++;
+                    }
+                }
+            } else {
+                for (let k = j + 1; k < rating.length; k++) {
+                    if (rating[j] > rating[k]) {
+                        count++;
+                    }
+                }
+            }
+        }
+    }
+
+    return count;
+}
