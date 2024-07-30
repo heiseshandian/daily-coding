e554ed46c7620b5401a43b975a64fff1b8fefec5
baymax <476803711@qq.com>
2024-07-30 13:58:57 +0800

    feat: 每日一题

--- src/problems/string.ts
+++ src/problems/string.ts
@@ -2631,3 +2631,45 @@ export function findAndReplacePattern(
 
     return res;
 }
+
+/*
+https://leetcode.com/problems/minimum-deletions-to-make-string-balanced/description/?envType=daily-question&envId=2024-07-30
+1653. Minimum Deletions to Make String Balanced
+You are given a string s consisting only of characters 'a' and 'b'​​​​.
+
+You can delete any number of characters in s to make s balanced. s is balanced if there is no pair of indices (i,j) such that i < j and s[i] = 'b' and s[j]= 'a'.
+
+Return the minimum number of deletions needed to make s balanced.
+
+Example 1:
+
+Input: s = "aababbab"
+Output: 2
+Explanation: You can either:
+Delete the characters at 0-indexed positions 2 and 6 ("aababbab" -> "aaabbb"), or
+Delete the characters at 0-indexed positions 3 and 6 ("aababbab" -> "aabbbb").
+
+Example 2:
+
+Input: s = "bbaaaaabb"
+Output: 2
+Explanation: The only solution is to delete the first two characters.
+
+Constraints:
+
+	1 <= s.length <= 10^5
+	s[i] is 'a' or 'b'​​.
+*/
+export function minimumDeletions(s: string): number {
+    let deletions = 0;
+    let counts = 0;
+    for (const ch of s) {
+        if (ch === 'b') {
+            counts += 1;
+        } else if (counts > 0) {
+            deletions += 1;
+            counts -= 1;
+        }
+    }
+    return deletions;
+}
