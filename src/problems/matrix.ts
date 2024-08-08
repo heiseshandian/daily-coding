8aeeb69c59d24e9368715356855d3593303ae4fa
baymax <476803711@qq.com>
2024-08-08 22:45:59 +0800

    feat: 每日一题

--- src/problems/matrix.ts
+++ src/problems/matrix.ts
@@ -1748,3 +1748,72 @@ export function restoreMatrix(rowSum: number[], colSum: number[]): number[][] {
 
     return matrix;
 }
+
+/*
+https://leetcode.com/problems/spiral-matrix-iii/description/?envType=daily-question&envId=2024-08-08
+885. Spiral Matrix III
+You start at the cell (rStart, cStart) of an rows x cols grid facing east. The northwest corner is at the first row and column in the grid, and the southeast corner is at the last row and column.
+
+You will walk in a clockwise spiral shape to visit every position in this grid. Whenever you move outside the grid's boundary, we continue our walk outside the grid (but may return to the grid boundary later.). Eventually, we reach all rows * cols spaces of the grid.
+
+Return an array of coordinates representing the positions of the grid in the order you visited them.
+
+Example 1:
+
+Input: rows = 1, cols = 4, rStart = 0, cStart = 0
+Output: [[0,0],[0,1],[0,2],[0,3]]
+
+Example 2:
+
+Input: rows = 5, cols = 6, rStart = 1, cStart = 4
+Output: [[1,4],[1,5],[2,5],[2,4],[2,3],[1,3],[0,3],[0,4],[0,5],[3,5],[3,4],[3,3],[3,2],[2,2],[1,2],[0,2],[4,5],[4,4],[4,3],[4,2],[4,1],[3,1],[2,1],[1,1],[0,1],[4,0],[3,0],[2,0],[1,0],[0,0]]
+
+Constraints:
+
+	1 <= rows, cols <= 100
+	0 <= rStart < rows
+	0 <= cStart < cols
+*/
+export function spiralMatrixIII(
+    rows: number,
+    cols: number,
+    rStart: number,
+    cStart: number
+): number[][] {
+    const result: number[][] = [];
+    const total = rows * cols;
+
+    const isValid = (x: number, y: number): boolean =>
+        x >= 0 && x < rows && y >= 0 && y < cols;
+
+    let x = rStart;
+    let y = cStart;
+    const circle = (len: number, reverse: boolean) => {
+        const delta = reverse ? 1 : -1;
+
+        let start = len;
+        while (start-- > 0) {
+            if (isValid(x, y)) {
+                result.push([x, y]);
+            }
+            y += delta;
+        }
+
+        start = len;
+        while (start-- > 0) {
+            if (isValid(x, y)) {
+                result.push([x, y]);
+            }
+            x += delta;
+        }
+    };
+
+    let len = 1;
+    let reverse = true;
+    while (result.length < total) {
+        circle(len++, reverse);
+        reverse = !reverse;
+    }
+
+    return result;
+}
