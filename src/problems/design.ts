96fb08a9438ac281f2543dc39ab51f24c847d547
baymax <476803711@qq.com>
2024-08-12 22:31:04 +0800

    feat: 每日一题

--- src/problems/design.ts
+++ src/problems/design.ts
@@ -1917,3 +1917,73 @@ export function timeLimit(fn: AsyncFn, t: number): AsyncFn {
         });
     };
 }
+
+/*
+https://leetcode.com/problems/kth-largest-element-in-a-stream/description/?envType=daily-question&envId=2024-08-12
+703. Kth Largest Element in a Stream
+Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Implement KthLargest class:
+
+	KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
+	int add(int val) Appends the integer val to the stream and returns the element representing the kth largest element in the stream.
+
+Example 1:
+
+Input
+["KthLargest", "add", "add", "add", "add", "add"]
+[[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+Output
+[null, 4, 5, 5, 8, 8]
+
+Explanation
+KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+kthLargest.add(3);   // return 4
+kthLargest.add(5);   // return 5
+kthLargest.add(10);  // return 5
+kthLargest.add(9);   // return 8
+kthLargest.add(4);   // return 8
+
+Constraints:
+
+	1 <= k <= 10^4
+	0 <= nums.length <= 10^4
+	-10^4 <= nums[i] <= 10^4
+	-10^4 <= val <= 10^4
+	At most 104 calls will be made to add.
+	It is guaranteed that there will be at least k elements in the array when you search for the kth element.
+*/
+export class KthLargest {
+    heap: GenericHeap<number>;
+    k: number;
+
+    constructor(k: number, nums: number[]) {
+        this.k = k;
+
+        this.heap = new GenericHeap((a, b) => a - b);
+        const len = Math.min(k, nums.length);
+        for (let i = 0; i < len; i++) {
+            this.heap.push(nums[i]);
+        }
+
+        for (let i = len; i < nums.length; i++) {
+            if (this.heap.peek() < nums[i]) {
+                this.heap.pop();
+                this.heap.push(nums[i]);
+            }
+        }
+    }
+
+    add(val: number): number {
+        if (this.heap.size() < this.k) {
+            this.heap.push(val);
+            return this.heap.peek();
+        }
+
+        if (this.heap.peek() < val) {
+            this.heap.pop();
+            this.heap.push(val);
+        }
+        return this.heap.peek();
+    }
+}
