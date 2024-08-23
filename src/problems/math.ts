ddf350825daebfef608272178425f658c5aacfa2
baymax <476803711@qq.com>
2024-08-24 00:22:06 +0800

    feat: 每日一题

--- src/problems/math.ts
+++ src/problems/math.ts
@@ -1,4 +1,5 @@
 import { cache } from '../design-pattern/proxy';
+import { gcd } from '../common/index';
 /* 
 https://leetcode.com/problems/numbers-at-most-n-given-digit-set/
 
@@ -968,4 +969,48 @@ function getFactors(n: number): number[] {
     }
 
     return res;
+}
+
+/*
+https://leetcode.com/problems/fraction-addition-and-subtraction/description/?envType=daily-question&envId=2024-08-23
+592. Fraction Addition and Subtraction
+Given a string expression representing an expression of fraction addition and subtraction, return the calculation result in string format.
+
+The final result should be an irreducible fraction. If your final result is an integer, change it to the format of a fraction that has a denominator 1. So in this case, 2 should be converted to 2/1.
+
+Example 1:
+
+Input: expression = "-1/2+1/2"
+Output: "0/1"
+
+Example 2:
+
+Input: expression = "-1/2+1/2+1/3"
+Output: "1/3"
+
+Example 3:
+
+Input: expression = "1/3-1/2"
+Output: "-1/6"
+
+Constraints:
+
+	The input string only contains '0' to '9', '/', '+' and '-'. So does the output.
+	Each fraction (input and output) has the format ±numerator/denominator. If the first input fraction or the output is positive, then '+' will be omitted.
+	The input only contains valid irreducible fractions, where the numerator and denominator of each fraction will always be in the range [1, 10]. If the denominator is 1, it means this fraction is actually an integer in a fraction format defined above.
+	The number of given fractions will be in the range [1, 10].
+	The numerator and denominator of the final result are guaranteed to be valid and in the range of 32-bit int.
+*/
+export function fractionAddition(expression: string): string {
+    const segments = expression.match(/^\d+\/\d+|[+-]\d+\/\d+/gm)!;
+    let sum = 0;
+    let denominator = 1;
+    for (const exp of segments) {
+        const [num, d] = exp.split('/').map((a) => +a);
+        sum = sum * d + num * denominator;
+        denominator *= d;
+    }
+
+    const g = Math.abs(gcd(sum, denominator));
+    return `${sum / g}/${denominator / g}`;
 }
