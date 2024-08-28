a5ae410323900beb73a743e8e12ce300d38df4fe
baymax <476803711@qq.com>
2024-08-28 17:52:56 +0800

    feat: 每日一题

--- src/problems/matrix.ts
+++ src/problems/matrix.ts
@@ -1913,3 +1913,77 @@ export function numMagicSquaresInside(grid: number[][]): number {
 
     return count;
 }
+
+/*
+https://leetcode.com/problems/count-sub-islands/description/
+1905. Count Sub Islands
+You are given two m x n binary matrices grid1 and grid2 containing only 0's (representing water) and 1's (representing land). An island is a group of 1's connected 4-directionally (horizontal or vertical). Any cells outside of the grid are considered water cells.
+
+An island in grid2 is considered a sub-island if there is an island in grid1 that contains all the cells that make up this island in grid2.
+
+Return the number of islands in grid2 that are considered sub-islands.
+
+Example 1:
+
+Input: grid1 = [[1,1,1,0,0],[0,1,1,1,1],[0,0,0,0,0],[1,0,0,0,0],[1,1,0,1,1]], grid2 = [[1,1,1,0,0],[0,0,1,1,1],[0,1,0,0,0],[1,0,1,1,0],[0,1,0,1,0]]
+Output: 3
+Explanation: In the picture above, the grid on the left is grid1 and the grid on the right is grid2.
+The 1s colored red in grid2 are those considered to be part of a sub-island. There are three sub-islands.
+
+Example 2:
+
+Input: grid1 = [[1,0,1,0,1],[1,1,1,1,1],[0,0,0,0,0],[1,1,1,1,1],[1,0,1,0,1]], grid2 = [[0,0,0,0,0],[1,1,1,1,1],[0,1,0,1,0],[0,1,0,1,0],[1,0,0,0,1]]
+Output: 2 
+Explanation: In the picture above, the grid on the left is grid1 and the grid on the right is grid2.
+The 1s colored red in grid2 are those considered to be part of a sub-island. There are two sub-islands.
+
+Constraints:
+
+	m == grid1.length == grid2.length
+	n == grid1[i].length == grid2[i].length
+	1 <= m, n <= 500
+	grid1[i][j] and grid2[i][j] are either 0 or 1.
+*/
+export function countSubIslands(grid1: number[][], grid2: number[][]): number {
+    let found = true;
+
+    const infect = (i: number, j: number): void => {
+        if (grid2[i][j] !== 1) {
+            return;
+        }
+
+        grid2[i][j] = 2;
+        if (grid1[i][j] === 0) {
+            found = false;
+        }
+
+        if (i - 1 >= 0) {
+            infect(i - 1, j);
+        }
+        if (i + 1 < grid2.length) {
+            infect(i + 1, j);
+        }
+        if (j - 1 >= 0) {
+            infect(i, j - 1);
+        }
+        if (j + 1 < grid2[0].length) {
+            infect(i, j + 1);
+        }
+    };
+
+    let subIsLands = 0;
+    for (let i = 0; i < grid2.length; i++) {
+        for (let j = 0; j < grid2[0].length; j++) {
+            found = true;
+            if (grid2[i][j] === 1) {
+                infect(i, j);
+
+                if (found) {
+                    subIsLands++;
+                }
+            }
+        }
+    }
+
+    return subIsLands;
+}
