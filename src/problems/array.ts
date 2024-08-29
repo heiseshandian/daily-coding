4534ff2e3deafdde16e14d9998cfbcb362198503
baymax <476803711@qq.com>
2024-08-29 23:24:24 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -7301,3 +7301,52 @@ export function arrayChange(nums: number[], operations: number[][]): number[] {
     }
     return nums;
 }
+
+/*
+https://leetcode.com/problems/single-element-in-a-sorted-array/description/
+540. Single Element in a Sorted Array
+You are given a sorted array consisting of only integers where every element appears exactly twice, 
+except for one element which appears exactly once.
+
+Return the single element that appears only once.
+
+Your solution must run in O(log n) time and O(1) space.
+
+Example 1:
+Input: nums = [1,1,2,3,3,4,4,8,8]
+Output: 2
+Example 2:
+Input: nums = [3,3,7,7,10,11,11]
+Output: 10
+
+Constraints:
+
+	1 <= nums.length <= 10^5
+	0 <= nums[i] <= 10^5
+*/
+export function singleNonDuplicate(nums: number[]): number {
+    let left = 0;
+    let right = nums.length - 1;
+    while (left <= right) {
+        const mid = left + ((right - left) >> 1);
+        if (nums[mid] !== nums[mid - 1] && nums[mid] !== nums[mid + 1]) {
+            return nums[mid];
+        }
+
+        if (nums[mid] === nums[mid - 1]) {
+            if (mid - 1 - left > 0 && ((mid - 1 - left) & 1) === 1) {
+                right = mid - 2;
+            } else {
+                left = mid + 1;
+            }
+        } else if (nums[mid] === nums[mid + 1]) {
+            if (mid - left > 0 && ((mid - left) & 1) === 1) {
+                right = mid - 1;
+            } else {
+                left = mid + 2;
+            }
+        }
+    }
+
+    return -1;
+}
