0ae6bb2fec4c5323ae73a7cc6d2bb62ef25ae827
zhanggq52434 <zhanggq52434@hundsun.com>
2024-09-01 22:45:02 +0800

    feat: 从递归到动态规划

--- src/problems/dp.ts
+++ src/problems/dp.ts
@@ -2307,3 +2307,93 @@ export function getMinRemoved(heights: number[]) {
 
     return n - max;
 }
+
+/*
+https://leetcode.com/problems/minimum-cost-for-tickets/description/
+983. Minimum Cost For Tickets
+You have planned some train traveling one year in advance. The days of the year in which you will travel are given as an integer array days. Each day is an integer from 1 to 365.
+
+Train tickets are sold in three different ways:
+
+	a 1-day pass is sold for costs[0] dollars,
+	a 7-day pass is sold for costs[1] dollars, and
+	a 30-day pass is sold for costs[2] dollars.
+
+The passes allow that many days of consecutive travel.
+
+	For example, if we get a 7-day pass on day 2, then we can travel for 7 days: 2, 3, 4, 5, 6, 7, and 8.
+
+Return the minimum number of dollars you need to travel every day in the given list of days.
+
+Example 1:
+
+Input: days = [1,4,6,7,8,20], costs = [2,7,15]
+Output: 11
+Explanation: For example, here is one way to buy passes that lets you travel your travel plan:
+On day 1, you bought a 1-day pass for costs[0] = $2, which covered day 1.
+On day 3, you bought a 7-day pass for costs[1] = $7, which covered days 3, 4, ..., 9.
+On day 20, you bought a 1-day pass for costs[0] = $2, which covered day 20.
+In total, you spent $11 and covered all the days of your travel.
+
+Example 2:
+
+Input: days = [1,2,3,4,5,6,7,8,9,10,30,31], costs = [2,7,15]
+Output: 17
+Explanation: For example, here is one way to buy passes that lets you travel your travel plan:
+On day 1, you bought a 30-day pass for costs[2] = $15 which covered days 1, 2, ..., 30.
+On day 31, you bought a 1-day pass for costs[0] = $2 which covered day 31.
+In total, you spent $17 and covered all the days of your travel.
+
+Constraints:
+
+	1 <= days.length <= 365
+	1 <= days[i] <= 365
+	days is in strictly increasing order.
+	costs.length == 3
+	1 <= costs[i] <= 1000
+*/
+export function mincostTickets(days: number[], costs: number[]): number {
+    const durations = [1, 7, 30];
+
+    // 搞定 i 以及 i 之后的旅行最少需要多少钱
+    const dfs = cache((index: number): number => {
+        if (index === days.length) {
+            return 0;
+        }
+
+        let min = Infinity;
+        durations.forEach((v, i) => {
+            const target = v + days[index];
+            let next = index + 1;
+            while (next < days.length && target > days[next]) {
+                next++;
+            }
+
+            min = Math.min(min, costs[i] + dfs(next));
+        });
+
+        return min;
+    });
+
+    return dfs(0);
+}
+
+export function mincostTickets2(days: number[], costs: number[]): number {
+    const durations = [1, 7, 30];
+
+    const dp: number[] = Array(days.length + 1).fill(Infinity);
+    dp[days.length] = 0;
+
+    for (let i = days.length - 1; i >= 0; i--) {
+        durations.forEach((v, innerI) => {
+            const target = v + days[i];
+            let next = i + 1;
+            while (next < days.length && target > days[next]) {
+                next++;
+            }
+            dp[i] = Math.min(dp[i], dp[next] + costs[innerI]);
+        });
+    }
+
+    return dp[0];
+}
