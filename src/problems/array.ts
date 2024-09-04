cb00d7fa2cb7059530f0a816bec6624dd0a91be9
zhanggq52434 <zhanggq52434@hundsun.com>
2024-09-04 10:35:45 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -7576,3 +7576,103 @@ export function chalkReplacer(chalk: number[], k: number): number {
 
     return i - 1;
 }
+
+/*
+https://leetcode.com/problems/walking-robot-simulation/description/
+874. Walking Robot Simulation
+A robot on an infinite XY-plane starts at point (0, 0) facing north. The robot can receive a sequence of these three possible types of commands:
+
+	-2: Turn left 90 degrees.
+	-1: Turn right 90 degrees.
+	1 <= k <= 9: Move forward k units, one unit at a time.
+
+Some of the grid squares are obstacles. The ith obstacle is at grid point obstacles[i] = (xi, yi). If the robot runs into an obstacle, then it will instead stay in its current location and move on to the next command.
+
+Return the maximum Euclidean distance that the robot ever gets from the origin squared (i.e. if the distance is 5, return 25).
+
+Note:
+
+	North means +Y direction.
+	East means +X direction.
+	South means -Y direction.
+	West means -X direction.
+	There can be obstacle in [0,0].
+
+Example 1:
+
+Input: commands = [4,-1,3], obstacles = []
+Output: 25
+Explanation: The robot starts at (0, 0):
+1. Move north 4 units to (0, 4).
+2. Turn right.
+3. Move east 3 units to (3, 4).
+The furthest point the robot ever gets from the origin is (3, 4), which squared is 32 + 42 = 25 units away.
+
+Example 2:
+
+Input: commands = [4,-1,4,-2,4], obstacles = [[2,4]]
+Output: 65
+Explanation: The robot starts at (0, 0):
+1. Move north 4 units to (0, 4).
+2. Turn right.
+3. Move east 1 unit and get blocked by the obstacle at (2, 4), robot is at (1, 4).
+4. Turn left.
+5. Move north 4 units to (1, 8).
+The furthest point the robot ever gets from the origin is (1, 8), which squared is 12 + 82 = 65 units away.
+
+Example 3:
+
+Input: commands = [6,-1,-1,6], obstacles = []
+Output: 36
+Explanation: The robot starts at (0, 0):
+1. Move north 6 units to (0, 6).
+2. Turn right.
+3. Turn right.
+4. Move south 6 units to (0, 0).
+The furthest point the robot ever gets from the origin is (0, 6), which squared is 62 = 36 units away.
+
+Constraints:
+
+	1 <= commands.length <= 10^4
+	commands[i] is either -2, -1, or an integer in the range [1, 9].
+	0 <= obstacles.length <= 10^4
+	-3 * 10^4 <= xi, yi <= 10^4
+	The answer is guaranteed to be less than 231.
+*/
+
+export function robotSim(commands: number[], obstacles: number[][]): number {
+    const movements = [
+        { x: 0, y: 1 },
+        { x: 1, y: 0 },
+        { x: 0, y: -1 },
+        { x: -1, y: 0 },
+    ];
+
+    const obstacleMap = new Map<number, Set<number>>();
+    obstacles.forEach(([x, y]) => {
+        if (!obstacleMap.has(x)) obstacleMap.set(x, new Set());
+        obstacleMap.get(x)!.add(y);
+    });
+
+    let direction = 0;
+    let x = 0;
+    let y = 0;
+    let max = 0;
+    for (const command of commands) {
+        if (command < 0) {
+            direction = (direction + (command === -1 ? 1 : 3)) % 4;
+            continue;
+        }
+
+        // 由于每一步的 command 最大是 9 ，所以这里可以直接遍历，不用二分找最近的障碍
+        for (let i = 0; i < command; i++) {
+            const nextX = x + movements[direction].x;
+            const nextY = y + movements[direction].y;
+            if (obstacleMap.get(nextX)?.has(nextY)) break;
+            x = nextX;
+            y = nextY;
+        }
+        max = Math.max(max, x ** 2 + y ** 2);
+    }
+    return max;
+}
