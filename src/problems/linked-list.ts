c2b8086a8527d9e875aef98318ff05e0f3849c88
zhanggq52434 <zhanggq52434@hundsun.com>
2024-09-07 17:02:10 +0800

    feat: 每日一题

--- src/problems/linked-list.ts
+++ src/problems/linked-list.ts
@@ -4,6 +4,7 @@ import {
     reverseSingleLinkedList,
 } from '../algorithm/linked-list';
 import { getClosestMaxArr } from '../algorithm/monotonous-stack';
+import { TreeNode } from '../algorithm/tree';
 //https://leetcode.com/problems/copy-list-with-random-pointer/
 // 深度copy如下带有random指针的节点
 export class NodeWithRandom {
@@ -1136,4 +1137,69 @@ export function modifiedList(
     }
 
     return newHead;
+}
+
+/*
+https://leetcode.com/problems/linked-list-in-binary-tree/description/
+1367. Linked List in Binary Tree
+Given a binary tree root and a linked list with head as the first node. 
+
+Return True if all the elements in the linked list starting from the head correspond to some downward path connected in the binary tree otherwise return False.
+
+In this context downward path means a path that starts at some node and goes downwards.
+
+Example 1:
+
+Input: head = [4,2,8], root = [1,4,4,null,2,2,null,1,null,6,8,null,null,null,null,1,3]
+Output: true
+Explanation: Nodes in blue form a subpath in the binary Tree.  
+
+Example 2:
+
+Input: head = [1,4,2,6], root = [1,4,4,null,2,2,null,1,null,6,8,null,null,null,null,1,3]
+Output: true
+
+Example 3:
+
+Input: head = [1,4,2,6,8], root = [1,4,4,null,2,2,null,1,null,6,8,null,null,null,null,1,3]
+Output: false
+Explanation: There is no path in the binary tree that contains all the elements of the linked list from head.
+
+Constraints:
+
+	The number of nodes in the tree will be in the range [1, 2500].
+	The number of nodes in the list will be in the range [1, 100].
+	1 <= Node.val <= 100 for each node in the linked list and binary tree.
+*/
+export function isSubPath(
+    head: ListNode | null,
+    root: TreeNode | null
+): boolean {
+    if (!root) {
+        return false;
+    }
+
+    return (
+        isSubPathDfs(root, head) ||
+        isSubPath(head, root.left) ||
+        isSubPath(head, root.right)
+    );
+}
+
+function isSubPathDfs(root: TreeNode | null, head: ListNode | null): boolean {
+    if (!head) {
+        return true;
+    }
+    if (!root) {
+        return false;
+    }
+
+    if (root.val === head.val) {
+        return (
+            isSubPathDfs(root.left, head.next) ||
+            isSubPathDfs(root.right, head.next)
+        );
+    }
+
+    return false;
 }
