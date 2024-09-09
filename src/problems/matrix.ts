d0522cad75a5d02e27bc6c2f305cab9929a512fb
zhanggq52434 <zhanggq52434@hundsun.com>
2024-09-09 13:26:46 +0800

    feat: 每日一题

--- src/problems/matrix.ts
+++ src/problems/matrix.ts
@@ -1,5 +1,6 @@
 import { cache } from '../design-pattern/proxy';
 import { UnionFind } from '../algorithm/union-find';
+import { ListNode, SingleLinkedList } from '../algorithm/linked-list';
 // 锯齿形打印矩阵
 export function zigzagMatrix(matrix: number[][]) {
     if (!matrix || matrix.length === 0) {
@@ -1986,4 +1987,103 @@ export function countSubIslands(grid1: number[][], grid2: number[][]): number {
     }
 
     return subIsLands;
+}
+
+/*
+https://leetcode.com/problems/spiral-matrix-iv/description/
+2326. Spiral Matrix IV
+You are given two integers m and n, which represent the dimensions of a matrix.
+
+You are also given the head of a linked list of integers.
+
+Generate an m x n matrix that contains the integers in the linked list presented in spiral order (clockwise), starting from the top-left of the matrix. If there are remaining empty spaces, fill them with -1.
+
+Return the generated matrix.
+
+Example 1:
+
+Input: m = 3, n = 5, head = [3,0,2,6,8,1,7,9,4,2,5,5,0]
+Output: [[3,0,2,6,8],[5,0,-1,-1,1],[5,2,4,9,7]]
+Explanation: The diagram above shows how the values are printed in the matrix.
+Note that the remaining spaces in the matrix are filled with -1.
+
+Example 2:
+
+Input: m = 1, n = 4, head = [0,1,2]
+Output: [[0,1,2,-1]]
+Explanation: The diagram above shows how the values are printed from left to right in the matrix.
+The last space in the matrix is set to -1.
+
+Constraints:
+
+	1 <= m, n <= 10^5
+	1 <= m * n <= 10^5
+	The number of nodes in the list is in the range [1, m * n].
+	0 <= Node.val <= 1000
+*/
+export function spiralMatrix(
+    m: number,
+    n: number,
+    head: ListNode | null
+): number[][] {
+    const ret: number[][] = Array.from({ length: m }, () => Array(n).fill(-1));
+
+    let cur = head;
+    let x1 = 0;
+    let y1 = 0;
+    let x2 = m - 1;
+    let y2 = n - 1;
+
+    const fill = () => {
+        // north
+        for (let j = y1; j <= y2; j++) {
+            if (!cur) {
+                return;
+            }
+
+            ret[x1][j] = cur.val;
+            cur = cur.next;
+        }
+
+        // east
+        for (let i = x1 + 1; i < x2; i++) {
+            if (!cur) {
+                return;
+            }
+
+            ret[i][y2] = cur.val;
+            cur = cur.next;
+        }
+
+        // south
+        for (let j = y2; j > y1; j--) {
+            if (!cur) {
+                return;
+            }
+
+            ret[x2][j] = cur.val;
+            cur = cur.next;
+        }
+
+        // west
+        for (let i = x2; i > x1; i--) {
+            if (!cur) {
+                return;
+            }
+
+            ret[i][y1] = cur.val;
+            cur = cur.next;
+        }
+    };
+
+    while (cur) {
+        fill();
+
+        x1++;
+        y1++;
+        x2--;
+        y2--;
+    }
+
+    return ret;
 }
