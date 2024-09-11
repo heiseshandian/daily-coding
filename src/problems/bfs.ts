bd00dc7b8f2eef5d8d6e29f4fe424a8798aedf97
zhanggq52434 <zhanggq52434@hundsun.com>
2024-09-11 22:41:19 +0800

    feat: 分层图最短路问题

--- src/problems/bfs.ts
+++ src/problems/bfs.ts
@@ -76,3 +76,127 @@ export function maxDistance(grid: number[][]): number {
 
     return level - 1;
 }
+
+/*
+https://leetcode.com/problems/shortest-path-to-get-all-keys/description/
+https://www.bilibili.com/list/8888480?sort_field=pubtime&spm_id_from=333.999.0.0&oid=704553330&bvid=BV1Cm4y1g77W
+864. Shortest Path to Get All Keys
+You are given an m x n grid grid where:
+
+	'.' is an empty cell.
+	'#' is a wall.
+	'@' is the starting point.
+	Lowercase letters represent keys.
+	Uppercase letters represent locks.
+
+You start at the starting point and one move consists of walking one space in one of the four cardinal directions. You cannot walk outside the grid, or walk into a wall.
+
+If you walk over a key, you can pick it up and you cannot walk over a lock unless you have its corresponding key.
+
+For some 1 <= k <= 6, there is exactly one lowercase and one uppercase letter of the first k letters of the English alphabet in the grid. This means that there is exactly one key for each lock, and one lock for each key; and also that the letters used to represent the keys and locks were chosen in the same order as the English alphabet.
+
+Return the lowest number of moves to acquire all keys. If it is impossible, return -1.
+
+Example 1:
+
+Input: grid = ["@.a..","###.#","b.A.B"]
+Output: 8
+Explanation: Note that the goal is to obtain all the keys not to open all the locks.
+
+Example 2:
+
+Input: grid = ["@..aA","..B#.","....b"]
+Output: 6
+
+Example 3:
+
+Input: grid = ["@Aa"]
+Output: -1
+
+Constraints:
+
+	m == grid.length
+	n == grid[i].length
+	1 <= m, n <= 30
+	grid[i][j] is either an English letter, '.', '#', or '@'. 
+	There is exactly one '@' in the grid.
+	The number of keys in the grid is in the range [1, 6].
+	Each key in the grid is unique.
+	Each key in the grid has a matching lock.
+*/
+const ALPHA_REG = /[a-zA-Z]/;
+
+export function shortestPathAllKeys(grid: string[]): number {
+    const m = grid.length;
+    const n = grid[0].length;
+    let alphas = 0;
+
+    const queue: Array<[x: number, y: number, status: number]> = [];
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (ALPHA_REG.test(grid[i][j])) {
+                alphas++;
+            }
+
+            if (grid[i][j] === '@') {
+                queue.push([i, j, 0]);
+            }
+        }
+    }
+    const keys = alphas >> 1;
+    const keysMap = toStatusMap('abcdef'.split(''));
+    const locksMap = toStatusMap('ABCDEF'.split(''));
+
+    const visited: boolean[][][] = Array.from({ length: m }, () =>
+        Array.from({ length: n }, () => Array(1 << keys).fill(false))
+    );
+    const [startX, startY, startStatus] = queue[0];
+    visited[startX][startY][startStatus] = true;
+
+    let level = 0;
+    const moves: number[] = [-1, 0, 1, 0, -1];
+    const target = (1 << keys) - 1;
+    while (queue.length) {
+        const q = queue.slice();
+        queue.length = 0;
+        level++;
+
+        for (let i = 0; i < q.length; i++) {
+            const [x, y, status] = q[i];
+            if (status === target) {
+                return level - 1;
+            }
+
+            for (let j = 0; j < moves.length - 1; j++) {
+                const nX = x + moves[j];
+                const nY = y + moves[j + 1];
+
+                if (
+                    nX >= 0 &&
+                    nX < m &&
+                    nY >= 0 &&
+                    nY < n &&
+                    grid[nX][nY] !== '#'
+                ) {
+                    const s = status | (keysMap[grid[nX][nY]] ?? 0);
+                    const lock = locksMap[grid[nX][nY]] ?? 0;
+                    const hasKey = lock & s;
+                    if (!visited[nX][nY][s] && (!lock || hasKey)) {
+                        visited[nX][nY][s] = true;
+                        queue.push([nX, nY, s]);
+                    }
+                }
+            }
+        }
+    }
+
+    return -1;
+}
+
+function toStatusMap(alphas: string[]) {
+    return alphas.reduce((acc, cur, i) => {
+        acc[cur] = 1 << i;
+        return acc;
+    }, {});
+}
