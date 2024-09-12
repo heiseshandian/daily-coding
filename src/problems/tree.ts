886f31a951ef2857cb870026aa272f2ec1629987
zhanggq52434 <zhanggq52434@hundsun.com>
2024-09-12 10:07:19 +0800

    feat: 每日一题

--- src/problems/tree.ts
+++ src/problems/tree.ts
@@ -1,6 +1,7 @@
 import { Queue } from '../algorithm/queue';
 import { TreeNode, getMaxDistance } from '../algorithm/tree';
 import { cache } from '../design-pattern/proxy';
+import { UnionFind } from '../algorithm/union-find';
 /* 
 https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
 
@@ -3290,3 +3291,46 @@ export const methodsOfConstructTrees = cache((n: number, m: number): number => {
 
     return methods;
 });
+
+/*
+https://leetcode.com/problems/redundant-connection/description/
+684. Redundant Connection
+In this problem, a tree is an undirected graph that is connected and has no cycles.
+
+You are given a graph that started as a tree with n nodes labeled from 1 to n, with one additional edge added. The added edge has two different vertices chosen from 1 to n, and was not an edge that already existed. The graph is represented as an array edges of length n where edges[i] = [ai, bi] indicates that there is an edge between nodes ai and bi in the graph.
+
+Return an edge that can be removed so that the resulting graph is a tree of n nodes. If there are multiple answers, return the answer that occurs last in the input.
+
+Example 1:
+
+Input: edges = [[1,2],[1,3],[2,3]]
+Output: [2,3]
+
+Example 2:
+
+Input: edges = [[1,2],[2,3],[3,4],[1,4],[1,5]]
+Output: [1,4]
+
+Constraints:
+
+	n == edges.length
+	3 <= n <= 1000
+	edges[i].length == 2
+	1 <= ai < bi <= edges.length
+	ai != bi
+	There are no repeated edges.
+	The given graph is connected.
+*/
+export function findRedundantConnection(edges: number[][]): number[] {
+    const n = edges.length;
+    const unionFind = new UnionFind(n + 1);
+    for (const [from, to] of edges) {
+        if (unionFind.isSameSet(from, to)) {
+            return [from, to];
+        }
+
+        unionFind.union(from, to);
+    }
+
+    return [];
+}
