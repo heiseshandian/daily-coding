161a47950553858a3a82e01872d3ed4f6e1aead8
zhanggq52434 <zhanggq52434@hundsun.com>
2024-09-13 22:08:49 +0800

    feat: 线段重合问题

--- src/problems/greedy.ts
+++ src/problems/greedy.ts
@@ -1056,3 +1056,45 @@ export function maxDistance(arrays: number[][]): number {
 
     return maxDistance;
 }
+
+/*
+https://www.nowcoder.com/practice/1ae8d0b6bb4e4bcdbf64ec491f63fc37
+线段重合
+
+描述：
+  每一个线段都有start和end两个数据项，表示这条线段在X轴上从start位置开始到end位置结束。
+  给定一批线段，求所有重合区域中最多重合了几个线段，首尾相接的线段不算重合。
+  例如：线段[1,2]和线段[2.3]不重合。
+  线段[1,3]和线段[2,3]重合
+    
+输入描述：
+  输入描述：
+
+输出描述：
+  输出描述：
+
+示例：
+输入：
+  3
+  1 2
+  2 3
+  1 3
+输出：
+  2
+*/
+export function getMaxOverlapLines(lines: number[][]): number {
+    const heap = new GenericHeap<number>((a, b) => a - b);
+    lines.sort((a, b) => a[0] - b[0]);
+
+    let max = 1;
+    lines.forEach(([start, end]) => {
+        while (!heap.isEmpty() && heap.peek() <= start) {
+            heap.pop();
+        }
+        heap.push(end);
+
+        max = Math.max(max, heap.size());
+    });
+
+    return max;
+}
