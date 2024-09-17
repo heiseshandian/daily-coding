f2b010de1ecdd52f7c111019387d564b55a8495f
zhanggq52434 <zhanggq52434@hundsun.com>
2024-09-17 08:29:44 +0800

    feat: 每日一题

--- src/problems/string.ts
+++ src/problems/string.ts
@@ -3228,3 +3228,62 @@ function reverseBits(char: string) {
 
     return parseInt(reversed, 2).toString(16).toUpperCase();
 }
+
+/*
+https://leetcode.com/problems/uncommon-words-from-two-sentences/description/?envType=daily-question&envId=2024-09-17
+884. Uncommon Words from Two Sentences
+A sentence is a string of single-space separated words where each word consists only of lowercase letters.
+
+A word is uncommon if it appears exactly once in one of the sentences, and does not appear in the other sentence.
+
+Given two sentences s1 and s2, return a list of all the uncommon words. You may return the answer in any order.
+
+Example 1:
+
+Input: s1 = "this apple is sweet", s2 = "this apple is sour"
+
+Output: ["sweet","sour"]
+
+Explanation:
+
+The word "sweet" appears only in s1, while the word "sour" appears only in s2.
+
+Example 2:
+
+Input: s1 = "apple apple", s2 = "banana"
+
+Output: ["banana"]
+
+Constraints:
+
+	1 <= s1.length, s2.length <= 200
+	s1 and s2 consist of lowercase English letters and spaces.
+	s1 and s2 do not have leading or trailing spaces.
+	All the words in s1 and s2 are separated by a single space.
+*/
+export function uncommonFromSentences(s1: string, s2: string): string[] {
+    const s1Map = countRepeatTimes(s1.split(' '));
+    const s2Map = countRepeatTimes(s2.split(' '));
+
+    const ret: string[] = [];
+    Object.keys(s1Map).forEach((k) => {
+        if (s1Map[k] === 1 && s2Map[k] === undefined) {
+            ret.push(k);
+        }
+    });
+
+    Object.keys(s2Map).forEach((k) => {
+        if (s2Map[k] === 1 && s1Map[k] === undefined) {
+            ret.push(k);
+        }
+    });
+
+    return ret;
+}
+
+function countRepeatTimes(chars: string[]) {
+    return chars.reduce((s, c) => {
+        s[c] = (s[c] ?? 0) + 1;
+        return s;
+    }, {});
+}
