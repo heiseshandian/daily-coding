051162caec2b3b8d39a9f91eefbe8189b8415428
zhanggq52434 <zhanggq52434@hundsun.com>
2024-09-18 15:53:39 +0800

    feat: 每日一题

--- src/problems/math.ts
+++ src/problems/math.ts
@@ -1084,3 +1084,32 @@ export function getLucky(s: string, k: number): number {
 
     return +num;
 }
+
+/*
+https://leetcode.com/problems/largest-number/description/
+179. Largest Number
+Given a list of non-negative integers nums, arrange them such that they form the largest number and return it.
+
+Since the result may be very large, so you need to return a string instead of an integer.
+
+Example 1:
+
+Input: nums = [10,2]
+Output: "210"
+
+Example 2:
+
+Input: nums = [3,30,34,5,9]
+Output: "9534330"
+
+Constraints:
+
+	1 <= nums.length <= 100
+	0 <= nums[i] <= 10^9
+*/
+export function largestNumber(nums: number[]): string {
+    return nums
+        .sort((a, b) => Number(BigInt(`${b}${a}`) - BigInt(`${a}${b}`)))
+        .join('')
+        .replace(/^0+/, '0');
+}
