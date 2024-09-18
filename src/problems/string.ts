b328c3d2421dd591c0d555374740276384098449
zhanggq52434 <zhanggq52434@hundsun.com>
2024-09-18 16:04:16 +0800

    feat: 反转字符串

--- src/problems/string.ts
+++ src/problems/string.ts
@@ -3287,3 +3287,46 @@ function countRepeatTimes(chars: string[]) {
         return s;
     }, {});
 }
+
+/*
+https://leetcode.com/problems/reverse-only-letters/description/
+917. Reverse Only Letters
+Given a string s, reverse the string according to the following rules:
+
+	All the characters that are not English letters remain in the same position.
+	All the English letters (lowercase or uppercase) should be reversed.
+
+Return s after reversing it.
+
+Example 1:
+Input: s = "ab-cd"
+Output: "dc-ba"
+Example 2:
+Input: s = "a-bC-dEf-ghIj"
+Output: "j-Ih-gfE-dCba"
+Example 3:
+Input: s = "Test1ng-Leet=code-Q!"
+Output: "Qedo1ct-eeLg=ntse-T!"
+
+Constraints:
+
+	1 <= s.length <= 100
+	s consists of characters with ASCII values in the range [33, 122].
+	s does not contain '\"' or '\\'.
+*/
+export function reverseOnlyLetters(s: string): string {
+    const chars = s.split('');
+    let left = 0;
+    let right = chars.length - 1;
+    while (left < right) {
+        if (!alphaReg.test(chars[left])) {
+            left++;
+        } else if (!alphaReg.test(chars[right])) {
+            right--;
+        } else {
+            swap(chars, left++, right--);
+        }
+    }
+
+    return chars.join('');
+}
