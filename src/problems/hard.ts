0418ca4264a59d981490914ef5e741316f4807e5
zhanggq52434 <zhanggq52434@hundsun.com>
2024-09-20 15:04:41 +0800

    feat: 每日一题

--- src/problems/hard.ts
+++ src/problems/hard.ts
@@ -2812,3 +2812,48 @@ export function shortestPalindrome(s: string): string {
 
     return prefix + s;
 }
+
+/*
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/description/
+188. Best Time to Buy and Sell Stock IV
+You are given an integer array prices where prices[i] is the price of a given stock on the ith day, and an integer k.
+
+Find the maximum profit you can achieve. You may complete at most k transactions: i.e. you may buy at most k times and sell at most k times.
+
+Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+
+Example 1:
+
+Input: k = 2, prices = [2,4,1]
+Output: 2
+Explanation: Buy on day 1 (price = 2) and sell on day 2 (price = 4), profit = 4-2 = 2.
+
+Example 2:
+
+Input: k = 2, prices = [3,2,6,5,0,3]
+Output: 7
+Explanation: Buy on day 2 (price = 2) and sell on day 3 (price = 6), profit = 6-2 = 4. Then buy on day 5 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+
+Constraints:
+
+	1 <= k <= 100
+	1 <= prices.length <= 1000
+	0 <= prices[i] <= 1000
+*/
+export function maxProfit(k: number, prices: number[]): number {
+    const dp: number[][] = Array.from({ length: k + 1 }, () =>
+        Array(prices.length).fill(0)
+    );
+
+    for (let i = 1; i <= k; i++) {
+        // https://www.bilibili.com/list/8888480?sort_field=pubtime&spm_id_from=333.999.0.0&oid=494277801&bvid=BV1PN411j7aG
+        // 通过画格子观察优化枚举行为
+        let prev = dp[i - 1][0] - prices[0];
+        for (let j = 1; j < prices.length; j++) {
+            prev = Math.max(prev, dp[i - 1][j - 1] - prices[j - 1]);
+            dp[i][j] = Math.max(prev + prices[j], dp[i][j - 1]);
+        }
+    }
+
+    return dp[k][prices.length - 1];
+}
