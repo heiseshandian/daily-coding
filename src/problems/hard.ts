b969d5a0cb52e0d533a587c14a0b1cf9a9622ca6
zhanggq52434 <zhanggq52434@hundsun.com>
2024-09-20 10:57:14 +0800

    feat: 每日一题

--- src/problems/hard.ts
+++ src/problems/hard.ts
@@ -2765,3 +2765,50 @@ export function largestComponentSize(nums: number[]): number {
 
     return Math.max(...unionFind.size);
 }
+
+/*
+https://leetcode.com/problems/shortest-palindrome/description/
+214. Shortest Palindrome
+You are given a string s. You can convert s to a palindrome by adding characters in front of it.
+
+Return the shortest palindrome you can find by performing this transformation.
+
+Example 1:
+Input: s = "aacecaaa"
+Output: "aaacecaaa"
+Example 2:
+Input: s = "abcd"
+Output: "dcbabcd"
+
+Constraints:
+
+	0 <= s.length <= 10^4
+	s consists of lowercase English letters only.
+*/
+export function shortestPalindrome(s: string): string {
+    const isPalindrome = (left: number, right: number): boolean => {
+        while (left < right) {
+            if (s[left++] !== s[right--]) {
+                return false;
+            }
+        }
+
+        return true;
+    };
+
+    let i = s.length - 1;
+    while (i > 0) {
+        if (isPalindrome(0, i)) {
+            break;
+        }
+        i--;
+    }
+
+    const prefix = s
+        .slice(i + 1)
+        .split('')
+        .reverse()
+        .join('');
+
+    return prefix + s;
+}
