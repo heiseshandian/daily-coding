29d00eed3c1942d2d9c0368eee41b4c2322337c2
zhanggq52434 <zhanggq52434@hundsun.com>
2024-09-23 17:34:18 +0800

    feat: 每日一题

--- src/problems/string.ts
+++ src/problems/string.ts
@@ -3330,3 +3330,67 @@ export function reverseOnlyLetters(s: string): string {
 
     return chars.join('');
 }
+
+/*
+https://leetcode.com/problems/zigzag-conversion/description/
+6. Zigzag Conversion
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+
+And then read line by line: "PAHNAPLSIIGYIR"
+
+Write the code that will take a string and make this conversion given a number of rows:
+
+string convert(string s, int numRows);
+
+Example 1:
+
+Input: s = "PAYPALISHIRING", numRows = 3
+Output: "PAHNAPLSIIGYIR"
+
+Example 2:
+
+Input: s = "PAYPALISHIRING", numRows = 4
+Output: "PINALSIGYAHRPI"
+Explanation:
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+
+Example 3:
+
+Input: s = "A", numRows = 1
+Output: "A"
+
+Constraints:
+
+	1 <= s.length <= 1000
+	s consists of English letters (lower-case and upper-case), ',' and '.'.
+	1 <= numRows <= 1000
+*/
+export function convert(s: string, numRows: number): string {
+    const m = numRows;
+    const grouns = Math.ceil(s.length / (m + Math.max(0, m - 2)));
+    const n = grouns + Math.max(0, m - 2);
+    const matrix: string[][] = Array.from({ length: m }, () => Array(n));
+
+    let x = 0;
+    let y = 0;
+    let i = 0;
+    while (i < s.length) {
+        for (x = 0; x < m; x++) {
+            matrix[x][y] = s[i++];
+        }
+
+        for (x = m - 2; x > 0; x--) {
+            matrix[x][++y] = s[i++];
+        }
+        y++;
+    }
+
+    return matrix.reduce((acc, row) => acc + row.filter((v) => v).join(''), '');
+}
