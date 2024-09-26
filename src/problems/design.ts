c9dc9409875ac79f8c9c4bad04acccb24b3653ef
zhanggq52434 <zhanggq52434@hundsun.com>
2024-09-26 15:38:14 +0800

    feat: 每日一题

--- src/problems/design.ts
+++ src/problems/design.ts
@@ -2039,3 +2039,80 @@ export class NumArray {
         return this.prefix[right] - before;
     }
 }
+
+/*
+https://leetcode.com/problems/my-calendar-i/description/?envType=daily-question&envId=2024-09-26
+729. My Calendar I
+You are implementing a program to use as your calendar. We can add a new event if adding the event will not cause a double booking.
+
+A double booking happens when two events have some non-empty intersection (i.e., some moment is common to both events.).
+
+The event can be represented as a pair of integers start and end that represents a booking on the half-open interval [start, end), the range of real numbers x such that start <= x < end.
+
+Implement the MyCalendar class:
+
+	MyCalendar() Initializes the calendar object.
+	boolean book(int start, int end) Returns true if the event can be added to the calendar successfully without causing a double booking. Otherwise, return false and do not add the event to the calendar.
+
+Example 1:
+
+Input
+["MyCalendar", "book", "book", "book"]
+[[], [10, 20], [15, 25], [20, 30]]
+Output
+[null, true, false, true]
+
+Explanation
+MyCalendar myCalendar = new MyCalendar();
+myCalendar.book(10, 20); // return True
+myCalendar.book(15, 25); // return False, It can not be booked because time 15 is already booked by another event.
+myCalendar.book(20, 30); // return True, The event can be booked, as the first event takes every time less than 20, but not including 20.
+
+Constraints:
+
+	0 <= start < end <= 10^9
+	At most 1000 calls will be made to book.
+*/
+export class MyCalendar {
+    private booked: number[][];
+
+    constructor() {
+        this.booked = [];
+    }
+
+    book(start: number, end: number): boolean {
+        const { booked } = this;
+        let left = 0;
+        let right = booked.length - 1;
+        let closest = -1;
+        while (left <= right) {
+            const mid = left + ((right - left) >> 1);
+            if (booked[mid][1] <= start) {
+                closest = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        if (closest === -1) {
+            if (
+                booked.length === 0 ||
+                (booked.length > 0 && booked[0][0] >= end)
+            ) {
+                booked.unshift([start, end]);
+                return true;
+            }
+            return false;
+        }
+        if (booked.length === closest + 1) {
+            booked.push([start, end]);
+            return true;
+        }
+        if (booked[closest + 1][0] < end) {
+            return false;
+        }
+        booked.splice(closest + 1, 0, [start, end]);
+        return true;
+    }
+}
