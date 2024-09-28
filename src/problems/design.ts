fbd7e65d4b5524f0983f06121fa9d14b1136c948
zhanggq52434 <zhanggq52434@hundsun.com>
2024-09-28 10:31:36 +0800

    feat: 每日一题

--- src/problems/design.ts
+++ src/problems/design.ts
@@ -2116,3 +2116,107 @@ export class MyCalendar {
         return true;
     }
 }
+
+/*
+https://leetcode.com/problems/design-circular-deque/description/
+641. Design Circular Deque
+Design your implementation of the circular double-ended queue (deque).
+
+Implement the MyCircularDeque class:
+
+	MyCircularDeque(int k) Initializes the deque with a maximum size of k.
+	boolean insertFront() Adds an item at the front of Deque. Returns true if the operation is successful, or false otherwise.
+	boolean insertLast() Adds an item at the rear of Deque. Returns true if the operation is successful, or false otherwise.
+	boolean deleteFront() Deletes an item from the front of Deque. Returns true if the operation is successful, or false otherwise.
+	boolean deleteLast() Deletes an item from the rear of Deque. Returns true if the operation is successful, or false otherwise.
+	int getFront() Returns the front item from the Deque. Returns -1 if the deque is empty.
+	int getRear() Returns the last item from Deque. Returns -1 if the deque is empty.
+	boolean isEmpty() Returns true if the deque is empty, or false otherwise.
+	boolean isFull() Returns true if the deque is full, or false otherwise.
+
+Example 1:
+
+Input
+["MyCircularDeque", "insertLast", "insertLast", "insertFront", "insertFront", "getRear", "isFull", "deleteLast", "insertFront", "getFront"]
+[[3], [1], [2], [3], [4], [], [], [], [4], []]
+Output
+[null, true, true, true, false, 2, true, true, true, 4]
+
+Explanation
+MyCircularDeque myCircularDeque = new MyCircularDeque(3);
+myCircularDeque.insertLast(1);  // return True
+myCircularDeque.insertLast(2);  // return True
+myCircularDeque.insertFront(3); // return True
+myCircularDeque.insertFront(4); // return False, the queue is full.
+myCircularDeque.getRear();      // return 2
+myCircularDeque.isFull();       // return True
+myCircularDeque.deleteLast();   // return True
+myCircularDeque.insertFront(4); // return True
+myCircularDeque.getFront();     // return 4
+
+Constraints:
+
+	1 <= k <= 1000
+	0 <= value <= 1000
+	At most 2000 calls will be made to insertFront, insertLast, deleteFront, deleteLast, getFront, getRear, isEmpty, isFull.
+*/
+export class MyCircularDeque {
+    private queue: number[];
+    private k: number;
+
+    constructor(k: number) {
+        this.queue = [];
+        this.k = k;
+    }
+
+    insertFront(value: number): boolean {
+        if (this.isFull()) {
+            return false;
+        }
+
+        this.queue.unshift(value);
+        return true;
+    }
+
+    insertLast(value: number): boolean {
+        if (this.isFull()) {
+            return false;
+        }
+
+        this.queue.push(value);
+        return true;
+    }
+
+    deleteFront(): boolean {
+        if (!this.isEmpty()) {
+            this.queue.splice(0, 1);
+            return true;
+        }
+        return false;
+    }
+
+    deleteLast(): boolean {
+        if (!this.isEmpty()) {
+            this.queue.length--;
+            return true;
+        }
+
+        return false;
+    }
+
+    getFront(): number {
+        return this.queue[0] ?? -1;
+    }
+
+    getRear(): number {
+        return this.queue.at(-1) ?? -1;
+    }
+
+    isEmpty(): boolean {
+        return this.queue.length === 0;
+    }
+
+    isFull(): boolean {
+        return this.queue.length === this.k;
+    }
+}
