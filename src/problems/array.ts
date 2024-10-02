e62a2035add1f4f2c9ebb7b6ce33f1a6c24eb791
zhanggq52434 <zhanggq52434@hundsun.com>
2024-10-02 10:02:15 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -7829,3 +7829,54 @@ export function canArrange(arr: number[], k: number): boolean {
 
     return freq[0] % 2 === 0;
 }
+
+/*
+https://leetcode.com/problems/rank-transform-of-an-array/description/
+1331. Rank Transform of an Array
+Given an array of integers arr, replace each element with its rank.
+
+The rank represents how large the element is. The rank has the following rules:
+
+	Rank is an integer starting from 1.
+	The larger the element, the larger the rank. If two elements are equal, their rank must be the same.
+	Rank should be as small as possible.
+
+Example 1:
+
+Input: arr = [40,10,20,30]
+Output: [4,1,2,3]
+Explanation: 40 is the largest element. 10 is the smallest. 20 is the second smallest. 30 is the third smallest.
+
+Example 2:
+
+Input: arr = [100,100,100]
+Output: [1,1,1]
+Explanation: Same elements share the same rank.
+
+Example 3:
+
+Input: arr = [37,12,28,9,100,56,80,5,12]
+Output: [5,3,4,2,8,6,7,1,3]
+
+Constraints:
+
+	0 <= arr.length <= 10^5
+	-10^9 <= arr[i] <= 10^9
+*/
+export function arrayRankTransform(arr: number[]): number[] {
+    const sorted = arr.map((v, i) => [v, i]).sort((a, b) => a[0] - b[0]);
+    const ret = Array(arr.length);
+    let rank = 0;
+    let prev = -1;
+    for (const [v, i] of sorted) {
+        if (v === prev) {
+            ret[i] = rank;
+        } else {
+            ret[i] = ++rank;
+        }
+
+        prev = v;
+    }
+
+    return ret;
+}
