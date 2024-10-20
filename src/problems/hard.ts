ef8403e59661eeddd2bbc68d6470a3dd97b3f195
zhanggq52434 <zhanggq52434@hundsun.com>
2024-10-20 11:08:28 +0800

    feat: 每日一题

--- src/problems/hard.ts
+++ src/problems/hard.ts
@@ -3132,3 +3132,83 @@ export function smallestRange(nums: number[][]): number[] {
 
     return [left, right];
 }
+
+/*
+https://leetcode.com/problems/parsing-a-boolean-expression/description/
+1106. Parsing A Boolean Expression
+A boolean expression is an expression that evaluates to either true or false. It can be in one of the following shapes:
+
+	't' that evaluates to true.
+	'f' that evaluates to false.
+	'!(subExpr)' that evaluates to the logical NOT of the inner expression subExpr.
+	'&(subExpr1, subExpr2, ..., subExprn)' that evaluates to the logical AND of the inner expressions subExpr1, subExpr2, ..., subExprn where n >= 1.
+	'|(subExpr1, subExpr2, ..., subExprn)' that evaluates to the logical OR of the inner expressions subExpr1, subExpr2, ..., subExprn where n >= 1.
+
+Given a string expression that represents a boolean expression, return the evaluation of that expression.
+
+It is guaranteed that the given expression is valid and follows the given rules.
+
+Example 1:
+
+Input: expression = "&(|(f))"
+Output: false
+Explanation: 
+First, evaluate |(f) --> f. The expression is now "&(f)".
+Then, evaluate &(f) --> f. The expression is now "f".
+Finally, return false.
+
+Example 2:
+
+Input: expression = "|(f,f,f,t)"
+Output: true
+Explanation: The evaluation of (false OR false OR false OR true) is true.
+
+Example 3:
+
+Input: expression = "!(&(f,t))"
+Output: true
+Explanation: 
+First, evaluate &(f,t) --> (false AND true) --> false --> f. The expression is now "!(f)".
+Then, evaluate !(f) --> NOT false --> true. We return true.
+
+Constraints:
+
+	1 <= expression.length <= 10^4
+	expression[i] is one following characters: '(', ')', '&', '|', '!', 't', 'f', and ','.
+*/
+export function parseBoolExpr(expression: string): boolean {
+    const stack: string[] = [];
+
+    for (let i = 0; i < expression.length; i++) {
+        const char = expression[i];
+        if (char === '(') {
+            continue;
+        }
+        if (char === ')') {
+            const vals: string[] = [];
+            while (stack.length > 0) {
+                if (stack.at(-1) === '!') {
+                    stack.pop();
+                    stack.push(vals[0] === 't' ? 'f' : 't');
+                    break;
+                }
+                if (stack.at(-1) === '|') {
+                    stack.pop();
+                    stack.push(vals.includes('t') ? 't' : 'f');
+                    break;
+                }
+                if (stack.at(-1) === '&') {
+                    stack.pop();
+                    stack.push(vals.includes('f') ? 'f' : 't');
+                    break;
+                }
+
+                vals.push(stack.pop()!);
+            }
+            continue;
+        }
+        stack.push(char);
+    }
+
+    return stack[0] === 't';
+}
