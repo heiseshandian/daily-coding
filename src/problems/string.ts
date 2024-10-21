da3ec8804fa18de0bb8ecdc3c099c680a0577bd0
zhanggq52434 <zhanggq52434@hundsun.com>
2024-10-21 12:52:37 +0800

    feat: 每日一题

--- src/problems/string.ts
+++ src/problems/string.ts
@@ -3672,3 +3672,72 @@ export function maxUniqueSplit(s: string): number {
 
     return dfs(0, new Set());
 }
+
+/*
+https://leetcode.com/problems/number-of-ways-to-split-a-string/description/
+1573. Number of Ways to Split a String
+Given a binary string s, you can split s into 3 non-empty strings s1, s2, and s3 where s1 + s2 + s3 = s.
+
+Return the number of ways s can be split such that the number of ones is the same in s1, s2, and s3. Since the answer may be too large, return it modulo 109 + 7.
+
+Example 1:
+
+Input: s = "10101"
+Output: 4
+Explanation: There are four ways to split s in 3 parts where each part contain the same number of letters '1'.
+"1|010|1"
+"1|01|01"
+"10|10|1"
+"10|1|01"
+
+Example 2:
+
+Input: s = "1001"
+Output: 0
+
+Example 3:
+
+Input: s = "0000"
+Output: 3
+Explanation: There are three ways to split s in 3 parts.
+"0|0|00"
+"0|00|0"
+"00|0|0"
+
+Constraints:
+
+	3 <= s.length <= 10^5
+	s[i] is either '0' or '1'.
+*/
+export function numWays(s: string): number {
+    let sum = 0;
+    for (let i = 0; i < s.length; i++) {
+        sum += +s[i];
+    }
+    if (sum % 3 !== 0) {
+        return 0;
+    }
+
+    const MOD = 1e9 + 7;
+    if (sum === 0) {
+        return (((s.length - 1) * (s.length - 2)) / 2) % MOD;
+    }
+
+    const firstPart = sum / 3;
+    let firstWays = 0;
+    const secondPart = firstPart << 1;
+    let secondWays = 0;
+    let count = 0;
+    for (let i = 0; i < s.length; i++) {
+        count += +s[i];
+
+        if (count === firstPart) {
+            firstWays++;
+        }
+        if (count === secondPart) {
+            secondWays++;
+        }
+    }
+
+    return (firstWays * secondWays) % MOD;
+}
