09b70e6ffdf03edf5a8fe43615b959a338a55551
zhanggq52434 <zhanggq52434@hundsun.com>
2024-10-21 10:49:45 +0800

    feat: 每日一题

--- src/problems/string.ts
+++ src/problems/string.ts
@@ -3615,3 +3615,60 @@ export function findKthBit(n: number, k: number): string {
 
     return bits[k - 1];
 }
+
+/*
+https://leetcode.com/problems/split-a-string-into-the-max-number-of-unique-substrings/description/
+1593. Split a String Into the Max Number of Unique Substrings
+Given a string s, return the maximum number of unique substrings that the given string can be split into.
+
+You can split string s into any list of non-empty substrings, where the concatenation of the substrings forms the original string. However, you must split the substrings such that all of them are unique.
+
+A substring is a contiguous sequence of characters within a string.
+
+Example 1:
+
+Input: s = "ababccc"
+Output: 5
+Explanation: One way to split maximally is ['a', 'b', 'ab', 'c', 'cc']. Splitting like ['a', 'b', 'a', 'b', 'c', 'cc'] is not valid as you have 'a' and 'b' multiple times.
+
+Example 2:
+
+Input: s = "aba"
+Output: 2
+Explanation: One way to split maximally is ['a', 'ba'].
+
+Example 3:
+
+Input: s = "aa"
+Output: 1
+Explanation: It is impossible to split the string any further.
+
+Constraints:
+
+	1 <= s.length <= 16
+
+	s contains only lower case English letters.
+*/
+export function maxUniqueSplit(s: string): number {
+    const dfs = (i: number, visited: Set<string>): number => {
+        if (i === s.length) {
+            return visited.size;
+        }
+
+        let next = '';
+        let max = 0;
+        for (let j = i; j < s.length; j++) {
+            next += s[j];
+
+            if (!visited.has(next)) {
+                visited.add(next);
+                max = Math.max(max, dfs(j + 1, visited));
+                visited.delete(next);
+            }
+        }
+
+        return max;
+    };
+
+    return dfs(0, new Set());
+}
