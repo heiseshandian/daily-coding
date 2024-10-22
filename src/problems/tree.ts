81d0471fb7073b862aaeb4d7d2e140b5d19a77be
zhanggq52434 <zhanggq52434@hundsun.com>
2024-10-22 10:37:04 +0800

    feat: 每日一题

--- src/problems/tree.ts
+++ src/problems/tree.ts
@@ -2,6 +2,7 @@ import { Queue } from '../algorithm/queue';
 import { TreeNode, getMaxDistance } from '../algorithm/tree';
 import { cache } from '../design-pattern/proxy';
 import { UnionFind } from '../algorithm/union-find';
+import { GenericHeap } from '../algorithm/generic-heap';
 /* 
 https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
 
@@ -3388,4 +3389,79 @@ export function findRedundantConnection(edges: number[][]): number[] {
     }
 
     return [];
+}
+
+/*
+https://leetcode.com/problems/kth-largest-sum-in-a-binary-tree/description/
+2583. Kth Largest Sum in a Binary Tree
+You are given the root of a binary tree and a positive integer k.
+
+The level sum in the tree is the sum of the values of the nodes that are on the same level.
+
+Return the kth largest level sum in the tree (not necessarily distinct). If there are fewer than k levels in the tree, return -1.
+
+Note that two nodes are on the same level if they have the same distance from the root.
+
+Example 1:
+
+Input: root = [5,8,9,2,1,3,7,4,6], k = 2
+Output: 13
+Explanation: The level sums are the following:
+- Level 1: 5.
+- Level 2: 8 + 9 = 17.
+- Level 3: 2 + 1 + 3 + 7 = 13.
+- Level 4: 4 + 6 = 10.
+The 2nd largest level sum is 13.
+
+Example 2:
+
+Input: root = [1,2,null,3], k = 1
+Output: 3
+Explanation: The largest level sum is 3.
+
+Constraints:
+
+	The number of nodes in the tree is n.
+	2 <= n <= 10^5
+	1 <= Node.val <= 10^6
+	1 <= k <= n
+*/
+export function kthLargestLevelSum(root: TreeNode, k: number): number {
+    const minHeap = new GenericHeap((a, b) => a - b);
+    const queue: TreeNode[] = Array((1e5 >> 1) + 1);
+    // 使用静态数组模拟队列，避免频繁分配释放空间以及函数调用导致的性能损耗，大幅优化性能
+    let left = -1;
+    let right = -1;
+    queue[++right] = root;
+
+    let curEnd = root;
+    let nextEnd = root;
+    let sum = 0;
+    while (left < right) {
+        const node = queue[++left];
+        sum += node.val;
+        if (node.left) {
+            queue[++right] = node.left;
+            nextEnd = node.left;
+        }
+        if (node.right) {
+            queue[++right] = node.right;
+            nextEnd = node.right;
+        }
+
+        if (curEnd === node) {
+            curEnd = nextEnd;
+
+            if (minHeap.size() < k) {
+                minHeap.push(sum);
+            } else if (minHeap.peek() < sum) {
+                minHeap.pop();
+                minHeap.push(sum);
+            }
+
+            sum = 0;
+        }
+    }
+
+    return minHeap.size() < k ? -1 : minHeap.pop();
 }
