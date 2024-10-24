52187af644f4106aa5f52b12e6b63f9a9bed2273
zhanggq52434 <zhanggq52434@hundsun.com>
2024-10-24 09:24:26 +0800

    feat: 每日一题

--- src/problems/tree.ts
+++ src/problems/tree.ts
@@ -3470,3 +3470,50 @@ export function kthLargestLevelSum(root: TreeNode, k: number): number {
 
     return minHeap.size() < k ? -1 : minHeap.pop();
 }
+
+/*
+https://leetcode.com/problems/flip-equivalent-binary-trees/description/
+951. Flip Equivalent Binary Trees
+For a binary tree T, we can define a flip operation as follows: choose any node, and swap the left and right child subtrees.
+
+A binary tree X is flip equivalent to a binary tree Y if and only if we can make X equal to Y after some number of flip operations.
+
+Given the roots of two binary trees root1 and root2, return true if the two trees are flip equivalent or false otherwise.
+
+Example 1:
+
+Input: root1 = [1,2,3,4,5,6,null,null,null,7,8], root2 = [1,3,2,null,6,4,5,null,null,null,null,8,7]
+Output: true
+Explanation: We flipped at nodes with values 1, 3, and 5.
+
+Example 2:
+
+Input: root1 = [], root2 = []
+Output: true
+
+Example 3:
+
+Input: root1 = [], root2 = [1]
+Output: false
+
+Constraints:
+
+	The number of nodes in each tree is in the range [0, 100].
+	Each tree will have unique node values in the range [0, 99].
+*/
+export function flipEquiv(
+    root1: TreeNode | null,
+    root2: TreeNode | null
+): boolean {
+    if (root1 === null || root2 === null) {
+        return root1 === root2;
+    }
+
+    return (
+        root1.val === root2.val &&
+        ((flipEquiv(root1.left, root2.left) &&
+            flipEquiv(root1.right, root2.right)) ||
+            (flipEquiv(root1.left, root2.right) &&
+                flipEquiv(root1.right, root2.left)))
+    );
+}
