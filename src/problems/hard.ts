00a95e17a0f7c46c7b4f83f34d06fbb9702daa04
zhanggq52434 <zhanggq52434@hundsun.com>
2024-10-28 09:12:07 +0800

    feat: 树型dp问题

--- src/problems/hard.ts
+++ src/problems/hard.ts
@@ -10,6 +10,7 @@ import { SlidingWindow } from '../algorithm/sliding-window';
 import { GenericHeap } from '../algorithm/generic-heap';
 import { UnionFind } from '../algorithm/union-find';
 import { UnionFindWithSize } from '../algorithm/union-find-with-size';
+import { TreeNode } from '../algorithm/tree';
 
 /*
 https://leetcode.com/problems/find-the-longest-valid-obstacle-course-at-each-position/description/
@@ -3211,4 +3212,85 @@ export function parseBoolExpr(expression: string): boolean {
     }
 
     return stack[0] === 't';
+}
+
+/*
+https://leetcode.com/problems/height-of-binary-tree-after-subtree-removal-queries/description/?envType=daily-question&envId=2024-10-26
+2458. Height of Binary Tree After Subtree Removal Queries
+You are given the root of a binary tree with n nodes. Each node is assigned a unique value from 1 to n. You are also given an array queries of size m.
+
+You have to perform m independent queries on the tree where in the ith query you do the following:
+
+	Remove the subtree rooted at the node with the value queries[i] from the tree. It is guaranteed that queries[i] will not be equal to the value of the root.
+
+Return an array answer of size m where answer[i] is the height of the tree after performing the ith query.
+
+Note:
+
+	The queries are independent, so the tree returns to its initial state after each query.
+	The height of a tree is the number of edges in the longest simple path from the root to some node in the tree.
+
+Example 1:
+
+Input: root = [1,3,4,2,null,6,5,null,null,null,null,null,7], queries = [4]
+Output: [2]
+Explanation: The diagram above shows the tree after removing the subtree rooted at node with value 4.
+The height of the tree is 2 (The path 1 -> 3 -> 2).
+
+Example 2:
+
+Input: root = [5,8,9,2,1,3,7,4,6], queries = [3,2,4,8]
+Output: [3,2,3,2]
+Explanation: We have the following queries:
+- Removing the subtree rooted at node with value 3. The height of the tree becomes 3 (The path 5 -> 8 -> 2 -> 4).
+- Removing the subtree rooted at node with value 2. The height of the tree becomes 2 (The path 5 -> 8 -> 1).
+- Removing the subtree rooted at node with value 4. The height of the tree becomes 3 (The path 5 -> 8 -> 2 -> 6).
+- Removing the subtree rooted at node with value 8. The height of the tree becomes 2 (The path 5 -> 9 -> 3).
+
+Constraints:
+
+	The number of nodes in the tree is n.
+	2 <= n <= 10^5
+	1 <= Node.val <= n
+	All the values in the tree are unique.
+	m == queries.length
+	1 <= m <= min(n, 104)
+	1 <= queries[i] <= n
+	queries[i] != root.val
+*/
+export function treeQueries(
+    root: TreeNode | null,
+    queries: number[]
+): number[] {
+    const heights = [0];
+
+    const getHeight = (node: TreeNode | null) => {
+        if (!node) {
+            return 0;
+        }
+
+        const left = getHeight(node.left);
+        const right = getHeight(node.right);
+        heights[node.val] = Math.max(left, right) + 1;
+        return heights[node.val];
+    };
+
+    const dfs = (node: TreeNode | null, depth: number, maxHeight: number) => {
+        if (!node) {
+            return;
+        }
+        // update heights without current node
+        heights[node.val] = maxHeight;
+
+        const left = node.left ? heights[node.left.val] : 0;
+        const right = node.right ? heights[node.right.val] : 0;
+
+        dfs(node.left, depth + 1, Math.max(maxHeight, depth + right));
+        dfs(node.right, depth + 1, Math.max(maxHeight, depth + left));
+    };
+
+    getHeight(root);
+    dfs(root, 0, 0);
+
+    return queries.map((v) => heights[v]);
 }
