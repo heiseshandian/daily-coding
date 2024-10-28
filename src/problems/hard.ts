2bc81cd84c4e11b80c28caac50ee3ec154602dc3
zhanggq52434 <zhanggq52434@hundsun.com>
2024-10-28 14:57:55 +0800

    feat: 每日一题

--- src/problems/hard.ts
+++ src/problems/hard.ts
@@ -3294,3 +3294,107 @@ export function treeQueries(
 
     return queries.map((v) => heights[v]);
 }
+
+/*
+https://leetcode.com/problems/text-justification/description/
+68. Text Justification
+Given an array of strings words and a width maxWidth, format the text such that each line has exactly maxWidth characters and is fully (left and right) justified.
+
+You should pack your words in a greedy approach; that is, pack as many words as you can in each line. Pad extra spaces ' ' when necessary so that each line has exactly maxWidth characters.
+
+Extra spaces between words should be distributed as evenly as possible. If the number of spaces on a line does not divide evenly between words, the empty slots on the left will be assigned more spaces than the slots on the right.
+
+For the last line of text, it should be left-justified, and no extra space is inserted between words.
+
+Note:
+
+	A word is defined as a character sequence consisting of non-space characters only.
+	Each word's length is guaranteed to be greater than 0 and not exceed maxWidth.
+	The input array words contains at least one word.
+
+Example 1:
+
+Input: words = ["This", "is", "an", "example", "of", "text", "justification."], maxWidth = 16
+Output:
+[
+   "This    is    an",
+   "example  of text",
+   "justification.  "
+]
+
+Example 2:
+
+Input: words = ["What","must","be","acknowledgment","shall","be"], maxWidth = 16
+Output:
+[
+  "What   must   be",
+  "acknowledgment  ",
+  "shall be        "
+]
+Explanation: Note that the last line is "shall be    " instead of "shall     be", because the last line must be left-justified instead of fully-justified.
+Note that the second line is also left-justified because it contains only one word.
+
+Example 3:
+
+Input: words = ["Science","is","what","we","understand","well","enough","to","explain","to","a","computer.","Art","is","everything","else","we","do"], maxWidth = 20
+Output:
+[
+  "Science  is  what we",
+  "understand      well",
+  "enough to explain to",
+  "a  computer.  Art is",
+  "everything  else  we",
+  "do                  "
+]
+
+Constraints:
+
+	1 <= words.length <= 300
+	1 <= words[i].length <= 20
+	words[i] consists of only English letters and symbols.
+	1 <= maxWidth <= 100
+	words[i].length <= maxWidth
+*/
+export function fullJustify(words: string[], maxWidth: number): string[] {
+    const ret: string[] = [];
+    let line = words[0];
+    for (let i = 1; i < words.length; i++) {
+        const char = words[i];
+
+        if ((line + ' ' + char).length <= maxWidth) {
+            line += ' ' + char;
+        } else {
+            ret.push(justifyLine(line, maxWidth));
+            line = char;
+        }
+    }
+    const last = line.split(' ').join(' ');
+    ret.push(last.padEnd(maxWidth, ' '));
+
+    return ret;
+}
+
+function justifyLine(line: string, maxWidth: number): string {
+    const w = line.split(' ');
+    if (w.length === 1) {
+        return w[0] + ' '.repeat(maxWidth - w[0].length);
+    }
+
+    const wordsWidth = w.reduce((s, v) => s + v.length, 0);
+    const spaceWidth = maxWidth - wordsWidth;
+    const spaceCount = w.length - 1;
+    const base = (spaceWidth / spaceCount) | 0;
+    const extra = spaceWidth % spaceCount;
+
+    let l = '';
+    let i = 0;
+    while (i < extra) {
+        l += w[i++] + ' '.repeat(base + 1);
+    }
+    while (i < w.length - 1) {
+        l += w[i++] + ' '.repeat(base);
+    }
+    l += w[i];
+
+    return l;
+}
