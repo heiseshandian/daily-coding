2f3438f23d3a691e944864c6e753905a760924b4
zhanggq52434 <zhanggq52434@hundsun.com>
2024-10-28 08:24:04 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -8108,3 +8108,75 @@ export function removeSubfolders(folder: string[]): string[] {
 
     return ret;
 }
+
+/*
+https://leetcode.com/problems/longest-square-streak-in-an-array/description/
+2501. Longest Square Streak in an Array
+You are given an integer array nums. A subsequence of nums is called a square streak if:
+
+	The length of the subsequence is at least 2, and
+	after sorting the subsequence, each element (except the first element) is the square of the previous number.
+
+Return the length of the longest square streak in nums, or return -1 if there is no square streak.
+
+A subsequence is an array that can be derived from another array by deleting some or no elements without changing the order of the remaining elements.
+
+Example 1:
+
+Input: nums = [4,3,6,16,8,2]
+Output: 3
+Explanation: Choose the subsequence [4,16,2]. After sorting it, it becomes [2,4,16].
+- 4 = 2 * 2.
+- 16 = 4 * 4.
+Therefore, [4,16,2] is a square streak.
+It can be shown that every subsequence of length 4 is not a square streak.
+
+Example 2:
+
+Input: nums = [2,3,5,6,7]
+Output: -1
+Explanation: There is no square streak in nums so return -1.
+
+Constraints:
+
+	2 <= nums.length <= 10^5
+	2 <= nums[i] <= 10^5
+*/
+export function longestSquareStreak(nums: number[]): number {
+    nums.sort((a, b) => a - b);
+    let max = -1;
+    for (let i = 0; i < nums.length; i++) {
+        let next = nums[i] * nums[i];
+        let start = i + 1;
+        let count = 1;
+        while (next <= nums.at(-1)!) {
+            let left = start;
+            let right = nums.length - 1;
+            let found = false;
+            while (left <= right) {
+                const mid = left + ((right - left) >> 1);
+                if (nums[mid] === next) {
+                    count++;
+                    start = mid + 1;
+                    next = next * next;
+                    found = true;
+                    break;
+                }
+
+                if (nums[mid] < next) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+
+            if (!found) {
+                break;
+            }
+
+            max = Math.max(max, count);
+        }
+    }
+
+    return max;
+}
