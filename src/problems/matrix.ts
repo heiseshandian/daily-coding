772b11eb92f76f74beb2c30381b6ecd50f59bf88
zhanggq52434 <zhanggq52434@hundsun.com>
2024-10-29 11:33:21 +0800

    feat: 每日一题

--- src/problems/matrix.ts
+++ src/problems/matrix.ts
@@ -2154,3 +2154,82 @@ export function countSquares(matrix: number[][]): number {
 
     return count;
 }
+
+/*
+https://leetcode.com/problems/maximum-number-of-fish-in-a-grid/description/
+2658. Maximum Number of Fish in a Grid
+You are given a 0-indexed 2D matrix grid of size m x n, where (r, c) represents:
+
+	A land cell if grid[r][c] = 0, or
+	A water cell containing grid[r][c] fish, if grid[r][c] > 0.
+
+A fisher can start at any water cell (r, c) and can do the following operations any number of times:
+
+	Catch all the fish at cell (r, c), or
+	Move to any adjacent water cell.
+
+Return the maximum number of fish the fisher can catch if he chooses his starting cell optimally, or 0 if no water cell exists.
+
+An adjacent cell of the cell (r, c), is one of the cells (r, c + 1), (r, c - 1), (r + 1, c) or (r - 1, c) if it exists.
+
+Example 1:
+
+Input: grid = [[0,2,1,0],[4,0,0,3],[1,0,0,4],[0,3,2,0]]
+Output: 7
+Explanation: The fisher can start at cell (1,3) and collect 3 fish, then move to cell (2,3) and collect 4 fish.
+
+Example 2:
+
+Input: grid = [[1,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,1]]
+Output: 1
+Explanation: The fisher can start at cells (0,0) or (3,3) and collect a single fish. 
+
+Constraints:
+
+	m == grid.length
+	n == grid[i].length
+	1 <= m, n <= 10
+	0 <= grid[i][j] <= 10
+*/
+export function findMaxFish(grid: number[][]): number {
+    const moves: number[] = [-1, 0, 1, 0, -1];
+    const m = grid.length;
+    const n = grid[0].length;
+
+    const infect = (x: number, y: number) => {
+        if (grid[x][y] === 0) {
+            return 0;
+        }
+
+        let fish = grid[x][y];
+        grid[x][y] = 0;
+
+        for (let i = 0; i < 4; i++) {
+            const nextX = x + moves[i];
+            const nextY = y + moves[i + 1];
+
+            if (
+                nextX >= 0 &&
+                nextX < m &&
+                nextY >= 0 &&
+                nextY < n &&
+                grid[nextX][nextY] !== 0
+            ) {
+                fish += infect(nextX, nextY);
+            }
+        }
+
+        return fish;
+    };
+
+    let max = 0;
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] !== 0) {
+                max = Math.max(max, infect(i, j));
+            }
+        }
+    }
+
+    return max;
+}
