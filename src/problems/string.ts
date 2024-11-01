2be6b5f650580e6853e7aece8fbec64d2fe7f4f0
zhanggq52434 <zhanggq52434@hundsun.com>
2024-11-01 09:19:30 +0800

    feat: 每日一题

--- src/problems/string.ts
+++ src/problems/string.ts
@@ -3741,3 +3741,52 @@ export function numWays(s: string): number {
 
     return (firstWays * secondWays) % MOD;
 }
+
+/*
+https://leetcode.com/problems/delete-characters-to-make-fancy-string/description/?envType=daily-question&envId=2024-11-01
+1957. Delete Characters to Make Fancy String
+A fancy string is a string where no three consecutive characters are equal.
+
+Given a string s, delete the minimum possible number of characters from s to make it fancy.
+
+Return the final string after the deletion. It can be shown that the answer will always be unique.
+
+Example 1:
+
+Input: s = "leeetcode"
+Output: "leetcode"
+Explanation:
+Remove an 'e' from the first group of 'e's to create "leetcode".
+No three consecutive characters are equal, so return "leetcode".
+
+Example 2:
+
+Input: s = "aaabaaaa"
+Output: "aabaa"
+Explanation:
+Remove an 'a' from the first group of 'a's to create "aabaaaa".
+Remove two 'a's from the second group of 'a's to create "aabaa".
+No three consecutive characters are equal, so return "aabaa".
+
+Example 3:
+
+Input: s = "aab"
+Output: "aab"
+Explanation: No three consecutive characters are equal, so return "aab".
+
+Constraints:
+
+	1 <= s.length <= 10^5
+	s consists only of lowercase English letters.
+*/
+export function makeFancyString(s: string): string {
+    const original = s.split('');
+    const ret: string[] = [];
+    for (let i = 0; i < original.length; i++) {
+        if (original[i] !== ret.at(-1) || original[i] !== ret.at(-2)) {
+            ret.push(original[i]);
+        }
+    }
+
+    return ret.join('');
+}
