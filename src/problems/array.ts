18427aa01220a1421b31585bc946d3a40facda49
zhanggq52434 <zhanggq52434@hundsun.com>
2024-11-02 10:42:07 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -8284,6 +8284,10 @@ Constraints:
 	There are no leading or trailing spaces.
 */
 export function isCircularSentence(sentence: string): boolean {
+    if (sentence[0] !== sentence.at(-1)) {
+        return false;
+    }
+
     const words = sentence.split(' ');
     for (let i = 0; i < words.length - 1; i++) {
         if (words[i].at(-1) !== words[i + 1][0]) {
