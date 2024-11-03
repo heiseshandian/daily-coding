730495a01c3d49717bd07c1db697204c1d6ce9ea
zhanggq52434 <zhanggq52434@hundsun.com>
2024-11-03 10:40:18 +0800

    feat: bfs

--- src/problems/bfs.ts
+++ src/problems/bfs.ts
@@ -200,3 +200,88 @@ function toStatusMap(alphas: string[]) {
         return acc;
     }, {});
 }
+
+/*
+https://leetcode.com/problems/alphabet-board-path/description/
+1138. Alphabet Board Path
+On an alphabet board, we start at position (0, 0), corresponding to character board[0][0].
+
+Here, board = ["abcde", "fghij", "klmno", "pqrst", "uvwxy", "z"], as shown in the diagram below.
+
+We may make the following moves:
+
+	'U' moves our position up one row, if the position exists on the board;
+	'D' moves our position down one row, if the position exists on the board;
+	'L' moves our position left one column, if the position exists on the board;
+	'R' moves our position right one column, if the position exists on the board;
+	'!' adds the character board[r][c] at our current position (r, c) to the answer.
+
+(Here, the only positions that exist on the board are positions with letters on them.)
+
+Return a sequence of moves that makes our answer equal to target in the minimum number of moves.  You may return any path that does so.
+
+Example 1:
+Input: target = "leet"
+Output: "DDR!UURRR!!DDD!"
+Example 2:
+Input: target = "code"
+Output: "RR!DDRR!UUL!R!"
+
+Constraints:
+
+	1 <= target.length <= 100
+	target consists only of English lowercase letters.
+*/
+export function alphabetBoardPath(target: string): string {
+    const board = ['abcde', 'fghij', 'klmno', 'pqrst', 'uvwxy', 'z'];
+
+    const moves: number[] = [-1, 0, 1, 0, -1];
+    const directions: string[] = ['U', 'R', 'D', 'L'];
+
+    const bfs = (
+        x: number,
+        y: number,
+        t: string
+    ): [x: number, y: number, path: string] => {
+        const visited: boolean[][] = Array.from({ length: board.length }, () =>
+            Array(board[0].length).fill(false)
+        );
+        visited[x][y] = true;
+
+        const queue: [x: number, y: number, path: string][] = [[x, y, '']];
+        while (queue.length) {
+            const [x, y, path] = queue.shift()!;
+            if (board[x][y] === t) {
+                return [x, y, path];
+            }
+
+            for (let i = 0; i < 4; i++) {
+                const nextX = x + moves[i];
+                const nextY = y + moves[i + 1];
+                const direction = directions[i];
+
+                if (
+                    nextX >= 0 &&
+                    nextX < board.length &&
+                    nextY >= 0 &&
+                    nextY < board[nextX].length &&
+                    !visited[nextX][nextY]
+                ) {
+                    visited[nextX][nextY] = true;
+                    queue.push([nextX, nextY, path + direction]);
+                }
+            }
+        }
+
+        return [0, 0, ''];
+    };
+
+    let s = '';
+    let [x, y, path] = [0, 0, ''];
+    for (let i = 0; i < target.length; i++) {
+        [x, y, path] = bfs(x, y, target[i]);
+        s += path + '!';
+    }
+
+    return s;
+}
