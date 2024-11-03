f7363fc043a6c8c439a2db682e4c3745da32a3cf
zhanggq52434 <zhanggq52434@hundsun.com>
2024-11-03 22:44:56 +0800

    feat: 每日一题

--- src/problems/math.ts
+++ src/problems/math.ts
@@ -1265,3 +1265,43 @@ export function countMaxOrSubsets(nums: number[]): number {
 
     return dfs(0, 0);
 }
+
+/*
+https://leetcode.com/problems/ugly-number/description/
+263. Ugly Number
+An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.
+
+Given an integer n, return true if n is an ugly number.
+
+Example 1:
+
+Input: n = 6
+Output: true
+Explanation: 6 = 2 × 3
+
+Example 2:
+
+Input: n = 1
+Output: true
+Explanation: 1 has no prime factors, therefore all of its prime factors are limited to 2, 3, and 5.
+
+Example 3:
+
+Input: n = 14
+Output: false
+Explanation: 14 is not ugly since it includes the prime factor 7.
+
+Constraints:
+
+	2^31 <= n <= 2^31 - 1
+*/
+export function isUgly(n: number): boolean {
+    if (n <= 0) return false;
+    if (n <= 6) return true;
+
+    while (n % 2 === 0) n /= 2;
+    while (n % 3 === 0) n /= 3;
+    while (n % 5 === 0) n /= 5;
+
+    return n === 1;
+}
