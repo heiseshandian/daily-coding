e6bb9cd7a6a3870ea6c45e6ce76a584d5cb1edda
zhanggq52434 <zhanggq52434@hundsun.com>
2024-11-04 09:05:22 +0800

    feat: 每日一题

--- src/problems/string.ts
+++ src/problems/string.ts
@@ -3815,3 +3815,65 @@ Constraints:
 export function rotateString(s: string, goal: string): boolean {
     return s.length === goal.length && (s + s).includes(goal);
 }
+
+/*
+https://leetcode.com/problems/string-compression-iii/description/
+3163. String Compression III
+Given a string word, compress it using the following algorithm:
+
+	Begin with an empty string comp. While word is not empty, use the following operation:
+
+		Remove a maximum length prefix of word made of a single character c repeating at most 9 times.
+		Append the length of the prefix followed by c to comp.
+
+Return the string comp.
+
+Example 1:
+
+Input: word = "abcde"
+
+Output: "1a1b1c1d1e"
+
+Explanation:
+
+Initially, comp = "". Apply the operation 5 times, choosing "a", "b", "c", "d", and "e" as the prefix in each operation.
+
+For each prefix, append "1" followed by the character to comp.
+
+Example 2:
+
+Input: word = "aaaaaaaaaaaaaabb"
+
+Output: "9a5a2b"
+
+Explanation:
+
+Initially, comp = "". Apply the operation 3 times, choosing "aaaaaaaaa", "aaaaa", and "bb" as the prefix in each operation.
+
+	For prefix "aaaaaaaaa", append "9" followed by "a" to comp.
+	For prefix "aaaaa", append "5" followed by "a" to comp.
+	For prefix "bb", append "2" followed by "b" to comp.
+
+Constraints:
+
+	1 <= word.length <= 10^5
+	word consists only of lowercase English letters.
+*/
+export function compressedString(word: string): string {
+    let comp = '';
+    let count = 1;
+    let prev = word[0];
+
+    for (let i = 1; i < word.length; i++) {
+        if (word[i] === prev && count < 9) {
+            count++;
+        } else {
+            comp += `${count}${prev}`;
+            prev = word[i];
+            count = 1;
+        }
+    }
+    comp += `${count}${prev}`;
+
+    return comp;
+}
