1d9c8b287e3a0ac48f1e9a929b5f38eaab0ee783
zhanggq52434 <zhanggq52434@hundsun.com>
2024-11-16 08:38:21 +0800

    feat: 每日一题-滑动窗口

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -8476,3 +8476,77 @@ function countLessThanOrEqual(nums: number[], bound: number): number {
 
     return count;
 }
+
+/*
+https://leetcode.com/problems/find-the-power-of-k-size-subarrays-i/description/
+3254. Find the Power of K-Size Subarrays I
+You are given an array of integers nums of length n and a positive integer k.
+
+The power of an array is defined as:
+
+	Its maximum element if all of its elements are consecutive and sorted in ascending order.
+	-1 otherwise.
+
+You need to find the power of all subarrays of nums of size k.
+
+Return an integer array results of size n - k + 1, where results[i] is the power of nums[i..(i + k - 1)].
+
+Example 1:
+
+Input: nums = [1,2,3,4,3,2,5], k = 3
+
+Output: [3,4,-1,-1,-1]
+
+Explanation:
+
+There are 5 subarrays of nums of size 3:
+
+	[1, 2, 3] with the maximum element 3.
+	[2, 3, 4] with the maximum element 4.
+	[3, 4, 3] whose elements are not consecutive.
+	[4, 3, 2] whose elements are not sorted.
+	[3, 2, 5] whose elements are not consecutive.
+
+Example 2:
+
+Input: nums = [2,2,2,2,2], k = 4
+
+Output: [-1,-1]
+
+Example 3:
+
+Input: nums = [3,2,3,2,3,2], k = 2
+
+Output: [-1,3,-1,3,-1]
+
+Constraints:
+
+	1 <= n == nums.length <= 500
+	1 <= nums[i] <= 10^5
+	1 <= k <= n
+*/
+export function resultsArray(nums: number[], k: number): number[] {
+    const ret: number[] = Array(nums.length - k + 1);
+
+    let len = 1;
+    for (let right = 1; right < k; right++) {
+        if (nums[right] - nums[right - 1] === 1) {
+            len++;
+        } else {
+            len = 1;
+        }
+    }
+    ret[0] = len === k ? nums[k - 1] : -1;
+
+    for (let left = 1, right = k; right < nums.length; left++, right++) {
+        if (nums[right] - nums[right - 1] === 1) {
+            len++;
+        } else {
+            len = 1;
+        }
+
+        ret[left] = len >= k ? nums[right] : -1;
+    }
+
+    return ret;
+}
