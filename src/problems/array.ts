fd38df5208a16c15ab836216c4146ab209838d1a
zhanggq52434 <zhanggq52434@hundsun.com>
2024-11-18 23:20:55 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -8630,3 +8630,70 @@ export function decrypt(code: number[], k: number): number[] {
 
     return ret;
 }
+
+/*
+https://leetcode.com/problems/maximize-the-total-height-of-unique-towers/description/
+3301. Maximize the Total Height of Unique Towers
+You are given an array maximumHeight, where maximumHeight[i] denotes the maximum height the ith tower can be assigned.
+
+Your task is to assign a height to each tower so that:
+
+	The height of the ith tower is a positive integer and does not exceed maximumHeight[i].
+	No two towers have the same height.
+
+Return the maximum possible total sum of the tower heights. If it's not possible to assign heights, return -1.
+
+Example 1:
+
+Input: maximumHeight = [2,3,4,3]
+
+Output: 10
+
+Explanation:
+
+We can assign heights in the following way: [1, 2, 4, 3].
+
+Example 2:
+
+Input: maximumHeight = [15,10]
+
+Output: 25
+
+Explanation:
+
+We can assign heights in the following way: [15, 10].
+
+Example 3:
+
+Input: maximumHeight = [2,2,1]
+
+Output: -1
+
+Explanation:
+
+It's impossible to assign positive heights to each index so that no two towers have the same height.
+
+Constraints:
+
+	1 <= maximumHeight.length <= 10^5
+	1 <= maximumHeight[i] <= 10^9
+*/
+export function maximumTotalSum(maximumHeight: number[]): number {
+    maximumHeight.sort((a, b) => b - a);
+    if (maximumHeight[0] < maximumHeight.length) {
+        return -1;
+    }
+
+    let sum = 0;
+    let prev = Infinity;
+    for (let i = 0; i < maximumHeight.length; i++) {
+        if (maximumHeight[i] < prev) {
+            sum += maximumHeight[i];
+            prev = maximumHeight[i];
+        } else {
+            sum += --prev;
+        }
+    }
+
+    return prev > 0 ? sum : -1;
+}
