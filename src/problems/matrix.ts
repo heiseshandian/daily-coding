385eea2e7c2b6f17af49b430eb5f407371fd4ddd
zhanggq52434 <zhanggq52434@hundsun.com>
2024-11-24 09:33:39 +0800

    feat: 每日一题

--- src/problems/matrix.ts
+++ src/problems/matrix.ts
@@ -2403,3 +2403,59 @@ export function rotateTheBox(box: string[][]): string[][] {
 
     return rotated;
 }
+
+/*
+https://leetcode.com/problems/maximum-matrix-sum/description/
+1975. Maximum Matrix Sum
+You are given an n x n integer matrix. You can do the following operation any number of times:
+
+	Choose any two adjacent elements of matrix and multiply each of them by -1.
+
+Two elements are considered adjacent if and only if they share a border.
+
+Your goal is to maximize the summation of the matrix's elements. Return the maximum sum of the matrix's elements using the operation mentioned above.
+
+Example 1:
+
+Input: matrix = [[1,-1],[-1,1]]
+Output: 4
+Explanation: We can follow the following steps to reach sum equals 4:
+- Multiply the 2 elements in the first row by -1.
+- Multiply the 2 elements in the first column by -1.
+
+Example 2:
+
+Input: matrix = [[1,2,3],[-1,-2,-3],[1,2,3]]
+Output: 16
+Explanation: We can follow the following step to reach sum equals 16:
+- Multiply the 2 last elements in the second row by -1.
+
+Constraints:
+
+	n == matrix.length == matrix[i].length
+	2 <= n <= 250
+	-10^5 <= matrix[i][j] <= 10^5
+*/
+export function maxMatrixSum(matrix: number[][]): number {
+    let sum = 0;
+    let min = Infinity;
+    let negativeCount = 0;
+
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[0].length; j++) {
+            if (matrix[i][j] < 0) {
+                negativeCount++;
+                min = Math.min(min, -matrix[i][j]);
+                sum += -matrix[i][j];
+            } else {
+                sum += matrix[i][j];
+                min = Math.min(min, matrix[i][j]);
+            }
+        }
+    }
+
+    if (negativeCount & 1) {
+        sum -= min << 1;
+    }
+    return sum;
+}
