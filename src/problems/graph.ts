b08b51dfb8b202644ef17e3d4e8aa1e458afcf7c
zhanggq52434 <zhanggq52434@hundsun.com>
2024-11-27 11:18:22 +0800

    feat: bfs 找最短路径

--- src/problems/graph.ts
+++ src/problems/graph.ts
@@ -1358,3 +1358,94 @@ export function findChampion(n: number, edges: number[][]): number {
     }
     return -1;
 }
+
+/*
+https://leetcode.com/problems/shortest-distance-after-road-addition-queries-i/description/
+3243. Shortest Distance After Road Addition Queries I
+You are given an integer n and a 2D integer array queries.
+
+There are n cities numbered from 0 to n - 1. Initially, there is a unidirectional road from city i to city i + 1 for all 0 <= i < n - 1.
+
+queries[i] = [ui, vi] represents the addition of a new unidirectional road from city ui to city vi. After each query, you need to find the length of the shortest path from city 0 to city n - 1.
+
+Return an array answer where for each i in the range [0, queries.length - 1], answer[i] is the length of the shortest path from city 0 to city n - 1 after processing the first i + 1 queries.
+
+Example 1:
+
+Input: n = 5, queries = [[2,4],[0,2],[0,4]]
+
+Output: [3,2,1]
+
+Explanation: 
+
+After the addition of the road from 2 to 4, the length of the shortest path from 0 to 4 is 3.
+
+After the addition of the road from 0 to 2, the length of the shortest path from 0 to 4 is 2.
+
+After the addition of the road from 0 to 4, the length of the shortest path from 0 to 4 is 1.
+
+Example 2:
+
+Input: n = 4, queries = [[0,3],[0,2]]
+
+Output: [1,1]
+
+Explanation:
+
+After the addition of the road from 0 to 3, the length of the shortest path from 0 to 3 is 1.
+
+After the addition of the road from 0 to 2, the length of the shortest path remains 1.
+
+Constraints:
+
+	3 <= n <= 500
+	1 <= queries.length <= 500
+	queries[i].length == 2
+	0 <= queries[i][0] < queries[i][1] < n
+	1 < queries[i][1] - queries[i][0]
+	There are no repeated roads among the queries.
+*/
+export function shortestDistanceAfterQueries(
+    n: number,
+    queries: number[][]
+): number[] {
+    const tables: number[][] = Array.from({ length: n }, () => []);
+    for (let i = 0; i < n - 1; i++) {
+        tables[i].push(i + 1);
+    }
+
+    const queue: number[] = Array(n);
+    const getMinDistance = (): number => {
+        let left = 0;
+        let right = 0;
+        queue[right++] = 0;
+        let level = 0;
+        const visited: boolean[] = Array(n).fill(false);
+
+        while (left < right) {
+            const prevRight = right;
+            while (left < prevRight) {
+                const from = queue[left++];
+                if (from === n - 1) {
+                    return level;
+                }
+
+                tables[from].forEach((to) => {
+                    if (!visited[to]) {
+                        visited[to] = true;
+                        queue[right++] = to;
+                    }
+                });
+            }
+
+            level++;
+        }
+
+        return level;
+    };
+
+    return queries.map(([f, t]) => {
+        tables[f].push(t);
+        return getMinDistance();
+    });
+}
