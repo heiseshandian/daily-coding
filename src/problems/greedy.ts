bfea37d024201f0bb00e26997a512d88e53c5e7f
zhanggq52434 <zhanggq52434@hundsun.com>
2024-11-28 18:59:17 +0800

    feat: 每日一题

--- src/problems/greedy.ts
+++ src/problems/greedy.ts
@@ -1384,3 +1384,45 @@ export function minimizedMaximum(n: number, quantities: number[]): number {
 
     return closest;
 }
+
+/*
+https://leetcode.com/problems/maximum-possible-number-by-binary-concatenation/description/
+3309. Maximum Possible Number by Binary Concatenation
+You are given an array of integers nums of size 3.
+
+Return the maximum possible number whose binary representation can be formed by concatenating the binary representation of all elements in nums in some order.
+
+Note that the binary representation of any number does not contain leading zeros.
+
+Example 1:
+
+Input: nums = [1,2,3]
+
+Output: 30
+
+Explanation:
+
+Concatenate the numbers in the order [3, 1, 2] to get the result "11110", which is the binary representation of 30.
+
+Example 2:
+
+Input: nums = [2,8,16]
+
+Output: 1296
+
+Explanation:
+
+Concatenate the numbers in the order [2, 8, 16] to get the result "10100010000", which is the binary representation of 1296.
+
+Constraints:
+
+	nums.length == 3
+	1 <= nums[i] <= 127
+*/
+export function maxGoodNumber(nums: number[]): number {
+    const sorted = nums
+        .map((v) => v.toString(2))
+        .sort((a, b) => parseInt(`${b}${a}`, 2) - parseInt(`${a}${b}`, 2));
+
+    return parseInt(sorted.join(''), 2);
+}
