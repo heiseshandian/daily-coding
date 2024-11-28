1383cffbb7dacde82c27e82f8dea387ab3206b9e
zhanggq52434 <zhanggq52434@hundsun.com>
2024-11-28 11:10:49 +0800

    feat: 每日一题

--- src/problems/graph.ts
+++ src/problems/graph.ts
@@ -1449,3 +1449,83 @@ export function shortestDistanceAfterQueries(
         return getMinDistance();
     });
 }
+
+/*
+https://leetcode.com/problems/minimum-obstacle-removal-to-reach-corner/description/
+2290. Minimum Obstacle Removal to Reach Corner
+You are given a 0-indexed 2D integer array grid of size m x n. Each cell has one of two values:
+
+	0 represents an empty cell,
+	1 represents an obstacle that may be removed.
+
+You can move up, down, left, or right from and to an empty cell.
+
+Return the minimum number of obstacles to remove so you can move from the upper left corner (0, 0) to the lower right corner (m - 1, n - 1).
+
+Example 1:
+
+Input: grid = [[0,1,1],[1,1,0],[1,1,0]]
+Output: 2
+Explanation: We can remove the obstacles at (0, 1) and (0, 2) to create a path from (0, 0) to (2, 2).
+It can be shown that we need to remove at least 2 obstacles, so we return 2.
+Note that there may be other ways to remove 2 obstacles to create a path.
+
+Example 2:
+
+Input: grid = [[0,1,0,0,0],[0,1,0,1,0],[0,0,0,1,0]]
+Output: 0
+Explanation: We can move from (0, 0) to (2, 4) without removing any obstacles, so we return 0.
+
+Constraints:
+
+	m == grid.length
+	n == grid[i].length
+	1 <= m, n <= 10^5
+	2 <= m * n <= 10^5
+	grid[i][j] is either 0 or 1.
+	grid[0][0] == grid[m - 1][n - 1] == 0
+*/
+export function minimumObstacles(grid: number[][]): number {
+    const m = grid.length;
+    const n = grid[0].length;
+
+    const distances: number[][] = Array.from({ length: m }, () => Array(n));
+    const visited: boolean[][] = Array.from({ length: m }, () =>
+        Array(n).fill(false)
+    );
+    const minHeap = new GenericHeap<[i: number, j: number, distance: number]>(
+        (a, b) => a[2] - b[2]
+    );
+
+    distances[0][0] = 0;
+    minHeap.push([0, 0, 0]);
+
+    const moves: number[] = [-1, 0, 1, 0, -1];
+    while (minHeap.size() > 0) {
+        const [i, j, prevDistance] = minHeap.pop();
+        if (visited[i][j]) {
+            continue;
+        }
+        visited[i][j] = true;
+
+        for (let k = 0; k < moves.length - 1; k++) {
+            const nextI = i + moves[k];
+            const nextJ = j + moves[k + 1];
+
+            if (
+                nextI >= 0 &&
+                nextI < m &&
+                nextJ >= 0 &&
+                nextJ < n &&
+                !visited[nextI][nextJ] &&
+                (distances[nextI][nextJ] === undefined ||
+                    prevDistance + grid[nextI][nextJ] < distances[nextI][nextJ])
+            ) {
+                distances[nextI][nextJ] = prevDistance + grid[nextI][nextJ];
+                minHeap.push([nextI, nextJ, distances[nextI][nextJ]]);
+            }
+        }
+    }
+
+    return distances[m - 1][n - 1];
+}
