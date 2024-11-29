eebcc061f57f95c79aafeaa50d326f0ef5796547
zhanggq52434 <zhanggq52434@hundsun.com>
2024-11-29 15:57:18 +0800

    feat: 每日一题

--- src/problems/hard.ts
+++ src/problems/hard.ts
@@ -3742,3 +3742,103 @@ export function slidingPuzzle(board: number[][]): number {
     // If no solution is found, return -1
     return -1;
 }
+
+/*
+https://leetcode.com/problems/minimum-time-to-visit-a-cell-in-a-grid/description/
+2577. Minimum Time to Visit a Cell In a Grid
+You are given a m x n matrix grid consisting of non-negative integers where grid[row][col] represents the minimum time required to be able to visit the cell (row, col), which means you can visit the cell (row, col) only when the time you visit it is greater than or equal to grid[row][col].
+
+You are standing in the top-left cell of the matrix in the 0th second, and you must move to any adjacent cell in the four directions: up, down, left, and right. Each move you make takes 1 second.
+
+Return the minimum time required in which you can visit the bottom-right cell of the matrix. If you cannot visit the bottom-right cell, then return -1.
+
+Example 1:
+
+Input: grid = [[0,1,3,2],[5,1,2,5],[4,3,8,6]]
+Output: 7
+Explanation: One of the paths that we can take is the following:
+- at t = 0, we are on the cell (0,0).
+- at t = 1, we move to the cell (0,1). It is possible because grid[0][1] <= 1.
+- at t = 2, we move to the cell (1,1). It is possible because grid[1][1] <= 2.
+- at t = 3, we move to the cell (1,2). It is possible because grid[1][2] <= 3.
+- at t = 4, we move to the cell (1,1). It is possible because grid[1][1] <= 4.
+- at t = 5, we move to the cell (1,2). It is possible because grid[1][2] <= 5.
+- at t = 6, we move to the cell (1,3). It is possible because grid[1][3] <= 6.
+- at t = 7, we move to the cell (2,3). It is possible because grid[2][3] <= 7.
+The final time is 7. It can be shown that it is the minimum time possible.
+
+Example 2:
+
+Input: grid = [[0,2,4],[3,2,1],[1,0,4]]
+Output: -1
+Explanation: There is no path from the top left to the bottom-right cell.
+
+Constraints:
+
+	m == grid.length
+	n == grid[i].length
+	2 <= m, n <= 1000
+	4 <= m * n <= 10^5
+	0 <= grid[i][j] <= 10^5
+	grid[0][0] == 0
+*/
+export function minimumTime(grid: number[][]): number {
+    if (grid[0][1] > 1 && grid[1][0] > 1) {
+        return -1;
+    }
+
+    const m = grid.length;
+    const n = grid[0].length;
+    const distances: number[][] = Array.from({ length: m }, () => Array(n));
+    const visited: boolean[][] = Array.from({ length: m }, () =>
+        Array(n).fill(false)
+    );
+    const minHeap = new GenericHeap<[number, number, number]>(
+        (a, b) => a[2] - b[2]
+    );
+    const moves: number[] = [-1, 0, 1, 0, -1];
+
+    minHeap.push([0, 0, 0]);
+    distances[0][0] = 0;
+
+    while (minHeap.size() > 0) {
+        const [i, j, distance] = minHeap.pop();
+        if (i === m - 1 && j === n - 1) {
+            return distance;
+        }
+
+        if (visited[i][j]) {
+            continue;
+        }
+        visited[i][j] = true;
+
+        for (let k = 0; k < moves.length - 1; k++) {
+            const nextI = i + moves[k];
+            const nextJ = j + moves[k + 1];
+
+            if (
+                nextI >= 0 &&
+                nextI < m &&
+                nextJ >= 0 &&
+                nextJ < n &&
+                !visited[nextI][nextJ]
+            ) {
+                let nextDistance = distance + 1;
+                if (nextDistance < grid[nextI][nextJ]) {
+                    nextDistance =
+                        grid[nextI][nextJ] +
+                        ((grid[nextI][nextJ] - nextDistance) % 2);
+                }
+                if (
+                    distances[nextI][nextJ] === undefined ||
+                    nextDistance < distances[nextI][nextJ]
+                ) {
+                    distances[nextI][nextJ] = nextDistance;
+                    minHeap.push([nextI, nextJ, nextDistance]);
+                }
+            }
+        }
+    }
+
+    return distances[m - 1][n - 1];
+}
