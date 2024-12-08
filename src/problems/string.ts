ad5a0d1d65d14336a633a5e813166623f6c976f4
zhanggq52434 <zhanggq52434@hundsun.com>
2024-12-08 22:55:22 +0800

    feat: 每日一题

--- src/problems/string.ts
+++ src/problems/string.ts
@@ -4254,3 +4254,87 @@ export function canChange(start: string, target: string): boolean {
 
     return true;
 }
+
+/*
+https://leetcode.com/problems/find-and-replace-in-string/description/
+833. Find And Replace in String
+You are given a 0-indexed string s that you must perform k replacement operations on. The replacement operations are given as three 0-indexed parallel arrays, indices, sources, and targets, all of length k.
+
+To complete the ith replacement operation:
+
+	Check if the substring sources[i] occurs at index indices[i] in the original string s.
+	If it does not occur, do nothing.
+	Otherwise if it does occur, replace that substring with targets[i].
+
+For example, if s = "abcd", indices[i] = 0, sources[i] = "ab", and targets[i] = "eee", then the result of this replacement will be "eeecd".
+
+All replacement operations must occur simultaneously, meaning the replacement operations should not affect the indexing of each other. The testcases will be generated such that the replacements will not overlap.
+
+	For example, a testcase with s = "abc", indices = [0, 1], and sources = ["ab","bc"] will not be generated because the "ab" and "bc" replacements overlap.
+
+Return the resulting string after performing all replacement operations on s.
+
+A substring is a contiguous sequence of characters in a string.
+
+Example 1:
+
+Input: s = "abcd", indices = [0, 2], sources = ["a", "cd"], targets = ["eee", "ffff"]
+Output: "eeebffff"
+Explanation:
+"a" occurs at index 0 in s, so we replace it with "eee".
+"cd" occurs at index 2 in s, so we replace it with "ffff".
+
+Example 2:
+
+Input: s = "abcd", indices = [0, 2], sources = ["ab","ec"], targets = ["eee","ffff"]
+Output: "eeecd"
+Explanation:
+"ab" occurs at index 0 in s, so we replace it with "eee".
+"ec" does not occur at index 2 in s, so we do nothing.
+
+Constraints:
+
+	1 <= s.length <= 1000
+	k == indices.length == sources.length == targets.length
+	1 <= k <= 100
+	0 <= indexes[i] < s.length
+	1 <= sources[i].length, targets[i].length <= 50
+	s consists of only lowercase English letters.
+	sources[i] and targets[i] consist of only lowercase English letters.
+*/
+export function findReplaceString(
+    s: string,
+    indices: number[],
+    sources: string[],
+    targets: string[]
+): string {
+    const sorted = indices.map((v, i) => [v, i]).sort((a, b) => a[0] - b[0]);
+
+    const ret: string[] = Array(s.length);
+    let index = 0;
+    let i = 0;
+    while (i < s.length) {
+        if (i !== sorted[index]?.[0]) {
+            ret[i] = s[i++];
+            continue;
+        }
+
+        const mappedIndex = sorted[index][1];
+
+        let isMatch = true;
+        for (let k = 0; k < sources[mappedIndex].length; k++) {
+            if (sources[mappedIndex][k] !== s[i + k]) {
+                isMatch = false;
+                break;
+            }
+        }
+
+        if (isMatch) {
+            ret[i] = targets[mappedIndex];
+            i += sources[mappedIndex].length;
+        }
+        index++;
+    }
+
+    return ret.join('');
+}
