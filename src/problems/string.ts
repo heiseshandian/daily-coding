da18890d0f4b71b7ac8e6e0c47cc9090a65ebd75
zhanggq52434 <zhanggq52434@hundsun.com>
2024-12-10 12:26:47 +0800

    feat: 每日一题

--- src/problems/string.ts
+++ src/problems/string.ts
@@ -4338,3 +4338,69 @@ export function findReplaceString(
 
     return ret.join('');
 }
+
+/*
+https://leetcode.com/problems/find-longest-special-substring-that-occurs-thrice-i/description/
+2981. Find Longest Special Substring That Occurs Thrice I
+You are given a string s that consists of lowercase English letters.
+
+A string is called special if it is made up of only a single character. For example, the string "abc" is not special, whereas the strings "ddd", "zz", and "f" are special.
+
+Return the length of the longest special substring of s which occurs at least thrice, or -1 if no special substring occurs at least thrice.
+
+A substring is a contiguous non-empty sequence of characters within a string.
+
+Example 1:
+
+Input: s = "aaaa"
+Output: 2
+Explanation: The longest special substring which occurs thrice is "aa": substrings "aaaa", "aaaa", and "aaaa".
+It can be shown that the maximum length achievable is 2.
+
+Example 2:
+
+Input: s = "abcdef"
+Output: -1
+Explanation: There exists no special substring which occurs at least thrice. Hence return -1.
+
+Example 3:
+
+Input: s = "abcaba"
+Output: 1
+Explanation: The longest special substring which occurs thrice is "a": substrings "abcaba", "abcaba", and "abcaba".
+It can be shown that the maximum length achievable is 1.
+
+Constraints:
+
+	3 <= s.length <= 50
+	s consists of only lowercase English letters.
+*/
+export function maximumLength(s: string): number {
+    const nums: number[] = Array(s.length).fill(1);
+    for (let i = 0; i < s.length; i++) {
+        let j = i + 1;
+        while (j < s.length && s[j] === s[i]) {
+            j++;
+        }
+        nums[i] = j - i;
+    }
+
+    let len = -1;
+    for (let i = 0; i < s.length - 2; i++) {
+        for (let j = i + 1; j < s.length - 1; j++) {
+            if (s[j] !== s[i]) {
+                continue;
+            }
+
+            for (let k = j + 1; k < s.length; k++) {
+                if (s[k] !== s[i]) {
+                    continue;
+                }
+
+                len = Math.max(len, Math.min(nums[i], nums[j], nums[k]));
+            }
+        }
+    }
+
+    return len;
+}
