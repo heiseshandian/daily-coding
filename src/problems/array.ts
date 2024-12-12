6b28e56a71aa5165fd4081e5b4dc032c7beb81a7
zhanggq52434 <zhanggq52434@hundsun.com>
2024-12-12 08:53:12 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -9069,3 +9069,54 @@ export function isArraySpecial(nums: number[], queries: number[][]): boolean[] {
 
     return ret;
 }
+
+/*
+https://leetcode.com/problems/take-gifts-from-the-richest-pile/description/
+2558. Take Gifts From the Richest Pile
+You are given an integer array gifts denoting the number of gifts in various piles. Every second, you do the following:
+
+	Choose the pile with the maximum number of gifts.
+	If there is more than one pile with the maximum number of gifts, choose any.
+	Leave behind the floor of the square root of the number of gifts in the pile. Take the rest of the gifts.
+
+Return the number of gifts remaining after k seconds.
+
+Example 1:
+
+Input: gifts = [25,64,9,4,100], k = 4
+Output: 29
+Explanation: 
+The gifts are taken in the following way:
+- In the first second, the last pile is chosen and 10 gifts are left behind.
+- Then the second pile is chosen and 8 gifts are left behind.
+- After that the first pile is chosen and 5 gifts are left behind.
+- Finally, the last pile is chosen again and 3 gifts are left behind.
+The final remaining gifts are [5,8,9,4,3], so the total number of gifts remaining is 29.
+
+Example 2:
+
+Input: gifts = [1,1,1,1], k = 4
+Output: 4
+Explanation: 
+In this case, regardless which pile you choose, you have to leave behind 1 gift in each pile. 
+That is, you can't take any pile with you. 
+So, the total gifts remaining are 4.
+
+Constraints:
+
+	1 <= gifts.length <= 10^3
+	1 <= gifts[i] <= 10^9
+	1 <= k <= 10^3
+*/
+export function pickGifts(gifts: number[], k: number): number {
+    const heap = new GenericHeap((a, b) => b - a);
+    heap.initHeap(gifts);
+    while (k > 0 && heap.size() > 0 && heap.peek() > 1) {
+        k--;
+        const max = heap.pop();
+        const remains = Math.floor(Math.sqrt(max));
+        heap.push(remains);
+    }
+
+    return heap.container.reduce((s, n) => s + n);
+}
