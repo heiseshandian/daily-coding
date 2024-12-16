e8d66f9a062054c37a2946c7f56563c321ed5549
zhanggq52434 <zhanggq52434@hundsun.com>
2024-12-16 10:41:34 +0800

    feat: 每日一题

--- src/problems/heap.ts
+++ src/problems/heap.ts
@@ -568,3 +568,61 @@ export function maxTwoEvents(events: number[][]): number {
 
     return max;
 }
+
+/*
+https://leetcode.com/problems/final-array-state-after-k-multiplication-operations-i/description/
+3264. Final Array State After K Multiplication Operations I
+You are given an integer array nums, an integer k, and an integer multiplier.
+
+You need to perform k operations on nums. In each operation:
+
+	Find the minimum value x in nums. If there are multiple occurrences of the minimum value, select the one that appears first.
+	Replace the selected minimum value x with x * multiplier.
+
+Return an integer array denoting the final state of nums after performing all k operations.
+
+Example 1:
+
+Input: nums = [2,1,3,5,6], k = 5, multiplier = 2
+
+Output: [8,4,6,5,6]
+
+Explanation:
+
+OperationResultAfter operation 1[2, 2, 3, 5, 6]After operation 2[4, 2, 3, 5, 6]After operation 3[4, 4, 3, 5, 6]After operation 4[4, 4, 6, 5, 6]After operation 5[8, 4, 6, 5, 6]
+
+Example 2:
+
+Input: nums = [1,2], k = 3, multiplier = 4
+
+Output: [16,8]
+
+Explanation:
+
+OperationResultAfter operation 1[4, 2]After operation 2[4, 8]After operation 3[16, 8]
+
+Constraints:
+
+	1 <= nums.length <= 100
+	1 <= nums[i] <= 100
+	1 <= k <= 10
+	1 <= multiplier <= 5
+*/
+export function getFinalState(
+    nums: number[],
+    k: number,
+    multiplier: number
+): number[] {
+    const heap = new GenericHeap<number[]>(
+        (a, b) => a[0] - b[0] || a[1] - b[1]
+    );
+    heap.initHeap(nums.map((v, i) => [v, i]));
+
+    while (k > 0) {
+        k--;
+        const [v, i] = heap.pop();
+        heap.push([v * multiplier, i]);
+    }
+
+    return heap.container.sort((a, b) => a[1] - b[1]).map(([v]) => v);
+}
