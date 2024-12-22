9bb51564788da3cef52bc7114c882b50e4acdd96
zhanggq52434 <zhanggq52434@hundsun.com>
2024-12-22 13:32:03 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -9432,3 +9432,97 @@ export function findSpecialInteger(arr: number[]): number {
 
     return arr[0];
 }
+
+/*
+https://leetcode.com/problems/find-building-where-alice-and-bob-can-meet/description/
+2940. Find Building Where Alice and Bob Can Meet
+You are given a 0-indexed array heights of positive integers, where heights[i] represents the height of the ith building.
+
+If a person is in building i, they can move to any other building j if and only if i < j and heights[i] < heights[j].
+
+You are also given another array queries where queries[i] = [ai, bi]. On the ith query, Alice is in building ai while Bob is in building bi.
+
+Return an array ans where ans[i] is the index of the leftmost building where Alice and Bob can meet on the ith query. If Alice and Bob cannot move to a common building on query i, set ans[i] to -1.
+
+Example 1:
+
+Input: heights = [6,4,8,5,2,7], queries = [[0,1],[0,3],[2,4],[3,4],[2,2]]
+Output: [2,5,-1,5,2]
+Explanation: In the first query, Alice and Bob can move to building 2 since heights[0] < heights[2] and heights[1] < heights[2]. 
+In the second query, Alice and Bob can move to building 5 since heights[0] < heights[5] and heights[3] < heights[5]. 
+In the third query, Alice cannot meet Bob since Alice cannot move to any other building.
+In the fourth query, Alice and Bob can move to building 5 since heights[3] < heights[5] and heights[4] < heights[5].
+In the fifth query, Alice and Bob are already in the same building.  
+For ans[i] != -1, It can be shown that ans[i] is the leftmost building where Alice and Bob can meet.
+For ans[i] == -1, It can be shown that there is no building where Alice and Bob can meet.
+
+Example 2:
+
+Input: heights = [5,3,8,2,6,1,4,6], queries = [[0,7],[3,5],[5,2],[3,0],[1,6]]
+Output: [7,6,-1,4,6]
+Explanation: In the first query, Alice can directly move to Bob's building since heights[0] < heights[7].
+In the second query, Alice and Bob can move to building 6 since heights[3] < heights[6] and heights[5] < heights[6].
+In the third query, Alice cannot meet Bob since Bob cannot move to any other building.
+In the fourth query, Alice and Bob can move to building 4 since heights[3] < heights[4] and heights[0] < heights[4].
+In the fifth query, Alice can directly move to Bob's building since heights[1] < heights[6].
+For ans[i] != -1, It can be shown that ans[i] is the leftmost building where Alice and Bob can meet.
+For ans[i] == -1, It can be shown that there is no building where Alice and Bob can meet.
+
+Constraints:
+
+	1 <= heights.length <= 10^4
+	1 <= heights[i] <= 10^9
+	1 <= queries.length <= 10^4
+	queries[i] = [ai, bi]
+	0 <= ai, bi <= heights.length - 1
+*/
+export function leftmostBuildingQueries(
+    heights: number[],
+    queries: number[][]
+): number[] {
+    const sorted = queries
+        .map(([a, b], i) => [Math.min(a, b), Math.max(a, b), i])
+        .sort((a, b) => b[1] - a[1]);
+    const ret: number[] = Array(sorted.length);
+    const increasingStack: number[] = [heights.length - 1];
+
+    sorted.forEach(([left, right, i]) => {
+        if (left === right) {
+            ret[i] = left;
+        } else if (heights[left] < heights[right]) {
+            ret[i] = right;
+        } else {
+            for (let k = increasingStack.at(-1)! - 1; k >= right; k--) {
+                while (
+                    increasingStack.length &&
+                    heights[increasingStack.at(-1)!] <= heights[k]
+                ) {
+                    increasingStack.pop();
+                }
+
+                increasingStack.push(k);
+            }
+
+            let l = 0;
+            let r = increasingStack.length - 1;
+            let closest = -1;
+            while (l <= r) {
+                const mid = l + ((r - l) >> 1);
+                if (heights[increasingStack[mid]] > heights[left]) {
+                    closest = mid;
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+
+            if (closest === -1) {
+                ret[i] = -1;
+            } else {
+                ret[i] = increasingStack[closest];
+            }
+        }
+    });
+
+    return ret;
+}
