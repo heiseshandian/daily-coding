0ccc55e4c8b8088d1b71edafa8f4ee8164b8d456
zhanggq52434 <zhanggq52434@hundsun.com>
2024-12-23 11:47:18 +0800

    feat: 每日一题

--- src/problems/tree.ts
+++ src/problems/tree.ts
@@ -1,9 +1,14 @@
 import { Queue } from '../algorithm/queue';
-import { TreeNode, getMaxDistance } from '../algorithm/tree';
+import {
+    TreeNode,
+    deserializeByLevel,
+    getMaxDistance,
+} from '../algorithm/tree';
 import { cache } from '../design-pattern/proxy';
 import { UnionFind } from '../algorithm/union-find';
 import { GenericHeap } from '../algorithm/generic-heap';
 import { ListNode } from '../algorithm/linked-list';
+import { swap } from '../common';
 /* 
 https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
 
@@ -3570,4 +3575,98 @@ export function sortedListToBST(head: ListNode | null): TreeNode | null {
     root.right = right;
 
     return root;
+}
+
+/*
+https://leetcode.com/problems/minimum-number-of-operations-to-sort-a-binary-tree-by-level/description/
+2471. Minimum Number of Operations to Sort a Binary Tree by Level
+You are given the root of a binary tree with unique values.
+
+In one operation, you can choose any two nodes at the same level and swap their values.
+
+Return the minimum number of operations needed to make the values at each level sorted in a strictly increasing order.
+
+The level of a node is the number of edges along the path between it and the root node.
+
+Example 1:
+
+Input: root = [1,4,3,7,6,8,5,null,null,null,null,9,null,10]
+Output: 3
+Explanation:
+- Swap 4 and 3. The 2nd level becomes [3,4].
+- Swap 7 and 5. The 3rd level becomes [5,6,8,7].
+- Swap 8 and 7. The 3rd level becomes [5,6,7,8].
+We used 3 operations so return 3.
+It can be proven that 3 is the minimum number of operations needed.
+
+Example 2:
+
+Input: root = [1,3,2,7,6,5,4]
+Output: 3
+Explanation:
+- Swap 3 and 2. The 2nd level becomes [2,3].
+- Swap 7 and 4. The 3rd level becomes [4,6,5,7].
+- Swap 6 and 5. The 3rd level becomes [4,5,6,7].
+We used 3 operations so return 3.
+It can be proven that 3 is the minimum number of operations needed.
+
+Example 3:
+
+Input: root = [1,2,3,4,5,6]
+Output: 0
+Explanation: Each level is already sorted in increasing order so return 0.
+
+Constraints:
+
+	The number of nodes in the tree is in the range [1, 10^5].
+	1 <= Node.val <= 10^5
+	All the values of the tree are unique.
+*/
+export function minimumOperations(root: TreeNode): number {
+    const queue: TreeNode[] = Array(1e5 + 1);
+    let left = 0;
+    let right = 0;
+    queue[right++] = root;
+
+    let curEnd = root;
+    let nextEnd = root;
+    const level: number[] = [];
+    let minOp = 0;
+
+    while (left < right) {
+        const node = queue[left++];
+        level.push(node.val);
+        if (node.left) {
+            nextEnd = node.left;
+            queue[right++] = node.left;
+        }
+        if (node.right) {
+            nextEnd = node.right;
+            queue[right++] = node.right;
+        }
+
+        if (node === curEnd) {
+            const sorted = level.slice().sort((a, b) => a - b);
+            const map = {};
+            for (let i = 0; i < level.length; i++) {
+                map[level[i]] = i;
+            }
+            for (let i = 0; i < level.length; i++) {
+                if (level[i] !== sorted[i]) {
+                    minOp++;
+                    const idx = map[sorted[i]];
+                    [level[i], level[idx]] = [level[idx], level[i]];
+                    [map[level[i]], map[level[idx]]] = [
+                        map[level[idx]],
+                        map[level[i]],
+                    ];
+                }
+            }
+
+            level.length = 0;
+            curEnd = nextEnd;
+        }
+    }
+
+    return minOp;
 }
