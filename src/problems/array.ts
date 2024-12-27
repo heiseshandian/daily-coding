08d1dd208beb22d15d05d567f31b6aeec4510226
zhanggq52434 <zhanggq52434@hundsun.com>
2024-12-27 10:47:51 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -9526,3 +9526,41 @@ export function leftmostBuildingQueries(
 
     return ret;
 }
+
+/*
+https://leetcode.com/problems/best-sightseeing-pair/description/
+1014. Best Sightseeing Pair
+You are given an integer array values where values[i] represents the value of the ith sightseeing spot. Two sightseeing spots i and j have a distance j - i between them.
+
+The score of a pair (i < j) of sightseeing spots is values[i] + values[j] + i - j: the sum of the values of the sightseeing spots, minus the distance between them.
+
+Return the maximum score of a pair of sightseeing spots.
+
+Example 1:
+
+Input: values = [8,1,5,2,6]
+Output: 11
+Explanation: i = 0, j = 2, values[i] + values[j] + i - j = 8 + 5 + 0 - 2 = 11
+
+Example 2:
+
+Input: values = [1,2]
+Output: 2
+
+Constraints:
+
+	2 <= values.length <= 10^4
+	1 <= values[i] <= 1000
+*/
+export function maxScoreSightseeingPair(values: number[]): number {
+    let maxI = values[0] + 0;
+    let maxJ = -Infinity;
+    let max = -Infinity;
+    for (let i = 1; i < values.length; i++) {
+        maxJ = Math.max(values[i] - i);
+        max = Math.max(max, maxI + maxJ);
+        maxI = Math.max(maxI, values[i] + i);
+    }
+
+    return max;
+}
