cc5f01d8fe78042aaeb0fd671e2f25423fa8a304
zhanggq52434 <zhanggq52434@hundsun.com>
2024-12-30 10:10:41 +0800

    feat: 每日一题

--- src/problems/string.ts
+++ src/problems/string.ts
@@ -4404,3 +4404,58 @@ export function maximumLength(s: string): number {
 
     return len;
 }
+
+/*
+https://leetcode.com/problems/count-ways-to-build-good-strings/description/
+2466. Count Ways To Build Good Strings
+Given the integers zero, one, low, and high, we can construct a string by starting with an empty string, and then at each step perform either of the following:
+
+	Append the character '0' zero times.
+	Append the character '1' one times.
+
+This can be performed any number of times.
+
+A good string is a string constructed by the above process having a length between low and high (inclusive).
+
+Return the number of different good strings that can be constructed satisfying these properties. Since the answer can be large, return it modulo 109 + 7.
+
+Example 1:
+
+Input: low = 3, high = 3, zero = 1, one = 1
+Output: 8
+Explanation: 
+One possible valid good string is "011". 
+It can be constructed as follows: "" -> "0" -> "01" -> "011". 
+All binary strings from "000" to "111" are good strings in this example.
+
+Example 2:
+
+Input: low = 2, high = 3, zero = 1, one = 2
+Output: 5
+Explanation: The good strings are "00", "11", "000", "110", and "011".
+
+Constraints:
+
+	1 <= low <= high <= 10^5
+	1 <= zero, one <= low
+*/
+export function countGoodStrings(
+    low: number,
+    high: number,
+    zero: number,
+    one: number
+): number {
+    const mod = 1e9 + 7;
+
+    const dfs = cache((len: number): number => {
+        if (len > high) {
+            return 0;
+        }
+        if (len < low) {
+            return (dfs(len + zero) + dfs(len + one)) % mod;
+        }
+        return (1 + dfs(len + zero) + dfs(len + one)) % mod;
+    });
+
+    return dfs(0);
+}
