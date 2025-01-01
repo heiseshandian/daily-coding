9906438bfaa2149a1be0b9b908d22cd73a8ba0c6
zhanggq52434 <zhanggq52434@hundsun.com>
2025-01-01 09:39:46 +0800

    feat: 递归

--- src/problems/tree.ts
+++ src/problems/tree.ts
@@ -3768,3 +3768,52 @@ export function minimumDiameterAfterMerge(
         Math.floor((diameter1 + 1) / 2) + Math.floor((diameter2 + 1) / 2) + 1
     );
 }
+
+/*
+https://leetcode.com/problems/sum-of-root-to-leaf-binary-numbers/description/
+1022. Sum of Root To Leaf Binary Numbers
+You are given the root of a binary tree where each node has a value 0 or 1. Each root-to-leaf path represents a binary number starting with the most significant bit.
+
+	For example, if the path is 0 -> 1 -> 1 -> 0 -> 1, then this could represent 01101 in binary, which is 13.
+
+For all leaves in the tree, consider the numbers represented by the path from the root to that leaf. Return the sum of these numbers.
+
+The test cases are generated so that the answer fits in a 32-bits integer.
+
+Example 1:
+
+Input: root = [1,0,1,0,1,0,1]
+Output: 22
+Explanation: (100) + (101) + (110) + (111) = 4 + 5 + 6 + 7 = 22
+
+Example 2:
+
+Input: root = [0]
+Output: 0
+
+Constraints:
+
+	The number of nodes in the tree is in the range [1, 1000].
+	Node.val is 0 or 1.
+*/
+export function sumRootToLeaf(root: TreeNode | null): number {
+    let sum = 0;
+    const dfs = (node: TreeNode | null, val: number) => {
+        if (!node) {
+            return;
+        }
+
+        val = (val << 1) + node.val;
+        if (!node.left && !node.right) {
+            sum += val;
+            return;
+        }
+
+        dfs(node.left, val);
+        dfs(node.right, val);
+    };
+
+    dfs(root, 0);
+
+    return sum;
+}
