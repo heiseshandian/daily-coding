99030e842dd45f0535116fd9f29785bbce20ea75
zhanggq52434 <zhanggq52434@hundsun.com>
2025-01-02 09:01:25 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -9636,3 +9636,55 @@ export function maxSumOfThreeSubarrays(nums: number[], k: number): number[] {
 
     return result;
 }
+
+/*
+https://leetcode.com/problems/count-vowel-strings-in-ranges/description/
+2559. Count Vowel Strings in Ranges
+You are given a 0-indexed array of strings words and a 2D array of integers queries.
+
+Each query queries[i] = [li, ri] asks us to find the number of strings present in the range li to ri (both inclusive) of words that start and end with a vowel.
+
+Return an array ans of size queries.length, where ans[i] is the answer to the ith query.
+
+Note that the vowel letters are 'a', 'e', 'i', 'o', and 'u'.
+
+Example 1:
+
+Input: words = ["aba","bcb","ece","aa","e"], queries = [[0,2],[1,4],[1,1]]
+Output: [2,3,0]
+Explanation: The strings starting and ending with a vowel are "aba", "ece", "aa" and "e".
+The answer to the query [0,2] is 2 (strings "aba" and "ece").
+to query [1,4] is 3 (strings "ece", "aa", "e").
+to query [1,1] is 0.
+We return [2,3,0].
+
+Example 2:
+
+Input: words = ["a","e","i"], queries = [[0,2],[0,1],[2,2]]
+Output: [3,2,1]
+Explanation: Every string satisfies the conditions, so we return [3,2,1].
+
+Constraints:
+
+	1 <= words.length <= 10^5
+	1 <= words[i].length <= 40
+	words[i] consists only of lowercase English letters.
+	sum(words[i].length) <= 10^5
+	1 <= queries.length <= 10^5
+	0 <= li <= ri < words.length
+*/
+export function vowelStrings(words: string[], queries: number[][]): number[] {
+    const vowels = new Set(['a', 'e', 'i', 'o', 'u']);
+    const prefix = Array(words.length);
+    const startAndEndWithVoewl = (word: string) =>
+        vowels.has(word[0]) && vowels.has(word.slice(-1));
+
+    prefix[0] = startAndEndWithVoewl(words[0]) ? 1 : 0;
+    for (let i = 1; i < words.length; i++) {
+        prefix[i] = prefix[i - 1] + (startAndEndWithVoewl(words[i]) ? 1 : 0);
+    }
+
+    return queries.map(
+        ([left, right]) => prefix[right] - (left > 0 ? prefix[left - 1] : 0)
+    );
+}
