a4bff7a293743ecd88dd65c25c3b2b97fadc5024
zhanggq52434 <zhanggq52434@hundsun.com>
2025-01-05 11:11:18 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -4,7 +4,6 @@ import { getClosestMaxOrEqual, isEven, swap } from '../common';
 import { UnionSet } from '../algorithm/union-set';
 import { countBits } from '../common/bit';
 import { UnionFind } from '../algorithm/union-find';
-import { removeZeroSumSublists } from './linked-list';
 /* 
 https://leetcode.com/problems/find-the-maximum-number-of-marked-indices/description/
 
@@ -9687,4 +9686,58 @@ export function vowelStrings(words: string[], queries: number[][]): number[] {
     return queries.map(
         ([left, right]) => prefix[right] - (left > 0 ? prefix[left - 1] : 0)
     );
+}
+
+/*
+https://leetcode.com/problems/shifting-letters-ii/description/
+2381. Shifting Letters II
+You are given a string s of lowercase English letters and a 2D integer array shifts where shifts[i] = [starti, endi, directioni]. For every i, shift the characters in s from the index starti to the index endi (inclusive) forward if directioni = 1, or shift the characters backward if directioni = 0.
+
+Shifting a character forward means replacing it with the next letter in the alphabet (wrapping around so that 'z' becomes 'a'). Similarly, shifting a character backward means replacing it with the previous letter in the alphabet (wrapping around so that 'a' becomes 'z').
+
+Return the final string after all such shifts to s are applied.
+
+Example 1:
+
+Input: s = "abc", shifts = [[0,1,0],[1,2,1],[0,2,1]]
+Output: "ace"
+Explanation: Firstly, shift the characters from index 0 to index 1 backward. Now s = "zac".
+Secondly, shift the characters from index 1 to index 2 forward. Now s = "zbd".
+Finally, shift the characters from index 0 to index 2 forward. Now s = "ace".
+
+Example 2:
+
+Input: s = "dztz", shifts = [[0,0,0],[1,1,1]]
+Output: "catz"
+Explanation: Firstly, shift the characters from index 0 to index 0 backward. Now s = "cztz".
+Finally, shift the characters from index 1 to index 1 forward. Now s = "catz".
+
+Constraints:
+
+	1 <= s.length, shifts.length <= 10^4
+	shifts[i].length == 3
+	0 <= starti <= endi < s.length
+	0 <= directioni <= 1
+	s consists of lowercase English letters.
+*/
+export function shiftingLetters(s: string, shifts: number[][]): string {
+    const diff = Array(s.length + 1).fill(0);
+    shifts.forEach(([start, end, direction]) => {
+        diff[start] += direction === 1 ? 1 : -1;
+        diff[end + 1] += direction === 1 ? -1 : 1;
+    });
+
+    const alphas = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    const aCode = 'a'.charCodeAt(0);
+
+    let sum = 0;
+    const ret: string[] = Array(s.length);
+    for (let i = 0; i < s.length; i++) {
+        sum += diff[i];
+        sum %= 26;
+        const index = (s.charCodeAt(i) - aCode + sum + 26) % 26;
+        ret[i] = alphas[index];
+    }
+
+    return ret.join('');
 }
