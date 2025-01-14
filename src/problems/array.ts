3c70c8f01490fcccba61df5a022a1cce7598665e
zhanggq52434 <zhanggq52434@hundsun.com>
2025-01-14 13:12:42 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -9741,3 +9741,60 @@ export function shiftingLetters(s: string, shifts: number[][]): string {
 
     return ret.join('');
 }
+
+/*
+https://leetcode.com/problems/find-the-prefix-common-array-of-two-arrays/description/
+2657. Find the Prefix Common Array of Two Arrays
+You are given two 0-indexed integer permutations A and B of length n.
+
+A prefix common array of A and B is an array C such that C[i] is equal to the count of numbers that are present at or before the index i in both A and B.
+
+Return the prefix common array of A and B.
+
+A sequence of n integers is called a permutation if it contains all integers from 1 to n exactly once.
+
+Example 1:
+
+Input: A = [1,3,2,4], B = [3,1,2,4]
+Output: [0,2,3,4]
+Explanation: At i = 0: no number is common, so C[0] = 0.
+At i = 1: 1 and 3 are common in A and B, so C[1] = 2.
+At i = 2: 1, 2, and 3 are common in A and B, so C[2] = 3.
+At i = 3: 1, 2, 3, and 4 are common in A and B, so C[3] = 4.
+
+Example 2:
+
+Input: A = [2,3,1], B = [3,1,2]
+Output: [0,1,3]
+Explanation: At i = 0: no number is common, so C[0] = 0.
+At i = 1: only 3 is common in A and B, so C[1] = 1.
+At i = 2: 1, 2, and 3 are common in A and B, so C[2] = 3.
+
+Constraints:
+
+	1 <= A.length == B.length == n <= 50
+	1 <= A[i], B[i] <= n
+	It is guaranteed that A and B are both a permutation of n integers.
+*/
+export function findThePrefixCommonArray(A: number[], B: number[]): number[] {
+    const n = A.length;
+    const freq: number[] = Array(n + 1).fill(0);
+    const ret: number[] = Array(n);
+
+    let count = 0;
+    for (let i = 0; i < n; i++) {
+        freq[A[i]]++;
+        freq[B[i]]++;
+
+        if (A[i] === B[i]) {
+            count++;
+        } else {
+            count += freq[A[i]] === 2 ? 1 : 0;
+            count += freq[B[i]] === 2 ? 1 : 0;
+        }
+
+        ret[i] = count;
+    }
+
+    return ret;
+}
