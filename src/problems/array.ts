bb88236fdd57f81c7683accb5725e1f881a9d0c2
zhanggq52434 <zhanggq52434@hundsun.com>
2025-01-15 13:51:06 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -372,6 +372,90 @@ export function stoneGame(piles: number[]): boolean {
     return first(0, piles.length - 1) > last(0, piles.length - 1);
 }
 
+/*
+https://leetcode.com/problems/stone-game-ii/description/?envType=daily-question&envId=2024-08-20
+1140. Stone Game II
+Alice and Bob continue their games with piles of stones. There are a number of piles arranged in a row, and each pile has a positive integer number of stones piles[i]. The objective of the game is to end with the most stones.
+
+Alice and Bob take turns, with Alice starting first.
+
+On each player's turn, that player can take all the stones in the first X remaining piles, where 1 <= X <= 2M. Then, we set M = max(M, X). Initially, M = 1.
+
+The game continues until all the stones have been taken.
+
+Assuming Alice and Bob play optimally, return the maximum number of stones Alice can get.
+
+Example 1:
+
+Input: piles = [2,7,9,4,4]
+
+Output: 10
+
+Explanation:
+
+	If Alice takes one pile at the beginning, Bob takes two piles, then Alice takes 2 piles again. Alice can get 2 + 4 + 4 = 10 stones in total.
+	If Alice takes two piles at the beginning, then Bob can take all three piles left. In this case, Alice get 2 + 7 = 9 stones in total.
+
+So we return 10 since it's larger.
+
+Example 2:
+
+Input: piles = [1,2,3,4,5,100]
+
+Output: 104
+
+Constraints:
+
+	1 <= piles.length <= 100
+	1 <= piles[i] <= 10^4
+*/
+export function stoneGameII(piles: number[]): number {
+    const prefix = Array(piles.length);
+    prefix[0] = piles[0];
+    for (let i = 1; i < piles.length; i++) {
+        prefix[i] = prefix[i - 1] + piles[i];
+    }
+
+    const queryRangeSum = (left: number, right: number): number => {
+        if (left > 0) {
+            return prefix[right] - prefix[left - 1];
+        }
+        return prefix[right];
+    };
+
+    const first = cache((index: number, m: number): number => {
+        if (index >= piles.length) {
+            return 0;
+        }
+
+        let max = -Infinity;
+        for (let x = 1; x <= 2 * m && index + x <= piles.length; x++) {
+            max = Math.max(
+                max,
+                queryRangeSum(index, index + x - 1) +
+                    last(index + x, Math.max(m, x))
+            );
+        }
+
+        return max;
+    });
+
+    const last = cache((index: number, m: number): number => {
+        if (index >= piles.length) {
+            return 0;
+        }
+
+        let min = Infinity;
+        for (let x = 1; x <= 2 * m && index + x <= piles.length; x++) {
+            min = Math.min(min, first(index + x, Math.max(m, x)));
+        }
+
+        return min;
+    });
+
+    return first(0, 1);
+}
+
 /* 
 https://leetcode.com/problems/sort-characters-by-frequency/description/?envType=daily-question&envId=2024-02-07
 451. Sort Characters By Frequency
