e1fa454b6ffd48cb078ef5d6d447b7c3d33f43af
zhanggq52434 <zhanggq52434@hundsun.com>
2025-01-20 11:09:38 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -9882,3 +9882,60 @@ export function findThePrefixCommonArray(A: number[], B: number[]): number[] {
 
     return ret;
 }
+
+/*
+https://leetcode.com/problems/first-completely-painted-row-or-column/description/
+2661. First Completely Painted Row or Column
+You are given a 0-indexed integer array arr, and an m x n integer matrix mat. arr and mat both contain all the integers in the range [1, m * n].
+
+Go through each index i in arr starting from index 0 and paint the cell in mat containing the integer arr[i].
+
+Return the smallest index i at which either a row or a column will be completely painted in mat.
+
+Example 1:
+
+Input: arr = [1,3,4,2], mat = [[1,4],[2,3]]
+Output: 2
+Explanation: The moves are shown in order, and both the first row and second column of the matrix become fully painted at arr[2].
+
+Example 2:
+
+Input: arr = [2,8,7,4,1,3,5,6,9], mat = [[3,2,5],[1,4,6],[8,7,9]]
+Output: 3
+Explanation: The second column becomes fully painted at arr[3].
+
+Constraints:
+
+	m == mat.length
+	n = mat[i].length
+	arr.length == m * n
+	1 <= m, n <= 10^5
+	1 <= m * n <= 10^5
+	1 <= arr[i], mat[r][c] <= m * n
+	All the integers of arr are unique.
+	All the integers of mat are unique.
+*/
+export function firstCompleteIndex(arr: number[], mat: number[][]): number {
+    const m = mat.length;
+    const n = mat[0].length;
+    const rows = Array(m).fill(0);
+    const cols = Array(n).fill(0);
+    const positions: [number, number][] = Array(m * n + 1);
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            positions[mat[i][j]] = [i, j];
+        }
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        const [r, c] = positions[arr[i]];
+        rows[r]++;
+        cols[c]++;
+        if (rows[r] === n || cols[c] === m) {
+            return i;
+        }
+    }
+
+    return -1;
+}
