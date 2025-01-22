e807398fc5b3deb8421e6c90dae5104ed30a6d3b
zhanggq52434 <zhanggq52434@hundsun.com>
2025-01-22 13:38:04 +0800

    feat: 每日一题

--- src/problems/bfs.ts
+++ src/problems/bfs.ts
@@ -285,3 +285,91 @@ export function alphabetBoardPath(target: string): string {
 
     return s;
 }
+
+/*
+https://leetcode.com/problems/map-of-highest-peak/description/
+1765. Map of Highest Peak
+You are given an integer matrix isWater of size m x n that represents a map of land and water cells.
+
+	If isWater[i][j] == 0, cell (i, j) is a land cell.
+	If isWater[i][j] == 1, cell (i, j) is a water cell.
+
+You must assign each cell a height in a way that follows these rules:
+
+	The height of each cell must be non-negative.
+	If the cell is a water cell, its height must be 0.
+	Any two adjacent cells must have an absolute height difference of at most 1. A cell is adjacent to another cell if the former is directly north, east, south, or west of the latter (i.e., their sides are touching).
+
+Find an assignment of heights such that the maximum height in the matrix is maximized.
+
+Return an integer matrix height of size m x n where height[i][j] is cell (i, j)'s height. If there are multiple solutions, return any of them.
+
+Example 1:
+
+Input: isWater = [[0,1],[0,0]]
+Output: [[1,0],[2,1]]
+Explanation: The image shows the assigned heights of each cell.
+The blue cell is the water cell, and the green cells are the land cells.
+
+Example 2:
+
+Input: isWater = [[0,0,1],[1,0,0],[0,0,0]]
+Output: [[1,1,0],[0,1,1],[1,2,2]]
+Explanation: A height of 2 is the maximum possible height of any assignment.
+Any height assignment that has a maximum height of 2 while still meeting the rules will also be accepted.
+
+Constraints:
+
+	m == isWater.length
+	n == isWater[i].length
+	1 <= m, n <= 1000
+	isWater[i][j] is 0 or 1.
+	There is at least one water cell.
+
+Note: This question is the same as 542: https://leetcode.com/problems/01-matrix/
+*/
+export function highestPeak(isWater: number[][]): number[][] {
+    const m = isWater.length;
+    const n = isWater[0].length;
+
+    const queue: number[][] = [];
+    const height: number[][] = Array.from({ length: m }, () => Array(n));
+
+    const moves: number[] = [-1, 0, 1, 0, -1];
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (isWater[i][j] === 1) {
+                height[i][j] = 0;
+                queue.push([i, j]);
+            }
+        }
+    }
+
+    let level = 0;
+    while (queue.length) {
+        const q = queue.slice();
+        queue.length = 0;
+        level++;
+
+        for (let i = 0; i < q.length; i++) {
+            const [x, y] = q[i];
+            for (let j = 0; j < moves.length - 1; j++) {
+                const nextX = x + moves[j];
+                const nextY = y + moves[j + 1];
+
+                if (
+                    nextX >= 0 &&
+                    nextX < m &&
+                    nextY >= 0 &&
+                    nextY < n &&
+                    height[nextX][nextY] === undefined
+                ) {
+                    height[nextX][nextY] = level;
+                    queue.push([nextX, nextY]);
+                }
+            }
+        }
+    }
+
+    return height;
+}
