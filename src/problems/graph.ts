5221b60bdfad68f04810c4e100356860a54e4b61
zhanggq52434 <zhanggq52434@hundsun.com>
2025-01-27 10:25:17 +0800

    feat: 每日一题

--- src/problems/graph.ts
+++ src/problems/graph.ts
@@ -1792,3 +1792,87 @@ export function eventualSafeNodes(graph: number[][]): number[] {
 
     return result.sort((a, b) => a - b);
 }
+
+/*
+https://leetcode.com/problems/course-schedule-iv/description/
+1462. Course Schedule IV
+There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course ai first if you want to take course bi.
+
+	For example, the pair [0, 1] indicates that you have to take course 0 before you can take course 1.
+
+Prerequisites can also be indirect. If course a is a prerequisite of course b, and course b is a prerequisite of course c, then course a is a prerequisite of course c.
+
+You are also given an array queries where queries[j] = [uj, vj]. For the jth query, you should answer whether course uj is a prerequisite of course vj or not.
+
+Return a boolean array answer, where answer[j] is the answer to the jth query.
+
+Example 1:
+
+Input: numCourses = 2, prerequisites = [[1,0]], queries = [[0,1],[1,0]]
+Output: [false,true]
+Explanation: The pair [1, 0] indicates that you have to take course 1 before you can take course 0.
+Course 0 is not a prerequisite of course 1, but the opposite is true.
+
+Example 2:
+
+Input: numCourses = 2, prerequisites = [], queries = [[1,0],[0,1]]
+Output: [false,false]
+Explanation: There are no prerequisites, and each course is independent.
+
+Example 3:
+
+Input: numCourses = 3, prerequisites = [[1,2],[1,0],[2,0]], queries = [[1,0],[1,2]]
+Output: [true,true]
+
+Constraints:
+
+	2 <= numCourses <= 100
+	0 <= prerequisites.length <= (numCourses * (numCourses - 1) / 2)
+	prerequisites[i].length == 2
+	0 <= ai, bi <= numCourses - 1
+	ai != bi
+	All the pairs [ai, bi] are unique.
+	The prerequisites graph has no cycles.
+	1 <= queries.length <= 10^4
+	0 <= ui, vi <= numCourses - 1
+	ui != vi
+*/
+export function checkIfPrerequisite(
+    numCourses: number,
+    prerequisites: number[][],
+    queries: number[][]
+): boolean[] {
+    const tables: number[][] = Array.from({ length: numCourses }, () => []);
+    prerequisites.forEach(([from, to]) => {
+        tables[from].push(to);
+    });
+    const isReachable = Array.from({ length: numCourses }, () =>
+        Array(numCourses).fill(false)
+    );
+
+    const bfs = (i: number) => {
+        const visited: boolean[] = Array(numCourses).fill(false);
+        const queue: number[] = Array(numCourses);
+        let left = 0;
+        let right = 0;
+        queue[right++] = i;
+
+        while (left < right) {
+            const first = queue[left++];
+            visited[first] = true;
+
+            tables[first].forEach((next) => {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    queue[right++] = next;
+                    isReachable[i][next] = true;
+                }
+            });
+        }
+    };
+    for (let i = 0; i < numCourses; i++) {
+        bfs(i);
+    }
+
+    return queries.map(([from, to]) => isReachable[from][to]);
+}
