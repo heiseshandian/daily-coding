ff8b4583a26af1bea41ced45f85313536346cf94
zhanggq52434 <zhanggq52434@hundsun.com>
2025-02-10 11:24:19 +0800

    feat: 每日一题

--- src/problems/string.ts
+++ src/problems/string.ts
@@ -4844,3 +4844,55 @@ export function canBeValid(s: string, locked: string): boolean {
 
     return true;
 }
+
+/*
+https://leetcode.com/problems/clear-digits/description/?envType=daily-question&envId=2025-02-10
+3174. Clear Digits
+You are given a string s.
+
+Your task is to remove all digits by doing this operation repeatedly:
+
+	Delete the first digit and the closest non-digit character to its left.
+
+Return the resulting string after removing all digits.
+
+Example 1:
+
+Input: s = "abc"
+
+Output: "abc"
+
+Explanation:
+
+There is no digit in the string.
+
+Example 2:
+
+Input: s = "cb34"
+
+Output: ""
+
+Explanation:
+
+First, we apply the operation on s[2], and s becomes "c4".
+
+Then we apply the operation on s[1], and s becomes "".
+
+Constraints:
+
+	1 <= s.length <= 100
+	s consists only of lowercase English letters and digits.
+	The input is generated such that it is possible to delete all digits.
+*/
+export function clearDigits(s: string): string {
+    const stack: string[] = [];
+    for (const char of s) {
+        if (/\d/.test(char)) {
+            stack.pop();
+        } else {
+            stack.push(char);
+        }
+    }
+
+    return stack.join('');
+}
