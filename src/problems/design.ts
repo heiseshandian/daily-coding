1dad3c7d091fe62f0a98dfd6bd9088faf2c2fe42
zhanggq52434 <zhanggq52434@hundsun.com>
2025-02-14 11:18:01 +0800

    feat: 每日一题

--- src/problems/design.ts
+++ src/problems/design.ts
@@ -2666,3 +2666,82 @@ export class WordFilter {
         return this.dic.get(pref + '#' + suff) ?? -1;
     }
 }
+
+/*
+https://leetcode.com/problems/product-of-the-last-k-numbers/description/?envType=daily-question&envId=2025-02-14
+1352. Product of the Last K Numbers
+Design an algorithm that accepts a stream of integers and retrieves the product of the last k integers of the stream.
+
+Implement the ProductOfNumbers class:
+
+	ProductOfNumbers() Initializes the object with an empty stream.
+	void add(int num) Appends the integer num to the stream.
+	int getProduct(int k) Returns the product of the last k numbers in the current list. You can assume that always the current list has at least k numbers.
+
+The test cases are generated so that, at any time, the product of any contiguous sequence of numbers will fit into a single 32-bit integer without overflowing.
+
+Example:
+
+Input
+["ProductOfNumbers","add","add","add","add","add","getProduct","getProduct","getProduct","add","getProduct"]
+[[],[3],[0],[2],[5],[4],[2],[3],[4],[8],[2]]
+
+Output
+[null,null,null,null,null,null,20,40,0,null,32]
+
+Explanation
+ProductOfNumbers productOfNumbers = new ProductOfNumbers();
+productOfNumbers.add(3);        // [3]
+productOfNumbers.add(0);        // [3,0]
+productOfNumbers.add(2);        // [3,0,2]
+productOfNumbers.add(5);        // [3,0,2,5]
+productOfNumbers.add(4);        // [3,0,2,5,4]
+productOfNumbers.getProduct(2); // return 20. The product of the last 2 numbers is 5 * 4 = 20
+productOfNumbers.getProduct(3); // return 40. The product of the last 3 numbers is 2 * 5 * 4 = 40
+productOfNumbers.getProduct(4); // return 0. The product of the last 4 numbers is 0 * 2 * 5 * 4 = 0
+productOfNumbers.add(8);        // [3,0,2,5,4,8]
+productOfNumbers.getProduct(2); // return 32. The product of the last 2 numbers is 4 * 8 = 32 
+
+Constraints:
+
+	0 <= num <= 100
+	1 <= k <= 10^4
+	At most 4 * 104 calls will be made to add and getProduct.
+	The product of the stream at any point in time will fit in a 32-bit integer.
+
+Follow-up: Can you implement both GetProduct and Add to work in O(1) time complexity instead of O(k) time complexity?
+*/
+export class ProductOfNumbers {
+    prefix: number[];
+
+    constructor() {
+        this.prefix = [];
+    }
+
+    add(num: number): void {
+        const prev = this.prefix.length - 1;
+        if (num === 0) {
+            this.prefix.length = 0;
+            return;
+        }
+
+        if (prev === -1) {
+            this.prefix.push(num);
+        } else {
+            this.prefix[prev + 1] = this.prefix[prev] * num;
+        }
+    }
+
+    getProduct(k: number): number {
+        const len = this.prefix.length;
+        if (len - k <= -1) {
+            return 0;
+        }
+
+        if (len - k - 1 >= 0) {
+            return this.prefix[len - 1] / this.prefix[len - k - 1];
+        } else {
+            return this.prefix[len - 1];
+        }
+    }
+}
