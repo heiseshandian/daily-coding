7a7a917f1fc6c15ee215f76743a769fec022d277
zhanggq52434 <zhanggq52434@hundsun.com>
2025-02-15 11:33:03 +0800

    feat: 每日一题

--- src/problems/math.ts
+++ src/problems/math.ts
@@ -1367,3 +1367,68 @@ export function findNthDigit(n: number): number {
 
     return n === 0 ? closest : +closest.toString().split('')[n - 1];
 }
+
+/*
+https://leetcode.com/problems/find-the-punishment-number-of-an-integer/description/?envType=daily-question&envId=2025-02-15
+2698. Find the Punishment Number of an Integer
+Given a positive integer n, return the punishment number of n.
+
+The punishment number of n is defined as the sum of the squares of all integers i such that:
+
+	1 <= i <= n
+	The decimal representation of i * i can be partitioned into contiguous substrings such that the sum of the integer values of these substrings equals i.
+
+Example 1:
+
+Input: n = 10
+Output: 182
+Explanation: There are exactly 3 integers i in the range [1, 10] that satisfy the conditions in the statement:
+- 1 since 1 * 1 = 1
+- 9 since 9 * 9 = 81 and 81 can be partitioned into 8 and 1 with a sum equal to 8 + 1 == 9.
+- 10 since 10 * 10 = 100 and 100 can be partitioned into 10 and 0 with a sum equal to 10 + 0 == 10.
+Hence, the punishment number of 10 is 1 + 81 + 100 = 182
+
+Example 2:
+
+Input: n = 37
+Output: 1478
+Explanation: There are exactly 4 integers i in the range [1, 37] that satisfy the conditions in the statement:
+- 1 since 1 * 1 = 1. 
+- 9 since 9 * 9 = 81 and 81 can be partitioned into 8 + 1. 
+- 10 since 10 * 10 = 100 and 100 can be partitioned into 10 + 0. 
+- 36 since 36 * 36 = 1296 and 1296 can be partitioned into 1 + 29 + 6.
+Hence, the punishment number of 37 is 1 + 81 + 100 + 1296 = 1478
+
+Constraints:
+
+	1 <= n <= 1000
+*/
+export function punishmentNumber(n: number): number {
+    let sum = 0;
+
+    const split = (num: string, target: number): boolean => {
+        if (Number(num) === target) {
+            return true;
+        }
+
+        for (let i = 1; i < num.length; i++) {
+            const left = num.slice(0, i);
+            const right = num.slice(i);
+            const nextTarget = target - Number(left);
+            if (nextTarget >= 0 && split(right, nextTarget)) {
+                return true;
+            }
+        }
+
+        return false;
+    };
+
+    for (let i = 1; i <= n; i++) {
+        const num = i * i;
+        if (split(num.toString(), i)) {
+            sum += num;
+        }
+    }
+
+    return sum;
+}
