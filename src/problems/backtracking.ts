17b7536cb682ebb82c1f6bc68d73c53eac114604
zhanggq52434 <zhanggq52434@hundsun.com>
2025-02-16 13:29:15 +0800

    feat: 每日一题

--- src/problems/backtracking.ts
+++ src/problems/backtracking.ts
@@ -449,3 +449,78 @@ export function maxStrength(nums: number[]): number {
 
     return max;
 }
+
+/*
+https://leetcode.com/problems/construct-the-lexicographically-largest-valid-sequence/description/?envType=daily-question&envId=2025-02-16
+1718. Construct the Lexicographically Largest Valid Sequence
+Given an integer n, find a sequence that satisfies all of the following:
+
+	The integer 1 occurs once in the sequence.
+	Each integer between 2 and n occurs twice in the sequence.
+	For every integer i between 2 and n, the distance between the two occurrences of i is exactly i.
+
+The distance between two numbers on the sequence, a[i] and a[j], is the absolute difference of their indices, |j - i|.
+
+Return the lexicographically largest sequence. It is guaranteed that under the given constraints, there is always a solution. 
+
+A sequence a is lexicographically larger than a sequence b (of the same length) if in the first position where a and b differ, sequence a has a number greater than the corresponding number in b. For example, [0,1,9,0] is lexicographically larger than [0,1,5,6] because the first position they differ is at the third number, and 9 is greater than 5.
+
+Example 1:
+
+Input: n = 3
+Output: [3,1,2,3,2]
+Explanation: [2,3,2,1,3] is also a valid sequence, but [3,1,2,3,2] is the lexicographically largest valid sequence.
+
+Example 2:
+
+Input: n = 5
+Output: [5,3,1,4,3,5,2,4,2]
+
+Constraints:
+
+	1 <= n <= 20
+*/
+export function constructDistancedSequence(n: number): number[] {
+    let ret: number[] = [];
+    const done = (1 << n) - 1;
+    const len = (n << 1) - 1;
+    const backtrack = (current: number[], used: number) => {
+        if (ret.length !== 0) {
+            return;
+        }
+        if (used === done) {
+            ret = current.slice();
+            return;
+        }
+
+        const index = current.findIndex((x) => x === 0);
+        for (let i = n - 1; i >= 2; i--) {
+            if (
+                !(used & (1 << (i - 1))) &&
+                index + i < len &&
+                current[index + i] === 0
+            ) {
+                current[index] = i;
+                current[index + i] = i;
+                backtrack(current, used | (1 << (i - 1)));
+                current[index] = 0;
+                current[index + i] = 0;
+            }
+        }
+        if (!(used & 1)) {
+            current[index] = 1;
+            backtrack(current, used | 1);
+            current[index] = 0;
+        }
+    };
+
+    const current: number[] = Array(len).fill(0);
+    current[0] = n;
+    if (n > 1) {
+        current[n] = n;
+    }
+
+    backtrack(current, 1 << (n - 1));
+
+    return ret;
+}
