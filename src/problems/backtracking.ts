f519667b628a5eb88d6b9767ba8530dd97d46d45
zhanggq52434 <zhanggq52434@hundsun.com>
2025-02-19 12:33:34 +0800

    feat: 每日一题

--- src/problems/backtracking.ts
+++ src/problems/backtracking.ts
@@ -600,3 +600,60 @@ export function smallestNumber(pattern: string): string {
 
     return ret;
 }
+
+/*
+https://leetcode.com/problems/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/description/?envType=daily-question&envId=2025-02-19
+1415. The k-th Lexicographical String of All Happy Strings of Length n
+A happy string is a string that:
+
+	consists only of letters of the set ['a', 'b', 'c'].
+	s[i] != s[i + 1] for all values of i from 1 to s.length - 1 (string is 1-indexed).
+
+For example, strings "abc", "ac", "b" and "abcbabcbcb" are all happy strings and strings "aa", "baa" and "ababbc" are not happy strings.
+
+Given two integers n and k, consider a list of all happy strings of length n sorted in lexicographical order.
+
+Return the kth string of this list or return an empty string if there are less than k happy strings of length n.
+
+Example 1:
+
+Input: n = 1, k = 3
+Output: "c"
+Explanation: The list ["a", "b", "c"] contains all happy strings of length 1. The third string is "c".
+
+Example 2:
+
+Input: n = 1, k = 4
+Output: ""
+Explanation: There are only 3 happy strings of length 1.
+
+Example 3:
+
+Input: n = 3, k = 9
+Output: "cab"
+Explanation: There are 12 different happy string of length 3 ["aba", "abc", "aca", "acb", "bab", "bac", "bca", "bcb", "cab", "cac", "cba", "cbc"]. You will find the 9th string = "cab"
+
+Constraints:
+
+	1 <= n <= 10
+	1 <= k <= 100
+*/
+export function getHappyString(n: number, k: number): string {
+    const happyStrings: string[] = [];
+    const backtrack = (i: number, s: string) => {
+        if (i === n) {
+            happyStrings.push(s);
+            return;
+        }
+
+        const last = s.at(-1);
+        for (const c of ['a', 'b', 'c']) {
+            if (c !== last) {
+                backtrack(i + 1, s + c);
+            }
+        }
+    };
+    backtrack(0, '');
+
+    return happyStrings.sort()[k - 1] ?? '';
+}
