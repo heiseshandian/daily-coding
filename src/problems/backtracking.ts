7900d6a134d77c79822827c4dfb1e4808b405feb
zhanggq52434 <zhanggq52434@hundsun.com>
2025-02-20 10:26:06 +0800

    feat: 每日一题

--- src/problems/backtracking.ts
+++ src/problems/backtracking.ts
@@ -657,3 +657,58 @@ export function getHappyString(n: number, k: number): string {
 
     return happyStrings.sort()[k - 1] ?? '';
 }
+
+/*
+https://leetcode.com/problems/find-unique-binary-string/description/?envType=daily-question&envId=2025-02-20
+1980. Find Unique Binary String
+Given an array of strings nums containing n unique binary strings each of length n, return a binary string of length n that does not appear in nums. If there are multiple answers, you may return any of them.
+
+Example 1:
+
+Input: nums = ["01","10"]
+Output: "11"
+Explanation: "11" does not appear in nums. "00" would also be correct.
+
+Example 2:
+
+Input: nums = ["00","01"]
+Output: "11"
+Explanation: "11" does not appear in nums. "10" would also be correct.
+
+Example 3:
+
+Input: nums = ["111","011","001"]
+Output: "101"
+Explanation: "101" does not appear in nums. "000", "010", "100", and "110" would also be correct.
+
+Constraints:
+
+	n == nums.length
+	1 <= n <= 16
+	nums[i].length == n
+	nums[i] is either '0' or '1'.
+	All the strings of nums are unique.
+*/
+export function findDifferentBinaryString(nums: string[]): string {
+    let ret = '';
+    const used = new Set(nums);
+
+    const backtrack = (len: number, sequence: string) => {
+        if (ret.length) {
+            return;
+        }
+        if (len === nums.length) {
+            if (!used.has(sequence)) {
+                ret = sequence;
+            }
+            return;
+        }
+
+        backtrack(len + 1, sequence + '1');
+        backtrack(len + 1, sequence + '0');
+    };
+
+    backtrack(0, '');
+
+    return ret;
+}
