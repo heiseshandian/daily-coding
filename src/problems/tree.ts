054eadae94622f5f1326f056e3c72fd1b8e51a35
zhanggq52434 <zhanggq52434@hundsun.com>
2025-02-21 11:13:40 +0800

    feat: 每日一题

--- src/problems/tree.ts
+++ src/problems/tree.ts
@@ -3817,3 +3817,100 @@ export function sumRootToLeaf(root: TreeNode | null): number {
 
     return sum;
 }
+
+/*
+https://leetcode.com/problems/find-elements-in-a-contaminated-binary-tree/description/?envType=daily-question&envId=2025-02-21
+1261. Find Elements in a Contaminated Binary Tree
+Given a binary tree with the following rules:
+
+	root.val == 0
+	For any treeNode:
+
+		If treeNode.val has a value x and treeNode.left != null, then treeNode.left.val == 2 * x + 1
+		If treeNode.val has a value x and treeNode.right != null, then treeNode.right.val == 2 * x + 2
+
+Now the binary tree is contaminated, which means all treeNode.val have been changed to -1.
+
+Implement the FindElements class:
+
+	FindElements(TreeNode* root) Initializes the object with a contaminated binary tree and recovers it.
+	bool find(int target) Returns true if the target value exists in the recovered binary tree.
+
+Example 1:
+
+Input
+["FindElements","find","find"]
+[[[-1,null,-1]],[1],[2]]
+Output
+[null,false,true]
+Explanation
+FindElements findElements = new FindElements([-1,null,-1]); 
+findElements.find(1); // return False 
+findElements.find(2); // return True 
+
+Example 2:
+
+Input
+["FindElements","find","find","find"]
+[[[-1,-1,-1,-1,-1]],[1],[3],[5]]
+Output
+[null,true,true,false]
+Explanation
+FindElements findElements = new FindElements([-1,-1,-1,-1,-1]);
+findElements.find(1); // return True
+findElements.find(3); // return True
+findElements.find(5); // return False
+
+Example 3:
+
+Input
+["FindElements","find","find","find","find"]
+[[[-1,null,-1,-1,null,-1]],[2],[3],[4],[5]]
+Output
+[null,true,false,false,true]
+Explanation
+FindElements findElements = new FindElements([-1,null,-1,-1,null,-1]);
+findElements.find(2); // return True
+findElements.find(3); // return False
+findElements.find(4); // return False
+findElements.find(5); // return True
+
+Constraints:
+
+	TreeNode.val == -1
+	The height of the binary tree is less than or equal to 20
+	The total number of nodes is between [1, 10^4]
+	Total calls of find() is between [1, 10^4]
+	0 <= target <= 10^6
+*/
+export class FindElements {
+    root: TreeNode | null = null;
+    values = new Set<number>();
+
+    constructor(root: TreeNode | null) {
+        if (!root) {
+            return;
+        }
+
+        this.root = root;
+        root.val = 0;
+        this.recover(this.root);
+    }
+
+    private recover(node: TreeNode) {
+        this.values.add(node.val);
+
+        if (node.left) {
+            node.left.val = node.val * 2 + 1;
+            this.recover(node.left);
+        }
+        if (node.right) {
+            node.right.val = node.val * 2 + 2;
+            this.recover(node.right);
+        }
+    }
+
+    find(target: number): boolean {
+        return this.values.has(target);
+    }
+}
