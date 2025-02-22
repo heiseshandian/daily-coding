2a9272ce89e1e4f97e8a1d53b3f2edb965fc0f11
zhanggq52434 <zhanggq52434@hundsun.com>
2025-02-22 12:46:39 +0800

    feat: 每日一题

--- src/problems/tree.ts
+++ src/problems/tree.ts
@@ -3914,3 +3914,63 @@ export class FindElements {
         return this.values.has(target);
     }
 }
+
+/*
+https://leetcode.com/problems/recover-a-tree-from-preorder-traversal/description/?envType=daily-question&envId=2025-02-22
+1028. Recover a Tree From Preorder Traversal
+We run a preorder depth-first search (DFS) on the root of a binary tree.
+
+At each node in this traversal, we output D dashes (where D is the depth of this node), then we output the value of this node.  If the depth of a node is D, the depth of its immediate child is D + 1.  The depth of the root node is 0.
+
+If a node has only one child, that child is guaranteed to be the left child.
+
+Given the output traversal of this traversal, recover the tree and return its root.
+
+Example 1:
+
+Input: traversal = "1-2--3--4-5--6--7"
+Output: [1,2,5,3,4,6,7]
+
+Example 2:
+
+Input: traversal = "1-2--3---4-5--6---7"
+Output: [1,2,5,3,null,6,null,4,null,7]
+
+Example 3:
+
+Input: traversal = "1-401--349---90--88"
+Output: [1,401,null,349,88,90]
+
+Constraints:
+
+	The number of nodes in the original tree is in the range [1, 1000].
+	1 <= Node.val <= 10^9
+*/
+export function recoverFromPreorder(traversal: string): TreeNode | null {
+    const NUM_REG = /(\d+)/;
+    const getRegByLevel = cache((level: number) =>
+        level === 0 ? NUM_REG : new RegExp(`^(?:-{${level}})(\\d+)`)
+    );
+
+    const build = (level: number): TreeNode | null => {
+        if (!traversal) {
+            return null;
+        }
+
+        const reg = getRegByLevel(level);
+        const match = traversal.match(reg);
+        if (!match) {
+            return null;
+        }
+
+        const [m, val] = match;
+        traversal = traversal.slice(m.length);
+        const head = new TreeNode(+val);
+        head.left = build(level + 1);
+        head.right = build(level + 1);
+
+        return head;
+    };
+
+    return build(0);
+}
