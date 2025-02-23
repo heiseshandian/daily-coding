4c754decbf3dccb76ac18bd0bcf93cf0efa93233
zhanggq52434 <zhanggq52434@hundsun.com>
2025-02-23 12:43:06 +0800

    feat: 每日一题

--- src/problems/tree.ts
+++ src/problems/tree.ts
@@ -3974,3 +3974,73 @@ export function recoverFromPreorder(traversal: string): TreeNode | null {
 
     return build(0);
 }
+
+/*
+https://leetcode.com/problems/construct-binary-tree-from-preorder-and-postorder-traversal/description/?envType=daily-question&envId=2025-02-23
+889. Construct Binary Tree from Preorder and Postorder Traversal
+Given two integer arrays, preorder and postorder where preorder is the preorder traversal of a binary tree of distinct values and postorder is the postorder traversal of the same tree, reconstruct and return the binary tree.
+
+If there exist multiple answers, you can return any of them.
+
+Example 1:
+
+Input: preorder = [1,2,4,5,3,6,7], postorder = [4,5,2,6,7,3,1]
+Output: [1,2,3,4,5,6,7]
+
+Example 2:
+
+Input: preorder = [1], postorder = [1]
+Output: [1]
+
+Constraints:
+
+	1 <= preorder.length <= 30
+	1 <= preorder[i] <= preorder.length
+	All the values of preorder are unique.
+	postorder.length == preorder.length
+	1 <= postorder[i] <= postorder.length
+	All the values of postorder are unique.
+	It is guaranteed that preorder and postorder are the preorder traversal and postorder traversal of the same binary tree.
+*/
+export function constructFromPrePost(
+    preorder: number[],
+    postorder: number[]
+): TreeNode | null {
+    const postMap = {};
+    postorder.forEach((v, i) => {
+        postMap[v] = i;
+    });
+
+    const build = (
+        preStart: number,
+        preEnd: number,
+        postStart: number,
+        postEnd: number
+    ): TreeNode | null => {
+        if (preStart > preEnd) {
+            return null;
+        }
+        if (preStart === preEnd) {
+            return new TreeNode(preorder[preStart]);
+        }
+
+        const head = new TreeNode(preorder[preStart]);
+        const leftLen = postMap[preorder[preStart + 1]] - postStart + 1;
+        head.left = build(
+            preStart + 1,
+            preStart + leftLen,
+            postStart,
+            postStart + leftLen - 1
+        );
+        head.right = build(
+            preStart + leftLen + 1,
+            preEnd,
+            postStart + leftLen,
+            postEnd
+        );
+
+        return head;
+    };
+
+    return build(0, preorder.length - 1, 0, postorder.length - 1);
+}
