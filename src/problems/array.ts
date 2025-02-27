a21c5b13bd59fc5cc26102bc45fbbebb79ce545d
zhanggq52434 <zhanggq52434@hundsun.com>
2025-02-27 10:42:11 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -10463,3 +10463,57 @@ export function maxAbsoluteSum(nums: number[]): number {
 
     return Math.max(max, Math.abs(min));
 }
+
+/*
+https://leetcode.com/problems/length-of-longest-fibonacci-subsequence/description/?envType=daily-question&envId=2025-02-27
+873. Length of Longest Fibonacci Subsequence
+A sequence x1, x2, ..., xn is Fibonacci-like if:
+
+	n >= 3
+	xi + xi+1 == xi+2 for all i + 2 <= n
+
+Given a strictly increasing array arr of positive integers forming a sequence, return the length of the longest Fibonacci-like subsequence of arr. If one does not exist, return 0.
+
+A subsequence is derived from another sequence arr by deleting any number of elements (including none) from arr, without changing the order of the remaining elements. For example, [3, 5, 8] is a subsequence of [3, 4, 5, 6, 7, 8].
+
+Example 1:
+
+Input: arr = [1,2,3,4,5,6,7,8]
+Output: 5
+Explanation: The longest subsequence that is fibonacci-like: [1,2,3,5,8].
+
+Example 2:
+
+Input: arr = [1,3,7,11,12,14,18]
+Output: 3
+Explanation: The longest subsequence that is fibonacci-like: [1,11,12], [3,11,14] or [7,11,18].
+
+Constraints:
+
+	3 <= arr.length <= 1000
+	1 <= arr[i] < arr[i + 1] <= 10^9
+*/
+export function lenLongestFibSubseq(arr: number[]): number {
+    const set = new Set(arr);
+    let max = 0;
+    for (let i = 0; i < arr.length - 2; i++) {
+        for (let j = i + 1; j < arr.length - 1; j++) {
+            let first = arr[i];
+            let second = arr[j];
+            let third = first + second;
+            let len = 2;
+            while (set.has(third)) {
+                len++;
+                first = second;
+                second = third;
+                third = first + second;
+            }
+
+            if (len > 2) {
+                max = Math.max(max, len);
+            }
+        }
+    }
+
+    return max;
+}
