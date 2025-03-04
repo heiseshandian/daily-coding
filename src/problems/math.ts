a95ffa6204491395a6dbcd194491cb85b71c98c6
zhanggq52434 <zhanggq52434@hundsun.com>
2025-03-04 09:27:37 +0800

    feat: 每日一题

--- src/problems/math.ts
+++ src/problems/math.ts
@@ -1426,3 +1426,42 @@ export function punishmentNumber(n: number): number {
 
   return sum;
 }
+
+/*
+https://leetcode.com/problems/check-if-number-is-a-sum-of-powers-of-three/description/?envType=daily-question&envId=2025-03-04
+1780. Check if Number is a Sum of Powers of Three
+Given an integer n, return true if it is possible to represent n as the sum of distinct powers of three. Otherwise, return false.
+
+An integer y is a power of three if there exists an integer x such that y == 3x.
+
+Example 1:
+
+Input: n = 12
+Output: true
+Explanation: 12 = 3^1 + 3^2
+
+Example 2:
+
+Input: n = 91
+Output: true
+Explanation: 91 = 3^0 + 3^2 + 3^4
+
+Example 3:
+
+Input: n = 21
+Output: false
+
+Constraints:
+
+	1 <= n <= 10^7
+*/
+export function checkPowersOfThree(n: number): boolean {
+  while (n > 0) {
+    if (n % 3 === 2) {
+      return false;
+    }
+    n = Math.floor(n / 3);
+  }
+
+  return true;
+}
