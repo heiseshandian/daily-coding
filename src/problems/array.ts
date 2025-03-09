70bc3e54cfbfcb2965e67657be72cdbeaa73ec7a
zhanggq52434 <zhanggq52434@hundsun.com>
2025-03-09 10:50:15 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -10613,3 +10613,84 @@ export function pivotArray(nums: number[], pivot: number): number[] {
 
   return ret;
 }
+
+/*
+https://leetcode.com/problems/alternating-groups-ii/description/?envType=daily-question&envId=2025-03-09
+3208. Alternating Groups II
+There is a circle of red and blue tiles. You are given an array of integers colors and an integer k. The color of tile i is represented by colors[i]:
+
+	colors[i] == 0 means that tile i is red.
+	colors[i] == 1 means that tile i is blue.
+
+An alternating group is every k contiguous tiles in the circle with alternating colors (each tile in the group except the first and last one has a different color from its left and right tiles).
+
+Return the number of alternating groups.
+
+Note that since colors represents a circle, the first and the last tiles are considered to be next to each other.
+
+Example 1:
+
+Input: colors = [0,1,0,1,0], k = 3
+
+Output: 3
+
+Explanation:
+
+Alternating groups:
+
+Example 2:
+
+Input: colors = [0,1,0,0,1,0,1], k = 6
+
+Output: 2
+
+Explanation:
+
+Alternating groups:
+
+Example 3:
+
+Input: colors = [1,1,0,1], k = 4
+
+Output: 0
+
+Explanation:
+
+Constraints:
+
+	3 <= colors.length <= 10^5
+	0 <= colors[i] <= 1
+	3 <= k <= colors.length
+*/
+export function numberOfAlternatingGroups(colors: number[], k: number): number {
+  let repeatedTile = -1;
+  for (let i = 0; i < colors.length; i++) {
+    if (colors[i] === colors[(i + 1) % colors.length]) {
+      repeatedTile = i;
+      break;
+    }
+  }
+
+  if (repeatedTile === -1) {
+    return colors.length;
+  }
+
+  let alternatives = 1;
+  let count = 0;
+  for (let i = 0; i < colors.length; i++) {
+    let start = (repeatedTile + i) % colors.length;
+    let next = (start + 1) % colors.length;
+
+    if (colors[start] !== colors[next]) {
+      alternatives++;
+    } else {
+      alternatives = 1;
+    }
+
+    if (alternatives >= k) {
+      count++;
+    }
+  }
+
+  return count;
+}
