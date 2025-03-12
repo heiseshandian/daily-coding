ae106d100275035287d8eaf21b2333b71976725d
zhanggq52434 <zhanggq52434@hundsun.com>
2025-03-12 11:19:26 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -10289,3 +10289,76 @@ export function pivotArray(nums: number[], pivot: number): number[] {
 
   return ret;
 }
+
+/*
+https://leetcode.com/problems/maximum-count-of-positive-integer-and-negative-integer/description/?envType=daily-question&envId=2025-03-12
+2529. Maximum Count of Positive Integer and Negative Integer
+Given an array nums sorted in non-decreasing order, return the maximum between the number of positive integers and the number of negative integers.
+
+	In other words, if the number of positive integers in nums is pos and the number of negative integers is neg, then return the maximum of pos and neg.
+
+Note that 0 is neither positive nor negative.
+
+Example 1:
+
+Input: nums = [-2,-1,-1,1,2,3]
+Output: 3
+Explanation: There are 3 positive integers and 3 negative integers. The maximum count among them is 3.
+
+Example 2:
+
+Input: nums = [-3,-2,-1,0,0,1,2]
+Output: 3
+Explanation: There are 2 positive integers and 3 negative integers. The maximum count among them is 3.
+
+Example 3:
+
+Input: nums = [5,20,66,1314]
+Output: 4
+Explanation: There are 4 positive integers and 0 negative integers. The maximum count among them is 4.
+
+Constraints:
+
+	1 <= nums.length <= 2000
+	-2000 <= nums[i] <= 2000
+	nums is sorted in a non-decreasing order.
+
+Follow up: Can you solve the problem in O(log(n)) time complexity?
+*/
+export function maximumCount(nums: number[]): number {
+  if (nums[0] > 0 || nums.at(-1)! < 0) {
+    return nums.length;
+  }
+  if (nums[0] === 0 && nums.at(-1) === 0) {
+    return 0;
+  }
+
+  let left = 0;
+  let right = nums.length - 1;
+  let closest = left;
+  while (left <= right) {
+    const mid = left + ((right - left) >> 1);
+    if (nums[mid] < 0) {
+      closest = mid;
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  const negtive = closest + 1;
+
+  left = closest + 1;
+  right = nums.length - 1;
+  closest = right;
+  while (left <= right) {
+    const mid = left + ((right - left) >> 1);
+    if (nums[mid] > 0) {
+      closest = mid;
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return Math.max(negtive, nums.length - closest);
+}
