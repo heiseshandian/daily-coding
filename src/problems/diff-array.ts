162ccc1d9fbafa8af58dbc3bfcad27e1236d3373
zhanggq52434 <zhanggq52434@hundsun.com>
2025-03-15 11:24:13 +0800

    feat: 每日一题

--- src/problems/diff-array.ts
+++ src/problems/diff-array.ts
@@ -97,3 +97,114 @@ export function corpFlightBookings(bookings: number[][], n: number): number[] {
 
   return result;
 }
+
+/*
+https://leetcode.com/problems/zero-array-transformation-ii/description/?envType=daily-question&envId=2025-03-13
+3356. Zero Array Transformation II
+You are given an integer array nums of length n and a 2D array queries where queries[i] = [li, ri, vali].
+
+Each queries[i] represents the following action on nums:
+
+	Decrement the value at each index in the range [li, ri] in nums by at most vali.
+	The amount by which each value is decremented can be chosen independently for each index.
+
+A Zero Array is an array with all its elements equal to 0.
+
+Return the minimum possible non-negative value of k, such that after processing the first k queries in sequence, nums becomes a Zero Array. If no such k exists, return -1.
+
+Example 1:
+
+Input: nums = [2,0,2], queries = [[0,2,1],[0,2,1],[1,1,3]]
+
+Output: 2
+
+Explanation:
+
+	For i = 0 (l = 0, r = 2, val = 1):
+
+		Decrement values at indices [0, 1, 2] by [1, 0, 1] respectively.
+		The array will become [1, 0, 1].
+
+	For i = 1 (l = 0, r = 2, val = 1):
+
+		Decrement values at indices [0, 1, 2] by [1, 0, 1] respectively.
+		The array will become [0, 0, 0], which is a Zero Array. Therefore, the minimum value of k is 2.
+
+Example 2:
+
+Input: nums = [4,3,2,1], queries = [[1,3,2],[0,2,1]]
+
+Output: -1
+
+Explanation:
+
+	For i = 0 (l = 1, r = 3, val = 2):
+
+		Decrement values at indices [1, 2, 3] by [2, 2, 1] respectively.
+		The array will become [4, 1, 0, 0].
+
+	For i = 1 (l = 0, r = 2, val = 1):
+
+		Decrement values at indices [0, 1, 2] by [1, 1, 0] respectively.
+		The array will become [3, 0, 0, 0], which is not a Zero Array.
+
+Constraints:
+
+	1 <= nums.length <= 10^5
+	0 <= nums[i] <= 10^5
+	1 <= queries.length <= 10^5
+	queries[i].length == 3
+	0 <= li <= ri < nums.length
+	1 <= vali <= 5
+*/
+export function minZeroArray(nums: number[], queries: number[][]): number {
+  // 检查数组是否已经是零数组
+  if (nums.every((num) => num === 0)) {
+    return 0;
+  }
+
+  const n = nums.length;
+  let left = 0;
+  let right = queries.length;
+  let min = -1;
+
+  while (left <= right) {
+    const mid = left + ((right - left) >> 1);
+    const diff = buildDifferenceArray(nums);
+
+    for (let i = 0; i < mid; i++) {
+      const [l, r, v] = queries[i];
+      diff[l] -= v;
+      if (r + 1 < n) {
+        diff[r + 1] += v;
+      }
+    }
+
+    let sum = 0;
+    for (let i = 0; i < n; i++) {
+      sum += diff[i];
+      if (sum > 0) {
+        break;
+      }
+    }
+
+    if (sum <= 0) {
+      min = mid;
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return min;
+}
+
+function buildDifferenceArray(arr: number[]) {
+  let n = arr.length;
+  let diff = new Array(n).fill(0);
+  diff[0] = arr[0];
+  for (let i = 1; i < n; i++) {
+    diff[i] = arr[i] - arr[i - 1];
+  }
+  return diff;
+}
