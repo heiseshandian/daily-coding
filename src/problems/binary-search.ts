87671b2fdad79d20f479c665e4228e898aaa4f49
zhanggq52434 <zhanggq52434@hundsun.com>
2025-03-16 09:16:04 +0800

    feat: 每日一题

--- src/problems/binary-search.ts
+++ src/problems/binary-search.ts
@@ -203,3 +203,62 @@ export function maximumCandies(candies: number[], k: number): number {
 
   return right;
 }
+
+/*
+https://leetcode.com/problems/minimum-time-to-repair-cars/description/?envType=daily-question&envId=2025-03-16
+2594. Minimum Time to Repair Cars
+You are given an integer array ranks representing the ranks of some mechanics. ranksi is the rank of the ith mechanic. A mechanic with a rank r can repair n cars in r * n2 minutes.
+
+You are also given an integer cars representing the total number of cars waiting in the garage to be repaired.
+
+Return the minimum time taken to repair all the cars.
+
+Note: All the mechanics can repair the cars simultaneously.
+
+Example 1:
+
+Input: ranks = [4,2,3,1], cars = 10
+Output: 16
+Explanation: 
+- The first mechanic will repair two cars. The time required is 4 * 2 * 2 = 16 minutes.
+- The second mechanic will repair two cars. The time required is 2 * 2 * 2 = 8 minutes.
+- The third mechanic will repair two cars. The time required is 3 * 2 * 2 = 12 minutes.
+- The fourth mechanic will repair four cars. The time required is 1 * 4 * 4 = 16 minutes.
+It can be proved that the cars cannot be repaired in less than 16 minutes.​​​​​
+
+Example 2:
+
+Input: ranks = [5,1,8], cars = 6
+Output: 16
+Explanation: 
+- The first mechanic will repair one car. The time required is 5 * 1 * 1 = 5 minutes.
+- The second mechanic will repair four cars. The time required is 1 * 4 * 4 = 16 minutes.
+- The third mechanic will repair one car. The time required is 8 * 1 * 1 = 8 minutes.
+It can be proved that the cars cannot be repaired in less than 16 minutes.​​​​​
+
+Constraints:
+
+	1 <= ranks.length <= 10^5
+	1 <= ranks[i] <= 100
+	1 <= cars <= 10^6
+*/
+export function repairCars(ranks: number[], cars: number): number {
+  const canRepairCarsWithinMinutes = (minutes: number) =>
+    ranks.reduce((s, rank) => s + Math.floor(Math.sqrt(minutes / rank)), 0) >=
+    cars;
+
+  let left = 0;
+  let right = Math.max(...ranks) * Math.pow(Math.ceil(cars / ranks.length), 2);
+  let min = right;
+  while (left <= right) {
+    const mid = left + Math.floor((right - left) / 2);
+    if (canRepairCarsWithinMinutes(mid)) {
+      min = mid;
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return min;
+}
