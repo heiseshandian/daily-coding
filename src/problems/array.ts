236c527309f9dcd7aee206f77db4e342538932d3
zhanggq52434 <zhanggq52434@hundsun.com>
2025-03-19 11:26:07 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -10362,3 +10362,74 @@ export function maximumCount(nums: number[]): number {
 
   return Math.max(negtive, nums.length - closest);
 }
+
+/*
+https://leetcode.com/problems/minimum-operations-to-make-binary-array-elements-equal-to-one-i/description/?envType=daily-question&envId=2025-03-19
+3191. Minimum Operations to Make Binary Array Elements Equal to One I
+You are given a binary array nums.
+
+You can do the following operation on the array any number of times (possibly zero):
+
+	Choose any 3 consecutive elements from the array and flip all of them.
+
+Flipping an element means changing its value from 0 to 1, and from 1 to 0.
+
+Return the minimum number of operations required to make all elements in nums equal to 1. If it is impossible, return -1.
+
+Example 1:
+
+Input: nums = [0,1,1,1,0,0]
+
+Output: 3
+
+Explanation:
+We can do the following operations:
+
+	Choose the elements at indices 0, 1 and 2. The resulting array is nums = [1,0,0,1,0,0].
+	Choose the elements at indices 1, 2 and 3. The resulting array is nums = [1,1,1,0,0,0].
+	Choose the elements at indices 3, 4 and 5. The resulting array is nums = [1,1,1,1,1,1].
+
+Example 2:
+
+Input: nums = [0,1,1,1]
+
+Output: -1
+
+Explanation:
+It is impossible to make all elements equal to 1.
+
+Constraints:
+
+	3 <= nums.length <= 10^5
+	0 <= nums[i] <= 1
+*/
+export function minOperations(nums: number[]): number {
+  let currentFlipped = false;
+  let nextFlipped = false;
+  let nextNextFlipped = false;
+  let min = 0;
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (
+      (nums[i] === 0 && !currentFlipped) ||
+      (nums[i] === 1 && currentFlipped)
+    ) {
+      min++;
+      nextFlipped = !nextFlipped;
+      nextNextFlipped = !nextNextFlipped;
+    }
+
+    currentFlipped = nextFlipped;
+    nextFlipped = nextNextFlipped;
+    nextNextFlipped = false;
+  }
+
+  if (
+    (nums.at(-2) === 0 && !currentFlipped) ||
+    (nums.at(-2) === 1 && currentFlipped) ||
+    (nums.at(-1) === 0 && !nextFlipped) ||
+    (nums.at(-1) === 1 && nextFlipped)
+  ) {
+    return -1;
+  }
+  return min;
+}
