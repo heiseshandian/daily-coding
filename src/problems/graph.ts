d5ed269ba42874080f14bfaec097b113f0488d9d
zhanggq52434 <zhanggq52434@hundsun.com>
2025-03-20 10:15:50 +0800

    feat: 每日一题

--- src/problems/graph.ts
+++ src/problems/graph.ts
@@ -1863,3 +1863,109 @@ export function checkIfPrerequisite(
 
   return queries.map(([from, to]) => isReachable[from][to]);
 }
+
+/*
+https://leetcode.com/problems/minimum-cost-walk-in-weighted-graph/description/?envType=daily-question&envId=2025-03-20
+3108. Minimum Cost Walk in Weighted Graph
+There is an undirected weighted graph with n vertices labeled from 0 to n - 1.
+
+You are given the integer n and an array edges, where edges[i] = [ui, vi, wi] indicates that there is an edge between vertices ui and vi with a weight of wi.
+
+A walk on a graph is a sequence of vertices and edges. The walk starts and ends with a vertex, and each edge connects the vertex that comes before it and the vertex that comes after it. It's important to note that a walk may visit the same edge or vertex more than once.
+
+The cost of a walk starting at node u and ending at node v is defined as the bitwise AND of the weights of the edges traversed during the walk. In other words, if the sequence of edge weights encountered during the walk is w0, w1, w2, ..., wk, then the cost is calculated as w0 & w1 & w2 & ... & wk, where & denotes the bitwise AND operator.
+
+You are also given a 2D array query, where query[i] = [si, ti]. For each query, you need to find the minimum cost of the walk starting at vertex si and ending at vertex ti. If there exists no such walk, the answer is -1.
+
+Return the array answer, where answer[i] denotes the minimum cost of a walk for query i.
+
+Example 1:
+
+Input: n = 5, edges = [[0,1,7],[1,3,7],[1,2,1]], query = [[0,3],[3,4]]
+
+Output: [1,-1]
+
+Explanation:
+
+To achieve the cost of 1 in the first query, we need to move on the following edges: 0->1 (weight 7), 1->2 (weight 1), 2->1 (weight 1), 1->3 (weight 7).
+
+In the second query, there is no walk between nodes 3 and 4, so the answer is -1.
+
+Example 2:
+
+Input: n = 3, edges = [[0,2,7],[0,1,15],[1,2,6],[1,2,1]], query = [[1,2]]
+
+Output: [0]
+
+Explanation:
+
+To achieve the cost of 0 in the first query, we need to move on the following edges: 1->2 (weight 1), 2->1 (weight 6), 1->2 (weight 1).
+
+Constraints:
+
+	2 <= n <= 10^5
+	0 <= edges.length <= 10^5
+	edges[i].length == 3
+	0 <= ui, vi <= n - 1
+	ui != vi
+	0 <= wi <= 10^5
+	1 <= query.length <= 10^5
+	query[i].length == 2
+	0 <= si, ti <= n - 1
+	si != ti
+*/
+class BitwiseUnionFind {
+  private father: number[];
+  public distances: number[];
+
+  constructor(n: number) {
+    this.father = Array(n);
+
+    for (let i = 0; i < n; i++) {
+      this.father[i] = i;
+    }
+
+    this.distances = Array(n).fill(-1);
+  }
+
+  public find(i: number) {
+    if (i !== this.father[i]) {
+      this.father[i] = this.find(this.father[i]);
+    }
+
+    return this.father[i];
+  }
+
+  public isSameSet(a: number, b: number) {
+    return this.find(a) === this.find(b);
+  }
+
+  public union(a: number, b: number, weight: number) {
+    const fa = this.find(a);
+    const fb = this.find(b);
+    if (fa !== fb) {
+      this.father[fa] = fb;
+    }
+
+    this.distances[fb] &= weight & this.distances[fa];
+    this.distances[fa] = this.distances[fb];
+  }
+}
+
+export function minimumCost(
+  n: number,
+  edges: number[][],
+  query: number[][]
+): number[] {
+  const unionFind = new BitwiseUnionFind(n);
+  edges.forEach(([from, to, weight]) => {
+    unionFind.union(from, to, weight);
+  });
+
+  return query.map(([from, to]) => {
+    if (!unionFind.isSameSet(from, to)) {
+      return -1;
+    }
+    return unionFind.distances[unionFind.find(from)];
+  });
+}
