e047ad25f0a25d3bc64704f997072ad2f783509f
zhanggq52434 <zhanggq52434@hundsun.com>
2025-03-22 09:08:43 +0800

    refactor: 优化 countCompleteComponents 性能

--- src/problems/graph.ts
+++ src/problems/graph.ts
@@ -2068,3 +2068,82 @@ export function findAllRecipes(
 
   return result;
 }
+
+/*
+https://leetcode.com/problems/count-the-number-of-complete-components/description/?envType=problem-list-v2&envId=o5cftq05
+2685. Count the Number of Complete Components
+You are given an integer n. There is an undirected graph with n vertices, numbered from 0 to n - 1. You are given a 2D integer array edges where edges[i] = [ai, bi] denotes that there exists an undirected edge connecting vertices ai and bi.
+
+Return the number of complete connected components of the graph.
+
+A connected component is a subgraph of a graph in which there exists a path between any two vertices, and no vertex of the subgraph shares an edge with a vertex outside of the subgraph.
+
+A connected component is said to be complete if there exists an edge between every pair of its vertices.
+
+Example 1:
+
+Input: n = 6, edges = [[0,1],[0,2],[1,2],[3,4]]
+Output: 3
+Explanation: From the picture above, one can see that all of the components of this graph are complete.
+
+Example 2:
+
+Input: n = 6, edges = [[0,1],[0,2],[1,2],[3,4],[3,5]]
+Output: 1
+Explanation: The component containing vertices 0, 1, and 2 is complete since there is an edge between every pair of two vertices. On the other hand, the component containing vertices 3, 4, and 5 is not complete since there is no edge between vertices 4 and 5. Thus, the number of complete components in this graph is 1.
+
+Constraints:
+
+  1 <= n <= 50
+  0 <= edges.length <= n * (n - 1) / 2
+  edges[i].length == 2
+  0 <= ai, bi <= n - 1
+  ai != bi
+  There are no repeated edges.
+*/
+export function countCompleteComponents(n: number, edges: number[][]): number {
+  const tables: number[][] = Array.from({ length: n }, () => []);
+  edges.forEach(([from, to]) => {
+    tables[from].push(to);
+    tables[to].push(from);
+  });
+
+  const visited = Array(n).fill(false);
+
+  const bfs = (i: number) => {
+    let vertices = 0;
+    let edgeCount = 0;
+    const queue = Array(n);
+    let left = 0;
+    let right = 0;
+    queue[right++] = i;
+    visited[i] = true;
+
+    while (left < right) {
+      const r = right;
+      while (left < r) {
+        const node = queue[left++];
+        vertices++;
+
+        tables[node].forEach((t) => {
+          if (!visited[t]) {
+            visited[t] = true;
+            queue[right++] = t;
+          }
+          edgeCount++;
+        });
+      }
+    }
+
+    return vertices * (vertices - 1) === edgeCount;
+  };
+
+  let res = 0;
+  for (let i = 0; i < n; i++) {
+    if (!visited[i] && bfs(i)) {
+      res++;
+    }
+  }
+
+  return res;
+}
