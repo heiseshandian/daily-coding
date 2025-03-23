2e91dc2acd0c4fee19a5dc82bed73e3c0f4b279b
zhanggq52434 <zhanggq52434@hundsun.com>
2025-03-23 10:26:41 +0800

    feat: 每日一题

--- src/problems/graph.ts
+++ src/problems/graph.ts
@@ -2147,3 +2147,82 @@ export function countCompleteComponents(n: number, edges: number[][]): number {
 
   return res;
 }
+
+/*
+https://leetcode.com/problems/number-of-ways-to-arrive-at-destination/description/?envType=daily-question&envId=2025-03-23
+1976. Number of Ways to Arrive at Destination
+You are in a city that consists of n intersections numbered from 0 to n - 1 with bi-directional roads between some intersections. The inputs are generated such that you can reach any intersection from any other intersection and that there is at most one road between any two intersections.
+
+You are given an integer n and a 2D integer array roads where roads[i] = [ui, vi, timei] means that there is a road between intersections ui and vi that takes timei minutes to travel. You want to know in how many ways you can travel from intersection 0 to intersection n - 1 in the shortest amount of time.
+
+Return the number of ways you can arrive at your destination in the shortest amount of time. Since the answer may be large, return it modulo 109 + 7.
+
+Example 1:
+
+Input: n = 7, roads = [[0,6,7],[0,1,2],[1,2,3],[1,3,3],[6,3,3],[3,5,1],[6,5,1],[2,5,1],[0,4,5],[4,6,2]]
+Output: 4
+Explanation: The shortest amount of time it takes to go from intersection 0 to intersection 6 is 7 minutes.
+The four ways to get there in 7 minutes are:
+- 0 ➝ 6
+- 0 ➝ 4 ➝ 6
+- 0 ➝ 1 ➝ 2 ➝ 5 ➝ 6
+- 0 ➝ 1 ➝ 3 ➝ 5 ➝ 6
+
+Example 2:
+
+Input: n = 2, roads = [[1,0,10]]
+Output: 1
+Explanation: There is only one way to go from intersection 0 to intersection 1, and it takes 10 minutes.
+
+Constraints:
+
+	1 <= n <= 200
+	n - 1 <= roads.length <= n * (n - 1) / 2
+	roads[i].length == 3
+	0 <= ui, vi <= n - 1
+	1 <= timei <= 10^9
+	ui != vi
+	There is at most one road connecting any two intersections.
+	You can reach any intersection from any other intersection.
+*/
+export function countPaths(n: number, roads: number[][]): number {
+  const MOD = 1e9 + 7;
+  const tables: number[][][] = Array.from({ length: n }, () => []);
+  roads.forEach(([from, to, time]) => {
+    tables[from].push([to, time]);
+    tables[to].push([from, time]);
+  });
+
+  const visited = Array(n).fill(false);
+  const distances = Array(n).fill(Infinity);
+  const ways = Array(n).fill(0);
+  const minHeap = new GenericHeap<[node: number, distance: number]>(
+    (a, b) => a[1] - b[1]
+  );
+
+  ways[0] = 1;
+  minHeap.push([0, 0]);
+  distances[0] = [0, 0];
+  while (!minHeap.isEmpty()) {
+    const [from, time] = minHeap.pop();
+    visited[from] = true;
+
+    tables[from].forEach(([to, t]) => {
+      if (visited[to]) {
+        return;
+      }
+
+      const prevTime = distances[to];
+      const currentTime = time + t;
+      if (currentTime < prevTime) {
+        distances[to] = currentTime;
+        minHeap.push([to, currentTime]);
+        ways[to] = ways[from];
+      } else if (currentTime === prevTime) {
+        ways[to] = (ways[to] + ways[from]) % MOD;
+      }
+    });
+  }
+
+  return ways.at(-1);
+}
