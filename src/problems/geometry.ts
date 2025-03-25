31b39b2972ba3cbf30f046bc6e8392a3d19e5c35
zhanggq52434 <zhanggq52434@hundsun.com>
2025-03-25 09:33:33 +0800

    feat: 每日一题

--- src/problems/geometry.ts
+++ src/problems/geometry.ts
@@ -232,3 +232,93 @@ export function isReachableAtTime(
   }
   return true;
 }
+
+/*
+https://leetcode.com/problems/check-if-grid-can-be-cut-into-sections/description/
+3394. Check if Grid can be Cut into Sections
+You are given an integer n representing the dimensions of an n x n grid, with the origin at the bottom-left corner of the grid. You are also given a 2D array of coordinates rectangles, where rectangles[i] is in the form [startx, starty, endx, endy], representing a rectangle on the grid. Each rectangle is defined as follows:
+
+	(startx, starty): The bottom-left corner of the rectangle.
+	(endx, endy): The top-right corner of the rectangle.
+
+Note that the rectangles do not overlap. Your task is to determine if it is possible to make either two horizontal or two vertical cuts on the grid such that:
+
+	Each of the three resulting sections formed by the cuts contains at least one rectangle.
+	Every rectangle belongs to exactly one section.
+
+Return true if such cuts can be made; otherwise, return false.
+
+Example 1:
+
+Input: n = 5, rectangles = [[1,0,5,2],[0,2,2,4],[3,2,5,3],[0,4,4,5]]
+
+Output: true
+
+Explanation:
+
+The grid is shown in the diagram. We can make horizontal cuts at y = 2 and y = 4. Hence, output is true.
+
+Example 2:
+
+Input: n = 4, rectangles = [[0,0,1,1],[2,0,3,4],[0,2,2,3],[3,0,4,3]]
+
+Output: true
+
+Explanation:
+
+We can make vertical cuts at x = 2 and x = 3. Hence, output is true.
+
+Example 3:
+
+Input: n = 4, rectangles = [[0,2,2,4],[1,0,3,2],[2,2,3,4],[3,0,4,2],[3,2,4,4]]
+
+Output: false
+
+Explanation:
+
+We cannot make two horizontal or two vertical cuts that satisfy the conditions. Hence, output is false.
+
+Constraints:
+
+	3 <= n <= 10^9
+	3 <= rectangles.length <= 10^5
+	0 <= rectangles[i][0] < rectangles[i][2] <= n
+	0 <= rectangles[i][1] < rectangles[i][3] <= n
+	No two rectangles overlap.
+*/
+export function checkValidCuts(_n: number, rectangles: number[][]): boolean {
+  const x = Array(rectangles.length);
+  const y = Array(rectangles.length);
+  for (let i = 0; i < rectangles.length; i++) {
+    const [startx, starty, endx, endy] = rectangles[i];
+    x[i] = [startx, endx];
+    y[i] = [starty, endy];
+  }
+
+  return mergeIntervals(x).length >= 3 || mergeIntervals(y).length >= 3;
+}
+
+function mergeIntervals(intervals: number[][]): number[][] {
+  if (intervals.length <= 1) {
+    return intervals;
+  }
+  intervals.sort((a, b) => a[0] - b[0]);
+
+  const result: number[][] = [];
+  let prev = intervals[0];
+  let nextIndex = 1;
+  while (nextIndex < intervals.length) {
+    const next = intervals[nextIndex];
+    if (prev[1] > next[0]) {
+      prev[1] = Math.max(prev[1], next[1]);
+    } else {
+      result.push(prev);
+      prev = next;
+    }
+
+    nextIndex++;
+  }
+  result.push(prev);
+
+  return result;
+}
