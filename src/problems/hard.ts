bb6a90d600220b0ea75a1d6e0ee56c8a6e546ebf
zhanggq52434 <zhanggq52434@hundsun.com>
2025-03-29 19:04:06 +0800

    feat: 每日一题

--- src/problems/hard.ts
+++ src/problems/hard.ts
@@ -3970,3 +3970,135 @@ export function minCut(s: string): number {
 
   return dfs(0) - 1;
 }
+
+/*
+https://leetcode.com/problems/apply-operations-to-maximize-score/description/?envType=daily-question&envId=2025-03-29
+2818. Apply Operations to Maximize Score
+You are given an array nums of n positive integers and an integer k.
+
+Initially, you start with a score of 1. You have to maximize your score by applying the following operation at most k times:
+
+	Choose any non-empty subarray nums[l, ..., r] that you haven't chosen previously.
+	Choose an element x of nums[l, ..., r] with the highest prime score. If multiple such elements exist, choose the one with the smallest index.
+	Multiply your score by x.
+
+Here, nums[l, ..., r] denotes the subarray of nums starting at index l and ending at the index r, both ends being inclusive.
+
+The prime score of an integer x is equal to the number of distinct prime factors of x. For example, the prime score of 300 is 3 since 300 = 2 * 2 * 3 * 5 * 5.
+
+Return the maximum possible score after applying at most k operations.
+
+Since the answer may be large, return it modulo 109 + 7.
+
+Example 1:
+
+Input: nums = [8,3,9,3,8], k = 2
+Output: 81
+Explanation: To get a score of 81, we can apply the following operations:
+- Choose subarray nums[2, ..., 2]. nums[2] is the only element in this subarray. Hence, we multiply the score by nums[2]. The score becomes 1 * 9 = 9.
+- Choose subarray nums[2, ..., 3]. Both nums[2] and nums[3] have a prime score of 1, but nums[2] has the smaller index. Hence, we multiply the score by nums[2]. The score becomes 9 * 9 = 81.
+It can be proven that 81 is the highest score one can obtain.
+
+Example 2:
+
+Input: nums = [19,12,14,6,10,18], k = 3
+Output: 4788
+Explanation: To get a score of 4788, we can apply the following operations: 
+- Choose subarray nums[0, ..., 0]. nums[0] is the only element in this subarray. Hence, we multiply the score by nums[0]. The score becomes 1 * 19 = 19.
+- Choose subarray nums[5, ..., 5]. nums[5] is the only element in this subarray. Hence, we multiply the score by nums[5]. The score becomes 19 * 18 = 342.
+- Choose subarray nums[2, ..., 3]. Both nums[2] and nums[3] have a prime score of 2, but nums[2] has the smaller index. Hence, we multipy the score by nums[2]. The score becomes 342 * 14 = 4788.
+It can be proven that 4788 is the highest score one can obtain.
+
+Constraints:
+
+	1 <= nums.length == n <= 10^5
+	1 <= nums[i] <= 10^5
+	1 <= k <= min(n * (n + 1) / 2, 10^9)
+*/
+
+const countUniquePrimes = cache((n: number): number => {
+  let count = 0;
+  for (let i = 2; i * i <= n; i++) {
+    if (n % i === 0) {
+      count++;
+      while (n % i === 0) {
+        n /= i;
+      }
+    }
+  }
+  if (n > 1) count++;
+  return count;
+});
+
+const pow = (base: number, exponent: number, mod: bigint) => {
+  let baseBig = BigInt(base);
+  let exponentBig = BigInt(exponent);
+
+  if (exponentBig === 0n) {
+    return 1n;
+  }
+
+  baseBig = baseBig % mod;
+  let result = 1n;
+
+  while (exponentBig > 0n) {
+    if (exponentBig % 2n === 1n) {
+      result = (result * baseBig) % mod;
+    }
+
+    baseBig = (baseBig * baseBig) % mod;
+    exponentBig = exponentBig / 2n;
+  }
+
+  return result;
+};
+
+export function maximumScore(nums: number[], k: number): number {
+  const MOD = BigInt(1e9 + 7);
+  const n = nums.length;
+
+  const primes = nums.map((v) => countUniquePrimes(v));
+  const leftBound: number[] = new Array(n).fill(-1);
+  const rightBound: number[] = new Array(n).fill(n);
+  const stack: number[] = [];
+  for (let i = 0; i < n; i++) {
+    while (stack.length > 0 && primes[stack[stack.length - 1]] < primes[i]) {
+      stack.pop();
+    }
+    if (stack.length > 0) {
+      leftBound[i] = stack[stack.length - 1];
+    }
+    stack.push(i);
+  }
+  stack.length = 0;
+
+  for (let i = n - 1; i >= 0; i--) {
+    while (stack.length > 0 && primes[stack[stack.length - 1]] <= primes[i]) {
+      stack.pop();
+    }
+    if (stack.length > 0) {
+      rightBound[i] = stack[stack.length - 1];
+    }
+    stack.push(i);
+  }
+
+  const sorted = nums
+    .slice()
+    .map((v, i) => [v, i])
+    .sort((a, b) => b[0] - a[0]);
+
+  let max = 1n;
+  let i = 0;
+  while (k) {
+    const [value, index] = sorted[i++];
+    const left = leftBound[index];
+    const right = rightBound[index];
+
+    const count = Math.min((index - left) * (right - index), k);
+
+    max = (max * pow(value, count, MOD)) % MOD;
+    k -= count;
+  }
+
+  return Number(max);
+}
