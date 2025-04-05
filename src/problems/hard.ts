ac550136e3302b829d055b04e000168cb118e2af
zhanggq52434 <zhanggq52434@hundsun.com>
2025-04-05 09:08:45 +0800

    feat: 每日一题

--- src/problems/hard.ts
+++ src/problems/hard.ts
@@ -4159,3 +4159,54 @@ export function putMarbles(weights: number[], k: number): number {
 
   return maxSum - minSum;
 }
+
+/*
+https://leetcode.com/problems/max-chunks-to-make-sorted-ii/description/
+768. Max Chunks To Make Sorted II
+You are given an integer array arr.
+
+We split arr into some number of chunks (i.e., partitions), and individually sort each chunk. After concatenating them, the result should equal the sorted array.
+
+Return the largest number of chunks we can make to sort the array.
+
+Example 1:
+
+Input: arr = [5,4,3,2,1]
+Output: 1
+Explanation:
+Splitting into two or more chunks will not return the required result.
+For example, splitting into [5, 4], [3, 2, 1] will result in [4, 5, 1, 2, 3], which isn't sorted.
+
+Example 2:
+
+Input: arr = [2,1,3,4,4]
+Output: 4
+Explanation:
+We can split into two chunks, such as [2, 1], [3, 4, 4].
+However, splitting into [2, 1], [3], [4], [4] is the highest number of chunks possible.
+
+Constraints:
+
+	1 <= arr.length <= 2000
+	0 <= arr[i] <= 10^8
+*/
+export function maxChunksToSorted(arr: number[]): number {
+  const n = arr.length;
+  const sorted = arr.slice().sort((a, b) => a - b);
+  const rightMin = Array(n);
+  rightMin[n - 1] = Infinity;
+  for (let i = n - 2; i >= 0; i--) {
+    rightMin[i] = Math.min(rightMin[i + 1], arr[i + 1]);
+  }
+
+  let max = -Infinity;
+  let count = 0;
+  for (let i = 0; i < n; i++) {
+    max = Math.max(max, arr[i]);
+    if (max === sorted[i] && rightMin[i] >= max) {
+      count++;
+    }
+  }
+
+  return count;
+}
