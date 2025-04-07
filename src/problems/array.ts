c38bdac2c8df651f7da16830c2569c60a1ef69e0
zhanggq52434 <zhanggq52434@hundsun.com>
2025-04-07 08:54:46 +0800

    feat: 每日一题

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -10641,3 +10641,43 @@ export function maximumTripletValue(nums: number[]): number {
 
   return max;
 }
+
+/*
+https://leetcode.com/problems/partition-equal-subset-sum/description/?envType=daily-question&envId=2025-04-07
+416. Partition Equal Subset Sum
+Given an integer array nums, return true if you can partition the array into two subsets such that the sum of the elements in both subsets is equal or false otherwise.
+
+Example 1:
+
+Input: nums = [1,5,11,5]
+Output: true
+Explanation: The array can be partitioned as [1, 5, 5] and [11].
+
+Example 2:
+
+Input: nums = [1,2,3,5]
+Output: false
+Explanation: The array cannot be partitioned into equal sum subsets.
+
+Constraints:
+
+	1 <= nums.length <= 200
+	1 <= nums[i] <= 100
+*/
+export function canPartition(nums: number[]): boolean {
+  const sum = nums.reduce((a, b) => a + b, 0);
+  if (sum % 2 !== 0) {
+    return false;
+  }
+
+  const target = sum / 2;
+  const dp = Array(target + 1).fill(false);
+  dp[0] = true;
+  for (const num of nums) {
+    for (let j = target; j >= num; j--) {
+      dp[j] = dp[j] || dp[j - num];
+    }
+  }
+
+  return dp[target];
+}
