144c3b7f755cdff7626ca6a5841858845b43b3cc
zhanggq52434 <zhanggq52434@hundsun.com>
2025-04-26 10:13:00 +0800

    feat: binary search

--- src/problems/binary-search.ts
+++ src/problems/binary-search.ts
@@ -351,3 +351,63 @@ export function longestNiceSubarray2(nums: number[]): number {
 
   return longestSubArr;
 }
+
+/*
+https://leetcode.com/problems/h-index-ii/description/?envType=problem-list-v2&envId=binary-search
+275. H-Index II
+Given an array of integers citations where citations[i] is the number of citations a researcher received for their ith paper and citations is sorted in non-descending order, return the researcher's h-index.
+
+According to the definition of h-index on Wikipedia: The h-index is defined as the maximum value of h such that the given researcher has published at least h papers that have each been cited at least h times.
+
+You must write an algorithm that runs in logarithmic time.
+
+Example 1:
+
+Input: citations = [0,1,3,5,6]
+Output: 3
+Explanation: [0,1,3,5,6] means the researcher has 5 papers in total and each of them had received 0, 1, 3, 5, 6 citations respectively.
+Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations each, their h-index is 3.
+
+Example 2:
+
+Input: citations = [1,2,100]
+Output: 2
+
+Constraints:
+
+	n == citations.length
+	1 <= n <= 10^5
+	0 <= citations[i] <= 1000
+	citations is sorted in ascending order.
+*/
+export function hIndex(citations: number[]): number {
+  const getNumOfAtLeastKCitations = (k: number): number => {
+    let left = 0;
+    let right = citations.length - 1;
+    let closest = citations.length;
+    while (left <= right) {
+      const mid = left + ((right - left) >> 1);
+      if (citations[mid] >= k) {
+        closest = mid;
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    }
+
+    return citations.length - closest;
+  };
+
+  let left = 0;
+  let right = citations.length - 1;
+  while (left <= right) {
+    const mid = left + ((right - left) >> 1);
+    if (getNumOfAtLeastKCitations(mid + 1) >= mid + 1) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return left;
+}
