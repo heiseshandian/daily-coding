a313a6fc52fdb2b8bbcc9c76d6675c0e2cc6771a
zhanggq52434 <zhanggq52434@hundsun.com>
2025-04-26 09:54:07 +0800

    feat: dp

--- src/review/dp.ts
+++ src/review/dp.ts
@@ -619,3 +619,58 @@ export function minCapability(nums: number[], k: number): number {
 
   return min;
 }
+
+/*
+https://leetcode.com/problems/rotate-function/description/?envType=problem-list-v2&envId=dynamic-programming
+396. Rotate Function
+You are given an integer array nums of length n.
+
+Assume arrk to be an array obtained by rotating nums by k positions clock-wise. We define the rotation function F on nums as follow:
+
+	F(k) = 0 * arrk[0] + 1 * arrk[1] + ... + (n - 1) * arrk[n - 1].
+
+Return the maximum value of F(0), F(1), ..., F(n-1).
+
+The test cases are generated so that the answer fits in a 32-bit integer.
+
+Example 1:
+
+Input: nums = [4,3,2,6]
+Output: 26
+Explanation:
+F(0) = (0 * 4) + (1 * 3) + (2 * 2) + (3 * 6) = 0 + 3 + 4 + 18 = 25
+F(1) = (0 * 6) + (1 * 4) + (2 * 3) + (3 * 2) = 0 + 4 + 6 + 6 = 16
+F(2) = (0 * 2) + (1 * 6) + (2 * 4) + (3 * 3) = 0 + 6 + 8 + 9 = 23
+F(3) = (0 * 3) + (1 * 2) + (2 * 6) + (3 * 4) = 0 + 2 + 12 + 12 = 26
+So the maximum value of F(0), F(1), F(2), F(3) is F(3) = 26.
+
+Example 2:
+
+Input: nums = [100]
+Output: 0
+
+Constraints:
+
+	n == nums.length
+	1 <= n <= 10^5
+	-100 <= nums[i] <= 100
+*/
+export function maxRotateFunction(nums: number[]): number {
+  const n = nums.length;
+  let prev = 0;
+  let next = 0;
+  let sum = 0;
+  for (let i = 0; i < n; i++) {
+    prev += i * nums[i];
+    sum += nums[i];
+  }
+
+  let max = prev;
+  for (let i = 1; i < n; i++) {
+    next = prev - sum + n * nums[i - 1];
+    max = Math.max(max, next);
+    prev = next;
+  }
+
+  return max;
+}
