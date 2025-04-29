7e65a7e42cdbdd04c87a936e6834f082a9cb4e93
zhanggq52434 <zhanggq52434@hundsun.com>
2025-04-29 18:36:27 +0800

    feat: math

--- src/problems/math.ts
+++ src/problems/math.ts
@@ -1620,3 +1620,57 @@ export function countGoodNumbers(n: number): number {
   const odd = pow(4n, BigInt(oddCount));
   return Number((even * odd) % MOD);
 }
+
+/*
+https://leetcode.com/problems/adding-two-negabinary-numbers/description/
+1073. Adding Two Negabinary Numbers
+Given two numbers arr1 and arr2 in base -2, return the result of adding them together.
+
+Each number is given in array format:  as an array of 0s and 1s, from most significant bit to least significant bit.  For example, arr = [1,1,0,1] represents the number (-2)^3 + (-2)^2 + (-2)^0 = -3.  A number arr in array, format is also guaranteed to have no leading zeros: either arr == [0] or arr[0] == 1.
+
+Return the result of adding arr1 and arr2 in the same format: as an array of 0s and 1s with no leading zeros.
+
+Example 1:
+
+Input: arr1 = [1,1,1,1,1], arr2 = [1,0,1]
+Output: [1,0,0,0,0]
+Explanation: arr1 represents 11, arr2 represents 5, the output represents 16.
+
+Example 2:
+
+Input: arr1 = [0], arr2 = [0]
+Output: [0]
+
+Example 3:
+
+Input: arr1 = [0], arr2 = [1]
+Output: [1]
+
+Constraints:
+
+	1 <= arr1.length, arr2.length <= 1000
+	arr1[i] and arr2[i] are 0 or 1
+	arr1 and arr2 have no leading zeros
+*/
+export function addNegabinary(arr1: number[], arr2: number[]): number[] {
+  let carry = 0;
+  const len = Math.max(arr1.length, arr2.length) + 3;
+  const ret = Array(len).fill(0);
+  let i = len - 1;
+  while (carry || arr1.length || arr2.length) {
+    if (arr1.length) {
+      carry += arr1.pop()!;
+    }
+    if (arr2.length) {
+      carry += arr2.pop()!;
+    }
+    ret[i--] = carry & 1;
+    carry = -(carry >> 1);
+  }
+
+  while (ret[i] === 0) {
+    i++;
+  }
+
+  return i === len ? [0] : ret.slice(i);
+}
