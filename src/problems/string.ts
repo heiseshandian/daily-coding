51845a4f83b32416157e4e1ac687290433ae59b2
guoquan <guoquan@feedme.cc>
2025-06-07 18:36:46 +0800

    feat: 每日一题

--- src/problems/string.ts
+++ src/problems/string.ts
@@ -5015,3 +5015,67 @@ export function partitionLabels(s: string): number[] {
   }
   return ret;
 }
+
+/*
+https://leetcode.com/problems/lexicographically-minimum-string-after-removing-stars/description/?envType=daily-question&envId=2025-06-07
+3170. Lexicographically Minimum String After Removing Stars
+You are given a string s. It may contain any number of '*' characters. Your task is to remove all '*' characters.
+
+While there is a '*', do the following operation:
+
+	Delete the leftmost '*' and the smallest non-'*' character to its left. If there are several smallest characters, you can delete any of them.
+
+Return the lexicographically smallest resulting string after removing all '*' characters.
+
+Example 1:
+
+Input: s = "aaba*"
+
+Output: "aab"
+
+Explanation:
+
+We should delete one of the 'a' characters with '*'. If we choose s[3], s becomes the lexicographically smallest.
+
+Example 2:
+
+Input: s = "abc"
+
+Output: "abc"
+
+Explanation:
+
+There is no '*' in the string.
+
+Constraints:
+
+	1 <= s.length <= 10^5
+	s consists only of lowercase English letters and '*'.
+	The input is generated such that it is possible to delete all '*' characters.
+*/
+export function clearStars(s: string): string {
+  const deleted = new Set<number>();
+  const indexList: number[][] = Array.from({ length: 26 }, () => []);
+
+  const findClosestIndex = () => {
+    const closest = indexList.find((l) => l.length);
+    return closest?.pop() ?? -1;
+  };
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '*') {
+      deleted.add(findClosestIndex());
+    } else {
+      indexList[getCharIndex(s[i])].push(i);
+    }
+  }
+
+  let ret = '';
+  for (let i = 0; i < s.length; i++) {
+    if (!deleted.has(i) && s[i] !== '*') {
+      ret += s[i];
+    }
+  }
+
+  return ret;
+}
