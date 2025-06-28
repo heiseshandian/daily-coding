e49c0826fe0e17ef7ff8833f8993b1f009947e0c
guoquan <guoquan@feedme.cc>
2025-06-28 11:51:31 +0800

    feat: 前缀和

--- src/problems/array.ts
+++ src/problems/array.ts
@@ -10681,3 +10681,99 @@ export function canPartition(nums: number[]): boolean {
 
   return dp[target];
 }
+
+/*
+https://leetcode.com/problems/ways-to-make-a-fair-array/description/
+1664. Ways to Make a Fair Array
+You are given an integer array nums. You can choose exactly one index (0-indexed) and remove the element. Notice that the index of the elements may change after the removal.
+
+For example, if nums = [6,1,7,4,1]:
+
+	Choosing to remove index 1 results in nums = [6,7,4,1].
+	Choosing to remove index 2 results in nums = [6,1,4,1].
+	Choosing to remove index 4 results in nums = [6,1,7,4].
+
+An array is fair if the sum of the odd-indexed values equals the sum of the even-indexed values.
+
+Return the number of indices that you could choose such that after the removal, nums is fair. 
+
+Example 1:
+
+Input: nums = [2,1,6,4]
+Output: 1
+Explanation:
+Remove index 0: [1,6,4] -> Even sum: 1 + 4 = 5. Odd sum: 6. Not fair.
+Remove index 1: [2,6,4] -> Even sum: 2 + 4 = 6. Odd sum: 6. Fair.
+Remove index 2: [2,1,4] -> Even sum: 2 + 4 = 6. Odd sum: 1. Not fair.
+Remove index 3: [2,1,6] -> Even sum: 2 + 6 = 8. Odd sum: 1. Not fair.
+There is 1 index that you can remove to make nums fair.
+
+Example 2:
+
+Input: nums = [1,1,1]
+Output: 3
+Explanation: You can remove any index and the remaining array is fair.
+
+Example 3:
+
+Input: nums = [1,2,3]
+Output: 0
+Explanation: You cannot make a fair array after removing any index.
+
+Constraints:
+
+	1 <= nums.length <= 10^5
+	1 <= nums[i] <= 10^4
+*/
+export function waysToMakeFair(nums: number[]): number {
+  const n = nums.length;
+  if (n === 1) {
+    return 1;
+  }
+
+  const evenSums = Array(n);
+  const oddSums = Array(n);
+  evenSums[0] = nums[0];
+  oddSums[1] = nums[1];
+  for (let i = 2; i < n; i++) {
+    if (i & 1) {
+      oddSums[i] = oddSums[i - 2] + nums[i];
+    } else {
+      evenSums[i] = evenSums[i - 2] + nums[i];
+    }
+  }
+  if (n & 1) {
+    oddSums[n - 1] = oddSums[n - 2];
+  } else {
+    evenSums[n - 1] = evenSums[n - 2];
+  }
+
+  let ret = 0;
+  for (let i = 0; i < n; i++) {
+    let odd = 0;
+    let even = 0;
+    //sum before i
+    if (i & 1) {
+      even = evenSums[i - 1] ?? 0;
+      odd = oddSums[i - 2] ?? 0;
+    } else {
+      odd = oddSums[i - 1] ?? 0;
+      even = evenSums[i - 2] ?? 0;
+    }
+
+    //sum after i
+    if (i & 1) {
+      even += oddSums[n - 1] - oddSums[i];
+      odd += evenSums[n - 1] - (evenSums[i - 1] ?? 0);
+    } else {
+      odd += evenSums[n - 1] - (evenSums[i] ?? 0);
+      even += oddSums[n - 1] - (oddSums[i - 1] ?? 0);
+    }
+
+    if (odd === even) {
+      ret++;
+    }
+  }
+
+  return ret;
+}
